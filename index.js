01681c41eccd6ba81692393475f76e3f8409d024
mecathcart <maryellencathcart@gmail.com>
2012-03-22 09:00:00 -0400

    finished tutorial

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 var server = require("./server");
 var router = require("./router");
+var requestHandlers = require("./requestHandlers");
 
-server.start(router.route);
+var handle = {}
+handle["/"] = requestHandlers.start;
+handle["/start"] = requestHandlers.start;
+handle["/upload"] = requestHandlers.upload;
+handle["/show"] = requestHandlers.show;
+
+server.start(router.route, handle);
+
+
+
+
+
+
+
--- server.js
+++ server.js
@@ -1,16 +1,11 @@
 var http = require("http");
 var url = require("url");
 
-function start(route) {
+function start(route, handle) {
   function onRequest(request, response) {
     var pathname = url.parse(request.url).pathname;
     console.log("Request for " + pathname + " received.");
-
-    route(pathname);
-
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello World");
-    response.end();
+    route(handle, pathname, response, request);
   }
 
   http.createServer(onRequest).listen(8888);
@@ -18,3 +13,10 @@ function start(route) {
 }
 
 exports.start = start;
+
+
+
+
+
+
+
