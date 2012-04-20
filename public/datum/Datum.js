2e8ecc77e7bec9c7ee89b13b9709cc39065054ed
Emmy Cathcart <maryellencathcart@gmail.com>
2012-04-20 13:40:05 -0400

    datum suite

---
+++ public/datum/Datum.js
@@ -0,0 +1,47 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: mdotedot
+ * Date: 12-04-20
+ * Time: 1:26 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+var Datum = Datum || {};
+
+Datum.render = function(divid){
+    var d = document.getElementById(divid);
+    if(!d){
+        return;
+    }
+    d.innerHTML  ="hi";
+};
+
+//TODO
+
+Datum.update = function(){
+
+};
+
+Datum.create = function(){
+
+};
+
+Datum.delete = function(){
+
+};
+
+Datum.laTeXiT = function(){
+
+};
+
+Datum.addAudio= function(){
+
+};
+
+Datum.export= function(){
+
+};
+
+Datum.sync()= function(){
+
+}
