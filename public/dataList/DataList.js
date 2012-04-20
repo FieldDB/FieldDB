1c09a4c1c01fc9ef1d94b6a128e745f34629e40b
cesine <cesine@yahoo.com>
2012-04-20 14:27:17 -0400

    adding a dashboard sample

--- public/dataList/DataList.js
+++ public/dataList/DataList.js
@@ -5,6 +5,18 @@ DataList.render = function(divid){
     if(!d){
         return;
     }
-    d.innerHTML  ="hi";
+    var t = document.createElement("table");
+
+    for (var i = 0; i<15; i++){
+        if(i%2==0){
+            t.innerHTML+= "<tr><td class='even'><p>"+i+"This is the orthography,this is the text, this is the date</p></td></tr>";
+
+        }else{
+            t.innerHTML+= "<tr><td class='odd'><p>"+i+"This is the orthography, this is the text, this is the date</p></td></tr>";
+
+        }
+    }
+    d.appendChild(t);
+    console.log(d.innerHTML);
 };
 
