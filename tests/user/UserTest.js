d10a04af30387605fd246aa640a5bd6bec80a248
cesine <cesine@yahoo.com>
2012-05-10 17:31:02 -0400

    turning the user test back on

--- tests/user/UserTest.js
+++ tests/user/UserTest.js
@@ -3,10 +3,10 @@ TestCase("UserTest", {
     "test if subtitle is first and last name for humans": function() {
     
         var u = new User();
-//        u.set("firstname", "Bill"); 
-//        u.set("lastname", "Sapir");
-//        u.subtitle ();
-//        assertEquals("Bill Sapir", u.subtitle());
+        u.set("firstname", "Bill"); 
+        u.set("lastname", "Sapir");
+        u.subtitle ();
+        assertEquals("Bill Sapir", u.subtitle());
     }
 
 });
