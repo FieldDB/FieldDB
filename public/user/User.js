9ce36c98dae7e05770e67718ac5d6bd3c1d04e7b
Yuliya Manyakina <yulia.manyakina@gmail.com>
2012-05-10 16:11:36 -0400

    created UserTest, included backbone and underscore, edited jstd file. UserTest isn't working Issue #38

--- public/user/User.js
+++ public/user/User.js
@@ -9,20 +9,40 @@ var User = Backbone.Model.extend({
       // someAttribute: 5,
       // someAttribute2: 'Hello world',
       // someAttribute3: []
+	   username : "",
+	   id : "",
+	   password : "",
+	   email : "",
+	   firstname : "", 
+	   lastname : "",
+	   isTeam : false, 
+	   gravatar : "http://imgs.abduzeedo.com/files/best_week/coderdojo-octocat3.jpeg",
+	   researchInterest : "",
+	   affiliation : "",
+	   description : "",
+	   //Corpora are projects. They are a complete collection of datum.
+	   corpora : [], 
+	   //Datalists are selected parts of corpora (favourites, for example: passives, or data to be checked next week).
+	   dataLists : [],
+	   teams : [],
+	   sessionHistory : [],
+	   activityHistory : [],
+	   //Preferences are where we'll have things like background/skin options.
+	   prefs : new Preference()
    },
 
    // This is the constructor. It is called whenever you make a new User.
    initialize: function() {
       this.bind('error', function(model, error) {
          // TODO Handle validation errors
-      }
+      });
 
       // TODO Set up any other bindings (i.e. what to do when certain Events 
       //      happen). Example:
       // this.bind("change:someAttribute", function() {
       //    console.log("We just changed someAttribute");
       // });
-
+     
    },
 
    // This is used to validate any changes to the model. It is called whenever
@@ -35,7 +55,13 @@ var User = Backbone.Model.extend({
       //    return "Must use positive numbers";
       // }
    }
-
+   ,subtitle: function() {
+	       if(this.isTeam) {
+	    	   return this.affiliation;
+	       } else {
+	    	   return this.firstname + " " + this.lastname;
+	       }
+	    }
    // TODO Add any other methods that will manipulate the User attributes.
    //      Example:
    // ,
@@ -43,4 +69,6 @@ var User = Backbone.Model.extend({
    //    this.someAttribute++;
    // }
 });
+
+
 
---
+++ tests/user/UserTest.js
@@ -0,0 +1,12 @@
+
+TestCase("UserTest", {
+    "test if subtitle is first and last name for humans": function() {
+    
+        var u = new User();
+//        u.set("firstname", "Bill"); 
+//        u.set("lastname", "Sapir");
+//        u.subtitle ();
+//        assertEquals("Bill Sapir", u.subtitle());
+    }
+
+});
