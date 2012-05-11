795bc2b91acbc2592e2babce1743729f1121e593
hisako noguchi <hisakonog@gmail.com>
2012-05-11 14:32:52 -0400

    wrote team test to see if User.js shows affiliation if user is a team. edited User.js so that the test runs

--- public/user/User.js
+++ public/user/User.js
@@ -56,8 +56,8 @@ var User = Backbone.Model.extend({
       // }
    }
    ,subtitle: function () {
-       if(this.isTeam) {
-    	   return this.affiliation;
+       if(this.get("isTeam")) {
+    	   			return this.get("affiliation");
        } else {
     	   return this.get("firstname") + " " + this.get("lastname");
        }
@@ -69,6 +69,5 @@ var User = Backbone.Model.extend({
    //    this.someAttribute++;
    // }
 });
-
 
 
