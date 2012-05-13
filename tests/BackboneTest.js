6fb8063555ad6d299e758fc27be7d57d4401ef00
cesine <cesine@yahoo.com>
2012-05-13 11:18:36 -0400

    adding tests for libaries

---
+++ tests/BackboneTest.js
@@ -0,0 +1,7 @@
+it("should expose an attribute", function() {
+	  var episode = new Backbone.Model({
+	    title: "Hollywood - Part 2"
+	  });
+	  expect(episode.get("title"))
+	    .toEqual("Hollywood - Part 2");
+	});
