714cde72b9788c627c166b80d56fe8cc26fb446f
Emmy Cathcart <maryellencathcart@gmial.com>
2012-05-14 11:06:55 -0400

    added backbone stuff to datum page

--- public/datum/Datum.js
+++ public/datum/Datum.js
@@ -1,3 +1,73 @@
+var Datum = Backbone.Model.extend({
+   // This is an list of attributes and their default values
+   defaults: {
+      // TODO set up attributes and their defaults. Example:
+      // someAttribute: 5,
+      // someAttribute2: 'Hello world',
+      // someAttribute3: []
+       
+
+      //here are the attributes a datum minimally has to have, other fields can be added when the user designs their own fields later.
+       utterance : "",
+       gloss : "",
+       translation : "",
+       
+       //not sure if this goes in default, but we need a way to link up the datum with the data from its parent, the session.
+       sessionID : 0,
+       
+       //Preferences are where we'll have things like the extra fields the user wants.
+       prefs : new Preference()
+   },
+
+   // This is the constructor. It is called whenever you make a new User.
+   initialize: function() {
+      this.bind('error', function(model, error) {
+         // TODO Handle validation errors
+      });
+
+      // TODO Set up any other bindings (i.e. what to do when certain Events 
+      //      happen). Example:
+      // this.bind("change:someAttribute", function() {
+      //    console.log("We just changed someAttribute");
+      // });
+     
+   },
+
+   // This is used to validate any changes to the model. It is called whenever
+   // user.set('someAttribute', __) is called, but before the changes are
+   // actually made to someAttribute.
+   validate: function(attributes) {
+      // TODO Validation on the attributes. Returning a String counts as an error.
+      //      Example:
+      // if (attributes.someAttribute <= 0) {
+      //    return "Must use positive numbers";
+      // }
+   },
+   subtitle: function () {
+
+
+
+   //I'm a little confused about this method....is this different from preferences?
+   // TODO Add any other methods that will manipulate the User attributes.
+   //      Example:
+   // ,
+   // addOne: function() {
+   //    this.someAttribute++;
+   // }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
 /**
  * Created with JetBrains WebStorm.
  * User: mdotedot
@@ -6,36 +76,17 @@
  * To change this template use File | Settings | File Templates.
  */
 
+/*
+
+//TODO
+If we're using backbone, does this mean that we won't be using these functions anymore?? 
+
 var Datum = Datum || {};
 
 Datum.render = function(divid){
-    var d = document.getElementById(divid);
-    if(!d){
-        return;
-    }
-
-    d.innerHTML  ="<div id = 'wrapperL'>"
-    +"		<div id ='drop'>"
-        +"		<h3>Drop Audio Here!</h3>"
-        +"		</div>"
-    +""
-    +"		<div id= 'leftArrow'><img src='./../datum/arrowL.png'  /></div>"
-    +"	</div>"
-        +""
-        +"	<div id = 'wrapperR'>	"
-        +"			<div  id= 'rightArrow'><img src='./../datum/arrowR.png'  /></div>"
-        +""
-        +""
-        +"			<div id = 'latex'>"
-        +"			<a href='#' class='classname'>LaTeX it!</a>"
-        +"			</div>"
-        +"	</div>"
-      +"<iframe src='https://docs.google.com/spreadsheet/embeddedform?formkey=dEVnZG9fRDBzQWVkRXFPeTI1MENkTHc6MQ'width='710' height='500'></iframe>";
+           
 };
 
-//TODO
-
-/*
 Datum.update = function(){
 
 };
