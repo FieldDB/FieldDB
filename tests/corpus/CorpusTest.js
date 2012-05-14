726348db7be4bf850a3cb87986e10970aa99ced5
cesine <cesine@yahoo.com>
2012-05-13 21:45:17 -0400

    Adding tests for corpus for #64

---
+++ tests/corpus/CorpusTest.js
@@ -0,0 +1,37 @@
+
+(function() {
+	var specs = [];
+
+	describe("GitHub Corpus", function() {
+		it("should be able to import from GitHub repository", function() {
+			
+			expect(true).toBeTruthy();
+			
+
+		});
+
+	});
+	describe("Ask to create new Corpus on drag and drop import", function() {
+		it("should detect drag and drop", function() {
+			
+			expect(true).toBeTruthy();
+			
+
+		});
+
+	});
+	describe("Corpus availability", function() {
+		it("should have the most recent entries available", function() {
+			
+			expect(true).toBeTruthy();
+			
+
+		});
+		it("should store the corpus offine", function() {
+			
+			expect(true).toBeTruthy();
+			
+
+		});
+	});
+})();
