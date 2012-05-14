bb259335bd674f583a723f75330db4c4d1b48e5a
cesine <cesine@yahoo.com>
2012-05-13 21:46:45 -0400

    drag and drop is now working on the body, and creating a new Import object which will open the files for #62

---
+++ public/import/Import.js
@@ -0,0 +1,63 @@
+var Import = Backbone.Model.extend(
+/** @lends Comment.prototype */
+{
+	/**
+	 * @class The import class helps import csv, xml and raw text data into a corpus, or create a new corpus. 
+	 *
+	 * @property {FileList} files These are the file(s) that were dragged in.
+	 * @property {String} corpusid This is the corpusid where the data should be imported
+	 * @property {Array} fields The fields array contains titles of the data columns.
+	 * @property {Collection} datalist the datalist imported, to hold the data before it is saved.
+	 * @property {Event} event The drag/drop event.
+	 * 
+	 * @description The intialize serves to bind import to all drag and drop events. 
+	 *
+	 * @extends Backbone.Model
+	 * @constructs
+	 */
+	initialize : function() {
+		var output = [];
+		var files = this.get("files");
+		debug(files);
+		for ( var i = 0, f; f = files[i]; i++) {
+			output.push('<li><strong>', escape(f.name), '</strong> (', f.type
+					|| 'n/a', ') - ', f.size, ' bytes, last modified: ',
+					f.lastModifiedDate ? f.lastModifiedDate.toLocaleDateString()
+							: 'n/a', '</li>');
+		}
+		document.getElementById('status').innerHTML += '<ul>' + output.join('') + '</ul>';
+		
+		this.on('all', function(e) {
+			debug("Import event: " + e);
+		});
+	},
+
+	// This is an list of attributes and their default values
+	defaults : {
+		corpusid : "",
+		files : [],
+		fields : [],
+		dataList : []
+	},
+
+	/**
+	 * Describe the validation here.
+	 *
+	 * @param {Object} attributes The set of attributes to validate.
+	 *
+	 * @returns {String} The validation error, if there is one. Otherwise, doesn't
+	 * return anything.
+	 */
+	validate : function(attributes) {
+
+	},
+	importCSV : function() {
+
+	},
+	importXML : function() {
+
+	},
+	importText : function() {
+
+	}
+});
