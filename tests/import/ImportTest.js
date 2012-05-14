d4fc1d555756be44c3ee1a4b5043c97b02f9c5dd
cesine <cesine@yahoo.com>
2012-05-13 21:46:45 -0400

    drag and drop is now working on the body, and creating a new Import object which will open the files for #62

---
+++ tests/import/ImportTest.js
@@ -0,0 +1,32 @@
+(function() {
+	var specs = [];
+
+	describe("Import CSV", function() {
+		it("should detect drag and drop", function() {
+			
+			expect(true).toBeTruthy();
+			
+
+		});
+
+	});
+	describe("Import Word/text examples on three lines", function() {
+		it("should detect drag and drop", function() {
+			
+			expect(true).toBeTruthy();
+			
+
+		});
+
+	});
+
+	describe("Import ELAN XML", function() {
+		it("should detect drag and drop", function() {
+			
+			expect(true).toBeTruthy();
+			
+
+		});
+
+	});
+})();
