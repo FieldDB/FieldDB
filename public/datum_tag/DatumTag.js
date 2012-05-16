d3842f27ca0d1572b79ff080d031d09e769f4255
Emmy Cathcart <maryellencathcart@gmial.com>
2012-05-16 09:02:23 -0400

    ok i think i've removed all the <> now, to fix the documentation.

--- public/datum_tag/DatumTag.js
+++ public/datum_tag/DatumTag.js
@@ -2,10 +2,10 @@ var DatumTag = Backbone.Model.extend(
 /** @lends DatumTag.prototype */
 {
    /**
-    * @class <The DatumTag allows the user to label data with grammatical tags i.e. passive, causative.  This is useful for searches.  >
+    * @class The DatumTag allows the user to label data with grammatical tags i.e. passive, causative.  This is useful for searches.  
     *
     * 
-    * @description <The initialize function brings up a field in which the user can enter tags.>
+    * @description The initialize function brings up a field in which the user can enter tags.
     * @constructs
     */
    initialize: function() {
