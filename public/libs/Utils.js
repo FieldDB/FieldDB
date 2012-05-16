b19c5778c71fa7c1852fa14e96644df84c80b3a5
cesine <cesine@yahoo.com>
2012-05-16 16:16:33 -0400

    moving utils functions from offline app to a Utils class, testing it in jstestdriver made me realize that we lost the tests that are using require

--- public/libs/Utils.js
+++ public/libs/Utils.js
@@ -1,3 +1,47 @@
-var debug = function(message){
-	console.log(message);
-};
+define(["use!backbone"], function(Backbone) {
+	/**
+	 * @class Utils provides a number of utility functions which are handy around the  app. 
+	 * @property {Boolean} debugMode This boolean can be changed from true to false for production mode to speed up the app.   
+	 * 
+	 * @constructs
+	 */
+	var Utils = function() {
+		var debugMode = true;
+		/**
+		 * Console logs out, if not on Internet Explorer. Only logs out if debugMode is true. 
+		 */
+		var debug = function(message) {
+			if (navigator.appName == 'Microsoft Internet Explorer') {
+				return;
+			}
+			if (debugMode) {
+				console.log(message);
+			}
+		};
+		/**
+		 * Detects whether we are running offline on an Android app. 
+		 * 
+		 * Note: to Android app developers, append this to your user agent string to take advantage of the offline functionality of this app.  
+		 * @returns {Boolean} true if using offline Android
+		 */
+		var androidApp = function() {
+			return navigator.userAgent.indexOf("OfflineAndroidApp") > -1;
+		};
+		/**
+		 * Detects whether we are running offline in chrome extension. 
+		 * @returns {Boolean} true if using a Chrome Extension
+		 */
+		var chromeApp = function() {
+			return window.location.href.indexOf("chrome-extension") > -1;
+		};
+		/**
+		 * If not running offline on an android or in a chrome extension, assume we are online.
+		 * @returns {Boolean} true if not on offline Android or on a Chrome Extension
+		 */
+		var onlineOnly = function() {
+			return !androidApp() && !chromeApp();
+		};
+
+	};
+	return Utils;
+});
---
+++ tests/dashboard/DashboardTest.js
@@ -0,0 +1,25 @@
+require([
+    "public/libs/Utils"
+], function(Utils) {
+    (function() {
+    	
+    	//Testing to see where the app is running, if it is installed on android, installed in chrome or if it is a web widget.
+    	describe("Offline App", function() {
+    		it("should not be a Chrome extension", function() {
+    			var utils = new Utils();
+    			expect(! utils.chromeApp() ).toBeTruthy();
+    		});
+    		it("should not be an Offline Android app", function() {
+    			var utils = new Utils();
+    			expect(! utils.androidApp() ).toBeTruthy();
+    		});
+    		it("should be an Online app", function() {
+    			var utils = new Utils();
+    			expect( utils.onlineOnly() ).toBeTruthy();
+    		});
+    
+    	});
+    
+    	
+    })();
+});
