ac3691af9c72a009b393a0b3a3552d51f7ed41de
Emmy Cathcart <maryellencathcart@gmial.com>
2012-05-16 13:30:44 -0400

    wrote the Datum test so that it would work with require.js #82

--- public/datum_menu/DatumMenu.js
+++ public/datum_menu/DatumMenu.js
@@ -1,8 +1,8 @@
 define([
     "use!backbone",
-    "datum/Datum"
     
-], function(Backbone, Datum) {
+    
+], function(Backbone) {
   
     var DatumMenu = Backbone.Model.extend(
     /** @lends DatumMenu.prototype */
