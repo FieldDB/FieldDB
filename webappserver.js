2029545fba52297036eb4ac586e9dd03a6f7e783
cesine <cesine@yahoo.com>
2012-05-15 20:59:26 -0400

    started woking onpackage into node

--- webappserver.js
+++ webappserver.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var fs = require('fs');
 var _ = require('underscore');
+var passport = require('passport')
+, LocalStrategy = require('passport-local').Strategy;
+
 var app = express.createServer(express.logger());
 
 app.get('/',function(req,res){ 
@@ -8,6 +11,21 @@ app.get('/',function(req,res){
 });
 
 app.use(express.static(__dirname + '/public'));
+
+passport.use(new LocalStrategy(
+		  function(username, password, done) {
+		    User.findOne({ username: username }, function (err, user) {
+		      if (err) { return done(err); }
+		      if (!user) {
+		        return done(null, false, { message: 'Unknown user' });
+		      }
+		      if (!user.validPassword(password)) {
+		        return done(null, false, { message: 'Invalid password' });
+		      }
+		      return done(null, user);
+		    });
+		  }
+		));
 
 var port = process.env.PORT || 3001;
 app.listen(port, function() {
