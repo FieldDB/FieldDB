80e1cc16847ab746c277e6716fb59311269750f5
cesine <cesine@yahoo.com>
2012-05-16 08:30:26 -0400

    notes for @mecathcart for one way of implementing the datum status, not completed but enough to give a general idea for #69

---
+++ public/datum_status/DatumStatus.js
@@ -0,0 +1,16 @@
+var Datum = Backbone.Model.extend(
+/** @lends Datum.prototype */
+{
+   /**
+    * @class The datum status lets the fieldlinguists assign their own status categoriest o data (ie check with informant, check with x, checked, checked and wrong, hidden, deleted, whatever status they decide.
+    *
+      * @description The initialize function The datum status creates a new status object with the status set to the default (for example, checked)
+    * @extends Backbone.Model
+    * @constructs
+    */
+	defaults: {
+	      statuses: ["Checked","To be checked","Deleted"],
+	      active: 0,
+	      defaultStatus: 0
+	   },
+});
