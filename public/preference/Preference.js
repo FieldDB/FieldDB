bbcd21872eaa02615ed7eee5f78c97cd0e692e29
cesine <cesine@yahoo.com>
2012-05-17 08:21:40 -0400

    debugging error Uncaught Error: Mismatched anonymous define() module: function (Backbone) {   var Preference = Backbone.Model.extend(

--- public/preference/Preference.js
+++ public/preference/Preference.js
@@ -1,16 +1,18 @@
-define([ "use!backbone" ], function(Backbone) {
+define([ 
+        "use!backbone" 
+        ], function(Backbone) {
   var Preference = Backbone.Model.extend(
-  /** @lends Preference.prototype */
-  {
-    /**
-     * @class Hold preferences for users like the skin of teh app
-     * 
-     * @extends Backbone.Model
-     * @constructs
-     */
-    initialize : function() {
-    }
-  });
+      /** @lends Preference.prototype */
+      {
+        /**
+         * @class Hold preferences for users like the skin of teh app
+         * 
+         * @extends Backbone.Model
+         * @constructs
+         */
+        initialize : function() {
+        }
+      });
 
   return Preference;
 });
