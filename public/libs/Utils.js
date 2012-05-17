61e952f7989edafc2887e6feead810fa4f87f243
cesine <cesine@yahoo.com>
2012-05-16 22:44:47 -0400

    trying to get the utils to work but it is saying TypeError: Utils is not a constructor i in the require tests

--- public/libs/Utils.js
+++ public/libs/Utils.js
@@ -1,16 +1,19 @@
-define(["use!backbone"], function(Backbone) {
+define([ "use!backbone" ], function(Backbone) {
 	/**
-	 * @class Utils provides a number of utility functions which are handy around the  app. 
-	 * @property {Boolean} debugMode This boolean can be changed from true to false for production mode to speed up the app.   
+	 * @class Utils provides a number of utility functions which are handy
+	 *        around the app.
+	 * @property {Boolean} debugMode This boolean can be changed from true to
+	 *           false for production mode to speed up the app.
 	 * 
 	 * @constructs
 	 */
 	var Utils = function() {
-		var debugMode = true;
+		debugMode = true;
 		/**
-		 * Console logs out, if not on Internet Explorer. Only logs out if debugMode is true. 
+		 * Console logs out, if not on Internet Explorer. Only logs out if
+		 * debugMode is true.
 		 */
-		var debug = function(message) {
+		debug = function(message) {
 			if (navigator.appName == 'Microsoft Internet Explorer') {
 				return;
 			}
@@ -19,29 +22,36 @@ define(["use!backbone"], function(Backbone) {
 			}
 		};
 		/**
-		 * Detects whether we are running offline on an Android app. 
+		 * Detects whether we are running offline on an Android app.
 		 * 
-		 * Note: to Android app developers, append this to your user agent string to take advantage of the offline functionality of this app.  
+		 * Note: to Android app developers, append this to your user agent
+		 * string to take advantage of the offline functionality of this
+		 * app.
+		 * 
 		 * @returns {Boolean} true if using offline Android
 		 */
-		var androidApp = function() {
+		androidApp = function() {
 			return navigator.userAgent.indexOf("OfflineAndroidApp") > -1;
 		};
 		/**
-		 * Detects whether we are running offline in chrome extension. 
+		 * Detects whether we are running offline in chrome extension.
+		 * 
 		 * @returns {Boolean} true if using a Chrome Extension
 		 */
-		var chromeApp = function() {
+		chromeApp = function() {
 			return window.location.href.indexOf("chrome-extension") > -1;
 		};
 		/**
-		 * If not running offline on an android or in a chrome extension, assume we are online.
-		 * @returns {Boolean} true if not on offline Android or on a Chrome Extension
+		 * If not running offline on an android or in a chrome extension,
+		 * assume we are online.
+		 * 
+		 * @returns {Boolean} true if not on offline Android or on a Chrome
+		 *          Extension
 		 */
-		var onlineOnly = function() {
+		onlineOnly = function() {
 			return !androidApp() && !chromeApp();
 		};
-
 	};
+
 	return Utils;
 });
