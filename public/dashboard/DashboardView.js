8bd508a269d7765c2754c37c1fdf8c089a5f51ea
cesine <cesine@yahoo.com>
2012-05-18 17:51:40 -0400

    added activity feeds, and user templates, it is now adding a activity feed on the side as we click to add an entry. for #96

--- public/dashboard/DashboardView.js
+++ public/dashboard/DashboardView.js
@@ -4,8 +4,10 @@ define("dashboard/DashboardView", [
     "datum/DatumView",
     "datum_status/DatumStatus",
     "datum_status/DatumStatusView",
-    "libs/Utils"
-], function(Backbone, Datum, DatumView, DatumStatus, DatumStatusView, Utils) {
+    "libs/Utils",
+    "activity_feed/ActivityFeedItem",
+    "activity_feed/ActivityFeedItemView"
+], function(Backbone, Datum, DatumView, DatumStatus, DatumStatusView, Utils, ActivityFeedItem, ActivityFeedItemView) {
     var DashboardView = Backbone.View.extend(
     /** @lends DashboardView.prototype */
     {
@@ -36,12 +38,17 @@ define("dashboard/DashboardView", [
           
           // Add it to the global list of Datum
           datumList.add(datum);
-          
           //datumList.create({attestation: "Hello World!"});
           
           // Render it as a DatumView
           var view = new DatumView({model: datum});
           this.$("#content").append(view.render().el);
+
+          var action = new ActivityFeedItem();
+          activityFeed.add(action);
+         // Render an activity in the activity feed
+          var v = new ActivityFeedItemView({model: action});
+          this.$("#activity_feed").append(v.render().el);
        }
     });
 
---
+++ public/user/UserView.js
@@ -0,0 +1,29 @@
+define("user/UserView", [
+    "use!backbone", 
+    "use!handlebars", 
+    "text!user/user.handlebars",
+    "user/User"
+], function(Backbone, Handlebars, userTemplate, User) {
+    var UserView = Backbone.View.extend(
+    /** @lends UserView.prototype */
+    {
+        /**
+         * @class The layout of a single User. This view is used in the activity feeds, it is also embedable in the UserProfileView.
+         *
+         * @extends Backbone.View
+         * @constructs
+         */
+        initialize : function() {
+        },
+
+        model : User,
+        classname : "user",
+        template: Handlebars.compile(userTemplate),
+        render : function() {
+        	$(this.el).html(this.template(this.model.toJSON()));
+            return this;
+        }
+    });
+
+    return UserView;
+}); 
