acdbd9d07e23068133a8c6f27b01754eadd948a9
hisako noguchi <hisakonog@gmail.com>
2012-05-18 12:18:20 -0400

    stil in the midway of editing descriptions but I am switching the end machine, so commit imcomplete changes

--- public/permission/Permission.js
+++ public/permission/Permission.js
@@ -1,8 +1,20 @@
-define("permission/Permission", [
-], function () {
-
+define("permission/Permission", 
+		["use!backbone"], 
+		function (Backbone) {
+	
+	var Permission = Backbone.Model.extend(
+			
+		/** @lends Permission.prototype */ 
+	{
+		/** @class The permission class specifies the types of access permission a user has with respect to a corpus, e.g. read, write, admin.  
+		 */
+	
   return function () {
     return {};
-  };
+  
+  }); 
 
+  
 });
+	
+  
