2dce478e3d9b2dbe0a1455a7c5c57c10ed9385c2
cesine <cesine@yahoo.com>
2012-05-22 11:59:25 -0400

    adding sample widget client code

--- public/widget/list.js
+++ public/widget/list.js
@@ -1,6 +1,37 @@
 /*
  * This widget script is based off of the Twitter Widget script released under the Apache 2.0 license.
  * 
+ * 
+<script charset="utf-8" type="text/javascript" src="http://widgets.fieldlinguist.com/widget/list.js"></script><script type="text/javascript">// <![CDATA[
+    new Fieldlinguist.Widget({
+      version: 2,
+      type: 'search',
+      search: '#list2013',
+      interval: 30000,
+      title: 'Data List',
+      subject: '',
+      width: 250,
+      height: 300,
+      theme: {
+        shell: {
+          background: '#Fc8',
+          color: '#ffffff'
+        },
+        tweets: {
+          background: '#333333',
+          color: '#ffffff',
+          links: '#Fc8'
+        }
+      },
+      features: {
+        scrollbar: false,
+        loop: true,
+        live: true,
+        behavior: 'default'
+      }
+    }).render().start();
+// ]]></script>
+ * 
  * twitter-text-js 1.4.10
  *
  * Copyright 2011 Twitter, Inc.
