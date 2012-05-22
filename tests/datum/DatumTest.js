908ef4327feb7590c0cd03e9ed19632d5ede92e1
Emmy Cathcart <maryellencathcart@gmial.com>
2012-05-22 17:41:02 -0400

    renamed extra field class to datum field so that it would fit in the datum suite

--- tests/datum/DatumTest.js
+++ tests/datum/DatumTest.js
@@ -5,33 +5,24 @@ require([
 		describe("Test Datum", function() {
 			it("should set the utterance", function() {
 				var d = new Datum();
-				d.set("utterance", "tusunayawan");
 				expect("tusunayawan" == d.get("utterance")).toBeTruthy();
 			});
 
 
 			it("should have a gloss line ", function() {
 				var d = new Datum();
-				d.set("utterance", "tusunayawan");
-				d.set("gloss", "dance-IMP-1OM-3SG");
 				expect("dance-IMP-1OM-3SG" == d.get("gloss")).toBeTruthy();
 
 			});
 
 			it("should have a translation line ", function() {
 				var d = new Datum();
-				d.set("utterance", "tusunayawan");
-				d.set("gloss", "dance-IMP-1OM-3SG");
-				d.set("translation","I feel like dancing.");
 				expect("I feel like dancing." == d.get("translation")).toBeTruthy();
 
 			});
-			it("should have a context line ", function() {
+			it("should have grammatical tags", function() {
 				var d = new Datum();
-				d.set("context", "because I'm happy");
-				
-				expect("I feel like dancing." == d.get("translation")).toBeTruthy();
-
+				expect("impulsative" == d.get("grammaticalTags")).toBeTruthy()
 			});
 
 	});
