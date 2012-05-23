fe620f7e276bc6e0a762c071569c900e254ac026
hisakonog <hisakonog@gmail.com>
2012-05-22 21:16:13 -0400

    made user clickable on the dashboard, wrote tests for user generic

--- tests/user_generic/UserGenericTest.js
+++ tests/user_generic/UserGenericTest.js
@@ -1,10 +1,34 @@
- 
+require([
+ "user_generic/UserGeneric"
+ ], function(UserGeneric) {
+	
   describe("UserGeneric", function() {
-    it("should expose an attribute", function() {
+	  
+    it("should set default username", function() {
       //var UserGeneric = require("UserGeneric");
-      var episode = new Backbone.Model({
-        title : "Hollywood - Part 2"
-      });
-      expect(episode.get("title")).toEqual("Hollywood - Part 2");
+      var u = new UserGeneric();
+      expect(u.get("username")).toEqual("");
     });
+    
+    it("should set default password", function() {
+        //var UserGeneric = require("UserGeneric");
+        var u = new UserGeneric();
+        expect(u.get("password")).toEqual("");
+      });
+
+    it("should set default email", function() {
+        //var UserGeneric = require("UserGeneric");
+        var u = new UserGeneric();
+        expect(u.get("email")).toEqual("");
+      });
+  
+    it("should set default gravatar", function() {
+        //var UserGeneric = require("UserGeneric");
+        var u = new UserGeneric();
+        expect(u.get("gravatar")).toEqual("./../user/user_gravatar.png");
+      });
+
+
+
   });
+}); 
