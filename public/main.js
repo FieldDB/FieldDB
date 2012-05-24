740b8c39c24e8ae6cbcf71eeffbcd743c59c032f
trisapeace <trisapeace@gmail.com>
2012-05-24 14:23:40 -0400

    Adds Datum to the temporary,local datum_test CouchDB database. #107

--- public/main.js
+++ public/main.js
@@ -6,7 +6,8 @@ require.config({
         "jquery" : "libs/jquery",
         "underscore" : "libs/underscore",
         "backbone" : "libs/backbone",
-        "handlebars" : "libs/handlebars-1.0.0.beta.6"
+        "handlebars" : "libs/handlebars-1.0.0.beta.6",
+        "jquery.couch" : "libs/jquery.couch"
     },
     use : {
         "underscore" : {
@@ -14,21 +15,30 @@ require.config({
         },
 
         "backbone" : {
-            deps : ["use!underscore", "jquery"],
+            deps : ["use!underscore", "jquery", "jquery.couch", "libs/backbone-couchdb"],
             attach : function(_, $) {
                 return Backbone;
             }
         },
-        
+
         "handlebars" : {
-            attach: "Handlebars"
+            attach : "Handlebars"
         }
     }
 });
 
 // Initialization
 require([
+    "use!backbone",
     "app/App"
-], function(App) {
-	window.app = new App();
+], function(Backbone, App) {
+    // CouchDB configuration
+    Backbone.couch_connector.config.base_url = "http://localhost:5984"
+    Backbone.couch_connector.config.db_name = "datum_test";
+    // If set to true, the connector will listen to the changes feed
+    // and will provide your models with real time remote updates.
+    // But in this case we enable the changes feed for each Collection on our own.
+    Backbone.couch_connector.config.global_changes = false;
+
+    window.app = new App();
 });
