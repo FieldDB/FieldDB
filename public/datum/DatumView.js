18548a093cff607d78e5c6ee540f4246b9e5cd67
Emmy Cathcart <maryellencathcart@gmial.com>
2012-05-25 09:35:54 -0400

    made datum menu view appear as a partial in the datum

--- public/datum/DatumView.js
+++ public/datum/DatumView.js
@@ -6,8 +6,9 @@ define("datum/DatumView", [
     "text!datum_status/datum_status.handlebars",
     "text!datum_menu/datum_menu.handlebars",
     "datum_status/DatumStatus",
-    "datum_menu/DatumMenu"
-], function(Backbone, Handlebars, Datum, datumTemplate, datum_statusTemplate,datum_menuTemplate, DatumStatus,DatumMenu) {
+    "datum_menu/DatumMenu",
+    "datum_menu/DatumMenuView"
+], function(Backbone, Handlebars, Datum, datumTemplate, datum_statusTemplate,datum_menuTemplate, DatumStatus,DatumMenu,DatumMenuView) {
     var DatumView = Backbone.View.extend(
     /** @lends DatumView.prototype */
     {
@@ -29,13 +30,14 @@ define("datum/DatumView", [
         datstattemplate: Handlebars.compile(datum_statusTemplate),
         
         //I attempted to put in a partial for datum menu unsuccessfully.
-        template: Handlebars.compile(datumTemplate),
-        datummenu: new DatumMenu(),
-        datmentemplate: Handlebars.compile(datum_menuTemplate),
+        menuview: new DatumMenuView({model: new DatumMenu()}),
         
         render : function() {
         	Handlebars.registerPartial("datum_status", this.datstattemplate(this.datumstatus.toJSON()) );
-        	Handlebars.registerPartial("datum_menu", this.datmentemplate(this.datummenu.toJSON()) );
+        	//this is where i was!! 
+        	Handlebars.registerPartial("datum_menu", this.menuview.template(this.menuview.model.toJSON()) );
+
+        	//Handlebars.registerPartial("datum_menu", this.menuview.get("template")(this.menuview.get("model").toJSON()) );
             $(this.el).html(this.template(this.model.toJSON()));
             return this;
         }
