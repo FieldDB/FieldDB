1561530b7f04694c2c9fbf6be7080e506748515e
cesine <cesine@yahoo.com>
2012-05-27 19:59:46 -0400

    spies are working on view methods, but not quite yet on clicks. but this might easily have to do with the mulitple stars on one page.wq

--- tests/datum_menu/DatumMenuViewTest.js
+++ tests/datum_menu/DatumMenuViewTest.js
@@ -30,19 +30,57 @@ require([
 		describe("NewDatumMenuView", function() {
 			beforeEach(function() {
 				//this.clickSpy = sinon.spy(DatumMenuView.prototype, 'starDatum');
-				this.view = new DatumMenuView({model: new DatumMenu()});
-				this.view.render();
+				
 			});
-
-			it("should #star is clicked, it should trigger the starDatum method", function() {
-//				if(sinon != null){
-					var clickSpy =  sinon.spy(this.view, 'starDatum');
-					//$("#star").click();
-					this.view.$('#star').click();
-					//$('#star', this.view.el).click();
-					expect(clickSpy).toHaveBeenCalled();
-//				}
+			it("it should trigger the starDatum method", function() {
+				if(sinon != null){
+					var menu = new DatumMenuView({model: new DatumMenu()});
+					menu.render();
+					
+					var clickSpy =  sinon.spy();
+					menu.bind('starDatum', clickSpy);
+					menu.trigger('starDatum'); 
+					expect(clickSpy.called).toBeTruthy();
+				}
 			});
+			it("it should trigger the starDatum method when the #star is clicked", function() {
+				if(sinon != null){
+					var menu = new DatumMenuView({model: new DatumMenu()});
+					menu.render();
+					var clickSpy =  sinon.spy(menu, 'starDatum');
+//					$('#star').click();
+					
+					menu.trigger('starDatum'); 
+//					expect(clickSpy).toHaveBeenCalled();
+				
+				}
+			});
+			
+			it("should fire a callback when 'foo' is triggered", function() {
+				var Episode = Backbone.Model.extend({
+					  url: function() {
+					    return "/episode/" + this.id;
+					  }
+					});
+				  
+				
+				// Create an anonymous spy
+				  var spy = sinon.spy();
+				  
+				  // Create a new Backbone 'Episode' model
+				  var episode = new Episode({
+				    title: "Hollywood - Part 2"
+				  });
+				  
+				  // Call the anonymous spy method when 'foo' is triggered
+				  episode.bind('foo', spy); 
+				  
+				  // Trigger the foo event
+				  episode.trigger('foo'); 
+				  
+				  // Expect that the spy was called at least once
+				  expect(spy.called).toBeTruthy(); 
+				});
 		});
 
 	});
