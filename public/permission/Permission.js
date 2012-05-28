a50b7ede0681825ac8f23fef70499357487bdb0a
cesine <cesine@yahoo.com>
2012-05-28 13:17:27 -0400

    Renaming the Permission Group as Permission, and adding a Permission Collection as per the fine-grained archetecture screencast for #31

--- public/permission/Permission.js
+++ public/permission/Permission.js
@@ -1,57 +1,36 @@
-define("permission/Permission", 
+define(
+    [ "use!backbone"
+      ],
+      function(Backbone){
+      var Permission = Backbone.Model.extend(
 
-		[ "use!backbone"
-		  ],
-		  function(Backbone){
-	  var Permission = Backbone.Model.extend(
-			
-		  /** @lends Permission.prototype 	*/
-			  
-		{
-		  /**
-		   * @class The permission class specifies data access types (e.g. read, write, admin) for a user with respect to a corpus. 
-		   * 
-		   *   @extends Backbone.Model
-		   *   @constructs 
-		   */ 
-	        intialize: function() {
-	            
-	        },
-	        defaults: {
-	            type: "r"
-	        }
-	    });
-	    
-	    return Permission;
-	});
+          /** @lends Permission.prototype 	*/
 
+          {
+            /**
+             * @class The permission class specifies data access types (e.g. read,
+             *        write, admin) for a usergeneric with respect to a corpus.
+             * 
+             * @property {String} type Type is the permission type, which is one of {read, write, export}
+             * @property {UserGeneric} user User is the id of the {User, Bot, Informant, Team} with this permission. 
+             * s
+             * @extends Backbone.Model
+             * @constructs
+             */ 
+            intialize: function() {
 
-// var Permission = function(username, corpusid, [permissions]){
-//		  this.get("username") == username {
-//		  this.get("corpusid") == corpusid 
-// 
-//
-//  }
-//
-//
+            },
+            defaults: {
+              type: "r",
+              user: null
+            },
+            types: {
+              read: "r",
+              write: "w",
+              exportt: "e"
+            }
+          });
 
-//		["use!backbone"], 
-//		function (Backbone) {
-//	
-//	var Permission = Backbone.Model.extend(
-//			
-//		/** @lends Permission.prototype */ 
-//	{
-//		/** @class The permission class specifies the types of access permission a user has with respect to a corpus, e.g. read, write, admin.  
-//		 */
-//	
-//  return function () {
-//    return {};
-//  
-//  }); 
-//
-//  
-//});
-	
-  
+      return Permission;
+    });
 
