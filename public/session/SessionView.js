52cd9e4ddc6d73f8a75026b25b85ce632a77503f
Emmy Cathcart <maryellencathcart@gmial.com>
2012-05-30 12:43:09 -0400

    finished data entry user story (sorta) for #116

--- public/session/SessionView.js
+++ public/session/SessionView.js
@@ -2,8 +2,10 @@ define("session/SessionView", [
     "use!backbone", 
     "use!handlebars", 
     "session/Session",
-    "text!session/session.handlebars"
-], function(Backbone, Handlebars, Session, sessionTemplate) {
+    "text!session/session.handlebars",
+    "datum/Datum",
+    "datum/DatumView"
+], function(Backbone, Handlebars, Session, sessionTemplate, Datum, DatumView) {
     var SessionView = Backbone.View.extend(
     /** @lends SessionView.prototype */
     {
@@ -21,12 +23,29 @@ define("session/SessionView", [
         classname : "session",
 
         template: Handlebars.compile(sessionTemplate),
+        events:{
+        	"click #done": "addDatum" 
+        	
+        },
+
         	
         render : function() {
             $(this.el).html(this.template(this.model.toJSON()));
             return this;
+        },
+        
+        addDatum : function(){
+        	console.log("yay!");
+      	    var datumView = new DatumView({model: new Datum()});
+         	$("#extended-datum-view").append(datumView.render().el);
+         	$("#extended-datum-view").show();
+
+        	return true;
         }
+        
     });
+    
+  
 
     return SessionView;
 }); 
