7d724b5ff21001435ab8a5d71ced363fa26c5bbe
hisakonog <hisakonog@gmail.com>
2012-05-31 16:15:39 -0400

    removed DatumLatex and DatumsLatex models

--- public/datum/DatumLatexView.js
+++ public/datum/DatumLatexView.js
@@ -3,12 +3,16 @@ define("datum/DatumLatexView", [
     "use!handlebars", 
     "datum/Datum",
     "text!datum/datum_latex.handlebars"
-], function(Backbone, Handlebars, DatumLatex, datum_latexTemplate) {
+], function(Backbone, Handlebars, Datum, datum_latexTemplate) {
     var SearchView = Backbone.View.extend(
     /** @lends DatumLatexView.prototype */
     {
         /**
-         * @class Datum
+         * @class DatumLatex shows up as an item in the Data List as a result of  
+        *        search. As a default a DatumLatex item has three fields (utterance, 
+        *        gloss, translation) showing up in the Data List. Morphemes are 
+        *        aligned with corresponding gloss as in Latex, but this is not a 
+        *        true Latex format (just looking like Latex). 
          *
          * @extends Backbone.View
          * @constructs
