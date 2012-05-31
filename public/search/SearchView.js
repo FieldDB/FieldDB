d8f319e15befd9ff8b6a042ea1c626a2ea3c9893
hisakonog <hisakonog@gmail.com>
2012-05-31 10:36:05 -0400

    DataSearchBox should be modeled on Search, so moved the files under Search and renamed them

---
+++ public/search/SearchView.js
@@ -0,0 +1,32 @@
+define("search/SearchView", [
+    "use!backbone", 
+    "use!handlebars", 
+    "search/Search",
+    "text!search/search.handlebars"
+], function(Backbone, Handlebars, Search, searchTemplate) {
+    var SearchView = Backbone.View.extend(
+    /** @lends DatumFieldView.prototype */
+    {
+        /**
+         * @class Search
+         *
+         * @extends Backbone.View
+         * @constructs
+         */
+        initialize : function() {
+        },
+
+       // model : Search,
+
+        classname : "search",
+
+        template: Handlebars.compile(searchTemplate),
+        	
+        render : function() {
+            $(this.el).html(this.template(this.model.toJSON()));
+            return this;
+        }
+    });
+
+    return SearchView;
+}); 
