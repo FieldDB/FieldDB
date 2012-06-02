3741c2ef8e8786943e37920f19be161029fafed0
Emmy Cathcart <maryellencathcart@gmail.com>
2012-06-02 16:24:42 -0400

    new way to do BDD testing-- we are going to include all tests of one module in one file and then each describe is a user story. Each it should is a crucial technical component to making sure the module meets specification.

---
+++ tests/activity/ActivityTest.js
@@ -0,0 +1,71 @@
+require(
+    [ "activity/Activity", "activity/ActivityFeed", "activity/Activities",
+        "activity/ActivitiesView", "activity/ActivityView",
+        "activity/ActivityFeedView" ],
+    function(Activity, ActivityFeed, Activities, ActivitiesView, ActivityView,
+        ActivityFeedView) {
+      describe(
+          "As a user, I want to be updated on team news.",
+          function() {
+            it("should show my most recent team's activities by default.",
+                function() {
+                  expect(true).toBeTruthy();
+                });
+            it(
+                "should display a drop down box of my teams and members of my teams.",
+                function() {
+                  expect(true).toBeTruthy();
+                });
+            it(
+                "should let me enter anyone's user name to see their activity feed if its public.",
+                function() {
+                  expect(true).toBeTruthy();
+                });
+
+          });
+
+      describe(
+          "As a user, I want to make my activity private.",
+          function() {
+            it(
+                "should display a settings that allows a logged in user to make their activities private.",
+                function() {
+                  expect(true).toBeTruthy();
+                });
+          });
+
+      describe("As a team member, I want to able to delete team activities.",
+          function() {
+            it("should be able to delete one activity.", function() {
+              expect(true).toBeTruthy();
+            });
+          });
+
+      describe(
+          "As a user, I want to  be about see my activity even when I'm offline.",
+          function() {
+            it("should display a user's up to date activity when offline.",
+                function() {
+                  expect(true).toBeTruthy();
+                });
+
+            it("should notify user when team data is out of date/stale.",
+                function() {
+                  expect(true).toBeTruthy();
+                });
+          });
+
+      describe(
+          "As a user, I want to learn how to use the system by see how my teammates are using it.",
+          function() {
+            it("should have colorful icons to display verbs.", function() {
+              expect(true).toBeTruthy();
+            });
+            it(
+                "should have clickable activity feed items, which show the changed set.",
+                function() {
+                  expect(true).toBeTruthy();
+                });
+          });
+
+    });
