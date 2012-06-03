8184981544d33fb9c388b782712c842582bc85ad
cesine <cesine@yahoo.com>
2012-06-03 09:32:47 -0400

    using events and classes to bind the authentication buttons in a backbone way. for #133

--- public/authentication/AuthenticationView.js
+++ public/authentication/AuthenticationView.js
@@ -3,12 +3,14 @@ define([
          "use!handlebars",
          "text!authentication/authentication.handlebars",
          "authentication/Authentication",
-         "user/User"], function(
+         "user/User",
+         "user/UserView"], function(
              Backbone, 
              Handlebars,
              authTemplate, 
              Authentication,
-             User) {
+             User,
+             UserView) {
   var AuthenticationView = Backbone.View.extend(
   /** @lends AuthenticationView.prototype */
   {
@@ -19,18 +21,40 @@ define([
      * @constructs
      */
     initialize : function() {
+      this.on('all', function(e) {
+        this.render();
+      });
+      this.model.bind('change', this.render);
       this.render();
     },
     events : {
-      "change" : "render"
+      "change" : "render",
+      "click .logout": "logout",
+      "click .login": "login"
     },
     model : Authentication,
     template : Handlebars.compile(authTemplate),
     el : '#authentication',
     render : function() {
-      $(this.el).html(this.template(this.model.toJSON()));
-      console.log("\trendering login: "+ this.model.get("username"));
+      if(this.model != undefined){
+        $(this.el).html(this.template(this.model.toJSON()));
+        console.log("\trendering login: "+ this.model.get("username"));
+      }else{
+        console.log("\tLogin model was undefined.");
+      }
       return this;
+    },
+    logout: function(){
+      this.model.logout();
+      this.render();
+      $("#logout").hide();
+      $("#login").show();
+    },
+    login: function(){
+      this.model.login(document.getElementById("username").value, document.getElementById("password").value);
+      this.render();
+      $("#logout").show();
+      $("#login").hide();
     },
     loadSample : function() {
       this.model.set("user", new User({
