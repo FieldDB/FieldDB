b847eb58e9a9b9b344d2c3232310d78873fdb6af
trisapeace <trisapeace@gmail.com>
2012-06-04 14:31:52 -0400

    Clean-up. No major code changes.
    Took our app.* declarations in main.js that aren't being used.
    
    Factored out the renderLogin and renderLoout lines in AuthenticateView.js.

--- public/search/SearchView.js
+++ public/search/SearchView.js
@@ -1,6 +1,15 @@
-define([ "use!backbone", "use!handlebars", "search/Search",
-    "text!search/search.handlebars" ], function(Backbone, Handlebars, Search,
-    searchTemplate) {
+define([ 
+    "use!backbone", 
+    "use!handlebars", 
+    "search/Search",
+    "text!search/search.handlebars",
+    "libs/Utils"
+], function(
+    Backbone, 
+    Handlebars, 
+    Search,
+    searchTemplate
+) {
   var SearchView = Backbone.View.extend(
   /** @lends DatumFieldView.prototype */
   {
@@ -15,30 +24,45 @@ define([ "use!backbone", "use!handlebars", "search/Search",
       this.on('all', function(e) {
         this.render();
       });
+      
       this.model.bind('change', this.render);
+      
+      // Update display
       this.render();
     },
+    
     events : {
       "change" : "render",
       "all" : "render"
 //      "blur" : this.model.saveKeyword
     },
+    
     model : Search,
+    
     classname : "search",
+    
     template : Handlebars.compile(searchTemplate),
+    
     el : '#search',
+    
     render : function() {
-      if(this.model != undefined){
+      if (this.model != undefined) {
         $(this.el).html(this.template(this.model.toJSON()));
-        console.log("\trendering search: "+ this.model.get("searchKeywords"));
-      }else{
-        console.log("\tSearch model was undefined");
+        Utils.debug("\trendering search: "+ this.model.get("searchKeywords"));
+      } else {
+        Utils.debug("\tSearch model was undefined");
       }
+      
       return this;
     },
-    loadSample : function(){
+    
+    /**
+     * Initialize the sample Search.
+     */
+    loadSample : function() {
       this.model.set("searchKeywords","naya");
-      console.log("Changing search keyword: "+ this.model.get("searchKeywords"));
+      Utils.debug("Changing search keyword: "+ this.model.get("searchKeywords"));
+      
       this.render();
     }
   });
