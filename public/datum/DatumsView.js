38d6debb5f7d9b9de315372244d0deae2770468b
cesine <cesine@yahoo.com>
2012-06-04 23:19:44 -0400

    reverting to version in upstream

--- public/datum/DatumsView.js
+++ public/datum/DatumsView.js
@@ -1,84 +1,92 @@
-define([ "use!backbone", "use!handlebars", "datum/Datums",
-    "text!/datum/datum_latex.handlebars", "text!datum/datums.handlebars" ], function(Backbone, Handlebars,
-    Datums, datum_latexTemplate, datumsTemplate) {
-  var DatumsView = Backbone.View.extend(
-  /** @lends DatumsView.prototype */
-  {
-    /**
-     * @class
-     * 
-     * Uses PaginationView
-     * 
-     * @extends Backbone.View
-     * @constructs
-     */
-    initialize : function() {
-      // this.collection.on('reset', this.render, this);
-      // this.collection.on('change', this.render, this);
-    },
+define( [
+    "use!backbone", 
+    "use!handlebars", 
+    "datum/Datums",
+    "text!/datum/datum_latex.handlebars"
+], function(Backbone, Handlebars, Datums, datum_latexTemplate) {
+    var DatumsView = Backbone.View.extend(
+      /** @lends DatumsView.prototype */
+      {
+          /**
+           * @class 
+           * 
+           * Uses PaginationView
+           *
+           * @extends Backbone.View
+           * @constructs
+           */
+          initialize : function() {
+          },
 
-    events : {
-      'click a.servernext' : 'nextResultPage',
-      'click a.serverprevious' : 'previousResultPage',
-      'click a.orderUpdate' : 'updateSortBy',
-      'click a.serverlast' : 'gotoLast',
-      'click a.page' : 'gotoPage',
-      'click a.serverfirst' : 'gotoFirst',
-      'click a.serverpage' : 'gotoPage',
-      'click .serverhowmany a' : 'changeCount'
-    },
-    collection : Datums,
-    tagName : 'aside',
-    template : Handlebars.compile(datumsTemplate),
+      events: {
+        'click a.servernext': 'nextResultPage',
+        'click a.serverprevious': 'previousResultPage',
+        'click a.orderUpdate': 'updateSortBy',
+        'click a.serverlast': 'gotoLast',
+        'click a.page': 'gotoPage',
+        'click a.serverfirst': 'gotoFirst',
+        'click a.serverpage': 'gotoPage',
+        'click .serverhowmany a': 'changeCount'
 
-    render : function() {
-      $(this.el).html(this.template(this.model.toJSON()));
-      console.log("\trendering datums " );
-    
-//      this.$el.appendTo('#pagination');
-//      var html = this.template(this.collection.info());
-//      this.$el.html(html);
-    },
+      },
+      collection: Datums,
+      tagName: 'aside',
+      collection: Datums,
+      template: _.template($('#tmpServerPagination').html()),
 
-    updateSortBy : function(e) {
-      e.preventDefault();
-      var currentSort = $('#sortByField').val();
-      this.collection.updateOrder(currentSort);
-    },
+      initialize: function () {
 
-    nextResultPage : function(e) {
-      e.preventDefault();
-      this.collection.requestNextPage();
-    },
+//        this.collection.on('reset', this.render, this);
+//        this.collection.on('change', this.render, this);
 
-    previousResultPage : function(e) {
-      e.preventDefault();
-      this.collection.requestPreviousPage();
-    },
+        this.$el.appendTo('#pagination');
 
-    gotoFirst : function(e) {
-      e.preventDefault();
-      this.collection.goTo(this.collection.information.firstPage);
-    },
+      },
 
-    gotoLast : function(e) {
-      e.preventDefault();
-      this.collection.goTo(this.collection.information.lastPage);
-    },
+      render: function () {
+        var html = this.template(this.collection.info());
+        this.$el.html(html);
+      },
 
-    gotoPage : function(e) {
-      e.preventDefault();
-      var page = $(e.target).text();
-      this.collection.goTo(page);
-    },
+      updateSortBy: function (e) {
+        e.preventDefault();
+        var currentSort = $('#sortByField').val();
+        this.collection.updateOrder(currentSort);
+      },
 
-    changeCount : function(e) {
-      e.preventDefault();
-      var per = $(e.target).text();
-      this.collection.howManyPer(per);
-    }
+      nextResultPage: function (e) {
+        e.preventDefault();
+        this.collection.requestNextPage();
+      },
 
-  });
+      previousResultPage: function (e) {
+        e.preventDefault();
+        this.collection.requestPreviousPage();
+      },
 
-  return DatumsView;
-});
+      gotoFirst: function (e) {
+        e.preventDefault();
+        this.collection.goTo(this.collection.information.firstPage);
+      },
+
+      gotoLast: function (e) {
+        e.preventDefault();
+        this.collection.goTo(this.collection.information.lastPage);
+      },
+
+      gotoPage: function (e) {
+        e.preventDefault();
+        var page = $(e.target).text();
+        this.collection.goTo(page);
+      },
+
+      changeCount: function (e) {
+        e.preventDefault();
+        var per = $(e.target).text();
+        this.collection.howManyPer(per);
+      }
+
+    });
+
+    return DatumsView;
+}); 
