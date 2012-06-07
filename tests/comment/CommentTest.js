a2430376c464a66898aa1159f2fb0b28c4e7e2f1
yuliyam <yulia.manyakina@gmail.com>
2012-06-07 14:53:17 -0400

    Made a few test shells. Don't know how good they are.

---
+++ tests/comment/CommentTest.js
@@ -0,0 +1,24 @@
+// require([
+//    "comment/Comment"
+//], function(Comment) {
+//    describe("Comment Tests", function() {
+//        it("should allow users to take note of important things and " +
+//        		"communicate between each other", function() {
+//        	 expect(true).toBeTruthy();
+//        });
+//        it("should show a timestamp and a userid by default", function() {
+//        	 expect(true).toBeTruthy();
+//        });
+//        it("should be able to be edited", function() {
+//        	 expect(true).toBeTruthy();
+//        	 
+//        });
+//        it("should be able to be removed", function() {
+//        	expect(true).toBeTruthy();
+//        	 
+//        });
+//        it("should appear on datum, sessions, corpora, and dataLists",
+//            function() { expect(true).toBeTruthy();
+//        });
+//    });
+//});
