f6dc73882b75fd6351d3e4c2f1b5d0e8e75587a1
Yuliya Manyakina <yulia.manyakina@gmail.com>
2012-06-11 11:07:10 -0400

    Added PreferenceTest and folder.

---
+++ tests/preference/PreferenceTest.js
@@ -0,0 +1,30 @@
+//require([
+//    "preference/Preference"
+//], function(Preference) {
+//	
+//		describe("Test Preference", function() {
+//          it("should initialize the Preference menu", function() {
+//          var p = new Preference();
+//          expect(p).not.toBeNull();
+//          });
+//
+//
+//			it("should allow users to change their preferences", function() {
+//				expect(true).toBeTruthy();
+//			});
+//
+//
+//			it("should contain skin preference ", function() {
+//				var p = new Preference();
+//				expect("skin" == p.get("skin")).toBeTruthy();
+//
+//			});
+//
+//			it("should contain hotKey preference ", function() {
+//				var p = new Preference();
+//				expect("hotKey" == p.get("hotKey")).toBeTruthy();
+//
+//			});
+//
+//	});
+//});
