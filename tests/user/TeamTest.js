6c7fc8a072bce0534ada564f5a45c609f169375f
hisakonog <hisakonog@gmail.com>
2012-06-12 09:39:17 -0400

    wrote Informant, Team, InseertUnicode tests, some editing on permission, session, userGeneric but reverted to the previous

--- tests/user/TeamTest.js
+++ tests/user/TeamTest.js
@@ -2,8 +2,12 @@ require([
     "team/Team",
     "user/User"
 ], function(Team,User) {
+
+	//TODO @cesine Do we want to have a Team admin type of entity who can add and/delete team members and modify their permissions? 
+	//             Or every team member can do that? 
 	
-	describe("Test Team", function() {
+
+	describe("as a User I want to set up my team name", function() {
 		it("should set default team name", function() {
 			var u = new User();
 			expect(u.get("teamname")).toEqual("");
@@ -18,4 +22,27 @@ require([
 		
 		
 	});
+	
+	
+	describe("as a Team member, I want to be able to add and delete members", function() {
+		it("should add a new team member", function() {
+//			var u = new User(); 
+//            expect(true).toBeTruthy();
+        });
+
+		it("should delete a team member", function() {
+//            expect(true).toBeTruthy();
+        });
+
+		it("should modify member's permissions", function() {
+//			var permission = new Permission();
+//			expect(true).toBeTruthy();
+            
+        });
+		
+		
+	}); 
+	
+	
 });
+
