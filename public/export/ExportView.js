ad53a6933d1df56a1eb62613196ba9789e9853c3
yuliyam <yulia.manyakina@gmail.com>
2012-06-14 15:06:00 -0400

    Created Export page

---
+++ public/export/ExportView.js
@@ -0,0 +1,43 @@
+define([
+    "use!backbone", 
+    "use!handlebars", 
+    "export/Export",
+    "text!export/export.handlebars"
+], function(Backbone, Handlebars, Export, exportTemplate) {
+    var ExportView = Backbone.View.extend(
+    /** @lends ExportView.prototype */
+    {
+        /**
+         * @class ExportView
+         *
+         * @extends Backbone.View
+         * @constructs
+         */
+        initialize : function() {
+        },
+
+        model : Export,
+
+        classname : "export",
+
+        template: Handlebars.compile(exportTemplate),
+   	
+        render : function() {
+            $(this.el).html(this.template(this.model.toJSON()));
+            
+         // Display the ExportView
+          	this.setElement($("#export-view")); 
+          	$(this.el).html(this.template(this.model.toJSON()));
+        	 
+   
+            
+            return this;
+        },
+        
+        
+    });
+    
+  
+
+    return ExportView;
+}); 
