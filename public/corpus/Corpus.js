2cae1e099cbd1e38767537df44f6889e0df6bd66
Emmy Cathcart <maryellencathcart@gmail.com>
2012-06-20 13:40:38 -0400

    renaming userchosen to userchooseable, used to dispable select boxes for datum fields taht the system needs.

--- public/corpus/Corpus.js
+++ public/corpus/Corpus.js
@@ -93,31 +93,31 @@ define([
               label : "judgement",
               size : "3",
               encrypted: "",
-              userchosen: "disabled",
+              userchooseable: "disabled",
               help: "Use this field to establish your team's gramaticality/acceptablity judgements (*,#,? etc)"
             }),
             new DatumField({
               label : "utterence",
               encrypted: "checked",
-              userchosen: "disabled",
+              userchooseable: "disabled",
               help: "Use this as Line 1 in your examples for handouts (ie, either Orthography, or phonemic/phonetic representation)"
             }),
             new DatumField({
               label : "morphemes",
               encrypted: "checked",
-              userchosen: "disabled",
+              userchooseable: "disabled",
               help: "This line is used to determine the morpheme segmentation to generate glosses, it also optionally can show up in your LaTeXed examples if you choose to show morpheme segmentation in addtion ot line 1, gloss and translation."
             }),
             new DatumField({
               label : "gloss",
               encrypted: "checked",
-              userchosen: "disabled",
+              userchooseable: "disabled",
               help: "This line appears in the gloss line of your LaTeXed examples, we reccomend Leipzig conventions (. for fusional morphemes, - for morpehem boundaries etc) The system uses this line to partially help you in glossing. "
             }),
             new DatumField({
               label : "translation",
               encrypted: "checked",
-              userchosen: "disabled",
+              userchooseable: "disabled",
               help: "Use this as your primary translation. It does not need to be English, simply a language your team is comfortable with. If your consultant often gives you multiple languages for translation you can also add addtional translations in the customized fields. For example, your Quechua informants use Spanish for translations, then you can make all Translations in Spanish, and add an additional field for English if you want to generate a handout containing the datum. "
             })
           ]));
@@ -128,13 +128,13 @@ define([
             new DatumField({
               label : "language",
               encrypted: "",
-              userchosen: "disabled",
+              userchooseable: "disabled",
               help: "This is the langauge (or language family) if you would like to use it."
             }),
             new DatumField({
               label : "dialect",
               encrypted: "",
-              userchosen: "disabled",
+              userchooseable: "disabled",
               help: "You can use this field to be as precise as you would like abotu the dialect of this session."
             })
           ]));
