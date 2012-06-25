75ebb87cc4d123b9a486b52b527d6cc801a5db86
cesine <cesine@yahoo.com>
2012-06-25 14:30:02 +0000

    adding everyauth plus storing users in couch based on https://raw.github.com/pgte/nodetuts_26/ for #214

---
+++ app.js
@@ -0,0 +1,40 @@
+var express     = require('express')
+    ,everyauth  = require('everyauth')
+    ,util       = require('util')
+    ,https      = require('https')
+    ,crypto     = require('crypto')
+    ,fs         = require('fs')
+    ,users      = require('./lib/users');
+
+var authconf = require('./everyauthconfig.json');
+
+everyauth.twitter
+  .consumerKey(authconf.twitter.consumerKey)
+  .consumerSecret(authconf.twitter.consumerSecret)
+  .findOrCreateUser(function(session, accessToken, accessTokenSecret, twitterUserData) {
+    var promise = this.Promise();
+    users.findOrCreateByTwitterData(twitterUserData, accessToken, accessTokenSecret, promise);
+    return promise;
+  })
+  .redirectPath('/');
+
+var httpsOptions ={
+    key: fs.readFileSync('ifield.key'),
+    cert: fs.readFileSync('ifield.crt')};
+var app = express.createServer(httpsOptions);
+app.configure(function() {
+  app.use(express.logger());
+  app.use(express.bodyParser());
+  app.use(express.cookieParser());
+  app.use(express.session({secret: "90ndsj9dfdsfwewfead3"}));
+  app.use(everyauth.middleware());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+  app.use(express.errorHandler());
+  everyauth.helpExpress(app);
+});
+
+
+
+app.listen(3001);
+
