c3f7b6b7bc3b6b4c350ec99579781c96b32f1a17
trisapeace <trisapeace@gmail.com>
2012-06-28 13:28:26 -0400

    Refactored Export #240.

---
+++ public/export/ExportReadView.js
@@ -0,0 +1,40 @@
+define([
+    "use!backbone", 
+    "use!handlebars", 
+    "text!export/export_read_modal.handlebars",
+    "export/Export",
+], function(
+    Backbone, 
+    Handlebars, 
+    exportTemplate, 
+    Export
+) {
+  var ExportView = Backbone.View.extend(
+  /** @lends ExportView.prototype */
+  {
+    /**
+     * @class ExportView
+     *
+     * @extends Backbone.View
+     * @constructs
+     */
+    initialize : function() {
+    },
+
+    model : Export,
+
+    classname : "export",
+
+    template: Handlebars.compile(exportTemplate),
+
+    render : function() {
+     // Display the ExportView
+    	this.setElement($("#export-modal")); 
+    	$(this.el).html(this.template(this.model.toJSON()));
+  	      
+      return this;
+    }
+  });
+  
+  return ExportView;
+}); 
