3897922b8d4fe5aa0b6666f447a3a723f4efde17
cesine <cesine@yahoo.com>
2012-06-28 21:41:24 -0400

    I cant really get the data to load properly without authenticating the user on the server and pulling thier data down. since we are still on iris couch it will be extra work to fake the callbacks. we should return to this after we finish the pouch api on our auth server, and after we have users saved in the couch and we can authenticate them. for #188 #204  im sure there is a way to decouple all of this and test without all dependancies but i cant come up with it.

--- public/app/AppView.js
+++ public/app/AppView.js
@@ -369,6 +369,22 @@ define([
         });
       });
     },
+    replicateDatabasesWithCallback : function(callback) {
+      this.fullScreenDatumView.model.pouch(function(err, db) {
+        db.replicate.to(Utils.couchUrl, { continuous: false }, function(err, resp) {
+          Utils.debug("Replicate to");
+          Utils.debug(resp);
+          Utils.debug(err);
+          
+        });
+        db.replicate.from(Utils.couchUrl, { continuous: false }, function(err, resp) {
+          Utils.debug("Replicate from");
+          Utils.debug(resp);
+          Utils.debug(err);
+          callback();
+        });
+      });
+    },
     /**
      * Synchronize the activity feed server and local databases.
      * TODO change this line in case it is called on the wrong model
