094ae1cd9bd51e2659325795c93a952ef0d23ed8
hisakonog <hisakonog@gmail.com>
2012-06-29 15:55:21 -0400

    get HelpConvention Popover to work fixes #237

--- public/datum/DatumFieldEditView.js
+++ public/datum/DatumFieldEditView.js
@@ -44,6 +44,7 @@ define([
       "click .encrypted" : "updateEncrypted",
       "blur .help-text" : "updateHelp",
       "blur .datum_field_input" : "updateField",
+      "click .icon-question-sign" : "showHelpConvention"
     },
 
     /**
@@ -107,7 +108,15 @@ define([
      */
     updateField : function() {
       this.model.set("value", this.$el.children(".datum_field_input").val());
-    }   
+    }, 
+    
+    /**
+     * Show help convention in popover  
+     */
+    showHelpConvention : function() {
+    	this.$el.children(".help-conventions").popover("show") 
+    }
+    
   });
 
   return DatumFieldEditView;
