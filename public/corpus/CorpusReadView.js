2052984ce1c2eb88b86c9bf2067957d8f1b38f00
cesine <cesine@yahoo.com>
2012-06-29 08:50:17 -0400

    there were some views for corpus that were missing so i added them so i can make everything editable and everything fullscreenabel that is supposed to be fullscreenable for #230 and #168

--- public/corpus/CorpusReadView.js
+++ public/corpus/CorpusReadView.js
@@ -2,6 +2,7 @@ define([
     "use!backbone", 
     "use!handlebars", 
     "text!corpus/corpus_read_embedded.handlebars",
+    "text!corpus/corpus_read_fullscreen.handlebars",
     "text!corpus/corpus_read_link.handlebars",
     "text!corpus/corpus_summary_read_embedded.handlebars",
     "corpus/Corpus",
@@ -11,6 +12,7 @@ define([
     Backbone, 
     Handlebars, 
     corpusReadEmbeddedTemplate,
+    corpusFullscreenTemplate,
     corpusReadLinkTemplate,
     corpusReadSummaryTemplate,
     Corpus,
@@ -24,7 +26,9 @@ define([
      *        Navigation panel on the main dashboard screen, which
      *        displays a menu of things the User can do (ex. open a new
      *        session, browse all entries, etc.).
-     * 
+     * @property {String} format Must be set when the CorpusEditView is
+     * initialized. Valid values are "centreWell" ,
+     * "fullscreen", "link" and "leftSide"
      * 
      * @description Starts the Corpus and initializes all its children.
      * 
@@ -54,7 +58,7 @@ define([
     /**
      * The Handlebars template rendered as the CorpusReadView.
      */
-    template : Handlebars.compile(corpusReadEmbeddedTemplate),
+    templateEmbedded : Handlebars.compile(corpusReadEmbeddedTemplate),
     
     /**
      * The Handlebars template rendered as the CorpusReadLinkView.
@@ -65,26 +69,36 @@ define([
      * The Handlebars template rendered as the CorpusSummaryReadView.
      */
     templateSummary : Handlebars.compile(corpusReadSummaryTemplate),
-
+    
+    /**
+     * The Handlebars template rendered as the CorpusFullscreenView.
+     */
+    templateFullscreen : Handlebars.compile(corpusFullscreenTemplate),
+   
     /**
      * Renders the CorpusReadView and all of its child Views.
      */
     render : function() {
       Utils.debug("CORPUS render: " + this.el);
-      
+      if (this.model == undefined) {
+        Utils.debug("\tCorpus model was undefined.");
+        return this;
+      }
       if (this.format == "leftSide") {
-        if (this.model != undefined) {
           // Display the CorpusReadView
           this.setElement($("#corpus-quickview"));
           $(this.el).html(this.templateSummary(this.model.toJSON()));
-  
-        } else {
-          Utils.debug("\tCorpus model was undefined.");
-        }
       } else if (this.format == "link") {
-        // Display the CorpusGlimpseView
+        // Display the CorpusGlimpseView, dont set the element
         $(this.el).html(this.templateLink(this.model.toJSON()));
+      } else if (this.format == "fullscreen"){
+        this.setElement($("#corpus-fullscreen"));
+        $(this.el).html(this.templateFullscreen(this.model.toJSON()));
+      } else if (this.format == "centreWell"){
+        this.setElement($("#corpus-embedded"));
+        $(this.el).html(this.templateEmbedded(this.model.toJSON()));
       }
+      
 
       return this;
     },
