7b2a311cc6127607c39fa0fb1f610ab9f2e045ae
cesine <cesine@yahoo.com>
2012-06-30 11:49:56 -0400

    moving everyauth into a seperate module

--- app.js
+++ app.js
@@ -9,94 +9,13 @@ var express     = require('express')
     ,ObjectId       = mongoose.SchemaTypes.ObjectId
 
     ,mongooseAuth  = require('mongoose-auth')
-    ,everyauthconf = require('./lib/everyauthconfig')
+    ,Users = require('./lib/restfullmongooseusers.js')
     
     ,https      = require('https')
     ,crypto     = require('crypto')
     ,fs         = require('fs');
 
 var apphttpsdomain = "https://localhost:3001";
-
-
-/*
- * Everyauth set up
- */
-everyauth.debug = true;
-var UserSchema = new Schema({})
-, User;
-
-UserSchema.plugin(mongooseAuth, {
-  everymodule: {
-    everyauth: {
-        User: function () {
-          return User;
-        }
-    }
-  }
-, facebook: {
-    everyauth: {
-        myHostname: 'http://local.host:3000'
-      , appId: everyauthconf.facebook.appId
-      , appSecret: everyauthconf.facebook.appSecret
-      , redirectPath: '/'
-    }
-  }
-, twitter: {
-    everyauth: {
-        myHostname: 'http://local.host:3000'
-      , consumerKey: everyauthconf.twitter.consumerKey
-      , consumerSecret: everyauthconf.twitter.consumerSecret
-      , redirectPath: '/'
-    }
-  }
-, password: {
-      loginWith: 'email'
-    , extraParams: {
-          phone: String
-        , name: {
-              first: String
-            , last: String
-          }
-      }
-    , everyauth: {
-          getLoginPath: '/login'
-        , postLoginPath: '/login'
-        , loginView: 'login.jade'
-        , getRegisterPath: '/register'
-        , postRegisterPath: '/register'
-        , registerView: 'register.jade'
-        , loginSuccessRedirect: '/'
-        , registerSuccessRedirect: '/'
-      }
-  }
-, github: {
-    everyauth: {
-        myHostname: 'http://local.host:3000'
-      , appId: everyauthconf.github.appId
-      , appSecret: everyauthconf.github.appSecret
-      , redirectPath: '/'
-    }
-  }
-, google: {
-    everyauth: {
-        myHostname: 'http://localhost:3000'
-      , appId: everyauthconf.google.clientId
-      , appSecret: everyauthconf.google.clientSecret
-      , redirectPath: '/'
-      , scope: 'https://www.google.com/m8/feeds'
-    }
-  }
-});
-//Adds login: String
-
-mongoose.model('User', UserSchema);
-
-mongoose.connect('mongodb://localhost/test');
-
-User = mongoose.model('User');
-/*
- * End everyauth setup
- */
 
 var httpsOptions ={
     key: fs.readFileSync('ifield.key'),
