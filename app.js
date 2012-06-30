c404b95310da0cb1806a6fbfdcce537758181ea5
cesine <cesine@yahoo.com>
2012-06-30 11:40:29 -0400

    Switched to mongooseauth to persist users, which required installing mongodb. Brought over the sample code from https://github.com/bnoguchi/mongoose-auth it worked much more seamlessly then patching together the everyauth docs and a everyauth+cradle+couch tutorial fixes #251

--- app.js
+++ app.js
@@ -1,12 +1,102 @@
 var express     = require('express')
     ,util       = require('util')
+    
     ,everyauth  = require('everyauth')
+    ,Promise        = everyauth.Promise
+    
+    ,mongoose   = require('mongoose')
+    ,Schema         = mongoose.Schema
+    ,ObjectId       = mongoose.SchemaTypes.ObjectId
+
+    ,mongooseAuth  = require('mongoose-auth')
+    ,everyauthconf = require('./lib/everyauthconfig')
+    
     ,https      = require('https')
     ,crypto     = require('crypto')
-    ,fs         = require('fs')
-    ,users      = require('./lib/users');
+    ,fs         = require('fs');
 
 var apphttpsdomain = "https://localhost:3001";
+
+
+/*
+ * Everyauth set up
+ */
+everyauth.debug = true;
+var UserSchema = new Schema({})
+, User;
+
+UserSchema.plugin(mongooseAuth, {
+  everymodule: {
+    everyauth: {
+        User: function () {
+          return User;
+        }
+    }
+  }
+, facebook: {
+    everyauth: {
+        myHostname: 'http://local.host:3000'
+      , appId: everyauthconf.facebook.appId
+      , appSecret: everyauthconf.facebook.appSecret
+      , redirectPath: '/'
+    }
+  }
+, twitter: {
+    everyauth: {
+        myHostname: 'http://local.host:3000'
+      , consumerKey: everyauthconf.twitter.consumerKey
+      , consumerSecret: everyauthconf.twitter.consumerSecret
+      , redirectPath: '/'
+    }
+  }
+, password: {
+      loginWith: 'email'
+    , extraParams: {
+          phone: String
+        , name: {
+              first: String
+            , last: String
+          }
+      }
+    , everyauth: {
+          getLoginPath: '/login'
+        , postLoginPath: '/login'
+        , loginView: 'login.jade'
+        , getRegisterPath: '/register'
+        , postRegisterPath: '/register'
+        , registerView: 'register.jade'
+        , loginSuccessRedirect: '/'
+        , registerSuccessRedirect: '/'
+      }
+  }
+, github: {
+    everyauth: {
+        myHostname: 'http://local.host:3000'
+      , appId: everyauthconf.github.appId
+      , appSecret: everyauthconf.github.appSecret
+      , redirectPath: '/'
+    }
+  }
+, google: {
+    everyauth: {
+        myHostname: 'http://localhost:3000'
+      , appId: everyauthconf.google.clientId
+      , appSecret: everyauthconf.google.clientSecret
+      , redirectPath: '/'
+      , scope: 'https://www.google.com/m8/feeds'
+    }
+  }
+});
+//Adds login: String
+
+mongoose.model('User', UserSchema);
+
+mongoose.connect('mongodb://localhost/test');
+
+User = mongoose.model('User');
+/*
+ * End everyauth setup
+ */
 
 var httpsOptions ={
     key: fs.readFileSync('ifield.key'),
@@ -17,19 +107,40 @@ app.configure(function() {
   app.use(express.bodyParser());
   app.use(express.cookieParser());
   app.use(express.session({secret: "90ndsj9dfdsfwewfead3"}));
-  app.use(everyauth.middleware());
+//  app.use(everyauth.middleware()); //replaced by mongooseAuth
   app.use(express.static(__dirname + '/public'));
-  app.use(app.router);
+//  app.use(app.router); //see notes on https://github.com/bnoguchi/mongoose-auth/
+  app.use(mongooseAuth.middleware());
   app.use(express.errorHandler());
-  everyauth.helpExpress(app);
+//  everyauth.helpExpress(app); //replaced by mongooseauth
 });
 
-app.get('/auth', function (req, res) {
-  res.render('auth');
+//app.get('/auth', function (req, res) {
+//  res.render('auth');
+//});
+//app.get('/register', function(req, res){
+//  res.redirect("https://localhost:3001/register.html");
+//});
+
+/*
+ * Everyauth views
+ */
+app.configure( function () {
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
 });
-app.get('/register', function(req, res){
-  res.redirect("https://localhost:3001/register.html");
+
+app.get('/', function (req, res) {
+  res.render('home');
 });
+
+app.get('/logout', function (req, res) {
+    req.logout();
+    res.redirect('/');
+});
+/*
+ * End everyauth views
+ */
 
 
 app.get('/:usergeneric/:corpusordatalist', function(req, res){
@@ -49,6 +160,8 @@ app.get('/:usergeneric', function(req, res){
   res.redirect("https://localhost:3001\#user/"+req.params.usergeneric);
 });
 
+
+mongooseAuth.helpExpress(app);
 
 port = "3001";
 app.listen(port);
