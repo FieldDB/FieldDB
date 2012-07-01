66a552eb62090733855c49d12222a9d05e2eaa41
cesine <cesine@yahoo.com>
2012-06-30 23:16:21 -0400

    @mecathcart @hisakonog @trisapeace to open the app now, click on the button log in as sapir. if you are in a fresh incognito window you will have to refresh the page in order for it to actually load all of sapir's settings. but it should be good enough for you to use while i finish working on transfering our database to ec2

--- public/app/AppView.js
+++ public/app/AppView.js
@@ -352,29 +352,41 @@ define([
      * around and get a feel for the app by seeing the data in context.
      */
     loadSample : function() {
-      //these old methods were over writing the model with new data, effectively deleting old models which will be a terrible thing ot do in prodution
-//      // Sample Corpus data
+      // Sample Corpus data
 //      this.model.get("corpus").set({
 //        "title" : "Quechua Corpus",
 //        "titleAsUrl": "Quechua_Corpus",
 //        "description" : "This is a corpus which will let you explore the app and see how it works. "
 //            + "\nIt contains some data from one of our trips to Cusco, Peru."
 //      });
-//
-//      // Sample Session data
-//      this.model.get("currentSession").set("sessionFields", new DatumFields([
-//        {label: "user", value: ""},
-//        {label: "consultants", value: "John Doe and Mary Stewart"},
-//        {label: "language", value: ""},
-//        {label: "dialect", value: ""},
-//        {label: "dateElicited", value: new Date()},
-//        {label: "dateSEntered", value: new Date()},
-//        {label: "goal", value: "To Win!"}
-//      ]));
-//        
-      this.dataListEditLeftSideView.loadSample();
+      this.model.get("corpus").id ="822AFBA3-CE50-40F5-B983-315277DD9661";
+      this.model.get("corpus").fetch();
+      
+      // Sample Session data
+      this.model.get("currentSession").set("sessionFields", new DatumFields([
+        {label: "user", value: ""},
+        {label: "consultants", value: "Tillohash and Gladys"},
+        {label: "language", value: "Cusco Quechua"},
+        {label: "dialect", value: "Upper Cusco"},
+        {label: "dateElicited", value: new Date()},
+        {label: "dateSEntered", value: new Date()},
+        {label: "goal", value: "Explore verbs which can combine with -naya"}
+      ]));
+      //TODO cant load sesson from database with out restructuring? SESSION render: [object HTMLDivElement] Utils.js:52
+      //Uncaught TypeError: Object [object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object] has no method 'where' 
+      
+//      this.model.get("currentSession").id = "421CCC12-1487-4696-B7E9-AF80BBB9296C";
+//      this.model.get("currentSession").fetch();
+        
+      this.model.get("currentDataList").id = "45444C8F-D707-426D-A422-54CD4041A5A1";
+      this.model.get("currentDataList").fetch();
+      
+//      this.dataListEditLeftSideView.loadSample();
 
       this.authView.loadSample();
+      //cannot laod directly cause wa want to fake that sapir is logged in.
+//      this.authView.get("user").id = "5198E356-55AC-4E56-8F5D-CF3266C6457E";
+//      this.authView.get("user").fetch();
       
       this.searchView.loadSample();
     },
