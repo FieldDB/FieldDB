473bbf59e6b5228f42c7a735eaa5c6c444ae72fe
trisapeace <trisapeace@gmail.com>
2012-07-04 13:19:33 -0400

    Duplicate datum now uses parse() instead of restructure. Fixes #194.

--- public/datum/Datum.js
+++ public/datum/Datum.js
@@ -253,18 +253,15 @@ define([
     clone : function() {
       // Create a new Datum based on the current Datum
       var datum = new Datum({
-        audioVideo : this.get("audioVideo").toJSON(),
-        comments : this.get("comments").toJSON(),
-        dateEntered : this.get("dateEntered").toJSON(),
-        datumFields : this.get("datumFields").toJSON(),
-        datumState : this.get("datumState").toJSON(),
-        datumStates : this.get("datumStates").toJSON(),
-        datumTags : this.get("datumTags").toJSON(),
-        session: this.get("session").toJSON()
+        audioVideo : new AudioVideo(this.get("audioVideo").toJSON(), {parse: true}),
+        comments : new Comments(this.get("comments").toJSON(), {parse: true}),
+        dateEntered : this.get("dateEntered"),
+        datumFields : new DatumFields(this.get("datumFields").toJSON(), {parse: true}),
+        datumState : new DatumState(this.get("datumState").toJSON(), {parse: true}),
+        datumStates : new DatumStates(this.get("datumStates").toJSON(), {parse: true}),
+        datumTags : new DatumTags(this.get("datumTags").toJSON(), {parse: true}),
+        session: new Session(this.get("session").toJSON(), {parse: true})
       });
-      
-      // TODO Need to find a new way of doing this.
-      datum.restructure();
       
       return datum;
     }
