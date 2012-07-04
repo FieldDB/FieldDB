3b544a88719785553a00fdf1be8842d5fb9cb5c5
trisapeace <trisapeace@gmail.com>
2012-07-04 17:16:11 -0400

    Not working.
    Whenever I call (new Datum()).getAllDatumidsByDate(), it can't find my
    "get_datum_ids/by_date" view even though I can see it in the IndexedDB.

--- public/datum/Datum.js
+++ public/datum/Datum.js
@@ -138,6 +138,7 @@ define([
         
         db.query("get_datum_ids/by_date", {reduce: false}, function(err, response) {
           if ((!err) && (typeof callback == "function"))  {
+            console.log("Callback with: ", response.rows);
             callback(response.rows)
           }
         });
