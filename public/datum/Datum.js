4d939265a7f6678644ac1e14b580abd00626e575
trisapeace <trisapeace@gmail.com>
2012-07-06 18:05:19 -0400

    Sessions are not re-rendering correctly when their model changes #276.

--- public/datum/Datum.js
+++ public/datum/Datum.js
@@ -126,24 +126,25 @@ define([
      * the Datum's ID.
      */
     getAllDatumIdsByDate : function(callback) {
-      this.pouch(function(err, db) {
-        /*
-        Code for get_datum_ids/by_date
-        
-        function(doc) {
-          if (doc.dateEntered) {
-            emit(doc.dateEntered, doc.id);
-          }
-        }
-        */
-        
-        db.query("get_datum_ids/by_date", {reduce: false}, function(err, response) {
-          if ((!err) && (typeof callback == "function"))  {
-            console.log("Callback with: ", response.rows);
-            callback(response.rows)
-          }
-        });
-      });
+      // this.pouch(function(err, db) {
+        // /*
+        // Code for get_datum_ids/by_date
+//         
+        // function(doc) {
+          // if (doc.dateEntered) {
+            // emit(doc.dateEntered, doc.id);
+          // }
+        // }
+        // */
+//         
+        // db.query("get_datum_ids/by_date", {reduce: false}, function(err, response) {
+          // if ((!err) && (typeof callback == "function"))  {
+            // console.log("Callback with: ", response.rows);
+            // callback(response.rows)
+          // }
+        // });
+      // });
+      callback([]);
     },
     
     searchByQueryString : function(queryString, callback) {
