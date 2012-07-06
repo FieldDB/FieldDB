f0b5dd13d12c49b310bacd24b31759fe67a02eb9
trisapeace <trisapeace@gmail.com>
2012-07-06 13:02:41 -0400

    Bug fix: sessionFields wasn't being set with the proper syntax.

--- public/app/App.js
+++ public/app/App.js
@@ -158,14 +158,14 @@ define([
           Utils.debug("Session fetched successfully" + e);
           s.relativizePouchToACorpus(self.get("corpus"));
           s.set(
-              sessionFields , self.get("corpus").get("sessionFields").clone()
+              "sessionFields", self.get("corpus").get("sessionFields").clone()
           );
         },
         error : function(e) {
           Utils.debug("There was an error restructuring the session. Loading defaults..."+e);
           s.relativizePouchToACorpus(self.get("corpus"));
           s.set(
-              sessionFields , self.get("corpus").get("sessionFields").clone()
+              "sessionFields" , self.get("corpus").get("sessionFields").clone()
           );
         }
       });
