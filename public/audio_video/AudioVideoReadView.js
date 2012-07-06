2440ce32c37c3bc426445ff422514b0bde825dd3
trisapeace <trisapeace@gmail.com>
2012-07-06 11:51:17 -0400

    Added a dummy version for AudioVideoReadView. #273.

---
+++ public/audio_video/AudioVideoReadView.js
@@ -0,0 +1,71 @@
+// TODO Change this into a Read-only version. Right now it's just a copy of the Editable version
+
+define([ 
+    "use!backbone",
+    "use!handlebars", 
+    "text!audio_video/audio_video_edit_embedded.handlebars",
+    "audio_video/AudioVideo"
+], function(
+    Backbone, 
+    Handlebars,
+    audioVideoTemplate,
+    AudioVideo
+) {
+  var AudioVideoEditView = Backbone.View.extend(
+  /** @lends AudioVideoEditView.prototype */
+  {
+    /**
+     * @class This is the view of the Audio Video Model.
+     *
+     * @extends Backbone.View
+     * @constructs
+     */
+    initialize : function() {
+    },
+    
+    events : {
+      "dragenter" : "dragEnterAudio",
+      "dragover" : "dragOverAudio",
+      "dragleave" : "dragLeave",
+      "drop": "dropAudio"
+    },
+    
+    model : AudioVideo,
+
+    classname : "audio_video",
+
+    template : Handlebars.compile(audioVideoTemplate),
+
+    render : function() {
+      $(this.el).html(this.template(this.model.toJSON()));
+      
+      return this;
+    },
+    
+    dragEnterAudio: function(e) {
+      e.stopPropagation();
+      e.preventDefault();
+//      this.classList.add('dropping');
+    },
+    
+    dragOverAudio : function(e) {
+      e.stopPropagation();
+      e.preventDefault();
+      e.dataTransfer.dropEffect = 'copy'; // Explicitly show this is a copy.
+    },
+    
+    dragLeave: function(e) {
+//      this.classList.remove('dropping');
+    },
+    
+    dropAudio: function(e) {
+      e.stopPropagation();
+      e.preventDefault();
+//      this.classList.remove('dropping');
+      window.appView.term.addDroppedFiles(e.dataTransfer.files);
+      window.appView.term.output('<div>File(s) added!</div>');
+    }
+  });
+
+  return AudioVideoEditView;
+});
