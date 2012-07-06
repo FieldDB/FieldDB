22a5a1c1071eb689598a7c8a25cf5b4cd5857345
cesine <cesine@yahoo.com>
2012-07-06 12:42:46 -0400

    adding couchconnection to the loadbyids fucntion in App.js also, so to make sure that the fetch happens from the right corpus connection" "

--- public/corpus/Corpus.js
+++ public/corpus/Corpus.js
@@ -213,7 +213,7 @@ define([
       dataLists : DataLists, 
       permissions : Permissions,
       comments: Comments,
-      couchConnection : Utils.defaultCouchConnection()
+      couchConnection : localStorage.getItem("mostRecentCouchConnection") || Utils.defaultCouchConnection()
       
     },
     
@@ -289,6 +289,10 @@ define([
             Utils.debug("Replicate from " + couchurl);
             Utils.debug(resp);
             Utils.debug(err);
+            if(err == null || err == undefined){
+              //This was a valid connection, lets save it into localstorage.
+              localStorage.setItem("mostRecentCouchConnection",couchConnection);
+            }
             if(typeof fromcallback == "function"){
               fromcallback();
             }
