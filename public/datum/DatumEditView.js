4b4eace0db88b03fd4561ea0ca4312ba31881268
cesine <cesine@yahoo.com>
2012-07-10 19:01:43 -0400

    export as latex gb4e is in fixes #190

--- public/datum/DatumEditView.js
+++ public/datum/DatumEditView.js
@@ -72,6 +72,7 @@ define([
       "click #duplicate" : "duplicateDatum",
       "click .icon-plus" : "newDatum",
       "change .datum_state_select" : "updateDatumStates",
+      "click .LaTeX" : "laTeXiT"
     },
 
     /**
@@ -227,7 +228,19 @@ define([
      * switches over to their LaTeX file they only need to paste it in.
      */
     laTeXiT : function() {
-      return "";
+      utterance= this.model.get("datumFields").where({label: "utterance"})[0].get("value");
+      gloss = this.model.get("datumFields").where({label: "gloss"})[0].get("value");
+      translation= this.model.get("datumFields").where({label: "translation"})[0].get("value");
+      
+      $("#export-type-description").html(" as LaTeX (GB4E)");
+      $("#export-text-area").val( $("#export-text-area").val()+
+          "\n \\begin{exe} "
+          + "\n \\ex [*] \\gll "+utterance+" \\\\"
+          + "\n\t"+gloss+" \\\\"
+          + "\n\t\\glt `"+ translation +"'"
+          + "\n\\end{exe}\n\n"
+               );
+      $("#export-modal").modal("show");
     },
     
     /**
