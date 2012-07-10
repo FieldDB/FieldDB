7ee8596dff3da9e450d0829f89f498e3d0407f7a
cesine <cesine@yahoo.com>
2012-07-09 23:19:30 -0400

    adding pub sub to listen (subscribe) and publish save model events and failures in the storeCurrentDashboardIdsToLocalStorage function for #171

--- public/app/App.js
+++ public/app/App.js
@@ -223,11 +223,38 @@ define([
      * 
      */
     storeCurrentDashboardIdsToLocalStorage : function(callback){
+      /*
+       * Turn on pub sub to find out when all three have saved, then call the callback
+       */
+      var thiscallback = callback;
+      window.hub.unsubscribe("savedToPouch", null, this);
+      window.hub.unsubscribe("saveFailedToPouch", null, this);
+      
+      this.savedcount = 0;
+      this.savefailedcount = 0;
+      window.hub.subscribe("savedToPouch",function(arg){
+        alert("Saved "+ arg+ " to pouch.");
+        window.app.savedcount++;
+        if( window.app.savedcount.length == 3){
+          localStorage.setItem("userid", window.app.get("authentication").get("userPrivate").get("id"));//the user private should get their id from mongodb
+          //save ids to the user also so that the app can bring them back to where they were
+          if(typeof thiscallback == "function"){
+            thiscallback();
+          }
+          window.hub.unsubscribe("savedToPouch", null, window.app);
+        }
+      },this);
+      window.hub.subscribe("saveFailedToPouch",function(arg){
+        Utils.debug("Saved "+ arg+ " to pouch.");
+        window.app.savefailedcount++;
+        alert("Save failed "+arg);
+      },this);
       var self = this;
       this.get("currentSession").changeCorpus( self.get("corpus").get("couchConnection").corpusname, function(){
         self.get("currentSession").save(null, {
           success : function(model, response) {
-            console.log('Session save success');
+            Utils.debug('Session save success');
+            hub.publish("savedToPouch","session"+model.get("id"));
             try{
               window.app.get("authentication").get("userPrivate").get("mostRecentIds").sessionid = model.get("id");
             }catch(e){
@@ -235,14 +262,16 @@ define([
             }
           },
           error : function(e) {
-            console.log('Session save error' + e);
+            Utils.debug('Session save error' + e);
+            hub.publish("saveFailedToPouch","session");
           }
         });
       });
       this.get("currentDataList").changeCorpus( self.get("corpus").get("couchConnection").corpusname, function(){
         self.get("currentDataList").save(null, {
           success : function(model, response) {
-            console.log('Datalist save success');
+            Utils.debug('Datalist save success');
+            hub.publish("savedToPouch","datalist"+model.get("id"));
             try{
               window.app.get("authentication").get("userPrivate").get("mostRecentIds").datalistid = model.get("id");
             }catch(e){
@@ -250,47 +279,29 @@ define([
             }
           },
           error : function(e) {
-            console.log('Datalist save error' + e);
+            Utils.debug('Datalist save error' + e);
+            hub.publish("saveFailedToPouch","datalist");
           }
         });
       });
       this.get("corpus").changeCorpus( self.get("corpus").get("couchConnection"), function(){
         self.get("corpus").save(null, {
           success : function(model, response) {
-            console.log('Corpus save success');
+            Utils.debug('Corpus save success');
+            hub.publish("savedToPouch","corpus"+model.get("id"));
             try{
-              localStorage.setItem("mostRecentCouchConnection", model.get("couchConnection"));
+              localStorage.setItem("mostRecentCouchConnection", JSON.stringify(model.get("couchConnection")));
               window.app.get("authentication").get("userPrivate").get("mostRecentIds").corpusid = model.get("id");
             }catch(e){
               Utils.debug("Couldnt save the corpus id to the user's mostrecentids"+e);
             }
           },
           error : function(e) {
-            console.log('Corpus save error' + e);
+            Utils.debug('Corpus save error' + e);
+            hub.publish("saveFailedToPouch","corpus");
           }
         });
       });
-
-      var thiscallback = callback;
-        //Note: unable to use the success and fail of the backbone save to trigger this, so instead, waiting 1 second and hoping all the saves resulted in ids
-        window.setTimeout( function(){
-          //moved the save of these into the 
-//          var ids = {};
-//          ids.corpusid = window.app.get("corpus").get("id");
-//          ids.sessionid = window.app.get("currentSession").get("id");
-//          ids.datalistid = window.app.get("currentDataList").get("id");
-          
-//          localStorage.setItem("appids", JSON.stringify(ids));
-          localStorage.setItem("userid", window.app.get("authentication").get("userPrivate").get("id"));//the user private should get their id from mongodb
-          
-          //save ids to the user also so that the app can bring them back to where they were
-          
-          if(typeof thiscallback == "function"){
-            thiscallback();
-          }
-        }, 5000);
-        
-        
     }
 
   });
