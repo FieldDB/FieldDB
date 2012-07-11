5f4556bc3264dc090d2d59ba1d0691bf7499bd75
trisapeace <trisapeace@gmail.com>
2012-07-11 11:35:30 -0400

    Changed behaviour of the new user box so that it advances on Enter key rather than on blur.
    Also changes focus to the password field when you push enter in the username
    field or click the new user button.

--- public/user/UserWelcomeView.js
+++ public/user/UserWelcomeView.js
@@ -57,15 +57,20 @@ define([
         e.target.select();
       },
       
-      "blur .username" : function() {
-        if (this.$el.find(".username").val() != "YourNewUserNameGoesHere") {
-            this.model.set("username",$(".username").val());      
-            $(".confirm-password").show();
+      "keyup .username" : function(e) {
+        var code = e.keyCode || e.which;
+        
+        // code == 13 is the enter key
+        if ((code == 13) && (this.$el.find(".username").val() != "YourNewUserNameGoesHere")) {
+          this.model.set("username",$(".username").val());      
+          $(".confirm-password").show();
+          $(".password").focus()
         };
       },
-//      "click .new-user-button" : function() {
-//        $(".confirm-password").show();
-//      },
+     "click .new-user-button" : function() {
+       $(".confirm-password").show();
+       $(".password").focus()
+     },
       "click .register-new-user" : function() {
         Utils.debug("Attempting to register a new user: " + this.el);
         /*
