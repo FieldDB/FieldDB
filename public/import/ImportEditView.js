53fe1af2ee1517d2bbdfb1413549b0cbae799672
cesine <cesine@yahoo.com>
2012-07-10 23:43:19 -0400

    starting import for #192 untested

--- public/import/ImportEditView.js
+++ public/import/ImportEditView.js
@@ -8,6 +8,17 @@ define( [
     Import
 ) {
   var ImportEditView = Backbone.View.extend({
+    initialize : function(){
+      
+    },
+    events : {
+      "dragover .drop-zone" : function(evt) {
+        evt.stopPropagation();
+        evt.preventDefault();
+        evt.dataTransfer.dropEffect = 'copy'; // Explicitly show this is a copy.
+      },
+      "drop .drop-zone": "dropFile"
+    },
     tagName : "div",
     
     className : "import",
@@ -15,7 +26,15 @@ define( [
     model: Import,
     
     template: Handlebars.templates.import_edit_modal,
+    dropFile : function(evt) {
+      alert("dropped file");
+      evt.stopPropagation();
+      evt.preventDefault();
+      var draggedfiles = evt.dataTransfer.files; // FileList object.
+      // files is a FileList of File objects. List some properties.
+//      var importObj = new Import({files: draggedfiles});
 
+    },
     render : function() {
       this.setElement("#import-modal");
       $(this.el).html(this.template(this.model.toJSON()));
