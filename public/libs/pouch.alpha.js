a795a1247bec753b0b6ac52a1ed0aa2202651b32
trisapeace <trisapeace@gmail.com>
2012-07-11 14:08:41 -0400

    Fixed typo in the search map function #297.

--- public/libs/pouch.alpha.js
+++ public/libs/pouch.alpha.js
@@ -2307,7 +2307,7 @@ var IdbPouch = function(opts, callback) {
     // Note that spacing is very important. Do not reformat these values
     var validCouchViews = {
       getAllDatumIdsByDate : function(doc) {if (doc.dateEntered) {emit(doc.dateEntered, doc);}},
-      searchByQueryString : function(doc) {if ((doc.datumFields) && (doc.session)) {var obj = {};for (i = 0; i < doc.datumFields.length; i++) {if (doc.datumFields[i].value) {obj[doc.datumFields[i].label] = doc.datumFields[i].value;}}if (doc.session.sessionFields) {for (j = 0; j < doc.session.sessionFields.length; j++) {if (doc.session.sessionFields[j].value) {obj[doc.session.sessionFields[j].label] = doc.doc.session.sessionFields[j].value;}}}emit(obj, doc._id);}}
+      searchByQueryString : function(doc) {if ((doc.datumFields) && (doc.session)) {var obj = {};for (i = 0; i < doc.datumFields.length; i++) {if (doc.datumFields[i].value) {obj[doc.datumFields[i].label] = doc.datumFields[i].value;}}if (doc.session.sessionFields) {for (j = 0; j < doc.session.sessionFields.length; j++) {if (doc.session.sessionFields[j].value) {obj[doc.session.sessionFields[j].label] = doc.session.sessionFields[j].value;}}}emit(obj, doc._id);}}
     };
 
     // We may have passed in an anonymous function that used emit in
