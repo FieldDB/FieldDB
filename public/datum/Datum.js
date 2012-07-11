42c9053f22c716a6d2b2ddcd43c84d0389930020
trisapeace <trisapeace@gmail.com>
2012-07-11 13:54:58 -0400

    Added the search map function to pouch.alpha.js #297
    Also fixed a bug with the search map function that is being stored in
    the database by the server for new users.

--- public/datum/Datum.js
+++ public/datum/Datum.js
@@ -132,16 +132,6 @@ define([
       var self = this;
       this.changeCorpus(this.get("corpusname"),function(){
         self.pouch(function(err, db) {
-          /*
-        Code for get_datum_ids/by_date
-        
-        function(doc) {
-          if (doc.dateEntered) {
-            emit(doc.dateEntered, doc.id);
-          }
-        }
-           */
-          
           db.query("get_datum_ids/by_date", {reduce: false}, function(err, response) {
             if ((!err) && (typeof callback == "function"))  {
               console.log("Callback with: ", response.rows);
@@ -154,65 +144,47 @@ define([
     
     searchByQueryString : function(queryString, callback) {
       var self = this;
-      this.pouch(function(err, db) {
-        // Code for get_datum_field/get_datum_fields
-        //
-        // function(doc) {
-        //   if ((doc.datumFields) && (doc.session)) {
-        //     var obj = {};
-        //     for (i = 0; i < doc.datumFields.length; i++) {
-        //       if (doc.datumFields[i].value) {
-        //         obj[doc.datumFields[i].label] = doc.datumFields[i].value;
-        //       }
-        //     }
-        //     if (doc.session.sessionFields) {
-        //       for (j = 0; j < doc.session.sessionFields.length; j++) {
-        //         if (doc.session.sessionFields[j].value) {
-        //           obj[doc.session.sessionFields[j].label] = doc.doc.session.sessionFields[j].value;
-        //         }
-        //       }
-        //     }
-        //     emit(obj, doc._id);
-        //   }
-        // }
-        db.query("get_datum_field/get_datum_fields", {reduce: false}, function(err, response) {
-          var matchIds = [];
-          
-          if (!err) {
-            // Process the given query string into tokens
-            var queryTokens = self.processQueryString(queryString);
+      this.changeCorpus(this.get("corpusname"), function() {
+        self.pouch(function(err, db) {
+          db.query("get_datum_field/get_datum_fields", {reduce: false}, function(err, response) {
+            var matchIds = [];
             
-            // Go through all the rows of results
-            for (i in response.rows) {
-              // Determine if this datum matches the first search criteria
-              var thisDatumIsIn = self.matchesSingleCriteria(response.rows[i].key, queryTokens[0]);
+            if (!err) {
+              // Process the given query string into tokens
+              var queryTokens = self.processQueryString(queryString);
               
-              // Progressively determine whether the datum still matches based on
-              // subsequent search criteria
-              for (j = 1; j < queryTokens.length; j += 2) {
-                if (queryTokens[j] == "AND") {
-                  // Short circuit: if it's already false then it continues to be false
-                  if (!thisDatumIsIn) {
-                    break;
+              // Go through all the rows of results
+              for (i in response.rows) {
+                // Determine if this datum matches the first search criteria
+                var thisDatumIsIn = self.matchesSingleCriteria(response.rows[i].key, queryTokens[0]);
+                
+                // Progressively determine whether the datum still matches based on
+                // subsequent search criteria
+                for (j = 1; j < queryTokens.length; j += 2) {
+                  if (queryTokens[j] == "AND") {
+                    // Short circuit: if it's already false then it continues to be false
+                    if (!thisDatumIsIn) {
+                      break;
+                    }
+                    
+                    // Do an intersection
+                    thisDatumIsIn = thisDatumIsIn && self.matchesSingleCriteria(response.rows[i].key, queryTokens[j+1]);
+                  } else {
+                    // Do a union
+                    thisDatumIsIn = thisDatumIsIn || self.matchesSingleCriteria(response.rows[i].key, queryTokens[j+1]);
                   }
-                  
-                  // Do an intersection
-                  thisDatumIsIn = thisDatumIsIn && self.matchesSingleCriteria(response.rows[i].key, queryTokens[j+1]);
-                } else {
-                  // Do a union
-                  thisDatumIsIn = thisDatumIsIn || self.matchesSingleCriteria(response.rows[i].key, queryTokens[j+1]);
+                }
+                
+                // If the row's datum matches the given query string
+                if (thisDatumIsIn) {
+                  // Keep its datum's ID, which is the value
+                  matchIds.push(response.rows[i].value);
                 }
               }
-              
-              // If the row's datum matches the given query string
-              if (thisDatumIsIn) {
-                // Keep its datum's ID, which is the value
-                matchIds.push(response.rows[i].value);
-              }
             }
-          }
-          
-          callback(matchIds);
+            
+            callback(matchIds);
+          });
         });
       });
     },
