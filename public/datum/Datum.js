8db285212abbb6933d982c9151456338444ad50f
trisapeace <trisapeace@gmail.com>
2012-07-12 09:52:51 -0400

    Reformatting. No code changes.

--- public/datum/Datum.js
+++ public/datum/Datum.js
@@ -101,10 +101,10 @@ define([
     },
 
     changeCorpus : function(corpusname, callback) {
-      if(this.pouch == undefined){
+      if (this.pouch == undefined) {
         this.pouch = Backbone.sync.pouch(Utils.androidApp() ? Utils.touchUrl + corpusname : Utils.pouchUrl + corpusname);
       }
-      if(typeof callback == "function"){
+      if (typeof callback == "function") {
         callback();
       }
     },
