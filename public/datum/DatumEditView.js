c777e5b20f39084c8b69de9ca94fa1f9145e86c1
cesine <cesine@yahoo.com>
2012-07-13 08:00:36 -0400

    bug fixes from last night: i had duplicated the export id in the import and made the unicode drag too general so that all textareas can have unicode, now i just added it to datum fields. later we might want to let users drag unicode to comments and to the search box.

--- public/datum/DatumEditView.js
+++ public/datum/DatumEditView.js
@@ -139,7 +139,7 @@ define([
         window.setTimeout(function(){
           $('.datum-field').each(function(index, item) {
             item.classList.add( $(item).find("label").html() );
-            $("textarea").each(function(index){
+            $("datum_field_input").each(function(index){
               this.addEventListener('drop', window.appView.dragUnicodeToField, false);
               this.addEventListener('dragover', window.appView.handleDragOver, false);
             });
