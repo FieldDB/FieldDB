d83469a2c926aa12f005e980cb4e8f7f3942d7c6
trisapeace <trisapeace@gmail.com>
2012-07-13 17:33:48 -0400

    Going to the next page now works #326.
    But it is still using addOne instead of creating and rendering the datum
    from the database.
    
    Bug fixed with creating a new data list. They were keeping the datumIds from
    the old data lists.
    
    Now only re-render once the datum has been retrieved from the database.

--- public/data_list/DataListEditView.js
+++ public/data_list/DataListEditView.js
@@ -207,6 +207,7 @@ define( [
       // Clear the current data list
       this.model.attributes = {};
       this.model.set((new DataList()).toJSON());
+      this.model.set("datumIds", []);
       this.model.set("corpusname", app.get("corpus").get("corpusname"));
       
       // Clear the view
@@ -247,6 +248,10 @@ define( [
             success : function(model, response) {
               // Render at the top
               self.datumsView.collection.add(model, {at:0});
+              
+              // Display the updated data list
+              appView.renderReadonlyDataListViews();
+              appView.renderEditableDataListViews();
             }
           });
         });
@@ -258,29 +263,33 @@ define( [
         var numDatumCurrentlyDisplayed = this.datumsView.collection.length
         if ((numDatumCurrentlyDisplayed == 0) || (numDatumCurrentlyDisplayed % this.perPage != 0)) {
           // Fetch its model from the database
-            var d = new Datum({
-              corpusname : window.app.get("corpus").get("corpusname")
-            });
-            d.id = datumId;
-            var self = this;
-            d.changeCorpus(window.app.get("corpus").get("corpusname"), function(){
-              d.fetch({
-                success : function(model, response) {
-                  // If there is still room on the current page
-                  var numDatumCurrentlyDisplayed = self.datumsView.collection.length
-                  if ((numDatumCurrentlyDisplayed == 0) || (numDatumCurrentlyDisplayed % self.perPage != 0)) {
-                    // Render at the bottom
-                    self.datumsView.collection.add(model);
-                  }
+          var d = new Datum({
+            corpusname : window.app.get("corpus").get("corpusname")
+          });
+          d.id = datumId;
+          var self = this;
+          d.changeCorpus(window.app.get("corpus").get("corpusname"), function(){
+            d.fetch({
+              success : function(model, response) {
+                // If there is still room on the current page
+                var numDatumCurrentlyDisplayed = self.datumsView.collection.length
+                if ((numDatumCurrentlyDisplayed == 0) || (numDatumCurrentlyDisplayed % self.perPage != 0)) {
+                  // Render at the bottom
+                  self.datumsView.collection.add(model);
+                  
+                  // Display the updated data list
+                  appView.renderReadonlyDataListViews();
+                  appView.renderEditableDataListViews();
                 }
-              });
+              }
             });
+          });
+        } else {
+          // Display the updated data list
+          appView.renderReadonlyDataListViews();
+          appView.renderEditableDataListViews();
         }
       }
-      
-      // Display the updated data list
-      appView.renderReadonlyDataListViews();
-      appView.renderEditableDataListViews();
     },
 
     /**
