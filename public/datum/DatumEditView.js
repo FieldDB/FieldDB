2ee69787dd2565d521bd67cf27c187c74a22231e
trisapeace <trisapeace@gmail.com>
2012-07-13 15:12:52 -0400

    Wasn't changing corpus before these two datalist saves. #326

--- public/datum/DatumEditView.js
+++ public/datum/DatumEditView.js
@@ -280,12 +280,18 @@ define([
                 // Add it to the default data list
                 var defaultIndex = app.get("corpus").get("dataLists").length - 1;
                 app.get("corpus").get("dataLists").models[defaultIndex].get("datumIds").unshift(model.id);
-                self.model.changeCorpus(self.model.get("corpusname"), function() {
+                
+                // Save the default data list
+                app.get("corpus").get("dataLists").models[defaultIndex].changeCorpus(app.get("corpus").get("corpusname"), function() {
                   app.get("corpus").get("dataLists").models[defaultIndex].save();
+                });
+                
+                // Save the corpus
+                app.get("corpus").changeCorpus(app.get("corpus").get("couchConnection"), function() {
                   app.get("corpus").save();
                 });
                 
-                // If the default data list is the currently visible data list, re-render it
+                // If the default data list is the currently visible data list, add this datum to the view
                 if (app.get("corpus").get("dataLists").models[defaultIndex].cid == app.get("corpus").get("dataLists").models[defaultIndex].cid) {
                   appView.dataListEditLeftSideView.addOne(model.id, true);
                 }
