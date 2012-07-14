4784fd70b48b010ef883e8ace910012ee1a6af32
Emmy Cathcart <maryellencathcart@gmail.com>
2012-07-14 08:56:59 -0400

    div names changed @cesine @trisapeace how do I know that the variables I created for my updating collection views are connected to something thats retrieving the stuff from couch or pouch? I coudn't tlel yesterday because Sapir wasn't loading but now he's loading and I'm pretty sure my updating collections are not retrieving anything from pouch or couch, but maybe that code is alraedy written? I tried to how datalists were getting datums but I got confused

--- public/corpus/CorpusEditView.js
+++ public/corpus/CorpusEditView.js
@@ -166,15 +166,15 @@ define([
         this.commentEditView.render();
         
         // Display the DataListsView
-        this.dataListsView.el = this.$('.datalists'); 
+        this.dataListsView.el = this.$('.datalists-updating-collection'); 
         this.dataListsView.render();
         
         // Display the SessionsView
-        this.sessionsView.el = this.$('.sessions'); //TODO do not use such ambiguous class names, compare this with datum_field_settings below.  there is a highlyily hood that the sesson module will be using the same class name and will overwrite your renders.
+        this.sessionsView.el = this.$('.sessions-updating-collection'); //TODO do not use such ambiguous class names, compare this with datum_field_settings below.  there is a highlyily hood that the sesson module will be using the same class name and will overwrite your renders.
         this.sessionsView.render();
         
         // Display the PermissionsView
-        this.permissionsView.el = this.$('.permissions');
+        this.permissionsView.el = this.$('.permissions-updating-collection');
         this.permissionsView.render();
 
 
