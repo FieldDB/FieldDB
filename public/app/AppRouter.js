184e0d36c71a3efca1f69ae5ba3dc689100620de
Emmy Cathcart <maryellencathcart@gmail.com>
2012-07-15 15:21:15 -0400

    updating sessions collection link view now is actually al ink that takes you to that session

--- public/app/AppRouter.js
+++ public/app/AppRouter.js
@@ -32,7 +32,7 @@ define([
       "corpus/:corpusName/search"       : "showAdvancedSearch",//corpusName has to match the pouch of the corpus
       "corpus/"                         : "showFullscreenCorpus", 
       "data/:dataListId"                : "showFullscreenDataList",
-      "session/:sessionId"                : "showFullscreenSession",
+      "session/:sessionId"              : "showFullscreenSession",
       "user/:username"                  : "showFullscreenUser",
       "import"                          : "showImport",
       ""                                : "showDashboard"
@@ -172,9 +172,39 @@ define([
     /**
      * Displays the fullscreen view of the session.
      */
-    showFullscreenSession : function() {
-      Utils.debug("In showFullscreenSession");
+    showFullscreenSession : function(sessionId, corpusName) {
+      Utils.debug("In showFullscreenSession"  + corpusName + " *** "
+          + sessionId);
       
+      if(sessionId){
+        if(!corpusName){
+          corpusName = window.app.get("corpus").get("corpusname");
+        }
+        var cs = window.app.get("currentSession");
+        cs.set({
+          "corpusname" : corpusName});
+        cs.id = sessionId;
+
+        //this could move the corpus to the wrong couch if someones tries to see a datalist that is not in the current corpus, the current corpus might try to move to another pouch.
+        if(window.app.get("corpus").get("corpusname") != corpusName ){
+          alert("You are opening a session which is not in this corpus. Do you want to switch to the other corpus?");//TODO need nodejs to find out where that data list is from, in general we cant do this, nor should we.  we should jsut tell them data list not found in their database. since the only way to get to a data list now is through a corpus details page, this situation should not arrise.
+        }
+        
+        cs.changeCorpus(corpusName, function(){
+          //fetch only after having setting the right pouch which is what changeCorpus does.
+          cs.fetch({
+            success : function(e) {
+              Utils.debug("Session fetched successfully" +e);
+              //show pretty views after loading everything.
+              window.appView.renderSessionReadonlyViews();
+            },
+            error : function(e) {
+              alert("There was an error fetching the sessions. Loading defaults..."+e);
+            }
+          });
+        });      
+      }
+       
       this.hideEverything();
       $("#session-fullscreen").show();
     },
@@ -210,7 +240,7 @@ define([
           //fetch only after having setting the right pouch which is what changeCorpus does.
           dl.fetch({
             success : function(e) {
-              Utils.debug("Data list fetched successfully" +e);
+              Utils.debug(" fetched successfully" +e);
               //show pretty views after loading everything.
               window.appView.renderReadonlyDataListViews();
             },
