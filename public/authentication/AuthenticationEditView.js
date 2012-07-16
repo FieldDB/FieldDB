7b86e4d4a05ef1f3ab58cbff33c317406af17d2f
cesine <cesine@yahoo.com>
2012-07-15 22:11:15 -0400

    when user logs out, distroying cookie and bring to the login screen, this way the user must b connected ot the internet ot open their data. this seems like a bad idea. prefarbly show a local users page... for #334

--- public/authentication/AuthenticationEditView.js
+++ public/authentication/AuthenticationEditView.js
@@ -119,7 +119,12 @@ define([
     logout : function() {
       localStorage.removeItem("username");
       
-      this.authenticateAsPublic();
+//      this.authenticateAsPublic();
+      //Destropy cookies, and reload the page, it will put the user at the login page.
+      Utils.setCookie("username", undefined, -365);
+      Utils.setCookie("token", undefined, -365);
+      window.location.replace("/index.html")
+
     },
     
     /**
