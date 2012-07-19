4ce2a22b77a92af9a9c5b2012a4f06f4fbeb7eac
cesine <cesine@yahoo.com>
2012-07-18 23:49:45 -0400

    trying to store teh audio filename into the audiofile object but not geting access ot the this object

--- public/audio_video/AudioVideoEditView.js
+++ public/audio_video/AudioVideoEditView.js
@@ -17,21 +17,26 @@ define([
      * @constructs
      */
     initialize : function() {
+//      $(this.el).bind("dragover", _.bind(this.dragOverAudio, this));
+//      $(this.el).bind("dragenter", _.bind(this.dragEnterAudio, this));
+//      $(this.el).bind("dragleave", _.bind(this.dragLeave, this));
+//      $(this.el).bind("drop", _.bind(this.dropAudio, this));
+//      this._draghoverClassAdded = false;
     },
     
     events : {
-      "dragenter" : function(e){
+      "dragenter .drop-zone" : function(e){
         this.dragEnterAudio(e);
       },
-      "dragover" : function(e){
+      "dragover .drop-zone" : function(e){
         this.dragOverAudio(e);
       },
-      "dragleave" : function(e){
+      "dragleave .drop-zone" : function(e){
         this.dragLeave(e);
       },
-      "drop": function(e){
-        this.dropAudio(e, this);
-        this.set("filename",e.dataTransfer.files[0].name);
+      "drop .drop-zone": function(e){
+        this.model.set("filename", e.dataTransfer.files[0].name);
+        this.dropAudio(e);
       }
     },
     
@@ -45,6 +50,7 @@ define([
       var dropzone = document.createElement("input");
       dropzone.classList.add("drop-zone");
       dropzone.classList.add("pull-right");
+      var self = this;
       dropzone.addEventListener('drop', this.dropAudio, false);
       dropzone.addEventListener('dragover', this.dragOverAudio, false);
       dropzone.setAttribute("placeholder","Drop audio or video files here!");
@@ -69,7 +75,7 @@ define([
       this.classList.remove('halfopacity');
     },
     
-    dropAudio: function(e, self) {
+    dropAudio: function(e) {
       Utils.debug("Recieved a drop event ");
       if (e.stopPropagation) {
         e.stopPropagation(); // stops the browser from redirecting.
@@ -78,11 +84,12 @@ define([
         e.preventDefault(); 
       }      
       this.classList.remove('halfopacity');
+      //Use the terminal to put the file into the file system
       window.appView.term.addDroppedFiles(e.dataTransfer.files);
       window.appView.term.output('<div>File(s) added!</div>');
       this.value = e.dataTransfer.files[0].name;
 //      self.model.set("filename",e.dataTransfer.files[0].name);
-      alert("Audio file was copied! (cant play yet)");
+      alert("Audio file was copied! (click on play button to listen)");
       return false;
     }
   });
