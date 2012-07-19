22e8657b256a546440aa9840ec4478c5a652c04c
cesine <cesine@yahoo.com>
2012-07-18 23:18:37 -0400

    audio drag and drop was working, but couldnt test because couldnt connect to the play button. used terminal to ls the files and open the audio to find out the url for the audio files. fixes #208

--- public/datum/DatumEditView.js
+++ public/datum/DatumEditView.js
@@ -379,8 +379,8 @@ define([
     },
     playAudio : function(){
       if(this.model.get("audioVideo")){
-        if(this.model.get("audioVideo").get("filename")){
-          this.$el.find(".audio-file").attr("src", this.model.get("audioVideo").get("filename"));
+        if(this.model.get("audioVideo").get("filename") != undefined){
+          this.$el.find(".audio-file").attr("src", "filesystem:" + window.location.origin +"/temporary/"+this.model.get("audioVideo").get("filename"));
           this.$el.find(".audio-file").play();
         }
       }
