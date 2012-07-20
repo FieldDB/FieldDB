947b6a02871446d6063817ba29a2196837b64cf4
trisapeace <trisapeace@gmail.com>
2012-07-20 09:31:18 -0400

    Morpheme finder now handles utterances with multiple words. Fixes #370

--- public/glosser/Glosser.js
+++ public/glosser/Glosser.js
@@ -1,112 +1,131 @@
 var Glosser = Glosser || {};
 
-Glosser.morphemefinder = function(unparsedword, corpusname, callback) {
-  unparsedword = "@" + unparsedword + "@";
+/**
+ * Takes in an utterance line and, based on our current set of precendence
+ * rules, guesses what the morpheme line would be. The algorithm is
+ * very conservative.
+ * 
+ * @param {String} unparsedUtterance The raw utterance line.
+ * @param {String} corpusname The name of the corpus where this utterance
+ * occurs. We need this in order to compare it with rules from the same
+ * corpus.
+ * @param {Function} callback The optional callback that takes a single
+ * String parameter containing the guessed morphemes line. 
+ */
+Glosser.morphemefinder = function(unparsedUtterance, corpusname, callback) {
   var potentialParsePromise = '';
+  
+  // Get the rules from Couch
+  // TODO Move this part into the replication code so that this function will work
+  // offline
   $.ajax({
     type : 'GET',
     url : "https://ilanguage.iriscouch.com/" + corpusname
         + "/_design/get_precedence_rules_from_morphemes/_view/precedence_rules?group=true",
     success : function(rules) {
-      // console.log(rules);
+      // Parse the rules from JSON into an object
       rules = JSON.parse(rules);
-
-      /*
-       * Reduce the rules such that rules which are found in multiple source
-       * words are only used/included once.
-       */
+    
+      // Reduce the rules such that rules which are found in multiple source
+      // words are only used/included once.
       var reducedRules = _.chain(rules.rows).groupBy(function(rule) {
         return rule.key.x + "-" + rule.key.y;
       }).value();
 
-      /*
-       * Find the rules which match in local precedence in the unparsedword
-       */
-      var matchedRules = [];
-      for ( var r in reducedRules) {
-        if (unparsedword.indexOf(r.replace(/-/, "")) >= 0) {
-          matchedRules.push({
-            r : reducedRules[r]
-          })
-        }
-      }
+      // Divide the utterance line into words
+      var unparsedWords = unparsedUtterance.trim().split(/ +/);
+      
+      var parsedWords = [];
+      for (var word in unparsedWords) {
+        // Add the start/end-of-word character to the word
+        unparsedWords[word] = "@" + unparsedWords[word] + "@";
 
-      /*
-       * Attempt to find the longest template which the matching rules can
-       * generate from start to end
-       */
-      var prefixtemplate = [];
-      prefixtemplate.push("@");
-      for ( var i = 0; i < 10; i++) {
-        console.log(prefixtemplate[i]);
-        if (prefixtemplate[i] == undefined)
-          break;
-        for ( var j in matchedRules) {
-          if (prefixtemplate[i] == matchedRules[j].r[0].key.x) {
-            console.log(matchedRules[j].r[0].key.x);
-            if (prefixtemplate[i + 1]) { // ambiguity (two potential following
-                                          // morphemes)
-              prefixtemplate.pop();
-              break;
-            } else {
-              prefixtemplate[i + 1] = matchedRules[j].r[0].key.y;
-            }
+        // Find the rules which match in local precedence
+        var matchedRules = [];
+        for (var r in reducedRules) {
+          if (unparsedWords[word].indexOf(r.replace(/-/, "")) >= 0) {
+            matchedRules.push({
+              r : reducedRules[r]
+            })
           }
         }
-      }
 
-      /*
-       * If the prefix template hit ambiguity in the middle, try from the suffix
-       * in until it hits ambiguity
-       */
-      var suffixtemplate = [];
-      if (prefixtemplate[prefixtemplate.length - 1] != "@"
-          || prefixtemplate.length == 1) {
-        // Suffix:
-        suffixtemplate.push("@")
-        for ( var i = 0; i < 10; i++) {
-          console.log(suffixtemplate[i]);
-          if (suffixtemplate[i] == undefined)
+        // Attempt to find the longest template which the matching rules can
+        // generate from start to end
+        var prefixtemplate = [];
+        prefixtemplate.push("@");
+        for (var i = 0; i < 10; i++) {
+          if (prefixtemplate[i] == undefined) {
             break;
-          for ( var j in matchedRules) {
-            if (suffixtemplate[i] == matchedRules[j].r[0].key.y) {
-              console.log(matchedRules[j].r[0].key.y);
-              if (suffixtemplate[i + 1]) { // ambiguity (two potential
-                                            // following morphemes)
-                suffixtemplate.pop();
+          }
+          for (var j in matchedRules) {
+            if (prefixtemplate[i] == matchedRules[j].r[0].key.x) {
+              if (prefixtemplate[i + 1]) { // ambiguity (two potential following
+                                            // morphemes)
+                prefixtemplate.pop();
                 break;
               } else {
-                suffixtemplate[i + 1] = matchedRules[j].r[0].key.x;
+                prefixtemplate[i + 1] = matchedRules[j].r[0].key.y;
               }
             }
           }
         }
+
+        // If the prefix template hit ambiguity in the middle, try from the suffix
+        // in until it hits ambiguity
+        var suffixtemplate = [];
+        if (prefixtemplate[prefixtemplate.length - 1] != "@" || prefixtemplate.length == 1) {
+          // Suffix:
+          suffixtemplate.push("@")
+          for (var i = 0; i < 10; i++) {
+            if (suffixtemplate[i] == undefined) {
+              break;
+            }
+            for (var j in matchedRules) {
+              if (suffixtemplate[i] == matchedRules[j].r[0].key.y) {
+                if (suffixtemplate[i + 1]) { // ambiguity (two potential
+                                              // following morphemes)
+                  suffixtemplate.pop();
+                  break;
+                } else {
+                  suffixtemplate[i + 1] = matchedRules[j].r[0].key.x;
+                }
+              }
+            }
+          }
+        }
+        
+        // Combine prefix and suffix templates into one regular expression which
+        // can be tested against the word to find a potential parse.
+        // Regular expressions will look something like
+        //    (@)(.*)(hall)(.*)(o)(.*)(wa)(.*)(n)(.*)(@)
+        var template = [];
+        template = prefixtemplate.concat(suffixtemplate.reverse())
+        for (var slot in template) {
+          template[slot] = "(" + template[slot] + ")";
+        }
+        var regex = new RegExp(template.join("(.*)"), "");
+      
+        // Use the regular expression to find a guessed morphemes line
+        potentialParsePromise = unparsedWords[word]
+            .replace(regex, "$1-$2-$3-$4-$5-$6-$7-$8-$9") // Use backreferences to parse into morphemes
+            .replace(/\$[0-9]/g, "")// Remove any backreferences that weren't used
+            .replace(/@/g, "")      // Remove the start/end-of-line symbol
+            .replace(/--+/g, "-")   // Ensure that there is only ever one "-" in a row
+            .replace(/^-/, "")      // Remove "-" at the start of the word
+            .replace(/-$/, "");     // Remove "-" at the end of the word
+        Utils.debug("Potential parse of " + unparsedWords[word].replace(/@/g, "")
+            + " is " + potentialParsePromise);
+            
+        parsedWords.push(potentialParsePromise);
       }
-      /*
-       * Combine prefix and suffix templates into one regular expression which
-       * can be tested against the word to find a potential parse
-       */
-      var template = [];
-      template = prefixtemplate.concat(suffixtemplate.reverse())
-      for ( var slot in template) {
-        template[slot] = "(" + template[slot] + ")";
-      }
-      var regex = new RegExp(template.join("(.*)"), "");
-      console.log(unparsedword.replace(regex, "$1-$2-$3-$4-$5-$6-$7-$8-$9"));
-      potentialParsePromise = unparsedword.replace(regex,
-          "$1-$2-$3-$4-$5-$6-$7-$8-$9").replace(/\$[0-9]/g, "").replace(/@/g,
-          "").replace(/--+/g, "-").replace(/^-/, "").replace(/-$/, "")
-      /* "hall-pa-nay-wan" */
-      console.log("Potential parse of " + unparsedword.replace(/@/g, "")
-          + " is " + potentialParsePromise);
+          
+      // Call the callback with the guessed morphemes line
       if (typeof callback == "function") {
-        callback(potentialParsePromise);
+        callback(parsedWords.join(" "));
       }
 
-    },// end successful login\
-    error : function(data) {
-      console.log(data);
-    },
+    },// end successful login
     dataType : ""
   });
 
