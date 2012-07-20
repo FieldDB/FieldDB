8ca8badb991da336fafc5f34328696efb057b412
cesine <cesine@yahoo.com>
2012-07-20 17:02:26 -0400

    created a new toast user area so that we can talk to the user
    and tell them what is going on without using ugly alert boxes.
    
    @mecathcart @hisakonog to use these lovely toasts, simply call
    
    window.appView.toastUser("this is where your message goes","alert-info","Hint:");
    
    you will see more examples of them in my next commits.
    you can vary the 2nd argument for any of the twitter bootstrap alert classes to change the color, and you can put anything in the 2rd arugment which will be the bold faced text in the alert box

--- public/corpus/CorpusEditView.js
+++ public/corpus/CorpusEditView.js
@@ -276,6 +276,7 @@ define([
     
     newDataList : function() {
       //take the user to the search so they can create a data list using the search feature.
+      window.appView.toastUser("Taking you to the Advanced Search, this is the easiest way to make a new Data List","alert-info","How to make a new Data List");
       app.router.showEmbeddedSearch();
     },
     
