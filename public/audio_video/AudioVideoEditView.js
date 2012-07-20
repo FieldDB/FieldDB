40aef6117c8e6cc19d92db79578e26a7a4709882
cesine <cesine@yahoo.com>
2012-07-20 08:31:01 -0400

    drag and drop audio attachment is now working locally, just drag audio on the player and you can listen ot it. It is saved in the local file system for the chrome extension. fixes #372

--- public/audio_video/AudioVideoEditView.js
+++ public/audio_video/AudioVideoEditView.js
@@ -47,13 +47,25 @@ define([
     template : Handlebars.templates.audio_video_edit_embedded,
 
     render : function() {
-      var dropzone = document.createElement("input");
-      dropzone.classList.add("drop-zone");
-      dropzone.classList.add("pull-right");
-      var self = this;
+      //http://www.terrillthompson.com/tests/html5-audio.html
+      //users must download lame to make mp3 because html5 audio cant play wav? http://lame1.buanzo.com.ar/Lame_Library_v3.98.2_for_Audacity_on_OSX.dmg
+      var dropzone = document.createElement("audio");
+      if(this.model.get("filename")){
+        var sourceaudio = document.createElement("source");
+        sourceaudio.setAttribute("src",  "filesystem:" + window.location.origin +"/temporary/"+this.model.get("filename"));
+        sourceaudio.setAttribute("type", "audio/"+this.model.get("filename").split('.').pop() );
+        dropzone.appendChild(sourceaudio);
+        dropzone.setAttribute("preload","");
+      }
+      dropzone.setAttribute("controls", "");
+//      dropzone.setAttribute("tabindex","0"); //needed to play it using tab, this puts it first...
+      dropzone.setAttribute("id", "playerdatums");
+//      dropzone.setAttribute("loop", "true");
+//      dropzone.classList.add("drop-zone");
+//      dropzone.classList.add("pull-right");
+      dropzone.classList.add("datum-audio-player");
       dropzone.addEventListener('drop', this.dropAudio, false);
       dropzone.addEventListener('dragover', this.dragOverAudio, false);
-      dropzone.setAttribute("placeholder","Drop audio or video files here!");
       $(this.el).html(dropzone);
       return this;
     },
@@ -87,9 +99,14 @@ define([
       //Use the terminal to put the file into the file system
       window.appView.term.addDroppedFiles(e.dataTransfer.files);
       window.appView.term.output('<div>File(s) added!</div>');
-      this.value = e.dataTransfer.files[0].name;
-//      self.model.set("filename",e.dataTransfer.files[0].name);
-      alert("Audio file was copied! (click on play button to listen)");
+      var audio = $(this);
+      audio.empty();
+      //http://stackoverflow.com/questions/7953593/change-source-to-audio-html5-element
+      var newSrc = $("<source>").attr("src", "filesystem:" + window.location.origin +"/temporary/"+e.dataTransfer.files[0].name).appendTo(audio);
+      /****************/
+      audio.pause();
+      audio.load();//suspends and restores all audio element
+      /****************/
       return false;
     }
   });
