d1758d177cb1cb75468953f663c3b909a15855e4
cesine <cesine@yahoo.com>
2012-07-20 11:06:07 -0400

    tested lexicon, it is loading when we call app.get(corpus).buildLexiconFromTeamServer(sapir-firstcorpus) fixes #68

--- public/lexicon/Lexicon.js
+++ public/lexicon/Lexicon.js
@@ -30,13 +30,17 @@ define([
       lexiconNodes : LexiconNodes
     },
     buildLexiconFromCouch : function(corpusname, callback){
+      var self = this;
       $.ajax({
         type : 'GET',
-        url : "http://ilanguage.iriscouch.com/"+corpusname+"/_design/lexicon/_view/create_triples?group=true",
+        url : "https://ilanguage.iriscouch.com/"+corpusname+"/_design/lexicon/_view/create_triples?group=true",
         success : function(results) {
-          var lexiconTriples = results.rows;
+          var lexiconTriples = JSON.parse(results).rows;
           for(triple in lexiconTriples){
-            this.lexiconNodes.add(new LexiconNode({morpheme: lexiconTriples[triple].key.morpheme , allomorphs: [lexiconTriples[triple].key.morpheme], gloss: lexiconTriples[triple].key.gloss, value: lexiconTriples[triple].value}));
+            if (! self.get("lexiconNodes")){
+              self.set("lexiconNodes", new LexiconNodes());
+            }
+            self.get("lexiconNodes").add(new LexiconNode({morpheme: lexiconTriples[triple].key.morpheme , allomorphs: [lexiconTriples[triple].key.morpheme], gloss: lexiconTriples[triple].key.gloss, value: lexiconTriples[triple].value}));
           }
           if (typeof callback == "function"){
             callback();
