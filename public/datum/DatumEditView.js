67263e99e1bdca30aee56247cc71ad0a3abb5b64
cesine <cesine@yahoo.com>
2012-07-20 13:30:14 -0400

    guessing glosses if the user confirms the morphemes line

--- public/datum/DatumEditView.js
+++ public/datum/DatumEditView.js
@@ -122,39 +122,22 @@ define([
           }
           // If the guessed morphemes is different than the unparsed utterance 
           if (morphemesLine != utteranceLine && morphemesLine != "") {
+            //trigger the gloss guessing
+            this.guessGlosses(morphemesLine);
             // Ask the user if they want to use the guessed morphemes
             if (confirm("Would you like to use these morphemes:\n" + morphemesLine)) {
               // Replace the morphemes line with the guessed morphemes
               this.$el.find(".morphemes .datum_field_input").val(morphemesLine);
+              //redo the gloss guessing
+              this.guessGlosses(morphemesLine);
               
               this.needsSave = true;
             }
           }
         }
       },
-      "blur .morphemes .datum_field_input" : function(e) {
-        var morphemesLine = $(e.currentTarget).val();
-        if (morphemesLine) {
-          var glossLine = Glosser.glossFinder(morphemesLine);
-          if (this.$el.find(".gloss .datum_field_input").val() == "") {
-            // If the gloss line is empty, make it a copy of the morphemes
-            this.$el.find(".gloss .datum_field_input").val(morphemesLine);
-            
-            this.needsSave = true;
-          }
-          // If the guessed gloss is different than the existing glosses
-          if (glossLine != morphemesLine && glossLine != "") {
-            // Ask the user if they want to use the guessed gloss
-            if (confirm("Would you like to use this gloss:\n" + glossLine)) {
-              // Replace the gloss line with the guessed gloss
-              this.$el.find(".gloss .datum_field_input").val(glossLine);
-              
-              this.needsSave = true;
-            }
-          }
-          
-          
-        }
+      "blur .morphemes .datum_field_input" : function(e){
+        this.guessGlosses($(e.currentTarget).val());
       }
     },
 
@@ -426,6 +409,27 @@ define([
     playAudio : function(){
       if(this.model.get("audioVideo")){
           this.$el.find(".datum-audio-player")[0].play();
+      }
+    },
+    guessGlosses : function(morphemesLine) {
+      if (morphemesLine) {
+        var glossLine = Glosser.glossFinder(morphemesLine);
+        if (this.$el.find(".gloss .datum_field_input").val() == "") {
+          // If the gloss line is empty, make it a copy of the morphemes
+          this.$el.find(".gloss .datum_field_input").val(morphemesLine);
+          
+          this.needsSave = true;
+        }
+        // If the guessed gloss is different than the existing glosses
+        if (glossLine != morphemesLine && glossLine != "") {
+          // Ask the user if they want to use the guessed gloss
+          if (confirm("Would you like to use this gloss:\n" + glossLine)) {
+            // Replace the gloss line with the guessed gloss
+            this.$el.find(".gloss .datum_field_input").val(glossLine);
+            
+            this.needsSave = true;
+          }
+        }
       }
     }
   });
