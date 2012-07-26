4b7380138fddd950a2cfb5a8f91d62833cc9d5f3
cesine <cesine@yahoo.com>
2012-07-26 17:40:28 -0400

    debgging why app isnt loading properly after the redoing of how current datalists and the default datalist were getting saved

--- public/app/AppView.js
+++ public/app/AppView.js
@@ -383,9 +383,9 @@ define([
         $(this.el).html(this.template(this.model.toJSON()));
 
         //The authView is the dropdown in the top right corner which holds all the user menus
-        this.authView.render();
-        this.userPreferenceView.render();
-        this.hotkeyEditView.render();//.showModal();
+//        this.authView.render();
+//        this.userPreferenceView.render();
+//        this.hotkeyEditView.render();//.showModal();
         
         //This forces the top search to render.
         this.searchEditView.format = "centreWell";
