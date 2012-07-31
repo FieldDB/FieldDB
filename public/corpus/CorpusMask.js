ca4a19961dad954dc7845354da4fdd359ed2d4b7
hisakonog <hisakonog@gmail.com>
2012-07-31 14:59:12 -0400

    pulled newly added files from upstream, merge conflict with some of the rest, pending pull-merge until I check the conflict

---
+++ public/corpus/CorpusMask.js
@@ -0,0 +1,285 @@
+define([ 
+    "backbone",
+    "activity/Activity",
+    "comment/Comment",
+    "comment/Comments",
+    "confidentiality_encryption/Confidential",
+    "datum/DatumField",
+    "datum/DatumFields",
+    "datum/DatumState",
+    "datum/DatumStates",
+    "data_list/DataList",
+    "data_list/DataLists",
+    "user/Consultants",
+    "lexicon/Lexicon",
+    "permission/Permission",
+    "permission/Permissions",
+    "datum/Sessions",
+    "user/User",
+    "glosser/Glosser",
+    "libs/Utils"
+], function(
+    Backbone, 
+    Activity,
+    Comment, 
+    Comments,
+    Confidential,
+    DatumField,
+    DatumFields, 
+    DatumState,
+    DatumStates,
+    DataList,
+    DataLists,
+    Consultants,
+    Lexicon,
+    Permission,
+    Permissions,
+    Sessions,
+    User
+) {
+  var CorpusMask = Backbone.Model.extend(
+  /** @lends CorpusMask.prototype */
+  {
+    /**
+     * @class The CorpusMask is saved as corpusmask in the Couch repository, it is the publicly visible version of a corpus. By default it just says private but lets users see the data lists and sessions.
+     * 
+     * 
+     * @property {String} title This is used to refer to the corpus, and
+     *           what appears in the url on the main website eg
+     *           http://fieldlinguist.com/Sapir/SampleFieldLinguisticsCorpus
+     * @property {String} description This is a short description that
+     *           appears on the corpus details page
+     * @property {String} remote The git url of the remote eg:
+     *           git@fieldlinguist.com:Sapir/SampleFieldLinguisticsCorpus.git
+     *           
+     * @property {Consultants} consultants Collection of consultants who contributed to the corpus
+     * @property {DatumStates} datumstates Collection of datum states used to describe the state of datums in the corpus 
+     * @property {DatumFields} datumfields Collection of datum fields used in the corpus
+     * @property {Sessions} sessions Collection of sessions that belong to the corpus
+     * @property {DataLists} datalists Collection of data lists created under the corpus
+     * @property {Permissions} permissions Collection of permissions groups associated to the corpus 
+     * 
+     *           
+     * @property {Glosser} glosser The glosser listens to
+     *           orthography/utterence lines and attempts to guess the
+     *           gloss.
+     * @property {Lexicon} lexicon The lexicon is a list of morphemes,
+     *           allomorphs and glosses which are used to index datum, and
+     *           also to gloss datum.
+     * 
+     * @description The initialize function probably checks to see if
+     *              the corpus is new or existing and brings it down to
+     *              the user's client.
+     * 
+     * @extends Backbone.Model
+     * @constructs
+     */
+    initialize : function() {
+
+      //Hard code this corpus' id so that it will be findable without an id if one knows the corpus name
+      this.id = "corpus";
+      
+      //TODO use these states to show what is public and what is not.
+      if(typeof(this.get("datumStates")) == "function"){
+        this.set("datumStates", new DatumStates([ 
+          new DatumState()
+        ]));
+      }//end if to set datumStates
+      
+      //Keeping all items since this seems okay for public viewing if the user wants to let the public see it. 
+      if(typeof(this.get("datumFields")) == "function"){
+        this.set("datumFields", new DatumFields([ 
+          new DatumField({
+            label : "judgement",
+            size : "3",
+            shouldBeEncrypted: "",
+            userchooseable: "disabled",
+            help: "Use this field to establish your team's gramaticality/acceptablity judgements (*,#,? etc)"
+          }),
+          new DatumField({
+            label : "utterance",
+            shouldBeEncrypted: "checked",
+            userchooseable: "disabled",
+            help: "Use this as Line 1 in your examples for handouts (ie, either Orthography, or phonemic/phonetic representation)"
+          }),
+          new DatumField({
+            label : "morphemes",
+            shouldBeEncrypted: "checked",
+            userchooseable: "disabled",
+            help: "This line is used to determine the morpheme segmentation to generate glosses, it also optionally can show up in your LaTeXed examples if you choose to show morpheme segmentation in addtion ot line 1, gloss and translation."
+          }),
+          new DatumField({
+            label : "gloss",
+            shouldBeEncrypted: "checked",
+            userchooseable: "disabled",
+            help: "This line appears in the gloss line of your LaTeXed examples, we reccomend Leipzig conventions (. for fusional morphemes, - for morpehem boundaries etc) The system uses this line to partially help you in glossing. "
+          }),
+          new DatumField({
+            label : "translation",
+            shouldBeEncrypted: "checked",
+            userchooseable: "disabled",
+            help: "Use this as your primary translation. It does not need to be English, simply a language your team is comfortable with. If your consultant often gives you multiple languages for translation you can also add addtional translations in the customized fields. For example, your Quechua informants use Spanish for translations, then you can make all Translations in Spanish, and add an additional field for English if you want to generate a handout containing the datum. "
+          })
+        ]));
+      }//end if to set datumFields
+      
+      //Removed goal and consultants by default, keeping language and dialect since these seem okay to make public
+      if(typeof(this.get("sessionFields")) == "function"){
+        this.set("sessionFields", new DatumFields([ 
+          new DatumField({
+            label : "dialect",
+            shouldBeEncrypted: "",
+            userchooseable: "disabled",
+            help: "You can use this field to be as precise as you would like about the dialect of this session."
+          }),
+          new DatumField({
+            label : "language",
+            shouldBeEncrypted: "",
+            userchooseable: "disabled",
+            help: "This is the langauge (or language family) if you would like to use it."
+          }),
+          new DatumField({
+            label : "dateElicited",
+            shouldBeEncrypted: "",
+            userchooseable: "disabled",
+            help: "This is the date in which the session took place."
+          }),
+          new DatumField({
+            label : "user",
+            shouldBeEncrypted: "",
+            userchooseable: "disabled"
+          }),
+          new DatumField({
+            label : "dateSEntered",
+            shouldBeEncrypted: "",
+            userchooseable: "disabled",
+            help: "This is the date in which the session was entered."
+          }),
+        ]));
+        
+      }//end if to set sessionFields
+      
+      
+      // If there are no comments, create models
+      if (!this.get("comments")) {
+        this.set("comments", new Comments());
+      }
+      
+      if (!this.get("dataLists")) {
+        this.set("dataLists", new DataLists());
+      }
+      
+      if (!this.get("sessions")) {
+        this.set("sessions", new Sessions());
+      }
+      
+      if (!this.permissions) {
+        this.permissions = new Permissions();
+      }
+      
+    },
+    
+    defaults : {
+      title : "Private Corpus",
+      titleAsUrl :"PrivateCorpus",
+      description : "The details of this corpus are not public.",
+      consultants : Consultants,
+      datumStates : DatumStates,
+      datumFields : DatumFields, 
+      sessionFields : DatumFields,
+      searchFields : DatumFields,
+      couchConnection : JSON.parse(localStorage.getItem("mostRecentCouchConnection")) || Utils.defaultCouchConnection()
+    },
+    
+    // Internal models: used by the parse function
+    model : {
+      //removed confidential because we dont want the token to end up in a corpusmask, if it does, then the corpusmask wont be able to parse anyway.
+      consultants : Consultants,
+      datumStates : DatumStates,
+      datumFields : DatumFields, 
+      sessionFields : DatumFields,
+      searchFields : DatumFields,
+      sessions : Sessions, 
+      dataLists : DataLists, 
+      permissions : Permissions,
+      comments: Comments
+    },
+//    glosser: new Glosser(),//DONOT store in attributes when saving to pouch (too big)
+    lexicon: new Lexicon(),//DONOT store in attributes when saving to pouch (too big)
+    changeCorpus : function(couchConnection, callback) {
+      if (couchConnection == null || couchConnection == undefined) {
+        couchConnection = this.get("couchConnection");
+      }else{
+        this.set("couchConnection", couchConnection);
+      }
+      if (this.pouch == undefined) {
+        this.pouch = Backbone.sync
+        .pouch(Utils.androidApp() ? Utils.touchUrl
+            + couchConnection.corpusname : Utils.pouchUrl
+            + couchConnection.corpusname);
+      }
+
+      if (typeof callback == "function") {
+        callback();
+      }
+    }, 
+    saveAndInterConnectInApp : function(callback){
+      
+      if(typeof callback == "function"){
+        callback();
+      }
+    },
+    /**
+     * this function makes it possible to save the CorpusMask with a
+     * hardcoded id, it uses pouch's API directly
+     */
+    updateToPouch : function(){
+      var self = this;
+      this.changeCorpus(null, function(){
+        self.pouch(function(err,db){
+          var modelwithhardcodedid = self.toJSON();
+          modelwithhardcodedid._id = "corpus";
+          db.put(modelwithhardcodedid, function(err, response) {
+            console.log(response);
+          });
+        });
+      });
+    },
+    /**
+     * This function takes in a corpusname, which could be different
+     * from the current corpus incase there is a master corpus wiht
+     * more/better monolingual data.
+     * 
+     * @param corpusname
+     * @param callback
+     */
+    buildMorphologicalAnalyzerFromTeamServer : function(corpusname, callback){
+      if(!corpusname){
+        this.get("corpusname");
+      }
+      if(!callback){
+        callback = null;
+      }
+      Glosser.downloadPrecedenceRules(corpusname, callback);
+    },
+    /**
+     * This function takes in a corpusname, which could be different
+     * from the current corpus incase there is a master corpus wiht
+     * more/better monolingual data.
+     * 
+     * @param corpusname
+     * @param callback
+     */
+    buildLexiconFromTeamServer : function(corpusname, callback){
+      if(!corpusname){
+        this.get("corpusname");
+      }
+      if(!callback){
+        callback = null;
+      }
+      this.lexicon.buildLexiconFromCouch(corpusname,callback);
+    }
+  });
+    
+  return CorpusMask;
+});
---
+++ public/main_dashboard.js
@@ -0,0 +1,277 @@
+// Set the RequireJS configuration
+require.config({
+  paths : {
+    "text" : "libs/text",
+    "jquery" : "libs/jquery",
+    "hotkeys" : "libs/jquery.hotkeys",
+    "terminal" : "libs/terminal/terminal",
+    "underscore" : "libs/underscore",
+    "backbone" : "libs/backbone",
+    "handlebars" : "libs/handlebars.runtime",
+    "compiledTemplates" : "libs/compiled_handlebars",
+    "crypto" : "libs/Crypto_AES",
+    "pouch" : "libs/pouch.alpha",
+    "backbone_pouchdb" : "libs/backbone-pouchdb",
+    "backbone_couchdb" : "libs/backbone-couchdb",
+    "bootstrap" : "bootstrap/js/bootstrap",
+    "bootstrap-transition" : "bootstrap/js/bootstrap-transition",
+    "bootstrap-alert" : "bootstrap/js/bootstrap-alert",
+    "bootstrap-modal" : "bootstrap/js/bootstrap-modal",
+    "bootstrap-dropdown" : "bootstrap/js/bootstrap-dropdown",
+    "bootstrap-scrollspy" : "bootstrap/js/bootstrap-scrollspy",
+    "bootstrap-tab" : "bootstrap/js/bootstrap-tab",
+    "bootstrap-tooltip" : "bootstrap/js/bootstrap-tooltip",
+    "bootstrap-popover" : "bootstrap/js/bootstrap-popover",
+    "bootstrap-button" : "bootstrap/js/bootstrap-button",
+    "bootstrap-collapse" : "bootstrap/js/bootstrap-collapse",
+    "bootstrap-carousel" : "bootstrap/js/bootstrap-carousel",
+    "bootstrap-typeahead" : "bootstrap/js/bootstrap-typeahead"
+  },
+  shim : {
+    "underscore" : {
+      exports : "_"
+    },
+    
+    "jquery" : {
+      exports : "$"
+    },
+
+    "bootstrap" :{
+      deps : [ "jquery" ],
+      exports : function($) {
+        return $;
+      }
+    },
+    
+    "bootstrap-typeahead" :{
+      deps : [ "jquery", "bootstrap","bootstrap-transition", "bootstrap-alert",
+          "bootstrap-modal", "bootstrap-dropdown", "bootstrap-scrollspy",
+          "bootstrap-tab", "bootstrap-tooltip", "bootstrap-popover",
+          "bootstrap-button", "bootstrap-collapse", "bootstrap-carousel"
+           ],
+      exports : function($) {
+        return $;
+      }
+    },
+    
+    "pouch" :{
+      exports: "Pouch"
+    },
+
+    "backbone" : {
+      deps : [ "underscore", "jquery", "compiledTemplates" ],
+      exports : function(_, $) {
+        return Backbone;
+      }
+    },
+    "backbone_pouchdb" :{
+      deps : ["backbone", "pouch", "backbone_couchdb"],
+      exports : function(Backbone, Pouch, backbone_couchdb) {
+        return Backbone;
+      }
+    },
+    
+    "backbone_couchdb" :{
+      deps : ["backbone", "pouch"],
+      exports : function(Backbone, Pouch) {
+        return Backbone;
+      }
+    },
+
+    "handlebars" : {
+      deps : ["bootstrap","jquery"],
+      exports : "Handlebars"
+    },
+
+    "crypto" : {
+      exports : "CryptoJS"
+    },
+
+    "compiledTemplates" : {
+      deps : [ "handlebars" ],
+      exports : function(Handlebars) {
+        return Handlebars;
+      }
+    },
+    "terminal" : {
+      deps : ["bootstrap","jquery"],
+      exports : "Terminal"
+    }
+    
+  }
+});
+
+// Initialization
+require([
+    "app/App",
+    "app/AppView",
+    "app/AppRouter",
+    "corpus/Corpus",
+    "data_list/DataList",
+    "datum/Datum",
+    "datum/Session",
+    "user/User",
+    "user/UserWelcomeView",
+    "handlebars",
+    "compiledTemplates",
+    "backbone",
+    "backbone_pouchdb",
+    "libs/Utils"
+], function(
+    App,
+    AppView,
+    AppRouter,
+    Corpus,
+    DataList,
+    Datum,
+    Session,
+    User,
+    UserWelcomeView,
+    Handlebars,
+    compiledTemplates,
+    Backbone,
+    forcingpouchtoloadonbackboneearly
+) {
+  /*
+   * Helper functions
+   */
+  
+  /* if they are browsing online, and not using the App version, bring them to the app version */
+  if( window.location.href.indexOf("chrome-extension") == -1 ){
+    var x = window.confirm("iField works best in the Chrome Store where it has unlimited space to store your data, " +
+    		"and can go online and offline. " +
+    		"\n\nNote: This is an HTML5 webapp, not a webpage. It uses a database called 'IndexedDB'." +
+    		"\n Safari doesn't let you save a database in the browser. " +
+    		"\n Firefox almost works but not quite. It might work in a year or so." +
+    		"\n Internet Exporer 10 might work, but not IE 6-9."+
+    		"\n\nClick cancel to try it out here, but we can't guarentee your data will be saved in a database. " +
+    		"\nClick OK to go to the Chrome Store App." 
+    );
+    if (x){
+        window.location = "https://chrome.google.com/webstore/detail/niphooaoogiloklolkphlnhbbkdlfdlm";
+    }else{
+      //let them stay
+    }
+  }
+
+  /**
+   * This function is the only place that starts the app, notably the app view and app router. 
+   * It is called either by the main.js or by the UserWelcomeView.js
+   */
+  window.startApp = function(a, callback){
+    window.app = a;
+
+    // Create and display the AppView and its dependents
+    window.appView = new AppView({model: a}); 
+    window.appView.render();
+    
+    // Start the Router
+    app.router = new AppRouter();
+    Backbone.history.start();
+    
+    if(typeof callback == "function"){
+      callback();
+    }
+    
+  };
+  loadFreshApp = function(){
+    Utils.debug("Loading fresh app");
+    // Create a UserWelcomeView modal
+    var welcomeUserView = new UserWelcomeView();
+    welcomeUserView.render();
+    $('#user-welcome-modal').modal("show");
+  }
+  /*
+   * End functions
+   */
+  
+  
+  /*
+   * Start the pub sub hub
+   */
+  window.hub = {};
+  Utils.makePublisher(window.hub);
+  
+  /*
+   * Clear the app completely
+   * TODO this doesnt work any more because each corpus is in a different pouch.
+   */
+//  Pouch.destroy('idb://db');
+//  Pouch.destroy('idb://dbdefault');
+//    Pouch.destroy('idb://dbsapir-firstcorpus');
+//    localStorage.clear();
+//  localStorage.removeItem("appids");
+//  localStorage.removeItem("corpusname");
+//  ids.corpusid = "4C1A0D9F-D548-491D-AEE5-19028ED85F2B";
+//  ids.sessionid = "1423B167-D728-4315-80DE-A10D28D8C4AE";
+//  ids.datalistid = "1C1F1187-329F-4473-BBC9-3B15D01D6A11";
+    
+
+  /*
+   * Check for user's cookie and the dashboard so we can load it
+   */
+  var username = Utils.getCookie("username");
+  //if(username == "sapir"){
+  //  loadFreshApp();
+  //  return;
+  //}
+  if (username != null && username != "") {
+//    alert("Welcome again " + username); //Dont need to tell them this anymore, it seems perfectly stable.
+    var appjson = localStorage.getItem("mostRecentDashboard");
+    appjson = JSON.parse(appjson);
+    if (appjson == null){
+      alert("We don't know what dashbaord to load for you. Please login and it should fix this problem.");
+      loadFreshApp();
+      return;
+    }else if (appjson.length < 3) {
+      alert("There was something inconsistent with your prevous dashboard. Please login and it should fix the problem.");
+      loadFreshApp();
+      return;
+    }else{
+      Utils.debug("Loading app from localStorage");
+      var corpusname = null;
+      var couchConnection = null;
+      if(localStorage.getItem("mostRecentCouchConnection") == "undefined" || localStorage.getItem("mostRecentCouchConnection") == undefined || localStorage.getItem("mostRecentCouchConnection") ==  null){
+        alert("We can't accurately guess which corpus to load. Please login and it should fix the problem.");
+        loadFreshApp();
+        return;
+      }else{
+        corpusname = JSON.parse(localStorage.getItem("mostRecentCouchConnection")).corpusname;
+        couchConnection = JSON.parse(localStorage.getItem("mostRecentCouchConnection"));
+        if(!localStorage.getItem("db"+corpusname+"_id")){
+          alert("We couldn't open your local database. Please login and it should fix the problem.");
+          loadFreshApp(); 
+          return;
+        }else{
+          if(!localStorage.getItem("encryptedUser")){
+            alert("Your corpus is here, but your user details are missing. Please login and it should fix this problem.");
+            loadFreshApp();
+            return;
+          }else{
+            a = new App();
+            var auth = a.get("authentication");
+            var u = localStorage.getItem("encryptedUser");
+            auth.loadEncryptedUser(u, function(success, errors){
+              if(success == null){
+                alert("We couldnt log you in."+errors.join("<br/>") + " " + Utils.contactUs);  
+                loadFreshApp();
+                return;
+              }else{
+                a.createAppBackboneObjects(corpusname, function(){
+                  window.startApp(a, function(){
+                    window.app.loadBackboneObjectsById(couchConnection, appjson);
+                  });
+                });
+              }
+            });
+          }
+        }
+      }
+    }
+  } else {
+    //new user, let them register or login as themselves or sapir
+    loadFreshApp();
+ }
+  
+  
+});
