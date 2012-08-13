4d4f73d463e3ce1e596955b5d894458981f66f4e
Emmy Cathcart <maryellencathcart@gmail.com>
2012-08-13 10:30:34 -0400

    Datum state settings now has a checkbox for searchable.  however, I found that thhis .find syntax for localization skipped the first one in an updating collection view, I changed the syntax to this..find() and got the desired result.

--- public/datum/DatumStateEditView.js
+++ public/datum/DatumStateEditView.js
@@ -61,13 +61,14 @@ define( [
       }
       
       //localization
+      this.$el.find(".locale_Searchable").html(chrome.i18n.getMessage("locale_Searchable"));
       $(".locale_Green").html(chrome.i18n.getMessage("locale_Green"));
       $(".locale_Orange").html(chrome.i18n.getMessage("locale_Orange"));
       $(".locale_Red").html(chrome.i18n.getMessage("locale_Red"));
       $(".locale_Blue").html(chrome.i18n.getMessage("locale_Blue"));
       $(".locale_Teal").html(chrome.i18n.getMessage("locale_Teal"));
       $(".locale_Black").html(chrome.i18n.getMessage("locale_Black"));
-      $(".Default").html(chrome.i18n.getMessage("Default"));
+      $(".locale_Default").html(chrome.i18n.getMessage("locale_Default"));
       return this;
     },
     
