cafa187956bb2e1ef4d2a8f3fc79b38cc240a71a
cesine <cesine@yahoo.com>
2012-08-29 22:46:43 -0400

    adding a third category of server setup, localhost for those who know how to set up a server, and/or are testing new server functions. and develop with minimal setup ot use a developer server that i can manage which will have the most recent server set up (ahead of production) and a developers couchdb. and finally a production set up that uses the actual server url and couch, and requires produciton keys.

--- switch_to_production.sh
+++ switch_to_production.sh
@@ -7,9 +7,13 @@ sed 's/Utils.debugMode *= *true/Utils.debugMode = false/' public/libs/Utils.js  
 mv output public/libs/Utils.js
 sed 's/Utils.productionMode *= *false/Utils.productionMode = true/' public/libs/Utils.js  > output
 mv output public/libs/Utils.js
+sed 's/Utils.easyDevelopmentMode *= *true/Utils.easyDevelopmentMode = false/' public/libs/Utils.js  > output
+mv output public/libs/Utils.js
 
 echo ""
 echo ""
 echo "Put the Node server into production mode"
 sed 's/var productionMode *= *false/var productionMode = true/' app.js  > output
 mv output app.js
+sed 's/var productionMode *= *false/var productionMode = true/' lib/restfullmongooseusers.js  > output
+mv output lib/restfullmongooseusers.js
