b8b6a75f7cd1faa73dbd3b17e7b1dab3ff1478b3
cesine <cesine@yahoo.com>
2012-09-14 08:44:31 -0400

    fixing typo, tested the new authentication flow for craeting users, and syncing

--- public/authentication/Authentication.js
+++ public/authentication/Authentication.js
@@ -79,7 +79,7 @@ define([
         //TODO what if they log out, when they have change to their private data that hasnt been pushed to the server, the server will overwrite their details. should we automatically check here, or should we make htem a button when they are authetnticated to test if they ahve lost their prefs etc?
       }
       var self= this;
-      var authUrl = user.authUrl;
+      var authUrl = user.get("authUrl");
       $.ajax({
         type : 'POST',
         url : authUrl + "/login",
