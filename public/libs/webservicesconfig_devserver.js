758f6e6139f5acc1ae902d52bc208c1707f5068b
cesine <cesine@yahoo.com>
2012-09-15 12:31:00 -0400

    prepares codebase and fixes #569 next step is to divide the two functionality into two new repos, and keep the current one for the core which is shared between the chrome client and the website

--- public/libs/webservicesconfig_devserver.js
+++ public/libs/webservicesconfig_devserver.js
@@ -2,7 +2,13 @@
 var Utils = Utils || {};
 
 
-Utils.authUrl = "https://ifielddevs.fieldlinguist.com:3183";
+Utils.authUrl = "https://authdev.fieldlinguist.com:3183";
+Utils.audioUrl = "https://audiodev.fieldlinguist.com:3184";
+Utils.lexiconUrl = "https://lexicondev.fieldlinguist.com:3185";
+Utils.corpusUrl = "https://corpusdev.fieldlinguist.com:3186";
+Utils.activityUrl = "https://activitydev.fieldlinguist.com:3187";
+Utils.widgetUrl = "https://widgetdev.fieldlinguist.com:3188";
+
 Utils.defaultCouchConnection = function() {
   return {
     protocol : "https://",
--- public/libs/webservicesconfig_local.js
+++ public/libs/webservicesconfig_local.js
@@ -2,6 +2,12 @@
 var Utils = Utils || {};
 
 Utils.authUrl = "https://localhost:3183";
+Utils.audioUrl = "https://localhost:3184";
+Utils.lexiconUrl = "https://localhost:3185";
+Utils.corpusUrl = "https://localhost:3186";
+Utils.activityUrl = "https://localhost:3187";
+Utils.widgetUrl = "https://localhost:3188";
+
 /*
  * not using secure couch because it would require extra set up for developers
  * to run locally which is unneccesary
