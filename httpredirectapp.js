73dc40e3940254f6a2c0b539c89a1ce419fc3a1c
cesine <cesine@yahoo.com>
2012-09-15 12:31:00 -0400

    prepares codebase and fixes #569 next step is to divide the two functionality into two new repos, and keep the current one for the core which is shared between the chrome client and the website

--- httpredirectapp.js
+++ httpredirectapp.js
@@ -4,6 +4,6 @@ var express = require('express');
 //http://stackoverflow.com/questions/7450940/automatic-https-connection-redirect-with-node-js-express
 var http = express.createServer();
 http.get('*',function(req,res){  
-    res.redirect('https://ifield.fieldlinguist.com'+req.url);
+    res.redirect('https://auth.fieldlinguist.com'+req.url);
 });
 http.listen(8080);
