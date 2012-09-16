901be4116c5bbe5d6a00114a5e975e88e47ce184
Fieldlinguist <info@fieldlinguist.com>
2012-09-16 17:17:05 -0400

    updated express to the new method of creating server

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var express     = require('express')
 //read in the specified filenames as the security key and certificate
 node_config.httpsOptions.key = fs.readFileSync(node_config.httpsOptions.key);
 node_config.httpsOptions.cert = fs.readFileSync(node_config.httpsOptions.cert);
-var app = express.createServer(node_config.httpsOptions);
+var app = express(node_config.httpsOptions);
 
 //http://stackoverflow.com/questions/11181546/node-js-express-cross-domain-scripting%20
 //app.use(function(req, res, next) {
