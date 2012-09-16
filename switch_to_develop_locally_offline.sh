866f56eabb02dfc286622076d0ca23ad1b083d92
cesine <cesine@yahoo.com>
2012-09-15 23:07:47 -0400

    movign auth service into a seperate repository and moving dev.html to the webserver repository

--- switch_to_develop_locally_offline.sh
+++ switch_to_develop_locally_offline.sh
@@ -6,24 +6,13 @@ echo "Put the Chrome app source into debug mode to see the debugging output."
 sed 's/Utils.debugMode *= *false/Utils.debugMode = true/' public/libs/Utils.js  > output
 mv output public/libs/Utils.js
 
+echo ""
+echo ""
 echo "Tell the Chrome app to contact the local webservices."
 sed 's/webservicesconfig_[^,]*/webservicesconfig_local"/' public/main_dashboard.js  > output
 mv output public/main_dashboard.js
 sed 's/webservicesconfig_[^,]*/webservicesconfig_local"/' public/main.js  > output
 mv output public/main.js
-
-echo ""
-echo ""
-echo "Put the Node server to act as the localhost server"
-sed 's/nodeconfig_[^)]*)/nodeconfig_local")/' app.js  > output
-mv output app.js
-
-sed 's/nodeconfig_[^)]*)/nodeconfig_local")/' lib/restfullmongooseusers.js  > output
-mv output lib/restfullmongooseusers.js
-sed 's/everyauthconfig_[^)]*)/everyauthconfig_local")/' lib/restfullmongooseusers.js  > output
-mv output lib/restfullmongooseusers.js
-sed 's/couchkeys_[^)]*)/couchkeys_local")/' lib/restfullmongooseusers.js  > output
-mv output lib/restfullmongooseusers.js
 
 echo ""
 echo ""
