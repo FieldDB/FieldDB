510a6f056010841661d0c406a8158ad63cfbed66
Fieldlinguist <info@fieldlinguist.com>
2012-09-20 08:05:03 -0400

    fixing typo in welcome emails

--- service.js
+++ service.js
@@ -1,7 +1,7 @@
 var https = require('https')
   , express = require('express')
   , authenticationfunctions = require('./lib/userauthentication.js')
-  , node_config = require("./lib/nodeconfig_local")
+  , node_config = require("./lib/nodeconfig_devserver")
   , passport = require('passport')
   , flash = require('connect-flash')
   , fs = require('fs')
