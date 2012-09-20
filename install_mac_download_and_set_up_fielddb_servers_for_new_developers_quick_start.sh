0ceaa1425e7f6f48241c389149a64c4473df422e
cesine <cesine@yahoo.com>
2012-09-19 22:50:00 -0400

    Updating the install scripts to remove mongodb, and make g++, nodejs, and  couchdb optional on mac (users can install the binaries rather than installing from source), and couchdb optional on linux

--- install_mac_download_and_set_up_fielddb_servers_for_new_developers_quick_start.sh
+++ install_mac_download_and_set_up_fielddb_servers_for_new_developers_quick_start.sh
@@ -3,7 +3,7 @@
 # For wget using:  "curl -O --retry 999 --retry-max-time 0 -C -"
 
 git --version || { echo 'You dont have Git installed, please install it: sudo apt-get install git or http://git-scm.com/downloads' ; exit 1; }
-gcc --version || { echo 'You dont have a C++ compiler installed, please install it and other developer tools: sudo apt-get build-dep nodejs  or http://itunes.apple.com/us/app/xcode/id497799835?ls=1&mt=12' ; exit 1; }
+#gcc --version || { echo 'You dont have a C++ compiler installed, please install it and other developer tools: sudo apt-get build-dep nodejs  or http://itunes.apple.com/us/app/xcode/id497799835?ls=1&mt=12' ; exit 1; }
 
 echo "Making fielddb workspace"
 mkdir $HOME/fielddbworkspace
@@ -21,39 +21,26 @@ echo "Setting the upstream of the repository so that updates are easy to do"
 git remote add upstream git@github.com:OpenSourceFieldlinguistics/FieldDB.git
 git remote rm origin
 echo "Compiling the FieldDB handlebars html templates so you can see the app if you load it as an unpacked chrome extension...."
+npm install handlebars
 bash compile_handlebars.sh
 
 ## Webservice dependencies ###################################################
 
-echo -en '\E[47;35m'"\033[1mJ"   # Magenta
-echo "Downloading Node, a javascript server which is what FieldDB uses to run"
-cd $HOME/fielddbworkspace
-curl -O --retry 999 --retry-max-time 0 -C - http://nodejs.org/dist/v0.6.19/node-v0.6.19.tar.gz
-tar -zxvf node-v0.6.19.tar.gz
-cd node-v0.6.19
-echo "Next three lines give command to compile Node"
-./configure --prefix=$HOME/fielddbworkspace/node
-make
-make install
-echo "Next line puts the Node binary directory to path and append it to profile so that it is permanently available for the user"
-echo "export PATH=$HOME/fielddbworkspace/node/bin:$PATH" >> ~/.profile 
-source ~/.profile
+# We encourage mac users to download in this way, however this will only work if you have XCode, which is a 4 gig download. If you want, you can download and install the most recent Node binaries.
 
-
-## Authentication server dependencies ###################################################
-
-echo -en '\E[47;34m'"\033[1mE"
-echo  "Downloading the Mongo Database files, this is where users are stored."
-curl -O --retry 999 --retry-max-time 0 -C -  http://fastdl.mongodb.org/osx/mongodb-osx-x86_64-2.2.0.tgz 
-tar -zxvf mongodb-osx-x86_64-2.2.0.tgz
-mv mongodb-osx-x86_64-2.2.0 mongodb
-cd mongodb/bin
-echo "Creating folders to hold a Mongo database"
-mkdir $HOME/fielddbworkspace/usersdatabase
-mkdir $HOME/fielddbworkspace/usersdatabase/db
-echo "Attempting to turn on mongodb on its normal port"
-./mongod --dbpath $HOME/fielddbworkspace/usersdatabase/db  --fork --logpath $HOME/fielddbworkspace/logs/mongodb.log --logappend
-
+#echo -en '\E[47;35m'"\033[1mJ"   # Magenta
+#echo "Downloading Node, a javascript server which is what FieldDB uses to run"
+#cd $HOME/fielddbworkspace
+#curl -O --retry 999 --retry-max-time 0 -C - http://nodejs.org/dist/v0.6.19/node-v0.6.19.tar.gz
+#tar -zxvf node-v0.6.19.tar.gz
+#cd node-v0.6.19
+#echo "Next three lines give command to compile Node"
+#./configure --prefix=$HOME/fielddbworkspace/node
+#make
+#make install
+#echo "Next line puts the Node binary directory to path and append it to profile so that it is permanently available for the user"
+#echo "export PATH=$HOME/fielddbworkspace/node/bin:$PATH" >> ~/.profile 
+#source ~/.profile
 
 
 ## FieldDB Web services ###################################################
@@ -118,9 +105,9 @@ git remote rm origin
 echo "You have to turn on the Couch Database yourself, it is very easy to do. Double click on the app in $HOME/fielddbworkspace/couchdb"
 cd $HOME/fielddbworkspace
 curl -O --retry 999 --retry-max-time 0 -C - https://github.com/downloads/cloudnode/couchdbx-app/CouchDB%20Server-1.2.0-20120614.zip 
-unzip CouchDB%20Server-1.2.0-20120614.zip
-mkdir couchdb
-mv CouchDB\ Server.app couchdb
+#unzip CouchDB%20Server-1.2.0-20120614.zip
+#mkdir couchdb
+#mv CouchDB\ Server.app couchdb
 #double click on the file
 
 ## Running tests to see if everything downloaded and works ###################################################
