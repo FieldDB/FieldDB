3ac830a057638b674c9d09ecd2eea6934fb1a99f
Fieldlinguist <info@fieldlinguist.com>
2012-09-21 12:03:01 -0400

    bringing the switch scripts into the start server scripts so that the server is automatically in production mode when we start it, since we will most likeyl only use the start scripts when we are using a production server that needs to stay on

--- switch_to_develop_easy_setup.sh
+++ switch_to_develop_easy_setup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 echo ""
-echo ""
 echo "Put the Node server to act as the development server"
 sed 's/nodeconfig_[^)]*)/nodeconfig_devserver")/' service.js  > output
 mv output service.js
@@ -16,7 +15,5 @@ mv output lib/userauthentication.js
 sed 's/couchkeys_[^)]*)/couchkeys_devserver")/' lib/corpusmanagement.js  > output
 mv output lib/corpusmanagement.js
 
-echo ""
-echo ""
 echo "Now running in developer mode."
 
