6c8c3d3c45d4a4592a343c9c3ac52b56d2d5354f
cesine <cesine@yahoo.com>
2012-09-28 13:36:07 -0400

    html5 progress bars didnt work on android 4.1 with webview, instead using a progress bar libary.

---
+++ public/lessons_corpus/progress_bars.js
@@ -0,0 +1,45 @@
+var progress_key = '5065c8fa32981';
+
+$(document).ready(function() {
+  $("#pb1").progressBar();
+  $("#pb2").progressBar();
+  $("#pb3").progressBar();
+  $("#pb4").progressBar();
+  $("#pb5").progressBar();
+  $("#pb6").progressBar();
+  $("#pb7").progressBar();
+  $("#pb8").progressBar();
+  $("#pb9").progressBar();
+  $("#pb10").progressBar();
+  $("#pb11").progressBar();
+  $("#pb12").progressBar();
+  $("#pb13").progressBar();
+  $("#pb14").progressBar();
+  $("#pb15").progressBar();
+  $("#pb16").progressBar();
+  $("#pb17").progressBar();
+  $("#pb18").progressBar();
+  $("#pb19").progressBar();
+  $("#pb20").progressBar();
+//  $("#pb2").progressBar({
+//    barImage : 'images/progressbg_yellow.gif'
+//  });
+//  $("#pb3").progressBar({
+//    barImage : 'images/progressbg_orange.gif',
+//    showText : false
+//  });
+//  $("#pb4").progressBar(65, {
+//    showText : false,
+//    barImage : 'images/progressbg_red.gif'
+//  });
+//  $(".pb5").progressBar({
+//    max : 2000,
+//    textFormat : 'fraction',
+//    callback : function(data) {
+//      if (data.running_value == data.value) {
+//        alert("Callback example: Target reached!");
+//      }
+//    }
+//  });
+//  $("#uploadprogressbar").progressBar();
+});
