41eb8a2f8d28985a4128bc367fccac89a85a110c
cesine <cesine@yahoo.com>
2012-09-30 23:16:19 -0400

    vocab exercise view now can play syllables and record audio flow tested in html5

---
+++ public/lessons_corpus/vocab_exercise_details_view.js
@@ -0,0 +1,125 @@
+/*
+ * Handle the play/pause stimuli button
+ */
+document.getElementById("play_vocab_stimuli_button").onclick = function(e) {
+  if ($(e.target)[0].classList.toString().indexOf("icon-pause") == -1) {
+    OPrime.playAudioFile('vocab_audio_stimuli', function() {
+      // oncomplete change the text of the button to play
+      $($(e.target)[0]).toggleClass("icon-pause icon-play");
+    });
+    $($(e.target)[0]).toggleClass("icon-play icon-pause");
+  } else {
+    OPrime.pauseAudioFile('vocab_audio_stimuli');
+    $($(e.target)[0]).toggleClass("icon-pause icon-play");
+  }
+};
+
+/*
+ * Handle the stop stimuli button
+ */
+document.getElementById("stop_vocab_stimuli_button").onclick = function(e) {
+  OPrime.stopAudioFile('vocab_audio_stimuli');
+  if (document.getElementById("play_vocab_stimuli_button").classList.toString()
+      .indexOf("icon-play") == -1) {
+    $(document.getElementById("play_vocab_stimuli_button")).toggleClass(
+        "icon-play icon-pause");
+  }
+};
+
+/*
+ * Handle the record/stop response button
+ */
+document.getElementById("record_vocab_response_button").onclick = function(e) {
+  e.stopPropagation();
+  if (document.getElementById("record_vocab_response_button").classList
+      .toString().indexOf("icon-stop") == -1) {
+    OPrime.captureAudio("vocab_response.mp3", /* started */function(audioUrl) {
+      OPrime.debug("\nRecording successfully started " + audioUrl);
+
+      // Only change the icons once.
+      if (document.getElementById("record_vocab_response_button").classList
+          .toString().indexOf("icon-record") > -1) {
+        $(document.getElementById("record_vocab_response_button")).toggleClass(
+            "icon-record icon-stop");// set class to stop
+        $(document.getElementById("record_vocab_response_button")).html("");
+      }
+
+    }, /* Recording complete */function(audioUrl) {
+      OPrime.debug("Attaching sucessful recording to the result audio div "
+          + audioUrl);
+      document.getElementById("vocab_audio_response").src = audioUrl;
+      document.getElementById("record_vocab_response_button").removeAttribute(
+          "disabled", "disabled");
+      // Play recorded audio
+      OPrime.playAudioFile('vocab_audio_response');
+    });
+  } else {
+    document.getElementById("record_vocab_response_button").setAttribute(
+        "disabled", "disabled");
+    OPrime.stopAndSaveAudio("vocab_response.mp3", /* stopped */function(
+        audioUrl) {
+      if (document.getElementById("record_vocab_response_button").classList
+          .toString().indexOf("icon-stop") > -1) {
+        $(document.getElementById("record_vocab_response_button")).toggleClass(
+            "icon-stop icon-record");// set class to record
+        $(document.getElementById("record_vocab_response_button")).html(
+            '<img src="mic_white.png" />');
+      }
+
+      OPrime.debug("\nRecording successfully stopped " + audioUrl);
+    });
+  }
+};
+
+/*
+ * Handle the play response button
+ */
+document.getElementById("play_vocab_response_button").onclick = function(e) {
+  OPrime.playAudioFile('vocab_audio_response');
+};
+
+/*
+ * Handle the cue/play syllables
+ */
+var syllables = document.getElementsByClassName("playable_syllable");
+for ( var s in syllables) {
+  syllables[s].onclick = function(e) {
+    OPrime.playIntervalAudioFile('vocab_audio_stimuli', e.target.min,
+        e.target.max);
+    window.userHistory[e.target.value] = window.userHistory[e.target.value]
+        || [];
+    window.userHistory[e.target.value].push(JSON.stringify(new Date()));
+    window.saveUser();
+  };
+
+}
+/*
+ * Capturing user's play back of audio, and saving it and restoring it from
+ * localstorage
+ */
+var userHistory = localStorage.getItem("userHistory");
+if (userHistory) {
+  userHistory = JSON.parse(userHistory);
+  OPrime.debug("Welcome back userid " + userHistory.id);
+} else {
+  userHistory = {};
+  userHistory.id = Date.now();
+}
+OPrime.hub
+    .subscribe(
+        "playbackCompleted",
+        function() {
+          window.userHistory.completedEntireAudioFile = window.userHistory.completedEntireAudioFile
+              || [];
+          window.userHistory.completedEntireAudioFile.push(JSON
+              .stringify(new Date()));
+          window.saveUser();
+        }, userHistory);
+
+window.saveUser = function() {
+  localStorage.setItem("userHistory", JSON.stringify(window.userHistory));
+  OPrime.debug(JSON.stringify(window.userHistory));
+};
+
+// Android WebView is not calling the onbeforeunload to save the userHistory.
+window.onbeforeunload = window.saveUser;
