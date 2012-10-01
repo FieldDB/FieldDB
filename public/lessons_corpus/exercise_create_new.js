b106a1bc6d8ce12b943dce21a9849f878f775d71
cesine <cesine@yahoo.com>
2012-10-01 12:42:26 -0400

    Added new recording functionality to create a new vocab exercise, saving it into the localstorage of the user history.

--- public/lessons_corpus/exercise_create_new.js
+++ public/lessons_corpus/exercise_create_new.js
@@ -2,7 +2,106 @@ function capturePhoto() {
   alert("Capture photo");
 }
 
-function captureAudio() {
-  alert("Capture audio");
+document.getElementById("save_new_exercise").onclick = function(e) {
+  window.userHistory.userCreatedExercises = window.userHistory.userCreatedExercises || [];
+  var newexercise = {};
+  newexercise.userid = window.userHistory.id;
+  newexercise.dateCreated = JSON.stringify(new Date());
+  newexercise.audio_stimuli = document.getElementById("audio_response_file").src;
+  newexercise.image_stimuli = document.getElementById("image_stimuli_file").src;
+  newexercise.stimuli_transcription = document
+      .getElementById("audio_stimuli_transcription").value;
+  newexercise.stimuli_translation = document
+      .getElementById("audio_stimuli_translation").value;
+  OPrime.debug(JSON.stringify(newexercise));
+  window.userHistory.userCreatedExercises.push(newexercise);
+};
 
+/*
+ * Hide HTML5 audio controls on Android
+ */
+if (!OPrime.isAndroidApp()) {
+
+  document.getElementById("audio_response_file").setAttribute("controls",
+      "controls");
 }
+
+/*
+ * Handle the record/stop response button
+ */
+document.getElementById("record_vocab_response_button").onclick = function(e) {
+  e.stopPropagation();
+  var responsefilename = document.getElementById("audio_stimuli_transcription").value
+      .replace(/\W/g, "_")
+      + "_stimuli_" + Date.now() + ".mp3";
+  if (document.getElementById("record_vocab_response_button").classList
+      .toString().indexOf("icon-stop") == -1) {
+    OPrime.captureAudio(responsefilename, /* started */function(audioUrl) {
+      OPrime.debug("\nRecording successfully started " + audioUrl);
+
+      // Only change the icons once.
+      if (document.getElementById("record_vocab_response_button").classList
+          .toString().indexOf("icon-record") > -1) {
+        $(document.getElementById("record_vocab_response_button")).toggleClass(
+            "icon-record icon-stop");// set class to stop
+        $(document.getElementById("record_vocab_response_button")).html("");
+      }
+
+    }, /* Recording complete */function(audioUrl) {
+      OPrime.debug("Attaching sucessful recording to the result audio div "
+          + audioUrl);
+      document.getElementById("audio_response_file").src = audioUrl;
+      document.getElementById("record_vocab_response_button").removeAttribute(
+          "disabled", "disabled");
+      // Play recorded audio
+      OPrime.playAudioFile('audio_response_file');
+    });
+  } else {
+    document.getElementById("record_vocab_response_button").setAttribute(
+        "disabled", "disabled");
+    OPrime.stopAndSaveAudio(responsefilename, /* stopped */function(audioUrl) {
+      if (document.getElementById("record_vocab_response_button").classList
+          .toString().indexOf("icon-stop") > -1) {
+        $(document.getElementById("record_vocab_response_button")).toggleClass(
+            "icon-stop icon-record");// set class to record
+        $(document.getElementById("record_vocab_response_button")).html(
+            '<img src="mic_white.png" />');
+      }
+
+      OPrime.debug("\nRecording successfully stopped " + audioUrl);
+    });
+  }
+};
+
+/*
+ * Handle the play response button
+ */
+document.getElementById("play_response_button").onclick = function(e) {
+  OPrime.playAudioFile('audio_response_file');
+};
+
+/*
+ * Capturing user's play back of audio, and saving it and restoring it from
+ * localstorage
+ */
+var userHistory = localStorage.getItem("userHistory");
+if (userHistory) {
+  userHistory = JSON.parse(userHistory);
+  OPrime.debug("Welcome back userid " + userHistory.id);
+} else {
+  userHistory = {};
+  userHistory.id = Date.now();
+}
+OPrime.hub.subscribe("playbackCompleted", function(filename) {
+  window.userHistory[filename] = window.userHistory[filename] || [];
+  window.userHistory[filename].push(JSON.stringify(new Date()));
+  window.saveUser();
+}, userHistory);
+
+window.saveUser = function() {
+  localStorage.setItem("userHistory", JSON.stringify(window.userHistory));
+  OPrime.debug(JSON.stringify(window.userHistory));
+};
+
+// Android WebView is not calling the onbeforeunload to save the userHistory.
+window.onbeforeunload = window.saveUser;
