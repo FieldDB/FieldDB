35d32ca21119f312c2a434d888979e7b5e107603
cesine <cesine@yahoo.com>
2012-12-06 16:54:32 -0500

    fixed #750 the comment was adding a timestamp and user etc even if the comment was already existing.

--- public/comment/Comment.js
+++ public/comment/Comment.js
@@ -27,16 +27,17 @@ define( [
     initialize : function() {
       var t = JSON.stringify(new Date());
      
-      this.set("timestamp", new Date(JSON.parse(t)));
-      
-      this.set("gravatar", window.appView.authView.model.get("userPublic").get("gravatar"));
-      this.set("username", window.appView.authView.model.get("userPublic").get("username"));
+      if(!this.get("timestamp")){
+        this.set("timestamp", new Date(JSON.parse(t)));
+        this.set("gravatar", window.appView.authView.model.get("userPublic").get("gravatar"));
+        this.set("username", window.appView.authView.model.get("userPublic").get("username"));
+      }
 
     },
 
     defaults : {
       text : "",
-     username: ""
+      username: ""
     },
     
     // Internal models: used by the parse function
