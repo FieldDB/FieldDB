b569668c91673edead671535798c2021b63a7be3
Tobin Skinner <tobin.skinner@gmail.com>
2012-12-11 15:55:16 -0500

    Minified working activity widget.

--- angular_client/app/js/controllers.js
+++ angular_client/app/js/controllers.js
@@ -3,7 +3,7 @@
 /* Controllers */
 
 function MyCtrl1($scope, $resource, MostRecentActivities, GetSessionToken) {
-
+	console.log("Loading MyCtrl1");
   $scope.corpus = {
     description : "Data gathered durring the Field methods class at COLING 2012 when we were working with a Cherokee speaker.",
     gravatar : "https://secure.gravatar.com/avatar/54b53868cb4d555b804125f1a3969e87?s",
@@ -27,6 +27,7 @@ function MyCtrl1($scope, $resource, MostRecentActivities, GetSessionToken) {
 MyCtrl1.$inject = ['$scope', '$resource', 'MostRecentActivities', 'GetSessionToken'];
 
 function MyCtrl2($scope, $resource, MostRecentActivities, GetSessionToken) {
+	console.log("Loading MyCtrl2");
 
   GetSessionToken.run({
     "name" : "publicuser",
