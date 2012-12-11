19cda325a710865e74ceba16b1c3d98d82e22851
cesine <cesine@yahoo.com>
2012-12-11 12:44:07 -0500

    Added a header and footer to the activity feed containing the corpus details

--- angular_client/app/js/controllers.js
+++ angular_client/app/js/controllers.js
@@ -4,14 +4,23 @@
 
 function MyCtrl1($scope, $resource, MostRecentActivities, GetSessionToken) {
 
-	GetSessionToken.run({
-		"name" : "publicuser",
-		"password" : "none"
-	}).then(function() {
-		MostRecentActivities.async().then(function(activities) {
-			$scope.activities = activities;
-		});
-	});
+  $scope.corpus = {
+    description : "Data gathered durring the Field methods class at COLING 2012 when we were working with a Cherokee speaker.",
+    gravatar : "https://secure.gravatar.com/avatar/54b53868cb4d555b804125f1a3969e87?s",
+    title : "Cherokee Field Methods",
+    team : {
+      _id : "lingllama"
+    }
+  };
+
+  GetSessionToken.run({
+    "name" : "publicuser",
+    "password" : "none"
+  }).then(function() {
+    MostRecentActivities.async().then(function(activities) {
+      $scope.activities = activities;
+    });
+  });
 
 };
 
@@ -19,14 +28,14 @@ function MyCtrl1($scope, $resource, MostRecentActivities, GetSessionToken) {
 
 function MyCtrl2($scope, $resource, MostRecentActivities, GetSessionToken) {
 
-	GetSessionToken.run({
-		"name" : "publicuser",
-		"password" : "none"
-	}).then(function() {
-		MostRecentActivities.async().then(function(activities) {
-			$scope.activities = activities;
-		});
-	});
+  GetSessionToken.run({
+    "name" : "publicuser",
+    "password" : "none"
+  }).then(function() {
+    MostRecentActivities.async().then(function(activities) {
+      $scope.activities = activities;
+    });
+  });
 
 }
 // MyCtrl2.$inject = [];
