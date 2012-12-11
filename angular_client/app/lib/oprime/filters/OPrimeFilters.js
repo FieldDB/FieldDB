83fb1043dcab17af95f5eace41838f794e5007fe
cesine <cesine@yahoo.com>
2012-12-11 11:49:54 -0500

    Added a pretty date filter

--- angular_client/app/lib/oprime/filters/OPrimeFilters.js
+++ angular_client/app/lib/oprime/filters/OPrimeFilters.js
@@ -5,4 +5,8 @@ angular.module('OPrime.filters', []).filter('checkmark', function() {
   return function(input) {
     return input ? '\u2713' : '\u2718';
   };
+}).filter('agodate', function() {
+  return function(input) {
+    return OPrime.prettyDate(input);
+  };
 });
