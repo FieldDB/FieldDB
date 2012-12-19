e76a083f79eb41336d61e34085fcc0e125be7919
cesine <cesine@yahoo.com>
2012-12-18 19:22:47 -0500

    moving src to client, adding a manifest to the main module so we can test, and addign require js to handle minificaiton.

---
+++ angular_client/modules/activity/client/js/controllers.js
@@ -0,0 +1,63 @@
+'use strict';
+define(
+    [ "angular", "OPrime", "libs/oprime/services/CouchDB" ],
+    function(angular, OPrime, CouchDBServices) {
+      // Declare app level module which depends on filters, and services
+
+      /**
+       * TODO We probably dont need two controllers one for users one for teams,
+       * but rather maybe two controllers, once simple feed, one
+       * searchable/filtered?
+       * 
+       * @param $scope
+       * @param $resource
+       * @param MostRecentActivities
+       * @param GetSessionToken
+       * @returns
+       */
+      var TeamActivityFeedController = function TeamActivityFeedController(
+          $scope, $resource, MostRecentActivities, GetSessionToken) {
+        console.log("Loading TeamActivityFeedController");
+        $scope.corpus = {
+          description : "Data gathered durring the Field methods class at COLING 2012 when we were working with a Cherokee speaker.",
+          gravatar : "https://secure.gravatar.com/avatar/54b53868cb4d555b804125f1a3969e87?s",
+          title : "Cherokee Field Methods",
+          team : {
+            _id : "lingllama"
+          }
+        };
+
+        GetSessionToken.run({
+          "name" : "publicuser",
+          "password" : "none"
+        }).then(function() {
+          MostRecentActivities.async().then(function(activities) {
+            $scope.activities = activities;
+          });
+        });
+
+      };
+
+      TeamActivityFeedController.$inject = [ '$scope', '$resource',
+          'MostRecentActivities', 'GetSessionToken' ];
+
+      OPrime.debug("Defining TeamActivityFeedController.");
+
+      return TeamActivityFeedController;
+    });
+// function UserActivityFeedController($scope, $resource, MostRecentActivities,
+// GetSessionToken) {
+// console.log("Loading UserActivityFeedController");
+//
+// GetSessionToken.run({
+// "name" : "publicuser",
+// "password" : "none"
+// }).then(function() {
+// MostRecentActivities.async().then(function(activities) {
+// $scope.activities = activities;
+// });
+// });
+//
+// }
+// UserActivityFeedController.$inject = [ '$scope', '$resource',
+// 'MostRecentActivities', 'GetSessionToken' ];
