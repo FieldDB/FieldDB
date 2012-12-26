f2717a492946f519b554f6d94f42e0a6654c23ff
cesine <cesine@yahoo.com>
2012-12-26 14:22:37 -0500

    tested routes and wrote TODOs that are needed to make the new activity feeds. both in the angular and in the nodejs

--- angular_client/modules/activity/client/js/controllers.js
+++ angular_client/modules/activity/client/js/controllers.js
@@ -15,49 +15,48 @@ define(
        * @param GetSessionToken
        * @returns
        */
-      var ActivityFeedController = function ActivityFeedController(
-          $scope, $resource, MostRecentActivities, GetSessionToken) {
+      var ActivityFeedController = function ActivityFeedController($scope,
+          $routeParams, $resource, MostRecentActivities, GetSessionToken) {
         console.log("Loading ActivityFeedController");
+        /*
+         * TODO get a corpus item out of the non-activity feed, or out of the
+         * activity feed to display this information.
+         */
         $scope.corpus = {
           description : "Data gathered durring the Field methods class at COLING 2012 when we were working with a Cherokee speaker.",
           gravatar : "https://secure.gravatar.com/avatar/54b53868cb4d555b804125f1a3969e87?s",
           title : "Cherokee Field Methods",
           team : {
-            _id : "lingllama"
+            _id : $routeParams.username
           }
         };
+
+        /*
+         * TODO add the corpus connection here so that it can be declared in the
+         * route parameters, and passed to the service
+         */
+        var feedParams = {};
+        feedParams.username = $routeParams.username || "lingllama";
+        feedParams.corpusid = $routeParams.corpusid;
+        if (feedParams.corpusid) {
+          feedParams.corpusid = "-" + feedParams.corpusid;
+        }
 
         GetSessionToken.run({
           "name" : "publicuser",
           "password" : "none"
         }).then(function() {
-          MostRecentActivities.async().then(function(activities) {
+          MostRecentActivities.async(feedParams).then(function(activities) {
             $scope.activities = activities;
           });
         });
 
       };
 
-      ActivityFeedController.$inject = [ '$scope', '$resource',
+      ActivityFeedController.$inject = [ '$scope', '$routeParams', '$resource',
           'MostRecentActivities', 'GetSessionToken' ];
 
       OPrime.debug("Defining ActivityFeedController.");
 
       return ActivityFeedController;
     });
-// function UserActivityFeedController($scope, $resource, MostRecentActivities,
-// GetSessionToken) {
-// console.log("Loading UserActivityFeedController");
-//
-// GetSessionToken.run({
-// "name" : "publicuser",
-// "password" : "none"
-// }).then(function() {
-// MostRecentActivities.async().then(function(activities) {
-// $scope.activities = activities;
-// });
-// });
-//
-// }
-// UserActivityFeedController.$inject = [ '$scope', '$resource',
-// 'MostRecentActivities', 'GetSessionToken' ];
