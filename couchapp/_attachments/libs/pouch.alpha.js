d491f03500c7b376680ac1394b9977d31254dae9
cesine <cesine@yahoo.com>
2012-12-29 19:55:51 -0500

    moved views to 'pages' so they can all be deployed in one shot by the deployment of the couchapp

--- couchapp/_attachments/libs/pouch.alpha.js
+++ couchapp/_attachments/libs/pouch.alpha.js
@@ -2323,10 +2323,10 @@ var IdbPouch = function(opts, callback) {
      * 
      */
     window.validCouchViews = {
-        "get_ids/by_date" : {
+        "pages/by_date" : {
           map: function(doc) {if (doc.dateModified) {emit(doc.dateModified, doc);}}
         },
-        "get_datum_field/get_datum_fields" : {
+        "pages/get_datum_fields" : {
           map : function(doc) {if ((doc.datumFields) && (doc.session)) {var obj = {};for (i = 0; i < doc.datumFields.length; i++) {if (doc.datumFields[i].mask) {obj[doc.datumFields[i].label] = doc.datumFields[i].mask;}}if (doc.session.sessionFields) {for (j = 0; j < doc.session.sessionFields.length; j++) {if (doc.session.sessionFields[j].mask) {obj[doc.session.sessionFields[j].label] = doc.session.sessionFields[j].mask;}}}emit(obj, doc._id);}}
         }
     };
