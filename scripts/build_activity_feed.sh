d5f4ab2fc4a1257df580c0c4af3edf7b0fdb1213
cesine <cesine@yahoo.com>
2012-12-29 19:26:10 -0500

    deploying activity feeds to the user's activity feed couches

--- scripts/build_activity_feed.sh
+++ scripts/build_activity_feed.sh
@@ -5,3 +5,9 @@ node_modules/requirejs/bin/r.js -o  angular_client/modules/activity/client/build
 cp angular_client/modules/activity/release/js/main.js angular_client/modules/activity/client/libs/fielddb_activity_feed_widget.js
 
 cp angular_client/modules/activity/release/js/main.js couchapp/_attachments/activity/libs/fielddb_activity_feed_widget.js 
+
+# Putting activity feed files into the couchapp
+rm -rf couchapp_activities/_attachments
+
+cp -r angular_client/modules/activity/release couchapp_activities/_attachments
+
--- scripts/build_debug.sh
+++ scripts/build_debug.sh
@@ -9,5 +9,10 @@ bash scripts/build_activity_feed.sh
 
 #cd ../../FieldDB/couchapp
 cd couchapp
-couchapp push . https://admin:none@localhost:6984/devgina-firstcorpus
+couchapp push . https://admin:none@localhost:6984/ginalocal4-firstcorpus
 couchapp push . https://devgina:test@ifielddevs.iriscouch.com/devgina-firstcorpus
+
+# deploy activity feed too
+cd ../couchapp_activities
+couchapp push . https://admin:none@localhost:6984/ginalocal4-firstcorpus-activity_feed
+couchapp push . https://admin:none@localhost:6984/ginalocal4-activity_feed
