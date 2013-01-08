b29c9e489867a2368276637d8a6eb25eb82d2b41
cesine <cesine@yahoo.com>
2013-01-08 14:33:53 -0500

    adding a redirect if the user attempts to use the couchapp in a non https url, to take them to the https url

--- couchapp/_attachments/welcome_online_dashboard.js
+++ couchapp/_attachments/welcome_online_dashboard.js
@@ -1,5 +1,10 @@
 //if (localStorage.getItem("username") == "public" || !localStorage.getItem("username")) {
-  window.location.replace("corpus.html");
+if(window.location.origin != "localhost"){
+  if(window.location.protocol == "http"){
+    window.location.replace(window.location.href.replace("http","https"));
+  }
+}
+window.location.replace("corpus.html");
 // } else {
 // window.location.replace("user.html");
 // }
