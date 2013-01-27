82f0a42813eec1187d00a423bdf20fdde90f615e
cesine <cesine@yahoo.com>
2013-01-27 11:15:31 -0500

    fixes #823 now the morpheme visualizaiton shows beginning #_ and end _# of the word. which is a lot closer to a wiggly finite state machine.

--- couchapp/_attachments/glosser/Glosser.js
+++ couchapp/_attachments/glosser/Glosser.js
@@ -184,7 +184,7 @@ Glosser.glossFinder = function(morphemesLine){
   
   // Replace the gloss line with the guessed glosses
   return glossGroups.join(" ");
-}
+};
 /**
  * Takes as a parameters an array of rules which came from CouchDB precedence rule query.
  * Example Rule: {"key":{"x":"@","relation":"preceeds","y":"aqtu","context":"aqtu-nay-wa-n"},"value":2}
@@ -208,6 +208,13 @@ Glosser.generateForceDirectedRulesJsonForD3 = function(rules, pouchname) {
   morphemeLinks = [];
   morphemes = [];
   for ( var i in rules) {
+    /* make the @ more like what a linguist recognizes for word boundaries */
+    if(rules[i].key.x == "@"){
+      rules[i].key.x = "#_"
+    }
+    if(rules[i].key.y == "@"){
+      rules[i].key.y = "_#"
+    }
     var xpos = morphemes.indexOf(rules[i].key.x);
     if (xpos < 0) {
       morphemes.push(rules[i].key.x);
@@ -218,7 +225,8 @@ Glosser.generateForceDirectedRulesJsonForD3 = function(rules, pouchname) {
       morphemes.push(rules[i].key.y);
       ypos = morphemes.length - 1;
     }
-    if (rules[i].key.y != "@") {
+    //To avoid loops?
+    if (rules[i].key.y.indexOf("@") == -1) {
       morphemeLinks.push({
         source : xpos,
         target : ypos,
