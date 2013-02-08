77c00936b19d75f5a60a45676865f8f2ea0439ef
cesine <cesine@yahoo.com>
2013-02-07 22:53:40 -0500

    welcome dashboard is no longer needing to be build by require

--- scripts/build_fielddb_minified.sh
+++ scripts/build_fielddb_minified.sh
@@ -2,7 +2,7 @@
 
 #welcome is just a redirect to either user or corpus
 #node_modules/requirejs/bin/r.js -o  couchapp/_attachments/build_welcome_online_dashboard.js 
-cp couchapp/release/welcome_online_dashboard.js couchapp_minified/_attachments/
+#cp couchapp/release/welcome_online_dashboard.js couchapp_minified/_attachments/
 
 node_modules/requirejs/bin/r.js -o  couchapp/_attachments/build_user_online_dashboard.js
 cp couchapp/release/user_online_dashboard.js couchapp_minified/_attachments/
