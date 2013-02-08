0f9b62ebb442d37cc72420d8083c4785c5177f01
cesine <cesine@yahoo.com>
2013-02-08 01:46:46 -0500

    fixes #847 minifying the backup pouches dashboard too so we can deploy the minified version in the chrome store, it should be lighter weight. and maybe even easier for power users to use because all the code is in one js file, thye can search for functions...

--- couchapp/_attachments/backup_pouches_dashboard.js
+++ couchapp/_attachments/backup_pouches_dashboard.js
@@ -1,46 +1,19 @@
 // Set the RequireJS configuration
 require.config({
   paths : {
-    /* Bootstrap user interface javascript files */
-    "bootstrap" : "libs/bootstrap/js/bootstrap.min",
-
-    "crypto" : "libs/Crypto_AES",
 
     /* jQuery and jQuery plugins */
     "jquery" : "libs/jquery",
 
-    /* Handlebars html templating libraries and compiled templates */
-    "compiledTemplates" : "libs/compiled_handlebars",
-    "handlebars" : "libs/handlebars.runtime",
-
-    /* Backbone Model View Controller framework and its plugins and dependencies */
-    "backbone" : "libs/backbone",
-    "backbone_pouchdb" : "libs/backbone-pouchdb",
-    "backbone_couchdb" : "libs/backbone-couchdb",
     "pouch" : "libs/pouch.alpha",
-    "underscore" : "libs/underscore",
-
-    "terminal" : "libs/terminal/terminal",
-
-    "text" : "libs/text",
-
-    "xml2json" : "libs/xml2json",
 
     "oprime" : "libs/OPrime",
     "webservicesconfig" : "libs/webservicesconfig_devserver"
   },
   shim : {
-    "underscore" : {
-      exports : "_"
-    },
 
     "jquery" : {
       exports : "$"
-    },
-
-    "xml2json" : {
-      deps : [ "jquery" ],
-      exports : "X2JS"
     },
 
     "oprime" : {
@@ -51,50 +24,16 @@ require.config({
       exports : "OPrime"
     },
 
-    "bootstrap" : {
-      deps : [ "jquery" ],
-      exports : "bootstrap"
-    },
-
     "pouch" : {
+      deps : [ "jquery" ],
       exports : "Pouch"
-    },
-    "backbone" : {
-      deps : [ "underscore", "bootstrap", "compiledTemplates" ],
-      exports : "Backbone"
-    },
-    "backbone_pouchdb" : {
-      deps : [ "backbone", "pouch", "backbone_couchdb" ],
-      exports : "Backbone"
-    },
-    "backbone_couchdb" : {
-      deps : [ "backbone", "pouch" ],
-      exports : "Backbone"
-    },
-
-    "handlebars" : {
-      deps : [ "bootstrap", "jquery" ],
-      exports : "Handlebars"
-    },
-
-    "crypto" : {
-      exports : "CryptoJS"
-    },
-
-    "compiledTemplates" : {
-      deps : [ "handlebars" ],
-      exports : "Handlebars"
-    },
-    "terminal" : {
-      deps : [ "bootstrap", "jquery" ],
-      exports : "Terminal"
     }
   }
 });
 
 // Initialization
 require(
-    [ "backbone_pouchdb", "oprime" ],
+    [ "pouch", "oprime" ],
     function(forcingpouchtoloadearly, OPrime) {
 
       $('#dashboard_loading_spinner')
@@ -227,163 +166,5 @@ require(
           alert("There was a problem contacting the server to automatically back up your databases so you can use version 1.38 and greater. Please contact us at opensource@lingsync.org, someone will help you back up your data manually.");
         }
       });
-//      $.ajax({
-//            type : 'POST',
-//            url : "https://corpusdev.lingsync.org/_session",
-//            data : corpusloginparams,
-//            contentType : "application/json",
-//            beforeSend: function(xhr) {
-//              xhr.setRequestHeader('Accept', 'application/json');
-//            },
-//            success : function(serverResults) {
-//              console.log("sucess",serverResults);
-//            },
-//            error : function(serverResults) {
-//              alert("There was a problem contacting the server to automatically back up your databases so you can use version 1.38 and greater. Please contact us at opensource@lingsync.org, someone will help you back up your data manually.");
-//            }
-//          });
-//      $.ajax({
-//        type: "POST", 
-//        url: "https://corpusdev.lingsync.org/_session", 
-//        dataType: "json",
-//        contentType : "application/json",
-//        data: corpusloginparams,
-//        beforeSend: function(xhr) {
-//            xhr.setRequestHeader('Accept', 'application/json');
-//        },
-//        complete: function(req) {
-//          var resp = $.parseJSON(req.responseText);
-//          if (req.status == 200) {
-//            if (options.success) options.success(resp);
-//          } else if (options.error) {
-//            options.error(req.status, resp.error, resp.reason);
-//          } else {
-//            alert("An error occurred logging in: " + resp.reason);
-//          }
-//        }
-//      });
-//      $.ajax({
-//        type : 'POST',
-//        url : "https://authdev.lingsync.org/login",
-//        beforeSend: function(xhr) {
-//          xhr.setRequestHeader('Accept', 'application/json');
-//        },
-//        data : {username:"public",password:"none"},
-//        success : function(serverResults) {
-//          console.log("sucess",serverResults);
-//        },
-//        error : function(serverResults) {
-//          alert("There was a problem contacting the server to automatically back up your databases so you can use version 1.38 and greater. Please contact us at opensource@lingsync.org, someone will help you back up your data manually.");
-//        }
-//      });
-//      var xhr = new XMLHttpRequest();
-//      xhr.open('POST', 'https://authdev.lingsync.org');
-//      xhr.onreadystatechange = function () {
-//        if (this.status == 200 && this.readyState == 4) {
-//          console.log('response: ' + this.responseText);
-//        }
-//      };
-//      xhr.send();
-//
-//      $.ajax("http://authdev.lingsync.org/login", {
-//        type : 'POST',
-//        contentType : "application/json",
-//        success : function(data) {
-//          console.log("success!", data);
-//        },
-//        fail : function(jqxhr, statusText) {
-//          console.log("fail!", jqxhr, statusText);
-//        }
-//      });
-//      
-//   // Create the XHR object.
-//      function createCORSRequest(method, url) {
-//        var xhr = new XMLHttpRequest();
-//        if ("withCredentials" in xhr) {
-//          // XHR for Chrome/Firefox/Opera/Safari.
-//          xhr.open(method, url, true);
-//        } else if (typeof XDomainRequest != "undefined") {
-//          // XDomainRequest for IE.
-//          xhr = new XDomainRequest();
-//          xhr.open(method, url);
-//        } else {
-//          // CORS not supported.
-//          xhr = null;
-//        }
-//        return xhr;
-//      }
-//
-//      // Helper method to parse the title tag from the response.
-//      function getTitle(text) {
-//        return text.match('<title>(.*)?</title>')[1];
-//      }
-//
-//      // Make the actual CORS request.
-//      function makeCorsRequest() {
-//        // All HTML5 Rocks properties support CORS.
-//        var url = 'https://authdev.lingsync.org';
-//
-//        var xhr = createCORSRequest('GET', url);
-//        if (!xhr) {
-//          alert('CORS not supported');
-//          return;
-//        }
-//
-//        // Response handlers.
-//        xhr.onload = function() {
-//          var text = xhr.responseText;
-//          alert('Response from CORS request to ' + url + ': ' + text);
-//        };
-//
-//        xhr.onerror = function() {
-//          alert('Woops, there was an error making the request.');
-//        };
-//
-//        xhr.send();
-//      }
-      
-      
-//      $.ajax({
-//        type : 'GET',
-//        url : "https://corpusdev.lingsync.org",
-//        data : {},
-//        success : function(serverResults) {
-//          console.log("sucess", serverResults);
-//        },
-//        error : function(serverResults) {
-//                        console.log("error");
-//
-//        }
-//      });
-
-      /* http://stackoverflow.com/questions/5584923/a-cors-post-request-works-from-plain-javascript-but-why-not-with-jquery */
-//      var request = new XMLHttpRequest();
-//      var params = "name=backupdatabases&password=none";
-//      request.open('POST', "https://corpusdev.lingsync.org/");
-//      request.onreadystatechange = function(e, f, g) {
-//        if (request.readyState == 4)
-//          if (OPrime.debugMode) OPrime.debug("readyState", e, f, g);
-//        if (OPrime.debugMode) OPrime.debug("Request", request);
-//      };
-//      request.setRequestHeader("Content-type", "text/plain");
-//      request.setRequestHeader("Content-length", params.length);
-//      // request.setRequestHeader("Connection", "close");
-//      request.send();
-
-      /* http://doc.instantreality.org/tutorial/http-communication-in-ecmascript-with-xmlhttprequest/ */
-      // xhr = new XMLHttpRequest();
-      // xhr.open('POST', 'https://corpusdev.lingsync.org/_session');
-      // // xhr.setRequestHeader("Content-type","text/plain");
-      // xhr.setRequestHeader("Content-type",
-      // "application/x-www-form-urlencoded");
-      //
-      // // xhr.setRequestHeader("Content-length", 0);
-      // xhr.onreadystatechange = function(e, f, g) {
-      // console.log("xhr.responseText"+xhr.responseText);
-      // var myobj = JSON.parse(xhr.responseText);
-      // console.log(myobj.couchdb);
-      //        console.log(myobj.version);
-      //      };
-      //      xhr.send();
 
     });
