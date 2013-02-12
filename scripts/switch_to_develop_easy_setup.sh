97d5286534262e3422ba507a0f3e7493afb69c72
cesine <cesine@yahoo.com>
2013-02-12 11:45:01 -0500

    moving the entire source tree out of the couchapp so that it is easier to find for @874 new devs

--- scripts/switch_to_develop_easy_setup.sh
+++ scripts/switch_to_develop_easy_setup.sh
@@ -3,39 +3,39 @@
 echo ""
 echo ""
 echo "Put the Chrome app source into debug mode to see the debugging output."
-sed 's/OPrime.debugMode *= *true/OPrime.debugMode = false/' couchapp/_attachments/libs/OPrime.js  > output
-mv output couchapp/_attachments/libs/OPrime.js
+sed 's/OPrime.debugMode *= *true/OPrime.debugMode = false/' backbone_client/libs/OPrime.js  > output
+mv output backbone_client/libs/OPrime.js
 
 
 echo ""
 echo ""
 echo "Put the dev analytics code."
-sed 's/_AnalyticsCode = "UA-[0123456789]*-1";/_AnalyticsCode = "UA-32705284-1";/' couchapp/_attachments/libs/analytics.js  > output
-mv output couchapp/_attachments/libs/analytics.js
+sed 's/_AnalyticsCode = "UA-[0123456789]*-1";/_AnalyticsCode = "UA-32705284-1";/' backbone_client/libs/analytics.js  > output
+mv output backbone_client/libs/analytics.js
 
 echo ""
 echo ""
 echo "Tell the Chrome app to contact the dev webservices."
-sed 's/webservicesconfig_[^,]*/webservicesconfig_devserver"/' couchapp/_attachments/corpus_dashboard.js  > output
-mv output couchapp/_attachments/corpus_dashboard.js
-sed 's/webservicesconfig_[^,]*/webservicesconfig_devserver"/' couchapp/_attachments/lingllama_dashboard.js  > output
-mv output couchapp/_attachments/lingllama_dashboard.js
-sed 's/webservicesconfig_[^,]*/webservicesconfig_devserver"/' couchapp/_attachments/user_dashboard.js  > output
-mv output couchapp/_attachments/user_dashboard.js
-sed 's/webservicesconfig_[^,]*/webservicesconfig_devserver"/' couchapp/_attachments/welcome_dashboard.js  > output
-mv output couchapp/_attachments/welcome_dashboard.js
+sed 's/webservicesconfig_[^,]*/webservicesconfig_devserver"/' backbone_client/corpus_dashboard.js  > output
+mv output backbone_client/corpus_dashboard.js
+sed 's/webservicesconfig_[^,]*/webservicesconfig_devserver"/' backbone_client/lingllama_dashboard.js  > output
+mv output backbone_client/lingllama_dashboard.js
+sed 's/webservicesconfig_[^,]*/webservicesconfig_devserver"/' backbone_client/user_dashboard.js  > output
+mv output backbone_client/user_dashboard.js
+sed 's/webservicesconfig_[^,]*/webservicesconfig_devserver"/' backbone_client/welcome_dashboard.js  > output
+mv output backbone_client/welcome_dashboard.js
 
-sed 's/webservicesconfig_[^,]*/webservicesconfig_devserver"/' couchapp/_attachments/corpus_online_dashboard.js  > output
-mv output couchapp/_attachments/corpus_online_dashboard.js
-sed 's/webservicesconfig_[^,]*/webservicesconfig_devserver"/' couchapp/_attachments/user_online_dashboard.js  > output
-mv output couchapp/_attachments/user_online_dashboard.js
-sed 's/webservicesconfig_[^,]*/webservicesconfig_devserver"/' couchapp/_attachments/welcome_online_dashboard.js  > output
-mv output couchapp/_attachments/welcome_online_dashboard.js
+sed 's/webservicesconfig_[^,]*/webservicesconfig_devserver"/' backbone_client/corpus_online_dashboard.js  > output
+mv output backbone_client/corpus_online_dashboard.js
+sed 's/webservicesconfig_[^,]*/webservicesconfig_devserver"/' backbone_client/user_online_dashboard.js  > output
+mv output backbone_client/user_online_dashboard.js
+sed 's/webservicesconfig_[^,]*/webservicesconfig_devserver"/' backbone_client/welcome_online_dashboard.js  > output
+mv output backbone_client/welcome_online_dashboard.js
 echo ""
 echo ""
 echo "Putting the Chrome app's manifest into the dev manifest for release into the Chrome store as the unstable bleeding egde chromeapp  for users who like to be on the bleeding edge"
-cp couchapp/_attachments/manifest_dev.json couchapp/_attachments/manifest.json
-cp couchapp/_attachments/images/icon128_dev.png couchapp/_attachments/images/icon.png
+cp backbone_client/manifest_dev.json backbone_client/manifest.json
+cp backbone_client/images/icon128_dev.png backbone_client/images/icon.png
 
 echo ""
 echo ""
