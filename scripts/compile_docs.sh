f85f8bf44838e373b7a78602cc311a2287ae153f
cesine <cesine@yahoo.com>
2013-02-12 11:45:01 -0500

    moving the entire source tree out of the couchapp so that it is easier to find for @874 new devs

--- scripts/compile_docs.sh
+++ scripts/compile_docs.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# This script builds the docs, you can also see them online here:
+# http://builder.ilanguage.ca/job/FieldDB/ws/javadoc/index.html
+
 rm -rf jsdocs
-java -jar jsrun.jar jsdocsrun.js couchapp/_attachments/ -t=templates/jsdoc -d=jsdocs
+java -jar jsrun.jar jsdocsrun.js backbone_client/ -t=templates/jsdoc -d=jsdocs
 date;
--- scripts/switch_to_develop_locally_offline.sh
+++ scripts/switch_to_develop_locally_offline.sh
@@ -3,39 +3,39 @@
 echo ""
 echo ""
 echo "Put the Chrome app source into debug mode to see the debugging output."
-sed 's/OPrime.debugMode *= *false/OPrime.debugMode = true/' couchapp/_attachments/libs/OPrime.js  > output
-mv output couchapp/_attachments/libs/OPrime.js
+sed 's/OPrime.debugMode *= *false/OPrime.debugMode = true/' backbone_client/libs/OPrime.js  > output
+mv output backbone_client/libs/OPrime.js
 
 echo ""
 echo ""
 echo "Put the dev analytics code."
-sed 's/_AnalyticsCode = "UA-[0123456789]*-1";/_AnalyticsCode = "UA-32705284-1";/' couchapp/_attachments/libs/analytics.js  > output
-mv output couchapp/_attachments/libs/analytics.js
+sed 's/_AnalyticsCode = "UA-[0123456789]*-1";/_AnalyticsCode = "UA-32705284-1";/' backbone_client/libs/analytics.js  > output
+mv output backbone_client/libs/analytics.js
 
 echo ""
 echo ""
 echo "Tell the Chrome app to contact the local webservices."
-sed 's/webservicesconfig_[^,]*/webservicesconfig_local"/' couchapp/_attachments/corpus_dashboard.js  > output
-mv output couchapp/_attachments/corpus_dashboard.js
-sed 's/webservicesconfig_[^,]*/webservicesconfig_local"/' couchapp/_attachments/lingllama_dashboard.js  > output
-mv output couchapp/_attachments/lingllama_dashboard.js
-sed 's/webservicesconfig_[^,]*/webservicesconfig_local"/' couchapp/_attachments/user_dashboard.js  > output
-mv output couchapp/_attachments/user_dashboard.js
-sed 's/webservicesconfig_[^,]*/webservicesconfig_local"/' couchapp/_attachments/welcome_dashboard.js  > output
-mv output couchapp/_attachments/welcome_dashboard.js
+sed 's/webservicesconfig_[^,]*/webservicesconfig_local"/' backbone_client/corpus_dashboard.js  > output
+mv output backbone_client/corpus_dashboard.js
+sed 's/webservicesconfig_[^,]*/webservicesconfig_local"/' backbone_client/lingllama_dashboard.js  > output
+mv output backbone_client/lingllama_dashboard.js
+sed 's/webservicesconfig_[^,]*/webservicesconfig_local"/' backbone_client/user_dashboard.js  > output
+mv output backbone_client/user_dashboard.js
+sed 's/webservicesconfig_[^,]*/webservicesconfig_local"/' backbone_client/welcome_dashboard.js  > output
+mv output backbone_client/welcome_dashboard.js
 
-sed 's/webservicesconfig_[^,]*/webservicesconfig_local"/' couchapp/_attachments/corpus_online_dashboard.js  > output
-mv output couchapp/_attachments/corpus_online_dashboard.js
-sed 's/webservicesconfig_[^,]*/webservicesconfig_local"/' couchapp/_attachments/user_online_dashboard.js  > output
-mv output couchapp/_attachments/user_online_dashboard.js
-sed 's/webservicesconfig_[^,]*/webservicesconfig_local"/' couchapp/_attachments/welcome_online_dashboard.js  > output
-mv output couchapp/_attachments/welcome_online_dashboard.js
+sed 's/webservicesconfig_[^,]*/webservicesconfig_local"/' backbone_client/corpus_online_dashboard.js  > output
+mv output backbone_client/corpus_online_dashboard.js
+sed 's/webservicesconfig_[^,]*/webservicesconfig_local"/' backbone_client/user_online_dashboard.js  > output
+mv output backbone_client/user_online_dashboard.js
+sed 's/webservicesconfig_[^,]*/webservicesconfig_local"/' backbone_client/welcome_online_dashboard.js  > output
+mv output backbone_client/welcome_online_dashboard.js
 
 echo ""
 echo ""
 echo "Putting the Chrome app's manifest into the local manifest for testing local webservices running on the same computer as the chrome extension"
-cp couchapp/_attachments/manifest_local.json couchapp/_attachments/manifest.json
-cp couchapp/_attachments/images/icon128_local.png couchapp/_attachments/images/icon.png
+cp backbone_client/manifest_local.json backbone_client/manifest.json
+cp backbone_client/images/icon128_local.png backbone_client/images/icon.png
 
 echo ""
 echo ""
