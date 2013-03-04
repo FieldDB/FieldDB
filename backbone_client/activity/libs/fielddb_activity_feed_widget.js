350c20b40c14d9c14a344a44bb58efb7b37996eb
cesine <cesine@yahoo.com>
2013-03-04 02:02:09 -0500

    activity wasnt using credentails so the user couldnt see their activities for #888

--- backbone_client/activity/libs/fielddb_activity_feed_widget.js
+++ backbone_client/activity/libs/fielddb_activity_feed_widget.js
@@ -15250,14 +15250,15 @@ define('js/controllers',
           $scope.corpus.title = "User Activity Feed";
         }
 
-        GetSessionToken.run({
-          "name" : "public",
-          "password" : "none"
-        }).then(function() {
+        
+//        GetSessionToken.run({
+//          "name" : "public",
+//          "password" : "none"
+//        }).then(function() {
           MostRecentActivities.async(feedParams).then(function(activities) {
             $scope.activities = activities;
           });
-        });
+//        });
 
       };
 
@@ -15315,47 +15316,60 @@ define('js/filters',[ "angular", "OPrime" ], function(angular, OPrime) {
   return ActivityFeedFilters;
 });
 
-define('js/services',[ "angular", "OPrime", "libs/oprime/services/CouchDB" ], function(
-    angular, OPrime, CouchDBServices) {
+define('js/services',
+    [ "angular", "OPrime", "libs/oprime/services/CouchDB" ],
+    function(angular, OPrime, CouchDBServices) {
 
-  console.log("Loading Activity.services");
+      console.log("Loading Activity.services");
 
-  /* Services */
+      /* Services */
 
-  /*
-   * TODO requires a view to users' activity feeds so that these can be generated,
-   * and add a check box so they user can make their feed public. if they check
-   * it, either add the publicuser to the activity feed database security
-   */
-  var ActivityFeedServices = angular.module('ActivityFeed.services',
-      [ 'ngResource' ]).value('version', '0.1').factory(
-      'MostRecentActivities',
-      function($http) {
-        return {
-          'async' : function(params) {
-            console.log("Fetching this activity feed: ", params);
-            var location = OPrime.couchURL();
-            var promise = $http.get(
-                location.protocol + location.domain + location.port + '/'
-                    + params.username + params.corpusid + '-activity_feed/'
-                    + '_design/activities/_view/activities?limit=20&decending=true')
-                .then(function(response) {
-                  // + JSON.stringify(response));
-                  // console.log("response", response);
-                  var results = [];
-                  for ( var i = 0; i < response.data.rows.length; i++) {
-                    results.push(response.data.rows[i].value);
+      /*
+       * TODO requires a view to users' activity feeds so that these can be
+       * generated, and add a check box so they user can make their feed public.
+       * if they check it, either add the publicuser to the activity feed
+       * database security
+       */
+      var ActivityFeedServices = angular
+          .module('ActivityFeed.services', [ 'ngResource' ])
+          .value('version', '0.1')
+          .factory(
+              'MostRecentActivities',
+              function($http) {
+                return {
+                  'async' : function(params) {
+                    console.log("Fetching this activity feed: ", params);
+                    var location = OPrime.couchURL();
+                    var promise = $http
+                        (
+                            { method: "GET",
+                              data: {},
+                              url : location.protocol
+                                  + location.domain
+                                  + location.port
+                                  + '/'
+                                  + params.username
+                                  + params.corpusid
+                                  + '-activity_feed/'
+                                  + '_design/activities/_view/activities?limit=20&decending=true',
+                              withCredentials : true
+                            }).then(function(response) {
+                          // + JSON.stringify(response));
+                          // console.log("response", response);
+                          var results = [];
+                          for ( var i = 0; i < response.data.rows.length; i++) {
+                            results.push(response.data.rows[i].value);
+                          }
+                          return results;
+                        });
+                    return promise;
                   }
-                  return results;
-                });
-            return promise;
-          }
-        };
-      });
+                };
+              });
 
-  return ActivityFeedServices;
+      return ActivityFeedServices;
 
-});
+    });
 
 define('js/directives',[ "angular", "OPrime" ], function(angular, OPrime) {
   /* Directives */
