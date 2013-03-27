b5a1be6af06f17d90bc0600fffdeaa586052607e
hisakonog <hisakonog@gmail.com>
2013-03-27 13:48:04 -0400

    updated add comment functionso that the comment add button saves the current state of corpus when the comment is added.

--- backbone_client/corpus/CorpusEditView.js
+++ backbone_client/corpus/CorpusEditView.js
@@ -109,8 +109,15 @@ define([
           e.stopPropagation();
           e.preventDefault();
         }
-        this.model.get("comments").unshift(this.commentEditView.model);
-        this.commentEditView.model = new Comment();
+        /* Ask the comment edit view to get it's current text */
+        this.commentEditView.updateComment();
+        /* Ask the collection to put a copy of the comment into the collection */
+        this.model.get("comments").insertNewCommentFromObject(this.commentEditView.model.toJSON());
+        /* empty the comment edit view. */
+        this.commentEditView.clearCommentForReuse();
+        /* save the state of the corpus when the comment is added, and render it*/
+        this.updatePouch();
+        this.commentReadView.render();
       }, 
       //Delete button remove a comment
       "click .remove-comment-button" : function(e) {
