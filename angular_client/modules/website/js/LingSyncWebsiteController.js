8f256cd1101ed595e02417cf95dab786794de0dc
Tobin Skinner <tobin.skinner@gmail.com>
2013-04-08 17:30:50 -0400

    Website backup.

--- angular_client/modules/website/js/LingSyncWebsiteController.js
+++ angular_client/modules/website/js/LingSyncWebsiteController.js
@@ -3,7 +3,10 @@
 /* Controllers */
 
 function LingSyncWebsiteMainController($scope, $http) {
-
+	$scope.show_section = 'share';
+	if ($scope.activePage == undefined) {
+		$scope.activePage = 'home';
+	}
 };
 
 LingSyncWebsiteMainController.$inject = [ '$scope', '$http' ];
