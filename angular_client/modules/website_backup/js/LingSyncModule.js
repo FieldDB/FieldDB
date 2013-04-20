17bb7a25f69b89e55e94c9e6269912fa34c9e427
Tobin Skinner <tobin.skinner@gmail.com>
2013-04-20 13:10:23 -0400

    Website backup.

---
+++ angular_client/modules/website_backup/js/LingSyncModule.js
@@ -0,0 +1,28 @@
+'use strict';
+
+// Declare app level module which depends on filters, and services
+angular.module('LingSyncWebsite', [ ]).config(
+    [ '$routeProvider', function($routeProvider) {
+      $routeProvider.when('/home', {
+        templateUrl : 'partials/home.html'
+      }).when('/technology', {
+        templateUrl : 'partials/technology.html' , controller: LingSyncWebsiteTechnologyController
+      }).when('/people', {
+        templateUrl : 'partials/people.html'
+      }).when('/tutorials', {
+        templateUrl : 'partials/tutorials.html'
+      }).when('/projects', {
+        templateUrl : 'partials/projects.html'
+      }).otherwise({
+        redirectTo : '/home'
+      });
+    } ]).directive('ngEnter', function() {
+  return function(scope, elm, attrs) {
+    elm.bind('keypress', function(e) {
+      if (e.charCode === 13) {
+        scope.$apply(attrs.ngEnter);
+        elm[0].blur();
+      }
+    });
+  };
+});
