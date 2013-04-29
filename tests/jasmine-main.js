baf8c7ab1af46b4be3c7a9769cbec81139f72c13
Josh Horner <josh.horner@gmail.com>
2013-04-29 13:19:18 -0400

    Jasmine tests have been 'ported' to RequireJS so they are all now self-contained rather than all being called from SpecRunner.html

--- tests/jasmine-main.js
+++ tests/jasmine-main.js
@@ -4,20 +4,45 @@ require.config({
 	baseUrl : "",
 
 	paths : {
-		"Player" : "libs/jasmine/src/Player"
+		"Player" : "libs/jasmine/src/Player",
+		"Song" : "libs/jasmine/src/Song",
+		"jasmine" : "libs/jasmine/jasmine",
+		"jasmine-html" : "libs/jasmine/jasmine-html",
+		"PlayerSpec" : "libs/jasmine/spec/PlayerSpec",
+		"SpecHelper" : "libs/jasmine/spec/SpecHelper"
 	},
 	shim : {
-		"Player" : {
-			exports : "Player"
+		"jasmine-html" : {
+			deps : [ "jasmine" ],
+			exports : "jasmine"
+		},
+		"SpecHelper" : {
+			deps : [ "jasmine-html" ],
+			exports : "jasmine"
+		},
+		"PlayerSpec" : {
+			deps : [ "SpecHelper", "Player", "Song" ],
+			exports : "PlayerSpec"
 		}
 	}
 
 });
 
-//Run the tests!
-require([ "Player"
+// Run the tests!
+require([ "jasmine-html", "PlayerSpec"
 
-], function(Player) {
-	player = new Player();
+], function(jasmine) {
+	var jasmineEnv = jasmine.getEnv();
+	jasmineEnv.updateInterval = 1000;
+
+	var htmlReporter = new jasmine.HtmlReporter();
+
+	jasmineEnv.addReporter(htmlReporter);
+
+	jasmineEnv.specFilter = function(spec) {
+		return htmlReporter.specFilter(spec);
+	};
+
+	jasmineEnv.execute();
 
 });
