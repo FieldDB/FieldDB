7d2f2298c7126848aeef1830d9f316d474dd24e9
Josh Horner <josh.horner@gmail.com>
2013-04-30 11:52:07 -0400

    Removed all references to our app, leaving only the tests behind.

--- tests/user/UserTest.js
+++ tests/user/UserTest.js
@@ -1,15 +1,16 @@
-require([ "user/User" ], function(User) {
+require([  ], function() {
 
   describe("User ", function() {
     it("should set user's first name", function() {
-      var u = new User();
-      u.set("firstname", "Bill");
-      expect("Bill" == u.get("firstname")).toBeTruthy();
+//      var u = new User();
+//      u.set("firstname", "Bill");
+//      expect("Bill" == u.get("firstname")).toBeTruthy();
+    	expect(true).toBeTruthy();
     });
     it("should have a subtitle constisting of firstname lastname ", function() {
-      var u = new User();
-      u.set("firstname", "Ed");
-      u.set("lastname", "LingLlama");
+//      var u = new User();
+//      u.set("firstname", "Ed");
+//      u.set("lastname", "LingLlama");
 //      expect("LingLlama" == u.subtitle()).toBeTruthy();
       expect(true).toBeTruthy();
 
