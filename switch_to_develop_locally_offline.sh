6c8237ad445333d652d9352887bd4875f3ac47e9
Josh Horner <josh.horner@gmail.com>
2013-07-02 16:59:07 -0400

    Updating bash scripts to include couchkeys. Updating package.json to include new node.js modules. Add new config files for multiple deployment targets to /libs folder.

--- switch_to_develop_locally_offline.sh
+++ switch_to_develop_locally_offline.sh
@@ -4,6 +4,8 @@ echo ""
 echo "Put the Node server to act as the localhost server"
 sed 's/nodeconfig_[^)]*)/nodeconfig_local")/' server.js  > output
 mv output server.js
+sed 's/couchkeys_[^)]*)/couchkeys_local")/' server.js  > output
+mv output server.js
 
 echo "Now running in local/offline developer mode using the localhost."
 
--- switch_to_producti.sh
+++ switch_to_producti.sh
@@ -4,6 +4,9 @@ echo ""
 echo "Put the Node server to act as the production server"
 sed 's/nodeconfig_[^)]*)/nodeconfig_production")/' server.js  > output
 mv output server.js
+sed 's/couchkeys_[^)]*)/couchkeys_production")/' server.js  > output
+mv output server.js
 sed 's/nodeconfig_[^)]*)/nodeconfig_production")/' httpredirectapp.js  > output
 mv output httpredirectapp.js
+
 echo "Now running in production mode. "
