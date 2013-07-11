99f9a95b9bd53821761acf75262fbdcdeace70f9
Tobin Skinner <tobin.skinner@gmail.com>
2013-07-11 13:15:43 -0400

    OpenSourceFieldlinguistics/FieldDB#1001 Set up glosser controller and service to get precedence rules.

--- angular_client/modules/spreadsheet/js/module.js
+++ angular_client/modules/spreadsheet/js/module.js
@@ -1,35 +1,50 @@
 console.log("Loading the Spreadsheet module.");
 
 'use strict';
-define([ "angular", "js/controllers/SpreadsheetController", "js/controllers/SettingsController",
-		"js/directives", "js/filters",
-		"js/services", "js/controllers/SandboxController" ], function(angular, SpreadsheetStyleDataEntryController, SpreadsheetStyleDataEntrySettingsController,
-				SpreadsheetStyleDataEntryDirectives, SpreadsheetStyleDataEntryFilters, SpreadsheetStyleDataEntryServices, SandboxController) {
-	/**
-	 * The main Spreadsheet Angular UI module.
-	 * 
-	 * @type {angular.Module}
-	 */
+define(
+    [ "angular", "js/controllers/SpreadsheetController",
+        "js/controllers/SettingsController",
+        "js/controllers/GlosserController", "js/directives", "js/filters",
+        "js/services", "js/controllers/SandboxController" ],
+    function(angular, SpreadsheetStyleDataEntryController,
+        SpreadsheetStyleDataEntrySettingsController,
+        SpreadsheetStyleDataEntryGlosserController,
+        SpreadsheetStyleDataEntryDirectives, SpreadsheetStyleDataEntryFilters,
+        SpreadsheetStyleDataEntryServices, SandboxController) {
+      /**
+       * The main Spreadsheet Angular UI module.
+       * 
+       * @type {angular.Module}
+       */
 
-	var SpreadsheetStyleDataEntry = angular.module('SpreadsheetStyleDataEntry',
-			[ 'SpreadsheetStyleDataEntry.services', 'SpreadsheetStyleDataEntry.directives', 'SpreadsheetStyleDataEntry.filters', 'ui.bootstrap' ]).config(
-			[ '$routeProvider', function($routeProvider) {
-				window.SpreadsheetStyleDataEntryController = SpreadsheetStyleDataEntryController;
-				console.log("Initializing the Spreadsheet module.");
-				$routeProvider.when('/spreadsheet_main', {
-					templateUrl : 'partials/main.html',
-				}).when('/settings', {
-					templateUrl : 'partials/settings.html',
-					controller : SpreadsheetStyleDataEntrySettingsController
-				}).when('/sandbox', {
-					templateUrl : 'partials/sandbox.html', controller: SandboxController,
-				}).when('/spreadsheet/template1', {
-					templateUrl : 'partials/template1.html',
-				}).when('/spreadsheet/template2', {
-					templateUrl : 'partials/template2.html',
-				}).otherwise({
-					redirectTo : '/spreadsheet_main'
-				});
-			} ]);
-	return SpreadsheetStyleDataEntry;
-});
+      var SpreadsheetStyleDataEntry = angular
+          .module(
+              'SpreadsheetStyleDataEntry',
+              [ 'SpreadsheetStyleDataEntry.services',
+                  'SpreadsheetStyleDataEntry.directives',
+                  'SpreadsheetStyleDataEntry.filters', 'ui.bootstrap' ])
+          .config(
+              [
+                  '$routeProvider',
+                  function($routeProvider) {
+                    window.SpreadsheetStyleDataEntryController = SpreadsheetStyleDataEntryController;
+                    window.SpreadsheetStyleDataEntryGlosserController = SpreadsheetStyleDataEntryGlosserController;
+                    console.log("Initializing the Spreadsheet module.");
+                    $routeProvider.when('/spreadsheet_main', {
+                      templateUrl : 'partials/main.html',
+                    }).when('/settings', {
+                      templateUrl : 'partials/settings.html',
+                      controller : SpreadsheetStyleDataEntrySettingsController
+                    }).when('/sandbox', {
+                      templateUrl : 'partials/sandbox.html',
+                      controller : SandboxController,
+                    }).when('/spreadsheet/template1', {
+                      templateUrl : 'partials/template1.html',
+                    }).when('/spreadsheet/template2', {
+                      templateUrl : 'partials/template2.html',
+                    }).otherwise({
+                      redirectTo : '/spreadsheet_main'
+                    });
+                  } ]);
+      return SpreadsheetStyleDataEntry;
+    });
