44e2ba15614bc6c1103b0ac7627ef1b23df27058
Josh Horner <josh.horner@gmail.com>
2013-08-01 16:50:09 -0400

    Updates Issue #25 to work on listing Couch info as CSV for automatic import into Lucene/Solr. Trying this way in order to dynamically generate column headers for schema matching.

---
+++ couchapp_minified/views/get_corpus_fields/map.js
@@ -0,0 +1,17 @@
+function(doc) {
+  /* if this document has been deleted, the ignore it and return immediately */
+  if(doc.trashed && doc.trashed.indexOf("deleted") > -1) return;
+  if ((doc.datumFields) && (doc.session)) {
+    emit("datumTotal", 1);
+    for ( var i in doc.datumFields) {
+      if (doc.datumFields[i].mask) {
+        emit(doc.datumFields[i].label + '_t', 1);
+      }
+    }
+    for ( var i in doc.session.sessionFields) {
+      if (doc.session.sessionFields[i].mask) {
+        emit(doc.session.sessionFields[i].label + '_t', 1);
+      }
+    }
+  }
+}
