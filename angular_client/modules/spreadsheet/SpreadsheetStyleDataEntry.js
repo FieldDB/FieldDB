288935a349846bcb9c822fb7e2c6d8b84c906988
Tobin Skinner <tobin.skinner@gmail.com>
2013-08-08 13:41:42 -0400

    Reformatted code for Sublime.

--- angular_client/modules/spreadsheet/SpreadsheetStyleDataEntry.js
+++ angular_client/modules/spreadsheet/SpreadsheetStyleDataEntry.js
@@ -2,25 +2,25 @@ console.log("Loading the Spreadsheet main");
 
 // Set the RequireJS configuration
 require.config({
-	paths : {
+	paths: {
 		/* AngularJS */
-		"angular" : "libs/angular/angular",
-		"angular-resource" : "libs/angular/angular-resource",
-		"bootstrap" : "libs/angular/ui-bootstrap.min",
-		"underscore" : "libs/underscore-min",
-		"glosser" : "libs/glosser/Glosser"
+		"angular": "libs/angular/angular",
+		"angular-resource": "libs/angular/angular-resource",
+		"bootstrap": "libs/angular/ui-bootstrap.min",
+		"underscore": "libs/underscore-min",
+		"glosser": "libs/glosser/Glosser"
 	},
-	shim : {
-		"angular" : {
-			exports : "angular"
+	shim: {
+		"angular": {
+			exports: "angular"
 		},
-		"angular-resource" : {
-			deps : [ "angular" ],
-			exports : "angular"
+		"angular-resource": {
+			deps: ["angular"],
+			exports: "angular"
 		},
-		"bootstrap" : {
-			deps : [ "angular-resource" ],
-			exports : "angular"
+		"bootstrap": {
+			deps: ["angular-resource"],
+			exports: "angular"
 		}
 	}
 });
@@ -29,9 +29,9 @@ require.config({
  * Declare only the variables that are needed here, the dependencies of the rest
  * will be discovered and loaded as needed by require.js
  */
-require(["bootstrap", "underscore", "glosser", "js/module.js" ],
-		function(angular) {
-	console.log("Initializing the Spreadsheet page.");
+require(["bootstrap", "underscore", "glosser", "js/module.js"],
+	function(angular) {
+		console.log("Initializing the Spreadsheet page.");
 
-	angular.bootstrap(document, [ 'SpreadsheetStyleDataEntry' ]);
-});
+		angular.bootstrap(document, ['SpreadsheetStyleDataEntry']);
+	});
--- angular_client/modules/spreadsheet/js/directives.js
+++ angular_client/modules/spreadsheet/js/directives.js
@@ -2,120 +2,116 @@ console.log("Loading the SpreadsheetStyleDataEntryDirectives.");
 
 'use strict';
 define(
-    [ "angular" ],
-    function(angular) {
-      var SpreadsheetStyleDataEntryDirectives = angular
-          .module('SpreadsheetStyleDataEntry.directives', [])
-          .directive('moduleVersion', [ 'version', function(version) {
-            return function(scope, element, attrs) {
-              element.text(version);
-            };
-          } ])
-          .directive(
-              'selectDropdown1',
-              function() {
-                return function(scope, element, attrs) {
-                  if (scope.field.label == scope.scopePreferences.template1[attrs.selectDropdown1].label) {
-                    element[0].selected = true;
-                  }
-                };
-              })
-          .directive(
-              'selectDropdown2',
-              function() {
-                return function(scope, element, attrs) {
-                  if (scope.field.label == scope.scopePreferences.template2[attrs.selectDropdown2].label) {
-                    element[0].selected = true;
-                  }
-                };
-              }).directive('selectDropdownSession', function() {
-            return function(scope, element, attrs) {
-              scope.$watch('activeSession', function() {
-                if (scope.session._id == scope.activeSession) {
-                  element[0].selected = true;
+  ["angular"],
+  function(angular) {
+    var SpreadsheetStyleDataEntryDirectives = angular
+      .module('SpreadsheetStyleDataEntry.directives', [])
+      .directive('moduleVersion', ['version',
+        function(version) {
+          return function(scope, element, attrs) {
+            element.text(version);
+          };
+        }
+      ])
+      .directive(
+        'selectDropdown1',
+        function() {
+          return function(scope, element, attrs) {
+            if (scope.field.label == scope.scopePreferences.template1[attrs.selectDropdown1].label) {
+              element[0].selected = true;
+            }
+          };
+        })
+      .directive(
+        'selectDropdown2',
+        function() {
+          return function(scope, element, attrs) {
+            if (scope.field.label == scope.scopePreferences.template2[attrs.selectDropdown2].label) {
+              element[0].selected = true;
+            }
+          };
+        }).directive('selectDropdownSession', function() {
+        return function(scope, element, attrs) {
+          scope.$watch('activeSession', function() {
+            if (scope.session._id == scope.activeSession) {
+              element[0].selected = true;
+            }
+          });
+        };
+      }).directive(
+        'arrowKey',
+        function() {
+          return function(scope, element, attrs) {
+            element.bind('keydown', function(e) {
+              if (e.keyCode === 40 && scope.$index == undefined) {
+                scope.selectRow(scope.$$childHead.datum);
+              } else if (e.keyCode === 40 && scope.$index < scope.data.length && scope.$$nextSibling) {
+                scope.selectRow(scope.$$nextSibling.datum);
+              } else if (e.keyCode === 38 && scope.$index == 0) {
+                scope.selectRow('newEntry');
+              } else if (e.keyCode === 38 && scope.$index > 0 && scope.$$prevSibling) {
+                scope.selectRow(scope.$$prevSibling.datum);
+              } else {
+                return;
+              }
+              scope.$apply();
+            });
+          };
+        }).directive(
+        'focus',
+        function($timeout) {
+          return function(scope, element) {
+            scope.$watch('selected', function() {
+              if (scope.selected == scope.datum || scope.selected == 'newEntry') {
+                $timeout(function() {
+                  element[0].focus();
+                }, 0);
+              };
+            });
+          };
+        }).directive(
+        'input',
+        function() {
+          return {
+            restrict: 'E',
+            require: 'ngModel',
+            link: function(scope, element, attrs, ngModel) {
+              if (attrs.class == undefined || attrs.class.indexOf("glossmorpheme") < 0) {
+                return;
+              }
+              // Override the input event and add custom 'glossmorpheme'
+              // logic
+
+              scope.$watch('glosserLoaded', function() {
+                if (attrs.placeholder == "Utterance") {
+                  element.bind('keyup', function(e) {
+                    var newUtterance = this.value;
+                    scope.$apply(function() {
+                      scope.morphemesGuess = Glosser.morphemefinder(
+                        newUtterance, scope.DB.pouchname);
+                    });
+                  });
+                }
+
+                if (attrs.placeholder == "Morphemes") {
+                  scope.$watch('morphemesGuess', function() {
+                    element.val(scope.morphemesGuess);
+                    ngModel.$setViewValue(scope.morphemesGuess);
+                    scope.glossGuess = Glosser
+                      .glossFinder(scope.morphemesGuess, scope.DB.pouchname);
+                  });
+                }
+
+                if (attrs.placeholder == "Gloss") {
+                  scope.$watch('glossGuess', function() {
+                    element.val(scope.glossGuess);
+                    ngModel.$setViewValue(scope.glossGuess);
+                  });
                 }
               });
-            };
-          }).directive(
-              'arrowKey',
-              function() {
-                return function(scope, element, attrs) {
-                  element.bind('keydown', function(e) {
-                    if (e.keyCode === 40 && scope.$index == undefined) {
-                      scope.selectRow(scope.$$childHead.datum);
-                    } else if (e.keyCode === 40
-                        && scope.$index < scope.data.length
-                        && scope.$$nextSibling) {
-                      scope.selectRow(scope.$$nextSibling.datum);
-                    } else if (e.keyCode === 38 && scope.$index == 0) {
-                      scope.selectRow('newEntry');
-                    } else if (e.keyCode === 38 && scope.$index > 0
-                        && scope.$$prevSibling) {
-                      scope.selectRow(scope.$$prevSibling.datum);
-                    } else {
-                      return;
-                    }
-                    scope.$apply();
-                  });
-                };
-              }).directive(
-              'focus',
-              function($timeout) {
-                return function(scope, element) {
-                  scope.$watch('selected', function() {
-                    if (scope.selected == scope.datum
-                        || scope.selected == 'newEntry') {
-                      $timeout(function() {
-                        element[0].focus();
-                      }, 0);
-                    }
-                    ;
-                  });
-                };
-              }).directive(
-              'input',
-              function() {
-                return {
-                  restrict : 'E',
-                  require : 'ngModel',
-                  link : function(scope, element, attrs, ngModel) {
-                    if (attrs.class == undefined
-                        || attrs.class.indexOf("glossmorpheme") < 0) {
-                      return;
-                    }
-                    // Override the input event and add custom 'glossmorpheme'
-                    // logic
+            }
+          };
+        });
 
-                    scope.$watch('glosserLoaded', function() {
-                      if (attrs.placeholder == "Utterance") {
-                        element.bind('keyup', function(e) {
-                          var newUtterance = this.value;
-                          scope.$apply(function() {
-                            scope.morphemesGuess = Glosser.morphemefinder(
-                                newUtterance, scope.DB.pouchname);
-                          });
-                        });
-                      }
-
-                      if (attrs.placeholder == "Morphemes") {
-                        scope.$watch('morphemesGuess', function() {
-                          element.val(scope.morphemesGuess);
-                          ngModel.$setViewValue(scope.morphemesGuess);
-                          scope.glossGuess = Glosser
-                              .glossFinder(scope.morphemesGuess, scope.DB.pouchname);
-                        });
-                      }
-
-                       if (attrs.placeholder == "Gloss") {
-                        scope.$watch('glossGuess', function() {
-                          element.val(scope.glossGuess);
-                          ngModel.$setViewValue(scope.glossGuess);
-                        });
-                      }
-                    });
-                  }
-                };
-              });
-
-      return SpreadsheetStyleDataEntryDirectives;
-    });
+    return SpreadsheetStyleDataEntryDirectives;
+  });
