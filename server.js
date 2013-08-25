4b4784303ccc237f90006eb09fab2c77a5d3c721
cesine <cesine@yahoo.com>
2013-08-25 09:17:06 -0400

    adding ability for users to choose display their chosen license on the public page
    for OpenSourceFieldlinguistics/FieldDB#1047 and fixes OpenSourceFieldlinguistics/FieldDB#207 now that the public urls are ready we can make sure that the user's corpora have terms of use so that they conform to EMELD best practices.

--- server.js
+++ server.js
@@ -60,6 +60,9 @@ app.get('/db/:pouchname', function(req, res) {
 
   getCorpusFromPouchname(pouchname)
     .then(function(result) {
+      result.corpus.corpusinfo.copyright = result.corpus.corpusinfo.copyright || result.team.username;
+      result.corpus.corpusinfo.license = result.corpus.corpusinfo.license || "Default: Creative Commons Attribution-ShareAlike (CC BY-SA).";
+      result.corpus.corpusinfo.terms = result.corpus.corpusinfo.terms || "Default: This license lets others remix, tweak, and build upon your work even for commercial purposes, as long as they credit you and license their new creations under the identical terms. This license is often compared to “copyleft” free and open source software licenses. All new works based on yours will carry the same license, so any derivatives will also allow commercial use. This is the license used by Wikipedia, and is recommended for materials that would benefit from incorporating content from Wikipedia and similarly licensed projects.";
       var data = {
         corpora: [result.corpus],
         ghash: result.team.gravatar,
@@ -81,6 +84,9 @@ app.get('/:user/:corpus/:pouchname', function(req, res) {
 
   getCorpusFromPouchname(pouchname)
     .then(function(result) {
+      result.corpus.corpusinfo.copyright = result.corpus.corpusinfo.copyright || result.team.username;
+      result.corpus.corpusinfo.license = result.corpus.corpusinfo.license || "Default: Creative Commons Attribution-ShareAlike (CC BY-SA).";
+      result.corpus.corpusinfo.terms = result.corpus.corpusinfo.terms || "Default: This license lets others remix, tweak, and build upon your work even for commercial purposes, as long as they credit you and license their new creations under the identical terms. This license is often compared to “copyleft” free and open source software licenses. All new works based on yours will carry the same license, so any derivatives will also allow commercial use. This is the license used by Wikipedia, and is recommended for materials that would benefit from incorporating content from Wikipedia and similarly licensed projects.";
       var data = {
         corpora: [result.corpus],
         ghash: result.team.gravatar,
