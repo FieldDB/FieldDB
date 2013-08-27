c366aa2a716ce8b2cc47c37b53bb2be0241c3057
hisakonog <hisakonog@gmail.com>
2013-08-27 08:11:37 -0400

    Update Corpus.js

--- backbone_client/corpus/Corpus.js
+++ backbone_client/corpus/Corpus.js
@@ -476,7 +476,7 @@ define(
                         {
                           title : "Default: Creative Commons Attribution-ShareAlike (CC BY-SA).",
                           humanReadable : "License Terms: This license lets others remix, tweak, and build upon your work even for commercial purposes, as long as they credit you and license their new creations under the identical terms. This license is often compared to “copyleft” free and open source software licenses. All new works based on yours will carry the same license, so any derivatives will also allow commercial use. This is the license used by Wikipedia, and is recommended for materials that would benefit from incorporating content from Wikipedia and similarly licensed projects.",
-                          link : "http://creativecommons.org/licenses/ "
+                          link : "http://creativecommons.org/licenses/by/3.0/ "
                         });
               }
               if (!this.get("termsOfUse")) {
