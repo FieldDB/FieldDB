75179323a55da17a39a4cf7eb8fc9144a813e881
cesine <cesine@yahoo.com>
2013-08-27 18:55:55 -0400

    for #207 put help texts for terms and licenses on both the read and edit views to help users understand if they need them an dwhat to think about when they want to use them so we can help them follow EMELD best practices without making the app to cumbersome and complex. updated the corpus details to uses objects for terms and license so that anyone can add more details in the future as properties of the object

--- backbone_client/corpus/Corpus.js
+++ backbone_client/corpus/Corpus.js
@@ -414,18 +414,37 @@ define([
         this.set("copyright",
           "Default: Add names of the copyright holders of the corpus.");
       }
-      if (!this.get("license")) {
-        this.set(
-          "license", {
+      var defaultLicense = {
             title: "Default: Creative Commons Attribution-ShareAlike (CC BY-SA).",
-            humanReadable: "License Terms: This license lets others remix, tweak, and build upon your work even for commercial purposes, as long as they credit you and license their new creations under the identical terms. This license is often compared to “copyleft” free and open source software licenses. All new works based on yours will carry the same license, so any derivatives will also allow commercial use. This is the license used by Wikipedia, and is recommended for materials that would benefit from incorporating content from Wikipedia and similarly licensed projects.",
-            link: "http://creativecommons.org/licenses/by/3.0/ "
-          });
+            humanReadable: "This license lets others remix, tweak, and build upon your work even for commercial purposes, as long as they credit you and license their new creations under the identical terms. This license is often compared to “copyleft” free and open source software licenses. All new works based on yours will carry the same license, so any derivatives will also allow commercial use. This is the license used by Wikipedia, and is recommended for materials that would benefit from incorporating content from Wikipedia and similarly licensed projects.",
+            link: "http://creativecommons.org/licenses/by-sa/3.0/"
+          };
+      if (!this.get("license")) {
+        this.set("license", defaultLicense);
       }
-      if (!this.get("termsOfUse")) {
-        this.set("termsOfUse", {
+      var licenseUpdated = this.get("license");
+      if(typeof licenseUpdated == "string"){
+        licenseUpdated = {};
+      }
+      if (!licenseUpdated.title) {
+        licenseUpdated.title = defaultLicense.title;
+        this.set("license", licenseUpdated);
+      }
+      if (!licenseUpdated.humanReadable) {
+        licenseUpdated.humanReadable = defaultLicense.humanReadable;
+        this.set("license", licenseUpdated);
+      }
+      if (!licenseUpdated.link) {
+        licenseUpdated.link = defaultLicense.link;
+        this.set("license", licenseUpdated);
+      }
+      var defaultTerms = {
           humanReadable: "Sample: The materials included in this corpus are available for research and educational use. If you want to use the materials for commercial purposes, please notify the author(s) of the corpus (myemail@myemail.org) prior to the use of the materials. Users of this corpus can copy and redistribute the materials included in this corpus, under the condition that the materials copied/redistributed are properly attributed.  Modification of the data in any copied/redistributed work is not allowed unless the data source is properly cited and the details of the modification is clearly mentioned in the work. Some of the items included in this corpus may be subject to further access conditions specified by the owners of the data and/or the authors of the corpus."
-        });
+        };
+      var termsUpdated = this.get("termsOfUse");
+      if(!termsUpdated || typeof termsUpdated == "string"){
+        termsUpdated = defaultTerms;
+        this.set("termsOfUse", defaultTerms);
       }
     },
 
