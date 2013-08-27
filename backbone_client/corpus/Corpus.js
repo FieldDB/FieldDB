f3df86fb902a3011876db6210e2e159a9fe987c2
hisakonog <hisakonog@gmail.com>
2013-08-27 08:09:10 -0400

    Update Corpus.js
    very minor change before merging

--- backbone_client/corpus/Corpus.js
+++ backbone_client/corpus/Corpus.js
@@ -475,8 +475,8 @@ define(
                         "license",
                         {
                           title : "Default: Creative Commons Attribution-ShareAlike (CC BY-SA).",
-                          title : "License Terms: This license lets others remix, tweak, and build upon your work even for commercial purposes, as long as they credit you and license their new creations under the identical terms. This license is often compared to “copyleft” free and open source software licenses. All new works based on yours will carry the same license, so any derivatives will also allow commercial use. This is the license used by Wikipedia, and is recommended for materials that would benefit from incorporating content from Wikipedia and similarly licensed projects.",
-                          title : "Link: http://creativecommons.org/licenses/ "
+                          humanReadable : "License Terms: This license lets others remix, tweak, and build upon your work even for commercial purposes, as long as they credit you and license their new creations under the identical terms. This license is often compared to “copyleft” free and open source software licenses. All new works based on yours will carry the same license, so any derivatives will also allow commercial use. This is the license used by Wikipedia, and is recommended for materials that would benefit from incorporating content from Wikipedia and similarly licensed projects.",
+                          link : "http://creativecommons.org/licenses/ "
                         });
               }
               if (!this.get("termsOfUse")) {
