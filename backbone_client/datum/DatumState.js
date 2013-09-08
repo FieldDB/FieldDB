5475729498f3a28c43fe5bccf3351eb38d247560
cesine <cesine@yahoo.com>
2013-09-07 23:23:08 -0400

    copies of the original backbone models
    the originals are becoming a general api which can serve as docs and test cases for new clients and servers

---
+++ backbone_client/datum/DatumState.js
@@ -0,0 +1,55 @@
+define([ 
+    "backbone", 
+    "user/UserMask" 
+], function(
+    Backbone,
+    UserMask
+) {
+  var DatumState = Backbone.Model.extend(
+  /** @lends DatumState.prototype */
+  {
+    /**
+     * @class The datum state lets the fieldlinguists assign their own state
+     *        categories to data (ie check with consultant, check with x,
+     *        checked, checked and wrong, hidden, deleted), whatever state they
+     *        decide. They an make each state have a color so that the team can
+     *        see quickly if there is something that needs to be done with that
+     *        data. We also added an optional field, Consultant that they can use
+     *        to say who they want to check with in case they have mulitple
+     *        consultants and the consultants have different grammaticality
+     *        judgements. When users change the state of the datum, we will add
+     *        a note in the datum's comments field so that the history of its
+     *        state is kept in an annotated format.
+     * 
+     * @description The initialize function The datum state creates a new state
+     *              object with the state set to the default (for example,
+     *              checked)
+     * 
+     * @extends Backbone.Model
+     * @constructs
+     */
+    initialize : function() {
+    },
+    
+    defaults : {
+//      state : "Checked",
+      color : "",
+//      consultant : UserMask,//TODO comment out htis line when we confirm that state is working
+      showInSearchResults : "checked",
+      selected : ""
+    },
+    
+    // Internal models: used by the parse function
+    internalModels : {
+      consultant : UserMask
+    },
+    saveAndInterConnectInApp : function(callback){
+      
+      if(typeof callback == "function"){
+        callback();
+      }
+    }
+  });
+
+  return DatumState;
+});
---
+++ backbone_client/user/UserGeneric.js
@@ -0,0 +1,97 @@
+define([ 
+    "backbone", 
+    "hotkey/HotKey",
+    "permission/Permission",
+    "user/UserPreference",
+    "user/UserMask",
+    "CryptoJS",
+    "OPrime"
+], function(
+    Backbone,
+    HotKey,
+    Permission,
+    UserPreference,
+    UserMask
+) {
+  var UserGeneric = Backbone.Model.extend(
+  /** @lends UserGeneric.prototype */
+  {
+    /**
+     * @class A generic user has a repository and permission groups
+     *        (read, write, admin). It can not login.
+     * 
+     * @property {String} username This is a username used when login.
+     * @property {String} password This is a password used when login. It should be secure (containing 1 digit, 1 uppercase) because it is what protects the confidentiality of the corpus.
+     * @property {String} email This is user's email
+     * @property {String} gravatar This is user's gravatar
+     * @property {String} researchInterest This is user's field of
+     *           interest (eg. semantics etc)
+     * @property {String} affiliation This is user's affiliation
+     * @property {String} description This user's description
+     * @property {String} subtitle This user's subtitle
+     * @property {Array} corpuses The corpus connections of the corpuses owned by
+     *           this user
+     * @property {Array} dataLists The datalist IDs of the datalists owned
+     *           by this user.
+     * @property {UserPreference} prefs This is where we'll have things like
+     *           background/skin.
+     * 
+     * @description The initialize function probably checks to see if
+     *              the user is existing or new and creates a new
+     *              account if it is new.
+     * 
+     * @extends Backbone.Model
+     * @constructs
+     */
+
+    // This is the constructor. It is called whenever you make a new
+    // User.
+    initialize : function() {
+      if (OPrime.debugMode) OPrime.debug("USERGENERIC init");
+
+    },
+      
+    // Internal models: used by the parse function
+    internalModels : {
+      prefs : UserPreference,
+      permissions : Permission, //TODO this needs to become plural
+      hotkeys : HotKey, //TODO this needs to become plural
+      publicSelf : UserMask
+    },
+
+    addCurrentCorpusToUser : function(){
+      var cc = window.app.get("corpus").get("couchConnection");
+      if(window.app.get("corpus").id != undefined){
+        cc.corpusid =  window.app.get("corpus").id;
+        this.get("corpuses").push(cc);
+      }else{
+        window.appView.toastUser("The corpus has no id, cant add it to the user.","alert-danger","Bug!");
+      }
+    },
+    saveAndInterConnectInApp : function(callback){
+      //TODO override in derived classes?
+      if(typeof callback == "function"){
+        callback();
+      }
+    }, 
+     getGravatar : function(email){
+      var existingGravatar = this.get("gravatar");
+      if(existingGravatar.indexOf("gravatar.com") > -1){
+        existingGravatar = existingGravatar.replace("https://secure.gravatar.com/avatar/","");
+        this.set("gravatar", existingGravatar);
+        return existingGravatar;
+      }
+      if(email){
+        var hash = CryptoJS.MD5(email).toString();
+        this.set("gravatar", hash);
+        return hash;
+      }
+      if(existingGravatar.indexOf("/") > -1){
+        existingGravatar = existingGravatar.replace(/\//g,"").replace("userpublic_gravatar.png","968b8e7fb72b5ffe2915256c28a9414c");
+      }
+      return existingGravatar;
+    }
+  });
+
+  return UserGeneric;
+});
