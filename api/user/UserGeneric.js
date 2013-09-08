4c213de735310d57199d6f8174ac6bca5026a19d
cesine <cesine@yahoo.com>
2013-09-08 00:12:41 -0400

    converting backbone class into something more generic

--- api/user/UserGeneric.js
+++ api/user/UserGeneric.js
@@ -1,97 +1,74 @@
-define([ 
-    "backbone", 
-    "hotkey/HotKey",
-    "permission/Permission",
-    "user/UserPreference",
-    "user/UserMask",
-    "CryptoJS",
-    "OPrime"
-], function(
-    Backbone,
-    HotKey,
-    Permission,
-    UserPreference,
-    UserMask
-) {
-  var UserGeneric = Backbone.Model.extend(
-  /** @lends UserGeneric.prototype */
-  {
-    /**
-     * @class A generic user has a repository and permission groups
-     *        (read, write, admin). It can not login.
-     * 
-     * @property {String} username This is a username used when login.
-     * @property {String} password This is a password used when login. It should be secure (containing 1 digit, 1 uppercase) because it is what protects the confidentiality of the corpus.
-     * @property {String} email This is user's email
-     * @property {String} gravatar This is user's gravatar
-     * @property {String} researchInterest This is user's field of
-     *           interest (eg. semantics etc)
-     * @property {String} affiliation This is user's affiliation
-     * @property {String} description This user's description
-     * @property {String} subtitle This user's subtitle
-     * @property {Array} corpuses The corpus connections of the corpuses owned by
-     *           this user
-     * @property {Array} dataLists The datalist IDs of the datalists owned
-     *           by this user.
-     * @property {UserPreference} prefs This is where we'll have things like
-     *           background/skin.
-     * 
-     * @description The initialize function probably checks to see if
-     *              the user is existing or new and creates a new
-     *              account if it is new.
-     * 
-     * @extends Backbone.Model
-     * @constructs
-     */
+var Hotkey = require("hotkey/HotKey");
+var Permission = require("permission/Permission");
+var UserPreference = require("user/UserPreference");
+var UserMask = require("user/UserMask");
 
-    // This is the constructor. It is called whenever you make a new
-    // User.
-    initialize : function() {
-      if (OPrime.debugMode) OPrime.debug("USERGENERIC init");
+/**
+ * @lends UserGeneric.prototype
+ * 
+ * @class A generic user has a repository and permission groups
+ *        (read, write, admin). It can not login.
+ *
+ *
+ * @description Contains basic functions to manipulate User json and schema,
+ * can be used as a shared model between clients and servers
+ *
+ * @property {String} username This is a username used when login.
+ * @property {String} password This is a password used when login. It should be secure (containing 1 digit, 1 uppercase) because it is what protects the confidentiality of the corpus.
+ * @property {String} email This is user's email
+ * @property {String} gravatar This is user's gravatar
+ * @property {String} researchInterest This is user's field of
+ *           interest (eg. semantics etc)
+ * @property {String} affiliation This is user's affiliation
+ * @property {String} description This user's description
+ * @property {String} subtitle This user's subtitle
+ * @property {Array} corpuses The corpus connections of the corpuses owned by
+ *           this user
+ * @property {Array} dataLists The datalist IDs of the datalists owned
+ *           by this user.
+ * @property {UserPreference} prefs This is where we'll have things like
+ *           background/skin.
+ *
+ * @description The initialize function probably checks to see if
+ *              the user is existing or new and creates a new
+ *              account if it is new.
+ *
+ * @extends Object
+ *
+ */
+var UserGeneric = function() {
 
-    },
-      
-    // Internal models: used by the parse function
-    internalModels : {
-      prefs : UserPreference,
-      permissions : Permission, //TODO this needs to become plural
-      hotkeys : HotKey, //TODO this needs to become plural
-      publicSelf : UserMask
-    },
+  // Internal models: used by the parse function
+  this.internalModels = {
+    prefs: UserPreference,
+    permissions: Permission, //TODO this needs to become plural
+    hotkeys: HotKey, //TODO this needs to become plural
+    publicSelf: UserMask
+  };
 
-    addCurrentCorpusToUser : function(){
-      var cc = window.app.get("corpus").get("couchConnection");
-      if(window.app.get("corpus").id != undefined){
-        cc.corpusid =  window.app.get("corpus").id;
-        this.get("corpuses").push(cc);
-      }else{
-        window.appView.toastUser("The corpus has no id, cant add it to the user.","alert-danger","Bug!");
-      }
-    },
-    saveAndInterConnectInApp : function(callback){
-      //TODO override in derived classes?
-      if(typeof callback == "function"){
-        callback();
-      }
-    }, 
-     getGravatar : function(email){
-      var existingGravatar = this.get("gravatar");
-      if(existingGravatar.indexOf("gravatar.com") > -1){
-        existingGravatar = existingGravatar.replace("https://secure.gravatar.com/avatar/","");
-        this.set("gravatar", existingGravatar);
-        return existingGravatar;
-      }
-      if(email){
-        var hash = CryptoJS.MD5(email).toString();
-        this.set("gravatar", hash);
-        return hash;
-      }
-      if(existingGravatar.indexOf("/") > -1){
-        existingGravatar = existingGravatar.replace(/\//g,"").replace("userpublic_gravatar.png","968b8e7fb72b5ffe2915256c28a9414c");
-      }
+  this.addCorpusToUser = function(corpus){
+    this.corpuses.unshift(corpus);
+  };
+  this.save = function(user) {
+    console.log(JSON.stringify(user, null, 2));
+  };
+  this.getGravatar = function(optionalEmail) {
+    var existingGravatar = this.gravatar;
+    if (existingGravatar.indexOf("gravatar.com") > -1) {
+      existingGravatar = existingGravatar.replace("https://secure.gravatar.com/avatar/", "");
+      this.gravatar = existingGravatar;
       return existingGravatar;
     }
-  });
+    if (optionalEmail) {
+      var hash = CryptoJS.MD5(optionalEmail).toString();
+      this.set("gravatar", hash);
+      return hash;
+    }
+    if (existingGravatar.indexOf("/") > -1) {
+      existingGravatar = existingGravatar.replace(/\//g, "").replace("userpublic_gravatar.png", "968b8e7fb72b5ffe2915256c28a9414c");
+    }
+    return existingGravatar;
+  };
+};
 
-  return UserGeneric;
-});
+module.exports = UserGeneric;
