bc5a61d8f484930246b13dcb7efdbcfcc7598072
cesine <cesine@yahoo.com>
2013-09-16 16:26:16 -0400

    removing cruft from backbone couchapp examples

--- couchapp_minified/views/messages/map.js
+++
@@ -1,7 +0,0 @@
-function(doc) {
-  /* if this document has been deleted, the ignore it and return immediately */
-  if(doc.trashed && doc.trashed.indexOf("deleted") > -1) return;
-  if (doc.collection == "messages") {
-    emit(doc._id, doc);
-  }
-};
