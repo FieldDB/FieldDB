11b139982462d934520b2bff1002fcc4e60783fe
cesine <cesine@yahoo.com>
2013-09-29 13:20:56 -0400

    cleaned out some unused fields and added PublishedInWrittenDocument and ToBeCheckedWithASpeakerForNaturalness to validation status by default

--- backbone_client/bot/InuktitutCleaningBot.js
+++ backbone_client/bot/InuktitutCleaningBot.js
@@ -20,106 +20,35 @@ var Bot = function(pouchname, corpusid, corpustitle){
 			return;
 		}
 
-		/* new fields */
-		var userField = {
-			label: "enteredByUser",
-			value: "MultilingualCorporaExtractor",
-			mask: "MultilingualCorporaExtractor",
-			encrypted: "",
-			shouldBeEncrypted: "",
-			help: "The user who originally entered the datum",
-			showToUserTypes: "all",
-			readonly: true,
-			userchooseable: "disabled"
-		};
-		var modifiedByUser = {
-			label: "modifiedByUser",
-			value: name,
-			mask: name,
-			encrypted: "",
-			shouldBeEncrypted: "",
-			help: "An array of users who modified the datum",
-			showToUserTypes: "all",
-			readonly: true,
-			users: [{
-				gravatar: gravatar,
-				username: name,
-				collection: "users",
-				firstname: "Cleaner",
-				lastname: "Bot"
-			}],
-			userchooseable: "disabled"
-		};
-
-
 		var changes = [];
-		var utterance = "";
-		var allomorphsField = {};
-		/* clean other things */
-		for(var field in datum.datumFields){
-			if(datum.datumFields[field].value && datum.datumFields[field].value.indexOf("gen:") > -1){
-				var oldValue = datum.datumFields[field].value;
-				datum.datumFields[field].value = datum.datumFields[field].value.replace(/^gen:\d\d*:\d\d* /g, "");
-				var removed = oldValue.replace(datum.datumFields[field].value, "");
-				datum.datumFields[field].mask = datum.datumFields[field].value;
-				changes.push("Removed "+ removed);
+		/* clean out fields that came from quechua corpus, and set the validation status to something more accurate for this corpus */
+		for (var field = datum.datumFields.length -1; field > 0; field --) {
+			// console.log("field "+datum.datumFields[field].label );
+			if(datum.datumFields[field].label === "notes" || datum.datumFields[field].label === "dateElicited" || datum.datumFields[field].label === "checkedWithConsultant" || datum.datumFields[field].label === "dialect " ){
+				var oldField = datum.datumFields.splice(field, 1);
+				console.log("Removed field " + oldField[0].label);
+				changes.push("Removed "+ oldField[0].label);
 			}
-			datum.datumFields[field].value = datum.datumFields[field].value.trim();
-			datum.datumFields[field].mask = datum.datumFields[field].mask.trim();
-			if(datum.datumFields[field].label === "utterance"){
-				utterance = datum.datumFields[field].value;
-			}
-			if(datum.datumFields[field].label === "Verse number from Bible Book Chapter"){
-				datum.datumFields[field].label = "verse";
-				datum.datumFields[field].help = "This is the bible verse number from the original source, for the book see the sessionFields";
-			}
-			if(datum.datumFields[field].label === "enteredByUser"){
-				datum.datumFields[field].value = userField.value;
-				datum.datumFields[field].mask = userField.mask;
-				userField = null;
-			}
-			if(datum.datumFields[field].label === "modifiedByUser"){
-				datum.datumFields[field].value = datum.datumFields[field].value  + ", "+ modifiedByUser.value;
-				datum.datumFields[field].mask =  datum.datumFields[field].mask  + ", "+ modifiedByUser.mask;
-				datum.datumFields[field].users.push(modifiedByUser.users[0]);
-				modifiedByUser = null;
-			}
-			if(datum.datumFields[field].label === "morphemes"){
-				/* copy the old morphemes field to a new field for allomorphs */
-				$.extend(allomorphsField, datum.datumFields[field]);
-				allomorphsField.label = "allomorphs";
-				allomorphsField.help = "We have decided to have an explicit allomorphs line for Inuktitut since the writing trandition uses allomorphs rather than morphemes, it is usually very hard to semi-automatically segment the words without an intermediary allomorph step.";
-			}
-
-
-		}
-		/* put it below the utterance line after all the fields have been cleaned*/
-		datum.datumFields.splice(2,0, allomorphsField);
-		/* put in the user and modified by user if they werent already there */
-		if(userField){
-			datum.datumFields.push(userField);
-		}
-		if(modifiedByUser){
-			datum.datumFields.push(modifiedByUser);
-		}
-
-		for(var field in datum.session.sessionFields){
-			if(datum.session.sessionFields[field].label === "Bible Book"){
-				datum.session.sessionFields[field].label = "book";
-				datum.session.sessionFields[field].help = "This is the bible book where this session came from.";
-			}
-			if(datum.session.sessionFields[field].label === "Bible Book Chapter"){
-				datum.session.sessionFields[field].label = "chapter";
-				datum.session.sessionFields[field].help = "This is the bible book chapter number where this datum came from.";
+			if (datum.datumFields[field].label === "validationStatus" ) {
+				// console.log("validationStatus: "+ datum.datumFields[field].value);
+				if (datum.datumFields[field].value.indexOf("Checked") === 0 || !datum.datumFields[field].value) {
+					if (!datum.datumFields[field].value) {
+						datum.datumFields[field].value = "PublishedInWrittenDocument";
+					}
+					datum.datumFields[field].value = datum.datumFields[field].value.replace(/^Checked/, "PublishedInWrittenDocument");
+					datum.datumFields[field].value = "ToBeCheckedWithASpeakerForNaturalness, " + datum.datumFields[field].value;
+					datum.datumFields[field].mask = datum.datumFields[field].value;
+				}else{
+					datum.datumFields[field].value = "ToBeCheckedWithASpeakerForNaturalness, PublishedInWrittenDocument, " + datum.datumFields[field].value;
+					datum.datumFields[field].mask = datum.datumFields[field].value;
+				}
+				changes.push("Flagged as "+ datum.datumFields[field].value);
 			}
 		}
-		/* move the chapter field to the datum */
-		var chapterField = datum.session.sessionFields.pop();
-		datum.datumFields.push(chapterField);
-
+		
 		var timestamp = Date.now();
 		/* Record this event in the comments */
-		var changeDescription = changes.join(", ") + " so that the IGT lines would line up again, and added an allomorphs line.";
+		var changeDescription = changes.join(", ");
 		datum.comments.push({
 			"text": changeDescription, 
 			"username": name,
@@ -136,14 +65,14 @@ var Bot = function(pouchname, corpusid, corpustitle){
 	};
 
 	var saveDocBackToCouchDB = function(cleanedDoc, directobjectMessage){
-		console.log("Here is what we would save ",modifiedDoc);
+		console.log("Here is what we would save ", cleanedDoc);
 		return ;
 		(function(modifiedDoc, oldrev, directobject){
 
 			database.saveDoc(modifiedDoc, {success: function(status){
 				console.log("Saved ", modifiedDoc, status);
 				var activity = {
-					"verb": "<a target='_blank' href='#diff/oldrev/"+oldrev+"/newrev/"+status._rev+"'>updated</a> ",
+					"verb": "<a target='_blank' href='#diff/oldrev/"+oldrev+"/newrev/"+status.rev+"'>updated</a> ",
 					"verbicon": "icon-pencil",
 					"directobjecticon": "icon-list",
 					"directobject": "<a target='_blank' href='#data/"+modifiedDoc._id+"'>"+directobject+"</a> ",
@@ -194,10 +123,6 @@ return {
 				    	database.openDoc(data[couchdatum].id,{
 				    		success: function(originalDoc){
 
-				    			// console.log(originalDoc._rev);
-				    			var id = originalDoc._id;
-				    			var oldrev= originalDoc._rev;
-				    			
 				    			/* transliterate the utterance line into romanized, then save the data back to the db */
 				    			cleaningFunction(originalDoc, saveDocBackToCouchDB);
 
