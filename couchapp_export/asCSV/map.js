a311d03e294456b6be27b5791e59e53eed27f41a
cesine <cesine@yahoo.com>
2013-10-01 16:15:41 -0400

    for #1161 reverting back to the normal datum fields map reduce

--- couchapp_export/asCSV/map.js
+++ couchapp_export/asCSV/map.js
@@ -4,18 +4,13 @@ function(doc) {
     if ((doc.datumFields) && (doc.session)) {
         var obj = {};
         for (i = 0; i < doc.datumFields.length; i++) {
-            if (doc.datumFields[i].mask && !(doc.datumFields[i].label == 'modifiedByUser')) {
-                if (doc.datumFields[i].label === 'enteredByUser') {
-                    obj.enteredByUser = doc.datumFields[i].mask.username;
-                } else {
-                    obj[doc.datumFields[i].label] = doc.datumFields[i].mask;
-                }
+            if (doc.datumFields[i].mask) {
+                obj[doc.datumFields[i].label] = doc.datumFields[i].mask;
             }
         }
         if (doc.session.sessionFields) {
             for (j = 0; j < doc.session.sessionFields.length; j++) {
-                if (doc.session.sessionFields[j].mask && !(doc.session.sessionFields[j].label == 'user' ||
-                    doc.session.sessionFields[j].label === 'dateSEntered')) {
+                if (doc.session.sessionFields[j].mask) {
                     obj[doc.session.sessionFields[j].label] = doc.session.sessionFields[j].mask;
                 }
             }
