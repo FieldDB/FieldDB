4c722bc4a867a4bd70586b6a54aaced1dbb4a678
cesine <cesine@yahoo.com>
2013-10-01 22:37:16 -0400

    removed duplicate code from the minified couchapp, now all is copied at build

--- couchapp_minified/views/byCollection/map.js
+++
@@ -1,7 +0,0 @@
-function(doc) {
-  /* if this document has been deleted, the ignore it and return immediately */
-  if(doc.trashed && doc.trashed.indexOf("deleted") > -1) return;
-  if (doc.collection) {
-    emit(doc.collection, doc);
-  }
-};
--- couchapp_minified/views/deleted/map.js
+++
@@ -1,6 +0,0 @@
-/* updated to be compatible with pre-1.38 databases */
-function(doc) {
-  /* if this document has been deleted, the ignore it and return immediately */
-  if(doc.trashed && doc.trashed.indexOf("deleted") > -1) 
-    emit(doc.dateModified, doc);
-};
