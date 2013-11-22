650f9ac1fa3d9a6f75abf63beaf6bf96f1ba80fc
cesine <cesine@yahoo.com>
2013-11-22 02:48:32 -0500

    fixes #1093 added corpus doc to all users on testing and mcgill

--- scripts/maintenance.js
+++ scripts/maintenance.js
@@ -35,6 +35,244 @@ $.couch.allDbs({
 });
 
 
+var template_corpus_doc = function() {
+    return {
+        "_id": "corpus",
+        "title": "Private Corpus",
+        "titleAsUrl": "private_corpus",
+        "description": "The details of this corpus are not public.",
+        "couchConnection": {
+            "protocol": "https://",
+            "domain": "corpusdev.lingsync.org",
+            "port": "443",
+            "pouchname": "default",
+            "path": "",
+            "corpusid": ""
+        },
+        "terms": {
+            "humanReadable": "Sample: The materials included in this corpus are available for research and educational use. If you want to use the materials for commercial purposes, please notify the author(s) of the corpus (myemail@myemail.org) prior to the use of the materials. Users of this corpus can copy and redistribute the materials included in this corpus, under the condition that the materials copied/redistributed are properly attributed.  Modification of the data in any copied/redistributed work is not allowed unless the data source is properly cited and the details of the modification is clearly mentioned in the work. Some of the items included in this corpus may be subject to further access conditions specified by the owners of the data and/or the authors of the corpus."
+        },
+        "license": {
+            "title": "Default: Creative Commons Attribution-ShareAlike (CC BY-SA).",
+            "humanReadable": "This license lets others remix, tweak, and build upon your work even for commercial purposes, as long as they credit you and license their new creations under the identical terms. This license is often compared to “copyleft” free and open source software licenses. All new works based on yours will carry the same license, so any derivatives will also allow commercial use. This is the license used by Wikipedia, and is recommended for materials that would benefit from incorporating content from Wikipedia and similarly licensed projects.",
+            "link": "http://creativecommons.org/licenses/by-sa/3.0/"
+        },
+        "copyright": "Default: Add names of the copyright holders of the corpus.",
+        "pouchname": "",
+        "datumFields": [{
+            "label": "judgement",
+            "value": "",
+            "mask": "",
+            "encrypted": "",
+            "shouldBeEncrypted": "",
+            "help": "Grammaticality/acceptability judgement of this data.",
+            "size": "3",
+            "showToUserTypes": "linguist",
+            "userchooseable": "disabled"
+        }, {
+            "label": "gloss",
+            "value": "",
+            "mask": "",
+            "encrypted": "",
+            "shouldBeEncrypted": "checked",
+            "help": "Metalanguage glosses of each individual morpheme (morphemes are pieces ofprefix, suffix) Sample entry: friend-fem-pl",
+            "showToUserTypes": "linguist",
+            "userchooseable": "disabled"
+        }, {
+            "label": "syntacticCategory",
+            "value": "",
+            "mask": "",
+            "encrypted": "",
+            "shouldBeEncrypted": "checked",
+            "help": "This optional field is used by the machine to help with search.",
+            "showToUserTypes": "machine",
+            "userchooseable": "disabled"
+        }, {
+            "label": "syntacticTreeLatex",
+            "value": "",
+            "mask": "",
+            "encrypted": "",
+            "shouldBeEncrypted": "checked",
+            "help": "This optional field is used by the machine to make LaTeX trees and help with search and data cleaning, in combination with morphemes and gloss (above). Sample entry: Tree [.S NP VP ]",
+            "showToUserTypes": "machine",
+            "userchooseable": "disabled"
+        }, {
+            "label": "tags",
+            "value": "",
+            "mask": "",
+            "encrypted": "",
+            "shouldBeEncrypted": "",
+            "help": "Tags for constructions or other info that you might want to use to categorize your data.",
+            "showToUserTypes": "all",
+            "userchooseable": "disabled"
+        }, {
+            "label": "validationStatus",
+            "value": "",
+            "mask": "",
+            "encrypted": "",
+            "shouldBeEncrypted": "",
+            "help": "Any number of tags of data validity (replaces DatumStates). For example: ToBeCheckedWithSeberina, CheckedWithRicardo, Deleted etc...",
+            "showToUserTypes": "all",
+            "userchooseable": "disabled"
+        }],
+        "sessionFields": [{
+            "label": "dialect",
+            "value": "",
+            "mask": "",
+            "encrypted": "",
+            "shouldBeEncrypted": "",
+            "help": "You can use this field to be as precise as you would like about the dialect of this session.",
+            "userchooseable": "disabled"
+        }, {
+            "label": "language",
+            "value": "",
+            "mask": "",
+            "encrypted": "",
+            "shouldBeEncrypted": "",
+            "help": "This is the langauge (or language family), if you would like to use it.",
+            "userchooseable": "disabled"
+        }, {
+            "label": "dateElicited",
+            "value": "",
+            "mask": "",
+            "encrypted": "",
+            "shouldBeEncrypted": "",
+            "help": "This is the date in which the session took place.",
+            "userchooseable": "disabled"
+        }],
+        "comments": []
+    };
+};
+
+/*
+create a corpus doc if one doesnt exist, with the corpusid if found
+ */
+$.couch.allDbs({
+    success: function(results) {
+        console.log(results);
+        for (var db in results) {
+
+            (function(dbname) {
+                if (dbname.indexOf("-") === -1 || dbname.indexOf("activity_feed") > -1) {
+                    console.log("This db is not a corpus " + dbname);
+                    return;
+                }
+                var localdbname = dbname;
+                var database = $.couch.db(dbname);
+
+                var saveCorpusDoc = function(corpusDoc, localdbname) {
+                    corpusDoc.couchConnection.pouchname = localdbname;
+                    corpusDoc.pouchname = localdbname;
+                    database.saveDoc(corpusDoc, {
+                        success: function(serverResults) {
+                            console.log("Saved corpus doc for " + localdbname);
+                        },
+                        error: function(serverResults) {
+                            console.log("There was a problem saving the doc." + localdbname);
+                        }
+                    });
+                };
+
+                var updateCorpusDoc = function(corpusDoc, localdbname) {
+                    database.view("pages/private_corpuses", {
+                        success: function(results) {
+                            var corpusid = "";
+                            try {
+                                corpusid = results.rows[0].value._id;
+                            } catch (e) {
+                                console.log("Corpus is pretty broken: " + localdbname);
+                                return;
+                            }
+                            console.log("Corpus id " + corpusid);
+                            corpusDoc.couchConnection.corpusid = corpusid;
+                            saveCorpusDoc(corpusDoc, localdbname);
+                        },
+                        error: function(error) {
+                            console.log("Error getting a private corpus doc", error);
+                            saveCorpusDoc(corpusDoc, localdbname);
+                        }
+                    });
+                };
+
+                database.openDoc("corpus", {
+                    success: function(results) {
+                        var doUpdate = false;
+                        try {
+                            console.log(results.couchConnection.corpusid + " corpus doc is in " + dbname);
+                            if (!results.couchConnection.corpusid) {
+                                doUpdate = true;
+                            }
+                        } catch (e) {
+                            console.log("There was a problem looking up the corpus id");
+                            doUpdate = true;
+                            results.couchConnection = {
+                                "protocol": "https://",
+                                "domain": "corpusdev.lingsync.org",
+                                "port": "443",
+                                "pouchname": "default",
+                                "path": "",
+                                "corpusid": ""
+                            };
+                        }
+                        
+                        if (doUpdate) {
+                            updateCorpusDoc(results, localdbname);
+                        }
+                    },
+                    error: function(error) {
+                        console.log("Error getting a corpus doc, creating one", error);
+                        updateCorpusDoc(template_corpus_doc(), localdbname);
+                    }
+                });
+
+
+
+            })(results[db]);
+
+        }
+    },
+    error: function(error) {
+        console.log("Error getting db list", error);
+    }
+});
+
+
+/*
+Verify corpus doc
+ */
+$.couch.allDbs({
+    success: function(results) {
+        console.log(results);
+        for (var db in results) {
+
+            (function(dbname) {
+                if (dbname.indexOf("-") === -1 || dbname.indexOf("activity_feed") > -1) {
+                    console.log("This db is not a corpus " + dbname);
+                    return;
+                }
+                var localdbname = dbname;
+                var database = $.couch.db(dbname);
+
+                database.openDoc("corpus", {
+                    success: function(results) {
+                        console.log("Corpus doc " + dbname, results.couchConnection);
+                    },
+                    error: function(error) {
+                        console.log("Error getting a corpus doc " + dbname, error);
+                    }
+                });
+            })(results[db]);
+        }
+    },
+    error: function(error) {
+        console.log("Error getting db list", error);
+    }
+});
+
+
+/*
+Deploy to all users
+ */
 $.couch.allDbs({
     success: function(results) {
         console.log(results);
@@ -42,20 +280,26 @@ $.couch.allDbs({
 
             (function(dbname) {
                 if (dbname.indexOf("-") === -1) {
-                    console.log("This db is not a corpus or activity feed " + dbname);
+                    console.log(dbname + "  is not a corpus or activity feed " );
                     return;
                 }
-                var sourceDB = "activity_feed";
+                var sourceDB = "";
                 if (dbname.indexOf("activity_feed") > -1) {
+                    if (dbname.split("-").length >= 3) {
+                        sourceDB = "new_corpus_activity_feed";
+                    } else {
+                        sourceDB = "new_user_activity_feed";
+                    }
+                } else {
                     sourceDB = "new_corpus";
                 }
-
-                $.couch.replicate("new_corpus", dbname, {
+                console.log(dbname + " is a " + sourceDB);
+                $.couch.replicate(sourceDB, dbname, {
                     success: function(result) {
                         console.log(dbname, result);
                     },
                     error: function(error) {
-                        console.log("Error deploying " + sourceDB + "app to db" + dbname, error);
+                        console.log("Error deploying " + sourceDB + " app to db" + dbname, error);
                     }
                 });
             })(results[db]);
@@ -68,7 +312,9 @@ $.couch.allDbs({
 });
 
 
-
+/*
+Remove extra quotes in security docs
+ */
 var count = 0;
 $.couch.allDbs({
     success: function(dbs) {
@@ -133,6 +379,9 @@ $.couch.allDbs({
 });
 
 
+/*
+Remove extra quotes in permissions
+ */
 var database = $.couch.db("_users");
 database.allDocs({
     success: function(result) {
