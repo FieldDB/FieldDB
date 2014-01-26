7414bc73036eea1decd868209594ad5a4087798e
cesine <cesine@yahoo.com>
2014-01-26 16:27:44 -0500

    making tags sentence case and splitting on spaces...envethough we just want comma

--- couchapp_dev/views/get_corpus_datum_tags/map.js
+++ couchapp_dev/views/get_corpus_datum_tags/map.js
@@ -6,10 +6,16 @@ function(doc) {
     if ((doc.datumFields) && (doc.session)) {
       for (var i in doc.datumFields) {
         if (doc.datumFields[i].label == "tags" && doc.datumFields[i].mask) {
-          var stati = doc.datumFields[i].mask.split(" ");
+          var stati = doc.datumFields[i].mask.split(/[, ]/);
           for (var state in stati) {
-            if (stati[state])
-              emit(stati[state], 1);
+            var tag = stati[state] || "";
+            tag = tag.toLocaleLowerCase().replace(/[,!@#$%^&*()]/g,'').trim()
+            if (tag) {
+              tag = tag.replace(/\w\S*/g, function(txt) {
+                return txt.charAt(0).toUpperCase() + txt.substr(1);
+              });
+              emit(tag, 1);
+            }
           }
         }
       }
