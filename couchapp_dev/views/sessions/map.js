db749c9bd587118ca27f23ca85e1100d5f55e97b
cesine <cesine@yahoo.com>
2014-01-26 14:44:45 -0500

    fixes #1289 added try catch around all map reduce and verfied presence of mask on datum fields to protect from enteredByUser which often was missing the mask

--- couchapp_dev/views/sessions/map.js
+++ couchapp_dev/views/sessions/map.js
@@ -1,8 +1,12 @@
 /* updated to be compatible with pre-1.38 databases */
 function(doc) {
-  /* if this document has been deleted, the ignore it and return immediately */
-  if(doc.trashed && doc.trashed.indexOf("deleted") > -1) return;
-  if (doc.collection == "sessions" || (doc.sessionFields && !doc.datumFields)) {
-    emit(doc.dateModified, doc);
-  }
+	try {
+		/* if this document has been deleted, the ignore it and return immediately */
+		if (doc.trashed && doc.trashed.indexOf("deleted") > -1) return;
+		if (doc.collection == "sessions" || (doc.sessionFields && !doc.datumFields)) {
+			emit(doc.dateModified, doc);
+		}
+	} catch (e) {
+		//emit(e, 1);
+	}
 };
