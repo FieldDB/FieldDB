2235d462ab99e0a457a5501b9e37b5b3869f9ceb
cesine <cesine@yahoo.com>
2014-02-08 21:29:15 -0500

    for #1362 letting the datum render themselves

--- backbone_client/datum/DatumReadView.js
+++ backbone_client/datum/DatumReadView.js
@@ -70,7 +70,7 @@ define([
       //   });
       // this.sessionView.format = "link";
 
-//      this.model.bind("change", this.render, this);
+     this.model.bind("change", this.render, this);
     },
 
     /**
