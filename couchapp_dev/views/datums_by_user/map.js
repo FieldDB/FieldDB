8a722e6af973a5650eafc646f2a0bcc24affdfaa
cesine <cesine@yahoo.com>
2014-02-14 22:48:48 -0500

    added a view for datum by user so field methods instructors can detect if a student needs help fixes #1375

---
+++ couchapp_dev/views/datums_by_user/map.js
@@ -0,0 +1,28 @@
+/* updated to be compatible with pre-1.38 databases */
+
+function(doc) {
+  try {
+    /* if this document has been deleted, the ignore it and return immediately */
+    if (doc.trashed && doc.trashed.indexOf("deleted") > -1) {
+      return;
+    }
+    if (doc.collection == "datums" || (doc.datumFields && doc.session)) {
+      var datum = {};
+      for (i = 0; i < doc.datumFields.length; i++) {
+        if (doc.datumFields[i].mask) {
+          datum[doc.datumFields[i].label] = doc.datumFields[i].mask;
+        }
+      }
+      if (doc.session.sessionFields) {
+        for (j = 0; j < doc.session.sessionFields.length; j++) {
+          if (doc.session.sessionFields[j].mask) {
+            datum[doc.session.sessionFields[j].label] = doc.session.sessionFields[j].mask;
+          }
+        }
+      }
+      emit(datum.user, datum);
+    }
+  } catch (e) {
+    //emit(e, 1);
+  }
+}
