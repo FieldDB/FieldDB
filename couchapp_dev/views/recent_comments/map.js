35da5d7e6d1db483daed36753eed9c763c0d15b8
cesine <cesine@yahoo.com>
2014-02-14 22:49:53 -0500

    added a view for datum with recent comments, handy for teams to use the comments to talk to eachother fixes #1374

---
+++ couchapp_dev/views/recent_comments/map.js
@@ -0,0 +1,41 @@
+/* updated to be compatible with pre-1.38 databases */
+
+function(doc) {
+  try {
+    /* if this document has been deleted, the ignore it and return immediately */
+    if (doc.trashed && doc.trashed.indexOf("deleted") > -1) {
+      return;
+    }
+    if (doc.collection == "datums" || (doc.datumFields && doc.session)) {
+      if (!doc.comments || doc.comments.length < 1) {
+        return
+      }
+
+      var obj = {};
+      for (i = 0; i < doc.datumFields.length; i++) {
+        if (doc.datumFields[i].mask) {
+          obj[doc.datumFields[i].label] = doc.datumFields[i].mask;
+        }
+      }
+      if (doc.session.sessionFields) {
+        for (j = 0; j < doc.session.sessionFields.length; j++) {
+          if (doc.session.sessionFields[j].mask) {
+            obj[doc.session.sessionFields[j].label] = doc.session.sessionFields[j].mask;
+          }
+        }
+      }
+
+      var mostRecentComment = doc.comments[doc.comments.length - 1]
+      var lastCommentsTimestampModified = mostRecentComment.timestampModified;
+      var milisecondsSinceLastComment = Date.now() - lastCommentsTimestampModified;
+      emit(milisecondsSinceLastComment, {
+        user: mostRecentComment.username,
+        time: new Date(mostRecentComment.timestampModified).toString(),
+        comment: mostRecentComment.text,
+        datum: obj
+      });
+    }
+  } catch (e) {
+    //emit(e, 1);
+  }
+}
