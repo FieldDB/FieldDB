41789ee214f1aacda23964535009d1cdfaa24f6b
cesine <cesine@yahoo.com>
2014-03-08 13:38:46 -0500

    adding the rest of the lexicon couchapp

---
+++ couchapp_lexicon/README.md
@@ -0,0 +1,3 @@
+This is a couchapp for the Lexicon viewer and generator
+
+You can deploy this couchapp to a corpus to turn on the lexicon for that corpus. 
---
+++ couchapp_lexicon/lists/get_data_as_csv.js
@@ -0,0 +1,37 @@
+function(head, req) {
+  start({
+    'headers': {
+      'Content-Type': 'text/csv'
+    }
+  });
+
+  /*  TODO: Use AJAX to retrieve field names*/
+  var datum_fields = {'rows':[{'key':{'original':'comments','typed':'comments_t'},'value':26},{'key':{'original':'consultants','typed':'consultants_t'},'value':72},{'key':{'original':'dateElicited','typed':'dateElicited_t'},'value':144},{'key':{'original':'gloss','typed':'gloss_t'},'value':72},{'key':{'original':'goal','typed':'goal_t'},'value':72},{'key':{'original':'morphemes','typed':'morphemes_t'},'value':72},{'key':{'original':'tags','typed':'tags_t'},'value':55},{'key':{'original':'translation','typed':'translation_t'},'value':71},{'key':{'original':'utterance','typed':'utterance_t'},'value':72},{'key':{'original':'validationStatus','typed':'validationStatus_t'},'value':72}]};
+  var datumFieldsAsLuceneSchema = [];
+  var datumFields = [];
+
+  /*  Rearrange the retrieved data fields into an array, and then send the 
+  joined array as column headers.*/
+  for (var i in datum_fields.rows) {
+    datumFieldsAsLuceneSchema.push(datum_fields.rows[i].key.typed);
+    datumFields.push(datum_fields.rows[i].key.original);
+  }
+
+  send('_id,' + datumFieldsAsLuceneSchema.join(',') + '\\n');
+
+  /*  Now, process each row returned from the original map/reduce in order
+  to create data rows for the CSV file.*/
+  while (row = getRow()) {
+
+    var rowAsCsv = row.value;
+    for (var field in datumFields) {
+      rowAsCsv += ',';
+      if (row.key[datumFields[field]]) {
+        rowAsCsv += '\"' + row.key[datumFields[field]] + '\"';
+      } else {
+        rowAsCsv += '';
+      }
+    }
+    send(rowAsCsv + '\\n');
+  }
+}
