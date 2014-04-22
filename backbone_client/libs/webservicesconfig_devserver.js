6f37ff35f5ccfc104dc4cdd01dacc336c9e3a7dc
cesine <cesine@yahoo.com>
2014-04-22 20:17:58 +0400

    using speechdev as url

--- backbone_client/libs/webservicesconfig_devserver.js
+++ backbone_client/libs/webservicesconfig_devserver.js
@@ -6,7 +6,7 @@ OPrime.apptype = "production";
 
 OPrime.websiteUrl = "https://wwwdev.lingsync.org";
 OPrime.authUrl = "https://authdev.lingsync.org";
-OPrime.audioUrl = "https://audiodev.lingsync.org";
+OPrime.audioUrl = "https://speechdev.lingsync.org";
 OPrime.lexiconUrl = "https://lexicondev.lingsync.org";
 OPrime.corpusUrl = "https://corpusdev.lingsync.org";
 OPrime.activityUrl = "https://activitydev.lingsync.org";
