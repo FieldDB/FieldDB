767754d506bacbcceebcac52ad1c38409e79c96b
cesine <cesine@yahoo.com>
2014-04-23 23:37:06 +0400

    yo angular

---
+++ angular_client/modules/corpuspages/app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('corpuspagesApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
