cbdc79538daa8fbb48b018948c745440029a8918
cesine <cesine@yahoo.com>
2014-04-24 20:17:10 +0400

    added user tests and browserify

--- tests/user/UserTest.js
+++ tests/user/UserTest.js
@@ -1,17 +1,53 @@
-describe("User ", function() {
-	it("should set user's first name", function() {
-		//      var u = new User();
-		//      u.set("firstname", "Bill");
-		//      expect("Bill" == u.get("firstname")).toBeTruthy();
-		expect(true).toBeTruthy();
-	});
-	it("should have a subtitle constisting of firstname lastname ", function() {
-		//      var u = new User();
-		//      u.set("firstname", "Ed");
-		//      u.set("lastname", "LingLlama");
-		//      expect("LingLlama" == u.subtitle()).toBeTruthy();
-		expect(true).toBeTruthy();
+var User = require("../../api/user/UserMask").UserMask;
 
-	});
+
+describe("User ", function() {
+
+  it("should should have username, firstname, lastname, gravatar, email and other options", function() {
+    var json = {
+      username: "bill",
+      gravatar: "67890954367898765",
+      anodfunction: function(input){
+        console.log(input);
+      }
+    };
+    var u = new User(json);
+    expect(u.username).toEqual(json.username);
+    expect(u.firstname).toBeDefined();
+    expect(u.lastname).toBeDefined();
+    expect(u.gravatar).toEqual(json.gravatar);
+    expect(u.email).toBeDefined();
+    expect(u.name).toBeDefined();
+    expect(u.affiliation).toBeDefined();
+    expect(u.researchInterest).toBeDefined();
+    expect(u.description).toBeDefined();
+
+    expect(u.toJSON()).toEqual({
+      username: 'bill',
+      gravatar: '67890954367898765',
+      version : 'v2.0.1',
+      firstname: '',
+      lastname: '',
+      email: '',
+      affiliation: '',
+      researchInterests: '',
+      description: ''
+    });
+  });
+
+  it("should have a name constisting of firstname lastname ", function() {
+    var u = new User();
+    expect(u.name).toBeDefined();
+    u.firstname = "Bill";
+    u.lastname = "Smith";
+    expect(u.name).toEqual("Bill Smith");
+  });
+
+  it("should have an empty or valid email address", function() {
+    var u = new User({
+      email: "invalidemail@hi"
+    });
+    expect(u.email).toEqual("");
+  });
 
 });
