7d58e6b23b6ba09b1da32cc8b0a1e201686f5502
cesine <cesine@yahoo.com>
2014-04-24 10:15:05 +0400

    corpus and user directives layout roughly like before

--- angular_client/modules/corpuspages/app/scripts/controllers/main.js
+++ angular_client/modules/corpuspages/app/scripts/controllers/main.js
@@ -1,10 +1,16 @@
 'use strict';
 
 angular.module('corpuspagesApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+	.controller('MainCtrl', function($scope) {
+		$scope.awesomeThings = [
+			'HTML5 Boilerplate',
+			'AngularJS',
+			'Karma'
+		];
+		$scope.app = {
+			team: null,
+			corpus: null,
+			corpora: null
+		};
+
+	});
