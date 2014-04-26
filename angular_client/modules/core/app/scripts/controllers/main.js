d6b95c9269be6d80ad69269de8568015e304d1f7
cesine <cesine@yahoo.com>
2014-04-26 12:32:42 +0400

    yo angular fielddb

---
+++ angular_client/modules/core/app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('fielddbApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
