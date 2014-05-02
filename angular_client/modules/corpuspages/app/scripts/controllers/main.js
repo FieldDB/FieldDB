ac3ec23713fa7a87f6f139ec34109cf011db8ee9
cesine <cesine@yahoo.com>
2014-05-02 21:43:32 +0400

    glosser is loading

--- angular_client/modules/corpuspages/app/scripts/controllers/main.js
+++ angular_client/modules/corpuspages/app/scripts/controllers/main.js
@@ -1,16 +1,15 @@
 'use strict';
 
-angular.module('corpuspagesApp')
-	.controller('MainCtrl', function($scope) {
-		$scope.awesomeThings = [
-			'HTML5 Boilerplate',
-			'AngularJS',
-			'Karma'
-		];
-		$scope.app = {
-			team: null,
-			corpus: null,
-			corpora: null
-		};
-		$scope.thisyear = (new Date()).getFullYear();
-	});
+angular.module('corpuspagesApp').controller('MainCtrl', function($scope) {
+	$scope.awesomeThings = [
+		'HTML5 Boilerplate',
+		'AngularJS',
+		'Karma'
+	];
+	$scope.team = null;
+	$scope.corpus = {
+		pouchname: 'glossersample-quechua'
+	};
+	$scope.corpora = null;
+	$scope.thisyear = (new Date()).getFullYear();
+});
