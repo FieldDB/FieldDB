89f13cc25c06108a553cdb35001248f4dbee8161
cesine <cesine@yahoo.com>
2014-05-04 00:51:31 +0400

    updating corpusmask tests

--- tests/corpus/CorpusMaskTest.js
+++ tests/corpus/CorpusMaskTest.js
@@ -58,6 +58,7 @@ describe("Corpus ", function() {
 			license: {
 				title: 'Default: Creative Commons Attribution-ShareAlike (CC BY-SA).',
 				humanReadable: 'This license lets others remix, tweak, and build upon your work even for commercial purposes, as long as they credit you and license their new creations under the identical terms. This license is often compared to “copyleft” free and open source software licenses. All new works based on yours will carry the same license, so any derivatives will also allow commercial use. This is the license used by Wikipedia, and is recommended for materials that would benefit from incorporating content from Wikipedia and similarly licensed projects.',
+				imageUrl: '',
 				link: 'http://creativecommons.org/licenses/by-sa/3.0/'
 			},
 			copyright: 'Default: Add names of the copyright holders of the corpus.',
