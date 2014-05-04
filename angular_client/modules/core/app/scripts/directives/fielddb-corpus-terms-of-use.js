bda576aaf81f97bb50b5fa5455760a0dae4b2d08
cesine <cesine@yahoo.com>
2014-05-04 09:01:29 +0400

    fetching details now displays in page

--- angular_client/modules/core/app/scripts/directives/fielddb-corpus-terms-of-use.js
+++ angular_client/modules/core/app/scripts/directives/fielddb-corpus-terms-of-use.js
@@ -7,17 +7,12 @@ angular.module('fielddbAngularApp').directive('fielddbCorpusTermsOfUse', functio
     restrict: 'A',
     transclude: false,
     scope: {
-      data: '=json'
+      corpus: '=json'
     },
     // controller: function($scope, $element, $attrs, $transclude) {},
     link: function postLink(scope, element, attrs) {
       console.log(attrs);
-      for (var information in scope.data) {
-        if (scope.data.hasOwnProperty(information)) {
-          corpus[information] = scope.data[information];
-        }
-      }
-      scope.corpus = corpus;
+
       // element.text('this is the corpus directive');
     }
   };
--- angular_client/modules/core/app/scripts/directives/user.js
+++ angular_client/modules/core/app/scripts/directives/user.js
@@ -1,23 +1,15 @@
 'use strict';
 
 angular.module('fielddbAngularApp').directive('user', function() {
-  var user = {};
   return {
     templateUrl: 'views/user.html',
     restrict: 'A',
     transclude: false,
     scope: {
-      data: '=json'
+      user: '=json'
     },
     // controller: function($scope, $element, $attrs, $transclude) {},
-    link: function postLink(scope, element, attrs) {
-      for (var information in scope.data) {
-        if (scope.data.hasOwnProperty(information)) {
-          user[information] = scope.data[information];
-        }
-      }
-      scope.user = user;
-      console.log(attrs);
+    link: function postLink(scope, element) {
       // element.text('this is the user directive');
     }
   };
