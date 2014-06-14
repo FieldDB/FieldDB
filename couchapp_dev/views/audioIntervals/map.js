f50f4924e96d93501691e1d7c7ee06309c439bf8
cesine <cesine@yahoo.com>
2014-06-14 15:52:53 +0400

    added datum's igt to the audio intervals for text grid creation for #1510

--- couchapp_dev/views/audioIntervals/map.js
+++ couchapp_dev/views/audioIntervals/map.js
@@ -3,9 +3,17 @@ function(doc) {
     /* if this document has been deleted, the ignore it and return immediately */
     if (doc.trashed && doc.trashed.indexOf("deleted") > -1) return;
     if ((doc.datumFields) && (doc.session)) {
-      if(doc.audioVideo && doc.audioVideo.length > 0){
-        for(var audioVideoIndex = 0; audioVideoIndex< doc.audioVideo.length; audioVideoIndex++){
-        emit(doc.audioVideo[audioVideoIndex].filename, doc.audioVideo[audioVideoIndex]);
+
+      if (doc.audioVideo && doc.audioVideo.length > 0) {
+        var obj = {};
+        for (i = 0; i < doc.datumFields.length; i++) {
+          if (doc.datumFields[i].mask) {
+            obj[doc.datumFields[i].label] = doc.datumFields[i].mask;
+          }
+        }
+        for (var audioVideoIndex = 0; audioVideoIndex < doc.audioVideo.length; audioVideoIndex++) {
+          obj.audioVideo = doc.audioVideo[audioVideoIndex];
+          emit(doc.audioVideo[audioVideoIndex].filename, obj);
         }
       }
     }
