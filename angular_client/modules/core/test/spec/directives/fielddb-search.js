6e48e5ee676910dac999f907f72ab7ea83e9d504
cesine <cesine@yahoo.com>
2014-07-30 11:32:31 -0400

    added tests to the search directive

--- angular_client/modules/core/test/spec/directives/fielddb-search.js
+++ angular_client/modules/core/test/spec/directives/fielddb-search.js
@@ -1,20 +1,35 @@
 'use strict';
+var debug = false;
 
-xdescribe('Directive: fielddbSearch', function () {
+describe('Directive: fielddb-search', function() {
 
-  // load the directive's module
-  beforeEach(module('fielddbAngularApp'));
+  // load the directive's module and the template
+  beforeEach(module('fielddbAngularApp', 'views/search.html'));
+  var el, scope, compileFunction;
 
-  var element,
-    scope;
-
-  beforeEach(inject(function ($rootScope) {
+  beforeEach(inject(function($rootScope, $compile) {
+    el = angular.element('<div data-fielddb-search json="search"></div>');
     scope = $rootScope.$new();
+    scope.search = {};
+    compileFunction = $compile(el);
+    // bring html from templateCache
+    scope.$digest();
+    if (debug) {
+      console.log('post compile', el.html()); // <== html here has {{}}
+    }
   }));
 
-  it('should make hidden element visible', inject(function ($compile) {
-    element = angular.element('<fielddb-search></fielddb-search>');
-    element = $compile(element)(scope);
-    expect(element.text()).toBe('this is the fielddbSearch directive');
-  }));
+  // http://stackoverflow.com/questions/17223850/how-to-test-directives-that-use-templateurl-and-controllers
+  it('should make a search element with contents from scope', function() {
+
+    inject(function() {
+      compileFunction(scope); // <== the html {{}} are bound
+      scope.$digest(); // <== digest to get the render to show the bound values
+      if (debug) {
+        console.log('post link', el.html());
+        console.log('scope search ', scope.search);
+      }
+      expect(angular.element(el.find('button')[0]).text().trim()).toEqual('Search…');
+    });
+  });
 });
