553915913045bd5ff2de6314b87b7825eb115b11
cesine <cesine@yahoo.com>
2014-08-04 15:20:14 -0400

    using Firefox because Travis cant find chrome

--- scripts/build_fielddb_angular_core.sh
+++ scripts/build_fielddb_angular_core.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+CURRENTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../"
 
 cd angular_client/modules/core &&
-npm install &&
-bower install &&
-echo "Using local fielddb commonjs"
-mkdir app/bower_components/fielddb;
-ln -s ../../../fielddb.js app/bower_components/fielddb/fielddb.js;
-
+npm install || exit 1;
+bower install || exit 1;
+echo "Using local fielddb commonjs";
+ls app/bower_components/fielddb/fielddb.js || {
+  mkdir app/bower_components/fielddb;
+  ln -s $CURRENTDIR/fielddb.js $CURRENTDIR/angular_client/modules/core/app/bower_components/fielddb/fielddb.js;
+} &&
 grunt
