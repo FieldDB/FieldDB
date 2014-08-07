d312cc5dbf82ba8e1bfbf645f000a8be3f63a7e8
cesine <cesine@yahoo.com>
2014-08-07 15:42:54 -0400

    added more protection against collection of undefined items

--- api/Collection.js
+++ api/Collection.js
@@ -180,7 +180,10 @@ Collection.prototype = Object.create(Object.prototype, {
       }
       for (var index in value) {
         /* parse internal models as a model if specified */
-        if (this.INTERNAL_MODELS && this.INTERNAL_MODELS.item && value[index].constructor !== this.INTERNAL_MODELS.item) {
+        if (!value[index]) {
+          this.warn(index + " is undefined on this member of the collection", value);
+        }
+        if (this.INTERNAL_MODELS && this.INTERNAL_MODELS.item && value[index] && value[index].constructor !== this.INTERNAL_MODELS.item) {
           value[index] = new this.INTERNAL_MODELS.item(value[index]);
         }
         this.add(value[index]);
