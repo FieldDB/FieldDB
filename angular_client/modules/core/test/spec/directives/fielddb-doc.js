f65bf5f38ad70ebf9349d3bbbe31dd2684249554
cesine <cesine@yahoo.com>
2014-08-08 11:54:40 -0400

    for #1415 generic doc directive that will detect the type of doc and use the correct directive for that document
    (currently assuming its a user)'

--- angular_client/modules/core/test/spec/directives/fielddb-doc.js
+++ angular_client/modules/core/test/spec/directives/fielddb-doc.js
@@ -1,20 +1,46 @@
 'use strict';
+var debug = false;
 
-describe('Directive: fielddbDoc', function () {
+describe('Directive: fielddb-doc', function() {
 
-  // load the directive's module
-  beforeEach(module('fielddbAngularApp'));
+  // load the directive's module and the template
+  beforeEach(module('fielddbAngularApp',  'views/user.html'));
+  var el, scope, compileFunction;
 
-  var element,
-    scope;
-
-  beforeEach(inject(function ($rootScope) {
+  beforeEach(inject(function($rootScope, $compile) {
+    el = angular.element('<div data-fielddb-doc json="team"></div> <div data-fielddb-doc json="user"></div>');
     scope = $rootScope.$new();
+    scope.user = {
+      firstname: 'Ling',
+      lastname: 'Llama',
+      description: 'I like memes.'
+    };
+    scope.team = {
+      firstname: 'Awesome',
+      lastname: 'Phonologists',
+      description: 'We love phonology.'
+    };
+    compileFunction = $compile(el);
+    // bring html from templateCache
+    scope.$digest();
+    if (debug) {
+      console.log('post compile', el.html()); // <== html here has {{}}
+    }
   }));
 
-  it('should make hidden element visible', inject(function ($compile) {
-    element = angular.element('<fielddb-doc></fielddb-doc>');
-    element = $compile(element)(scope);
-    expect(element.text()).toBe('this is the fielddbDoc directive');
-  }));
+  // http://stackoverflow.com/questions/17223850/how-to-test-directives-that-use-templateurl-and-controllers
+  it('should make a doc element with contents from scope', function() {
+
+    inject(function() {
+      compileFunction(scope); // <== the html {{}} are bound
+      scope.$digest(); // <== digest to get the render to show the bound values
+      if (debug) {
+        console.log('post link', el.html());
+        console.log('scope team ', scope.team);
+        console.log('scope user ', scope.user);
+      }
+      expect(angular.element(el.find('h1')[0]).text().trim()).toEqual('Awesome Phonologists');
+      expect(angular.element(el.find('h1')[1]).text().trim()).toEqual('Ling Llama');
+    });
+  });
 });
