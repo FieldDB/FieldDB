273ff174c7ca6ed37c52c1fb09e3de9fd4e96b2d
cesine <cesine@yahoo.com>
2014-08-08 10:52:34 -0400

    for #1576 adding tests to mock download of participants and ensure db request is sent

--- angular_client/modules/core/app/scripts/directives/fielddb-participants.js
+++ angular_client/modules/core/app/scripts/directives/fielddb-participants.js
@@ -9,56 +9,73 @@
 angular.module('fielddbAngularApp').directive('fielddbParticipants', function() {
 
   var db;
-  var controller = function($scope) {
+  var fetchParticipantsExponentialDecay = 2000;
 
-    var fetchParticipants = function() {
-      if (!FieldDB.BASE_DB_URL || !$scope.authentication ) {
+  var controller = function($scope, $timeout) {
+    var fetchParticipantsIfEmpty = function() {
+
+      if (!FieldDB.BASE_DB_URL || !$scope.corpus) {
+        fetchParticipantsExponentialDecay = fetchParticipantsExponentialDecay * 2;
+        $timeout(function() {
+          if ($scope.participants && $scope.participants.length > 0) {
+            return;
+          } else {
+            fetchParticipantsIfEmpty();
+          }
+        }, fetchParticipantsExponentialDecay);
+        console.log(' No URL specified, Waiting another ' + fetchParticipantsExponentialDecay + ' until trying to fetch participants again.');
+
         return;
       }
+
       db = db || new FieldDB.PsycholinguisticsDatabase({
-        // username: $scope.authentication.user.username,
         dbname: $scope.corpus.dbname,
         url: FieldDB.BASE_DB_URL,
         authUrl: FieldDB.BASE_AUTH_URL
       });
-      console.log('fetching docs for ', db.toJSON());
+      // console.log('fetching docs for ', db.toJSON());
       db.fetchCollection('speakers').then(function(results) {
-        console.log('downloaded ', results);
 
+        console.log('downloaded participants', results);
         $scope.participants = $scope.participants || [];
         results.map(function(row) {
           $scope.participants.push(row);
         });
-
         $scope.$digest();
 
       }, function(reason) {
-        console.log('No docs...', reason);
+
+        console.log('No participants docs...', reason);
+        fetchParticipantsExponentialDecay = fetchParticipantsExponentialDecay * 2;
+        $scope.corpus.fetchParticipantsExponentialDecay = fetchParticipantsExponentialDecay;
+        console.log(' No connetion, Waiting another ' + fetchParticipantsExponentialDecay + ' until trying to fetch participants again.');
+        $scope.$digest();
+
+        $timeout(function() {
+          if ($scope.participants && $scope.participants.length > 0) {
+            return;
+          } else {
+            fetchParticipantsIfEmpty();
+          }
+        }, fetchParticipantsExponentialDecay);
+
       });
 
     };
 
-    // $scope.$watch('authentication.user', function(newValue) {
-    //   if (newValue) {
-    //     console.log('user is authenticated.');
-    //     fetchParticipants();
-    //   }
-    // });
-
-    window.setTimeout(function() {
-      fetchParticipants();
-    }, 2000);
+    fetchParticipantsIfEmpty();
 
   };
-  controller.$inject = ['$scope'];
+  controller.$inject = ['$scope', '$timeout'];
 
   var directiveDefinitionObject = {
     templateUrl: 'views/participants.html', // or // function(tElement, tAttrs) { ... },
     restrict: 'A',
     transclude: false,
-    // scope: {
-    //   participants: '=json'
-    // },
+    scope: {
+      participants: '=json',
+      corpus: '=corpus'
+    },
     controller: controller,
     link: function postLink() {},
     priority: 0,
