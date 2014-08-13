e5719b87dac78af4931e8dd32d49a772f8c13480
cesine <cesine@yahoo.com>
2014-08-13 11:50:31 -0400

    private corpuses map reduce was not checked in (gitignore all files containing private)

---
+++ couchapp_dev/views/private_corpuses/map.js
@@ -0,0 +1,13 @@
+/* updated to be compatible with pre-1.38 databases */
+function(doc) {
+  try {
+    /* if this document has been deleted, the ignore it and return immediately */
+    if (doc.trashed && doc.trashed.indexOf("deleted") > -1) return;
+
+    if (doc.collection == "private_corpuses" || (doc.confidential && doc.confidential.secretkey)) {
+      emit(doc.timestamp, doc);
+    }
+  } catch (e) {
+    //emit(e, 1);
+  }
+};
