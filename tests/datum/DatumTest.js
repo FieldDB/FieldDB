5444c5fa16ed7a989b6b42c957459eb29208fadb
cesine <cesine@yahoo.com>
2014-08-14 18:31:18 -0400

    commonjs ification of datum for #20  #1415 #1437 #228 #1154 #1121 #1234 #1308 #296 #1591 #1258 #920 #1319 #1375 #1303

--- tests/datum/DatumTest.js
+++ tests/datum/DatumTest.js
@@ -1,19 +1,39 @@
+var Datum = require('./../../api/datum/Datum').Datum;
+
 describe("Test Datum", function() {
-	it("should set the utterance", function() {
-		expect(true).toBeTruthy();
-	});
+  it("should load", function() {
+    expect(Datum).toBeDefined();
 
+    var datum = new Datum();
+    expect(datum).toBeDefined();
+  });
 
-	it("should have a gloss line ", function() {
-		expect(true).toBeTruthy();
-	});
+  it("should set the utterance", function() {
+    var datum = new Datum();
+    datum.datumFields.utterance = "Noqata";
+    expect(datum.datumFields.utterance).toEqual("Noqata");
+  });
 
-	it("should have a translation line ", function() {
-		expect(true).toBeTruthy();
-	});
+  it("should have a gloss line ", function() {
+    var datum = new Datum();
+    datum.datumFields.gloss = "I-ACC";
+    expect(datum.datumFields.gloss).toEqual("I-ACC");
+  });
 
-	it("should have grammatical tags", function() {
-		expect(true).toBeTruthy();
-	});
+  it("should have a translation line ", function() {
+    var datum = new Datum();
+    datum.datumFields.translation = "I";
+    expect(datum.datumFields.translation).toEqual("I");
+  });
+
+  it("should have grammatical tags", function() {
+    var datum = new Datum();
+    datum.datumFields.judgement = "*";
+    expect(datum.datumFields.judgement).toEqual("*");
+
+    var datum = new Datum();
+    datum.datumFields.tags = "Causative";
+    expect(datum.datumFields.tags).toEqual("Causative");
+  });
 
 });
