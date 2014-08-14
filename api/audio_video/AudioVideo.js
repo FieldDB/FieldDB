f0822588cd7b9e6bf098a88be05d3755a438f20f
cesine <cesine@yahoo.com>
2014-08-14 19:39:56 -0400

    common jsifcaiton of audio video for #1467 #630 #1485 #1466 #1282 #1461 #629 #949 #1174 #1483 #1464 #1510 #1228 #737 #1220 #945 #486 #395 #944 #484 #1025 #372 #1038 #950 #1073 #208 #1035 #121 #1034 #543 #542 #544 #126 #125

--- api/audio_video/AudioVideo.js
+++ api/audio_video/AudioVideo.js
@@ -1,42 +1,70 @@
-define([ 
-    "backbone" 
-], function(
-    Backbone
-) {
-  var AudioVideo = Backbone.Model.extend(
-  /** @lends AudioVideo.prototype */
-  {
-    /**
-     * @class AudioVideo models allows a user to add audio and video files.
-     * 
-     * @description Initialize function
-     * 
-     * @extends Backbone.Model
-     * 
-     * @constructs
-     */
-    initialize : function() {
-    },
-    
-    defaults : {
-      URL : "",
-//      filename : "",
-      type: "audio", //or video,
-      // startTime: 0,
-      // endTime: 0
-    },
-    
-    // Internal models: used by the parse function
-    internalModels : {
-      // There are no nested models
-    },
-    saveAndInterConnectInApp : function(callback){
-      
-      if(typeof callback == "function"){
-        callback();
-      }
-    }
-  });
+/* globals FieldDB */
+var FieldDBObject = require('./../FieldDBObject').FieldDBObject;
 
-  return AudioVideo;
+/**
+ * @class The AudioVideo is a type of FieldDBObject with any additional fields or
+ * metadata that a team might use to ground/tag their primary data.
+ *
+ *
+ * @name  AudioVideo
+ * @extends FieldDBObject
+ * @constructs
+ */
+var AudioVideo = function AudioVideo(options) {
+  this.debug("Constructing AudioVideo length: ", options);
+  FieldDBObject.apply(this, arguments);
+};
+
+AudioVideo.prototype = Object.create(FieldDBObject.prototype, /** @lends AudioVideo.prototype */ {
+  constructor: {
+    value: AudioVideo
+  },
+
+  api: {
+    value: "speech"
+  },
+
+  id: {
+    get: function() {
+      return this._URL || FieldDBObject.DEFAULT_STRING;
+    },
+    set: function(value) {
+      if (value === this._URL) {
+        return;
+      }
+      if (!value) {
+        delete this._URL;
+        return;
+      }
+      if (value.trim) {
+        value = value.trim();
+      }
+      this._URL = value;
+    }
+  },
+
+  URL: {
+    get: function() {
+      if (!this._URL && this.filename) {
+        var baseurl = this.url ? this.url : FieldDB.BASE_SPEECH_URL;
+        return baseurl + '/' + this.dbname + '/' + this.filename;
+      }
+      return this._URL || FieldDBObject.DEFAULT_STRING;
+    },
+    set: function(value) {
+      if (value === this._URL) {
+        return;
+      }
+      if (!value) {
+        delete this._URL;
+        return;
+      }
+      if (value.trim) {
+        value = value.trim();
+      }
+      this._URL = value;
+    }
+  }
+
 });
+exports.AudioVideo = AudioVideo;
