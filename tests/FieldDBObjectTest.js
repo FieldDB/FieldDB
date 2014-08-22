90d231b7bdb8902be96e3ba7036ae9c9c29bcb81
cesine <cesine@yahoo.com>
2014-08-22 17:23:14 -0400

    distinguish between returned object and the first object
    so we can merge two objects into a third without affecting the originals, also adding a confirm method on objects so that programmers can override the method dependign on their userinterfaces technologies. for #124 #110 #818 #793

--- tests/FieldDBObjectTest.js
+++ tests/FieldDBObjectTest.js
@@ -192,9 +192,11 @@ describe("FieldDBObject", function() {
 });
 
 describe("merging", function() {
+  var aBaseObject;
+  var atriviallyDifferentObject;
 
-  it("should be able to merge one object if we want", function() {
-    var aBaseObject = new FieldDBObject({
+  beforeEach(function() {
+    aBaseObject = new FieldDBObject({
       externalString: "easy model",
       externalEqualString: "merging",
       externalArray: ["four"],
@@ -211,7 +213,7 @@ describe("merging", function() {
     });
     expect(aBaseObject.type).toEqual("FieldDBObject");
 
-    var atriviallyDifferentObject = new FieldDBObject({
+    atriviallyDifferentObject = new FieldDBObject({
       externalString: "trivial model",
       externalEqualString: "merging",
       externalArray: ["one", "two", "three"],
@@ -226,29 +228,108 @@ describe("merging", function() {
       }),
       // debugMode: true
     });
+  });
+
+  it("should be able to merge one object into another without affecting the second object", function() {
     expect(atriviallyDifferentObject.type).toEqual("FieldDBObject");
     expect(aBaseObject.externalString).toEqual("easy model");
 
     var resultObject = aBaseObject.merge("self", atriviallyDifferentObject, "overwrite");
-    expect(resultObject).toEqual(aBaseObject);
-    expect(resultObject.externalString).toEqual("trivial model");
-    expect(resultObject.externalEqualString).toEqual("merging");
-    expect(resultObject.externalArray).toEqual(['four', 'one', 'two', 'three']);
-    expect(resultObject.warnMessage).toContain("Overwriting contents of externalString (this may cause disconnection in listeners)");
+    expect(resultObject).toBe(aBaseObject);
+    expect(aBaseObject.externalString).toEqual("trivial model");
+    expect(aBaseObject.externalEqualString).toEqual("merging");
+    expect(aBaseObject.externalArray).toEqual(['four', 'one', 'two', 'three']);
+    expect(aBaseObject.warnMessage).toContain("Overwriting contents of externalString (this may cause disconnection in listeners)");
 
-    expect(resultObject.externalObject.internalString).toEqual("internal overwrite");
-    expect(resultObject.externalObject.internalTrue).toEqual(true);
-    expect(resultObject.externalObject.internalEmptyString).toEqual("");
-    expect(resultObject.externalObject.internalFalse).toEqual(false);
-    expect(resultObject.externalObject.internalNumber).toEqual(2);
-    expect(resultObject.externalObject.missingInTarget).toEqual("i'm a old property");
-    expect(resultObject.externalObject.missingInOriginal).toEqual("i'm a new property");
-    expect(resultObject.externalObject.warnMessage).toContain("Overwriting contents of internalString (this may cause disconnection in listeners)");
+    expect(aBaseObject.externalObject.internalString).toEqual("internal overwrite");
+    expect(aBaseObject.externalObject.internalTrue).toEqual(true);
+    expect(aBaseObject.externalObject.internalEmptyString).toEqual("");
+    expect(aBaseObject.externalObject.internalFalse).toEqual(false);
+    expect(aBaseObject.externalObject.internalNumber).toEqual(2);
+    expect(aBaseObject.externalObject.missingInTarget).toEqual("i'm a old property");
+    expect(aBaseObject.externalObject.missingInOriginal).toEqual("i'm a new property");
+    expect(aBaseObject.externalObject.warnMessage).toContain("Overwriting contents of internalString (this may cause disconnection in listeners)");
+
+    // Make sure atriviallyDifferentObject is as it was
+    expect(atriviallyDifferentObject).not.toEqual(aBaseObject);
+    expect(atriviallyDifferentObject.externalString).toEqual("trivial model");
+    expect(atriviallyDifferentObject.externalEqualString).toEqual("merging");
+    expect(atriviallyDifferentObject.externalArray).toEqual(['one', 'two', 'three']);
+    expect(atriviallyDifferentObject.warnMessage).toBeUndefined();
+
+    expect(atriviallyDifferentObject.externalObject.internalString).toEqual("internal overwrite");
+    expect(atriviallyDifferentObject.externalObject.internalTrue).toEqual(true);
+    expect(atriviallyDifferentObject.externalObject.internalEmptyString).toEqual("");
+    expect(atriviallyDifferentObject.externalObject.internalFalse).toEqual(false);
+    expect(atriviallyDifferentObject.externalObject.internalNumber).toEqual(2);
+    expect(atriviallyDifferentObject.externalObject.missingInTarget).toBeUndefined("i'm a old property");
+    expect(atriviallyDifferentObject.externalObject.missingInOriginal).toEqual("i'm a new property");
+    expect(atriviallyDifferentObject.externalObject.warnMessage).toBeUndefined();
 
   });
 
+  it("should be able to merge two objects into a third", function() {
+    expect(atriviallyDifferentObject.type).toEqual("FieldDBObject");
+    expect(aBaseObject.externalString).toEqual("easy model");
+
+    var aThirdObject = new FieldDBObject();
+    aThirdObject.merge(aBaseObject, atriviallyDifferentObject, "overwrite");
+    expect(aThirdObject).not.toBe(aBaseObject);
+    expect(aThirdObject.externalString).toEqual("trivial model");
+    expect(aThirdObject.externalEqualString).toEqual("merging");
+    expect(aThirdObject.externalArray).toEqual(['four', 'one', 'two', 'three']);
+    expect(aThirdObject.warnMessage).toContain("Overwriting contents of externalString (this may cause disconnection in listeners)");
+
+    expect(aThirdObject.externalObject.internalString).toEqual("internal overwrite");
+    expect(aThirdObject.externalObject.internalTrue).toEqual(true);
+    expect(aThirdObject.externalObject.internalEmptyString).toEqual("");
+    expect(aThirdObject.externalObject.internalFalse).toEqual(false);
+    expect(aThirdObject.externalObject.internalNumber).toEqual(2);
+    expect(aThirdObject.externalObject.missingInTarget).toEqual("i'm a old property");
+    expect(aThirdObject.externalObject.missingInOriginal).toEqual("i'm a new property");
+    expect(aThirdObject.externalObject.warnMessage).toContain("Overwriting contents of internalString (this may cause disconnection in listeners)");
+
+    // Make sure aBaseObject is as it was
+    expect(aBaseObject.externalString).toEqual("easy model");
+    expect(aBaseObject.externalEqualString).toEqual("merging");
+    expect(aBaseObject.externalArray).toEqual(['four']);
+    expect(aBaseObject.warnMessage).toBeUndefined();
+
+    expect(aBaseObject.externalObject.internalString).toEqual("internal");
+    expect(aBaseObject.externalObject.internalTrue).toEqual(true);
+    expect(aBaseObject.externalObject.internalEmptyString).toEqual("");
+    expect(aBaseObject.externalObject.internalFalse).toEqual(false);
+    expect(aBaseObject.externalObject.internalNumber).toEqual(2);
+    expect(aBaseObject.externalObject.missingInTarget).toEqual("i'm a old property");
+    expect(aBaseObject.externalObject.missingInOriginal).toBeUndefined();
+    expect(aBaseObject.externalObject.warnMessage).toBeUndefined();
+
+
+    // Make sure atriviallyDifferentObject is as it was
+    expect(atriviallyDifferentObject).not.toBe(aBaseObject);
+    expect(atriviallyDifferentObject.externalString).toEqual("trivial model");
+    expect(atriviallyDifferentObject.externalEqualString).toEqual("merging");
+    expect(atriviallyDifferentObject.externalArray).toEqual(['one', 'two', 'three']);
+    expect(atriviallyDifferentObject.warnMessage).toBeUndefined();
+
+    expect(atriviallyDifferentObject.externalObject.internalString).toEqual("internal overwrite");
+    expect(atriviallyDifferentObject.externalObject.internalTrue).toEqual(true);
+    expect(atriviallyDifferentObject.externalObject.internalEmptyString).toEqual("");
+    expect(atriviallyDifferentObject.externalObject.internalFalse).toEqual(false);
+    expect(atriviallyDifferentObject.externalObject.internalNumber).toEqual(2);
+    expect(atriviallyDifferentObject.externalObject.missingInTarget).toBeUndefined("i'm a old property");
+    expect(atriviallyDifferentObject.externalObject.missingInOriginal).toEqual("i'm a new property");
+    expect(atriviallyDifferentObject.externalObject.warnMessage).toBeUndefined();
+  });
+
+  it("should be able to ask the user what to do if overwrite is not specified", function() {
+    aBaseObject.merge("self", atriviallyDifferentObject);
+    expect(aBaseObject.confirmMessage).toContain("I found a conflict for externalString, Do you want to overwrite it from \"easy model\" -> \"trivial model\"");
+    expect(aBaseObject.externalObject.confirmMessage).toContain("I found a conflict for internalString, Do you want to overwrite it from \"internal\" -> \"internal overwrite\"");
+  });
+
   it("should reject to merge items with different ids", function() {
-    var aBaseObject = new FieldDBObject({
+    var aBaseObjectWithDifferentId = new FieldDBObject({
       externalString: "old string",
       externalObject: new FieldDBObject({
         _id: "aw1we24",
@@ -256,7 +337,7 @@ describe("merging", function() {
       }),
       // debugMode: true
     });
-    expect(aBaseObject.type).toEqual("FieldDBObject");
+    expect(aBaseObjectWithDifferentId.type).toEqual("FieldDBObject");
 
     var atriviallyDifferentObjectWithADifferentInternalObject = new FieldDBObject({
       externalString: "new string",
@@ -268,9 +349,9 @@ describe("merging", function() {
       // debugMode: true
     });
     expect(atriviallyDifferentObjectWithADifferentInternalObject.type).toEqual("FieldDBObject");
-    expect(aBaseObject.externalString).toEqual("old string");
+    expect(aBaseObjectWithDifferentId.externalString).toEqual("old string");
 
-    var resultObject = aBaseObject.merge("self", atriviallyDifferentObjectWithADifferentInternalObject, "overwrite");
+    var resultObject = aBaseObjectWithDifferentId.merge("self", atriviallyDifferentObjectWithADifferentInternalObject, "overwrite");
     expect(resultObject.externalString).toEqual("new string");
 
     expect(resultObject.externalObject._id).toEqual("aw1we24");
