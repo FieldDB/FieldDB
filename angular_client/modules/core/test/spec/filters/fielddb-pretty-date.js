4f90bb4d98a175d8050e4782f8a9b8311d56589d
cesine <cesine@yahoo.com>
2014-08-27 17:17:36 -0400

    adding tests for various date formats in the app to all be formatted using the pretty date format

--- angular_client/modules/core/test/spec/filters/fielddb-pretty-date.js
+++ angular_client/modules/core/test/spec/filters/fielddb-pretty-date.js
@@ -1,19 +1,39 @@
 'use strict';
 
-describe('Filter: fielddbPrettyDate', function () {
+describe('Filter: fielddbPrettyDate', function() {
 
   // load the filter's module
   beforeEach(module('fielddbAngularApp'));
 
   // initialize a new instance of the filter before each test
   var fielddbPrettyDate;
-  beforeEach(inject(function ($filter) {
+  beforeEach(inject(function($filter) {
     fielddbPrettyDate = $filter('fielddbPrettyDate');
   }));
 
-  it('should return the input prefixed with "fielddbPrettyDate filter:"', function () {
-    var text = 'angularjs';
-    expect(fielddbPrettyDate(text)).toBe('fielddbPrettyDate filter: ' + text);
+  it('should convert json dates with escaped quotes into YYYY-MM-DD HH:SS', function() {
+    var inputDate = '\"2014-07-14T14:39:10.916Z\"';
+    expect(fielddbPrettyDate(inputDate)).toBe('14/07/2014 10:39');
+  });
+
+  it('should convert json dates with quotes into YYYY-MM-DD HH:SS', function() {
+    var inputDate = '"2014-07-14T14:39:10.916Z"';
+    expect(fielddbPrettyDate(inputDate)).toBe('14/07/2014 10:39');
+  });
+
+  it('should convert json dates into YYYY-MM-DD HH:SS', function() {
+    var inputDate = '2014-07-14T14:39:10.916Z';
+    expect(fielddbPrettyDate(inputDate)).toBe('14/07/2014 10:39');
+  });
+
+  it('should convert timestamps into YYYY-MM-DD HH:SS', function() {
+    var inputDate = 1405348750916;
+    expect(fielddbPrettyDate(inputDate)).toBe('14/07/2014 10:39');
+  });
+
+  it('should convert dates into YYYY-MM-DD HH:SS', function() {
+    var inputDate = new Date(1405348750916);
+    expect(fielddbPrettyDate(inputDate)).toBe('14/07/2014 10:39');
   });
 
 });
