e9c23d060ffe330dfca3813a87e3a9954eaa2c61
cesine <cesine@yahoo.com>
2014-08-27 17:50:48 -0400

    the default locale for canada is DD-MM-YYYY although this will probably break the tests on Travis

--- angular_client/modules/core/test/spec/filters/fielddb-pretty-date.js
+++ angular_client/modules/core/test/spec/filters/fielddb-pretty-date.js
@@ -11,27 +11,27 @@ describe('Filter: fielddbPrettyDate', function() {
     fielddbPrettyDate = $filter('fielddbPrettyDate');
   }));
 
-  it('should convert json dates with escaped quotes into YYYY-MM-DD HH:SS', function() {
+  it('should convert json dates with escaped quotes into DD-MM-YYYY HH:SS', function() {
     var inputDate = '\"2014-07-14T14:39:10.916Z\"';
     expect(fielddbPrettyDate(inputDate)).toBe('14/07/2014 10:39');
   });
 
-  it('should convert json dates with quotes into YYYY-MM-DD HH:SS', function() {
+  it('should convert json dates with quotes into DD-MM-YYYY HH:SS', function() {
     var inputDate = '"2014-07-14T14:39:10.916Z"';
     expect(fielddbPrettyDate(inputDate)).toBe('14/07/2014 10:39');
   });
 
-  it('should convert json dates into YYYY-MM-DD HH:SS', function() {
+  it('should convert json dates into DD-MM-YYYY HH:SS', function() {
     var inputDate = '2014-07-14T14:39:10.916Z';
     expect(fielddbPrettyDate(inputDate)).toBe('14/07/2014 10:39');
   });
 
-  it('should convert timestamps into YYYY-MM-DD HH:SS', function() {
+  it('should convert timestamps into DD-MM-YYYY HH:SS', function() {
     var inputDate = 1405348750916;
     expect(fielddbPrettyDate(inputDate)).toBe('14/07/2014 10:39');
   });
 
-  it('should convert dates into YYYY-MM-DD HH:SS', function() {
+  it('should convert dates into DD-MM-YYYY HH:SS', function() {
     var inputDate = new Date(1405348750916);
     expect(fielddbPrettyDate(inputDate)).toBe('14/07/2014 10:39');
   });
