8e44da0e71ca2a5f92c0525d8b65dbf014c83a69
cesine <cesine@yahoo.com>
2014-08-27 18:19:46 -0400

    adding more tests for edge cases

--- angular_client/modules/core/test/spec/filters/fielddb-pretty-date.js
+++ angular_client/modules/core/test/spec/filters/fielddb-pretty-date.js
@@ -36,4 +36,35 @@ describe('Filter: fielddbPrettyDate', function() {
     expect(fielddbPrettyDate(inputDate)).toBe('14/07/2014 10:39');
   });
 
+  describe('edge cases', function() {
+
+    it('should show N/A if the date is 2000-09-06T16:31:30.988Z', function() {
+      var inputDate = new Date('2000-09-06T16:31:30.988Z');
+      expect(fielddbPrettyDate(inputDate)).toBe('N/A');
+
+      inputDate = '2000-09-06T16:31:30.988Z';
+      expect(fielddbPrettyDate(inputDate)).toBe('N/A');
+
+      inputDate = '"2000-09-06T16:31:30.988Z"';
+      expect(fielddbPrettyDate(inputDate)).toBe('N/A');
+    });
+
+    it('should show -- if the date is empty', function() {
+      var inputDate = '';
+      // expect(fielddbPrettyDate(inputDate)).toBe('--');
+
+      inputDate = undefined;
+      expect(fielddbPrettyDate(inputDate)).toBe('--');
+
+      inputDate = 0;
+      expect(fielddbPrettyDate(inputDate)).toBe('--');
+
+      inputDate = null;
+      expect(fielddbPrettyDate(inputDate)).toBe('--');
+
+      inputDate = '  ';
+      expect(fielddbPrettyDate(inputDate)).toBe('--');
+    });
+
+  });
 });
