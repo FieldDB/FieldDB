7dfce978a0e2ddbb5e40ec6b7301d257a12048d3
cesine <cesine@yahoo.com>
2014-08-27 17:49:21 -0400

    bringing over shortDate filter from spreadsheet app
    from https://github.com/OpenSourceFieldlinguistics/FieldDB/commit/41a987bdcc687096a620b72f270a2fe59bf94665
    
    although it looks like it had - instead of / from the beginning but not sure why since the standard date has / in it

--- angular_client/modules/core/app/scripts/filters/fielddb-short-date.js
+++ angular_client/modules/core/app/scripts/filters/fielddb-short-date.js
@@ -8,9 +8,27 @@
  * # fielddbShortDate
  * Filter in the fielddbAngularApp.
  */
-angular.module('fielddbAngularApp')
-  .filter('fielddbShortDate', function () {
-    return function (input) {
-      return 'fielddbShortDate filter: ' + input;
+angular.module('fielddbAngularApp').filter('fielddbShortDate', function() {
+    return function(input) {
+      if (!input) {
+        return "--";
+      }
+      // For unknown historical reasons in the spreadsheet app
+      // there were some dates that were unknown and were set
+      // to a random date like this:
+      if (input == "2000-09-06T16:31:30.988Z") {
+        return "N/A";
+      }
+      if (input.replace) {
+        input = input.replace(/\"/g, "");
+      }
+      if (!input.toLocaleDateString) {
+        input = new Date(input);
+      }
+      var minutes = input.getMinutes();
+      if (minutes < 10) {
+        minutes = "0" + minutes;
+      }
+      return input.toLocaleDateString();
     };
   });
