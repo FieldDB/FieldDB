c545a9fddfab05986d0a55eeee9c7d2679cf75fd
cesine <cesine@yahoo.com>
2014-08-27 18:07:02 -0400

    jshint

--- angular_client/modules/core/app/scripts/filters/fielddb-pretty-date.js
+++ angular_client/modules/core/app/scripts/filters/fielddb-pretty-date.js
@@ -11,24 +11,24 @@
 angular.module('fielddbAngularApp').filter('fielddbPrettyDate', function() {
   return function(input) {
     if (!input) {
-      return "--";
+      return '--';
     }
     // For unknown historical reasons in the spreadsheet app
     // there were some dates that were unknown and were set
     // to a random date like this:
-    if (input == "2000-09-06T16:31:30.988Z") {
-      return "N/A";
+    if (input == '2000-09-06T16:31:30.988Z') {
+      return 'N/A';
     }
     if (input.replace) {
-      input = input.replace(/\"/g, "");
+      input = input.replace(/\"/g, '');
     }
     if (!input.toLocaleDateString) {
       input = new Date(input);
     }
     var minutes = input.getMinutes();
     if (minutes < 10) {
-      minutes = "0" + minutes;
+      minutes = '0' + minutes;
     }
-    return input.toLocaleDateString() + " " + input.getHours() + ":" + minutes;
+    return input.toLocaleDateString() + ' ' + input.getHours() + ':' + minutes;
   };
 });
