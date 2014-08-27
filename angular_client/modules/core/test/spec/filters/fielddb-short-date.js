2a9acb42c34b733e33a064359e3f86c33e4263a7
cesine <cesine@yahoo.com>
2014-08-27 17:49:21 -0400

    bringing over shortDate filter from spreadsheet app
    from https://github.com/OpenSourceFieldlinguistics/FieldDB/commit/41a987bdcc687096a620b72f270a2fe59bf94665
    
    although it looks like it had - instead of / from the beginning but not sure why since the standard date has / in it

--- angular_client/modules/core/test/spec/filters/fielddb-short-date.js
+++ angular_client/modules/core/test/spec/filters/fielddb-short-date.js
@@ -11,9 +11,30 @@ describe('Filter: fielddbShortDate', function () {
     fielddbShortDate = $filter('fielddbShortDate');
   }));
 
-  it('should return the input prefixed with "fielddbShortDate filter:"', function () {
-    var text = 'angularjs';
-    expect(fielddbShortDate(text)).toBe('fielddbShortDate filter: ' + text);
+
+  it('should convert json dates with escaped quotes into DD-MM-YYYY HH:SS', function() {
+    var inputDate = '\"2014-07-14T14:39:10.916Z\"';
+    expect(fielddbShortDate(inputDate)).toBe('14/07/2014');
+  });
+
+  it('should convert json dates with quotes into DD-MM-YYYY HH:SS', function() {
+    var inputDate = '"2014-07-14T14:39:10.916Z"';
+    expect(fielddbShortDate(inputDate)).toBe('14/07/2014');
+  });
+
+  it('should convert json dates into DD-MM-YYYY HH:SS', function() {
+    var inputDate = '2014-07-14T14:39:10.916Z';
+    expect(fielddbShortDate(inputDate)).toBe('14/07/2014');
+  });
+
+  it('should convert timestamps into DD-MM-YYYY HH:SS', function() {
+    var inputDate = 1405348750916;
+    expect(fielddbShortDate(inputDate)).toBe('14/07/2014');
+  });
+
+  it('should convert dates into DD-MM-YYYY HH:SS', function() {
+    var inputDate = new Date(1405348750916);
+    expect(fielddbShortDate(inputDate)).toBe('14/07/2014');
   });
 
 });
