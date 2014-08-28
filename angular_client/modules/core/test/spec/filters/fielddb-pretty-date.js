5283ffe56453cdf0bface5cc9adc1925941ca3ef
cesine <cesine@yahoo.com>
2014-08-27 23:30:46 -0400

    creating an expected date using the function in the filters so that the timezone of the test runner will match

--- angular_client/modules/core/test/spec/filters/fielddb-pretty-date.js
+++ angular_client/modules/core/test/spec/filters/fielddb-pretty-date.js
@@ -7,33 +7,36 @@ describe('Filter: fielddbPrettyDate', function() {
 
   // initialize a new instance of the filter before each test
   var fielddbPrettyDate;
+  var expectedDate = new Date('2014-07-14T14:39:10.916Z');
+  var expectedFormattedDate = expectedDate.toLocaleDateString() + ' ' + expectedDate.getHours() + ':' + expectedDate.getMinutes();
+
   beforeEach(inject(function($filter) {
     fielddbPrettyDate = $filter('fielddbPrettyDate');
   }));
 
   it('should convert json dates with escaped quotes into DD-MM-YYYY HH:SS', function() {
     var inputDate = '\"2014-07-14T14:39:10.916Z\"';
-    expect(fielddbPrettyDate(inputDate)).toBe('07/14/2014 10:39');
+    expect(fielddbPrettyDate(inputDate)).toBe(expectedFormattedDate);
   });
 
   it('should convert json dates with quotes into DD-MM-YYYY HH:SS', function() {
     var inputDate = '"2014-07-14T14:39:10.916Z"';
-    expect(fielddbPrettyDate(inputDate)).toBe('07/14/2014 10:39');
+    expect(fielddbPrettyDate(inputDate)).toBe(expectedFormattedDate);
   });
 
   it('should convert json dates into DD-MM-YYYY HH:SS', function() {
     var inputDate = '2014-07-14T14:39:10.916Z';
-    expect(fielddbPrettyDate(inputDate)).toBe('07/14/2014 10:39');
+    expect(fielddbPrettyDate(inputDate)).toBe(expectedFormattedDate);
   });
 
   it('should convert timestamps into DD-MM-YYYY HH:SS', function() {
     var inputDate = 1405348750916;
-    expect(fielddbPrettyDate(inputDate)).toBe('07/14/2014 10:39');
+    expect(fielddbPrettyDate(inputDate)).toBe(expectedFormattedDate);
   });
 
   it('should convert dates into DD-MM-YYYY HH:SS', function() {
     var inputDate = new Date(1405348750916);
-    expect(fielddbPrettyDate(inputDate)).toBe('07/14/2014 10:39');
+    expect(fielddbPrettyDate(inputDate)).toBe(expectedFormattedDate);
   });
 
   describe('edge cases', function() {
