15cf243187a9ae321d731aab30f06fcd697877c4
cesine <cesine@yahoo.com>
2014-08-27 23:27:07 -0400

    travis is in the US so the locale wants 07/14 for dates not 14/07.. todo fix this in a more permanent way...

--- angular_client/modules/core/test/spec/filters/fielddb-pretty-date.js
+++ angular_client/modules/core/test/spec/filters/fielddb-pretty-date.js
@@ -13,27 +13,27 @@ describe('Filter: fielddbPrettyDate', function() {
 
   it('should convert json dates with escaped quotes into DD-MM-YYYY HH:SS', function() {
     var inputDate = '\"2014-07-14T14:39:10.916Z\"';
-    expect(fielddbPrettyDate(inputDate)).toBe('14/07/2014 10:39');
+    expect(fielddbPrettyDate(inputDate)).toBe('07/14/2014 10:39');
   });
 
   it('should convert json dates with quotes into DD-MM-YYYY HH:SS', function() {
     var inputDate = '"2014-07-14T14:39:10.916Z"';
-    expect(fielddbPrettyDate(inputDate)).toBe('14/07/2014 10:39');
+    expect(fielddbPrettyDate(inputDate)).toBe('07/14/2014 10:39');
   });
 
   it('should convert json dates into DD-MM-YYYY HH:SS', function() {
     var inputDate = '2014-07-14T14:39:10.916Z';
-    expect(fielddbPrettyDate(inputDate)).toBe('14/07/2014 10:39');
+    expect(fielddbPrettyDate(inputDate)).toBe('07/14/2014 10:39');
   });
 
   it('should convert timestamps into DD-MM-YYYY HH:SS', function() {
     var inputDate = 1405348750916;
-    expect(fielddbPrettyDate(inputDate)).toBe('14/07/2014 10:39');
+    expect(fielddbPrettyDate(inputDate)).toBe('07/14/2014 10:39');
   });
 
   it('should convert dates into DD-MM-YYYY HH:SS', function() {
     var inputDate = new Date(1405348750916);
-    expect(fielddbPrettyDate(inputDate)).toBe('14/07/2014 10:39');
+    expect(fielddbPrettyDate(inputDate)).toBe('07/14/2014 10:39');
   });
 
   describe('edge cases', function() {
--- angular_client/modules/core/test/spec/filters/fielddb-short-date.js
+++ angular_client/modules/core/test/spec/filters/fielddb-short-date.js
@@ -14,27 +14,27 @@ describe('Filter: fielddbShortDate', function () {
 
   it('should convert json dates with escaped quotes into DD-MM-YYYY HH:SS', function() {
     var inputDate = '\"2014-07-14T14:39:10.916Z\"';
-    expect(fielddbShortDate(inputDate)).toBe('14/07/2014');
+    expect(fielddbShortDate(inputDate)).toBe('07/14/2014');
   });
 
   it('should convert json dates with quotes into DD-MM-YYYY HH:SS', function() {
     var inputDate = '"2014-07-14T14:39:10.916Z"';
-    expect(fielddbShortDate(inputDate)).toBe('14/07/2014');
+    expect(fielddbShortDate(inputDate)).toBe('07/14/2014');
   });
 
   it('should convert json dates into DD-MM-YYYY HH:SS', function() {
     var inputDate = '2014-07-14T14:39:10.916Z';
-    expect(fielddbShortDate(inputDate)).toBe('14/07/2014');
+    expect(fielddbShortDate(inputDate)).toBe('07/14/2014');
   });
 
   it('should convert timestamps into DD-MM-YYYY HH:SS', function() {
     var inputDate = 1405348750916;
-    expect(fielddbShortDate(inputDate)).toBe('14/07/2014');
+    expect(fielddbShortDate(inputDate)).toBe('07/14/2014');
   });
 
   it('should convert dates into DD-MM-YYYY HH:SS', function() {
     var inputDate = new Date(1405348750916);
-    expect(fielddbShortDate(inputDate)).toBe('14/07/2014');
+    expect(fielddbShortDate(inputDate)).toBe('07/14/2014');
   });
 
 });
