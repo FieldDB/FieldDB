a73da6d40d87fe8367e47252ab9e140e95340ae9
cesine <cesine@yahoo.com>
2014-09-03 11:32:22 -0400

    commonjs-ification of comments
    for #348 #1297 #1505 #1120 #1374 #1300 #1407 #915 #1167 #1157 #1419 #1221 #376 #1109 #1244 #795 #342 #1130 #1489 #731 #750 #993 #71 #982 #303 #313 #966 #225 #901 #156

--- api/comment/Comment.js
+++ api/comment/Comment.js
@@ -1,108 +1,98 @@
-define( [
-    "backbone",
-    "user/UserMask"
-], function(
-    Backbone,
-    UserMask
-) {
-	var Comment = Backbone.Model.extend(
-  /** @lends Comment.prototype */
-  {
-    /**
-     * @class Comments allow users to collaborate between each other and take
-     *        note of important things, issues to be fixed, etc. These can
-     *        appear on datum, sessions corpora, and dataLists. Comments can
-     *        also be edited and removed.
-     * 
-     * @property {String} text Describe text here.
-     * @property {Number} username Describe username here.
-     * @property {Date} timestamp Describe timestamp here.
-     * 
-     * @description Initialize function has a timestamp and a username and waits
-     *              until text is entered.
-     * 
-     * @extends Backbone.Model
-     * @constructs
-     */
-    initialize : function() {
-      
-      if(!this.get("timestamp")){
-        this.set("timestamp", Date.now()); //copied from upstream code timestamp is back 
-        this.set("gravatar", window.app.get("authentication").get("userPublic").get("gravatar"));
-        this.set("username", window.app.get("authentication").get("userPublic").get("username"));
-      }else{
-        /* if the timestamp is an old one, replace it with a timestamp v 1.47 */
-        if((""+this.get("timestamp")).indexOf("Z")  > -1){
-          var olddate = new Date(this.get("timestamp"));
-          this.set("timestamp", olddate.getTime());
+var FieldDBObject = require('./../FieldDBObject').FieldDBObject;
+
+/**
+ * @class Comments allow users to collaborate between each other and take
+ *        note of important things, issues to be fixed, etc. These can
+ *        appear on datum, sessions corpora, and dataLists. Comments can
+ *        also be edited and removed.
+ *
+ * @property {String} text Describe text here.
+ * @property {Number} username Describe username here.
+ * @property {Date} timestamp Describe timestamp here.
+ *
+ * @name  Comment
+ * @description The initialize function brings up a field in which the user
+ *              can enter tags.@class FieldDBObject of Datum validation states
+ * @extends FieldDBObject
+ * @constructs
+ */
+var Comment = function Comment(options) {
+  this.debug("Constructing Comment ", options);
+  FieldDBObject.apply(this, arguments);
+};
+
+Comment.prototype = Object.create(FieldDBObject.prototype, /** @lends Comment.prototype */ {
+
+  constructor: {
+    value: Comment
+  },
+
+  build: {
+    value: function(usermask) {
+      this.timestamp = Date.now();
+      this.gravatar = usermask.gravatar;
+      this.username = usermask.username;
+    }
+  },
+
+  timestamp: {
+    get: function() {
+      return this._timestamp || this.label;
+    },
+    set: function(value) {
+      if (value === this._timestamp) {
+        return;
+      }
+      if (!value) {
+        delete this._timestamp;
+        return;
+      }
+      if (("" + value.indexOf("Z")) > -1) {
+        value = (new Date(value)).getTime();
+      }
+
+      this._timestamp = value;
+    }
+  },
+
+  /**
+   * The edit function allows users to edit a comment.
+   *
+   * @param {String}
+   *          newtext Takes new text and replaces old one.
+   *
+   */
+  edit: {
+    value: function(newtext) {
+      this.text = newtext;
+      this.timestampModified = Date.now();
+    }
+  },
+
+  commentCreatedActivity: {
+    value: function(indirectObjectString) {
+      var commentstring = this.text;
+      return [{
+          verb: "commented",
+          verbicon: "icon-comment",
+          directobjecticon: "",
+          directobject: "'" + commentstring + "'",
+          indirectobject: indirectObjectString,
+          teamOrPersonal: "team",
+          context: " via Offline App."
+        },
+
+        {
+          verb: "commented",
+          verbicon: "icon-comment",
+          directobjecticon: "",
+          directobject: "'" + commentstring + "'",
+          indirectobject: indirectObjectString,
+          teamOrPersonal: "personal",
+          context: " via Offline App."
         }
-      }
-      
-      if(this.get("filledWithDefaults")){
-        this.fillWithDefaults();
-        this.unset("filledWithDefaults");
-      }
-    },
-    fillWithDefaults : function(){
-      
-    },
-    defaults : {
-      text: "",
-      username: ""
-    },
-    
-    // Internal models: used by the parse function
-    internalModels : {
-      // There are no nested models
-    },
-
-    /**
-     * The remove function removes a comment.
-     */
-    remove : function() {
-    },
-    saveAndInterConnectInApp : function(callback){
-      
-      if(typeof callback == "function"){
-        callback();
-      }
-    },
-    /**
-     * The edit function allows users to edit a comment.
-     * 
-     * @param {String}
-     *          newtext Takes new text and replaces old one.
-     * 
-     */
-    edit : function(newtext) {
-      this.set("text", newtext);
-      this.set("timestampModified", Date.now());
+      ];
     }
-    ,
-    commentCreatedActivity : function(indirectObjectString) {
-      var commentstring = this.get("text");
-      window.app.addActivity({
-        verb : "commented",
-        verbicon : "icon-comment",
-        directobjecticon : "",
-        directobject : "'" + commentstring + "'",
-        indirectobject : indirectObjectString ,
-        teamOrPersonal : "team",
-        context : " via Offline App."
-      });
+  }
 
-      window.app.addActivity({
-        verb : "commented",
-        verbicon : "icon-comment",
-        directobjecticon : "",
-        directobject : "'" + commentstring + "'",
-        indirectobject : indirectObjectString ,
-        teamOrPersonal : "personal",
-        context : " via Offline App."
-      });
-
-    }
-  });
-
-  return Comment;
 });
