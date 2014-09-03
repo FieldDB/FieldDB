555a22158f98be6358463057d73846cfe6be12ee
cesine <cesine@yahoo.com>
2014-09-03 15:50:23 -0400

    running jshint upon file changes in angular core

--- angular_client/modules/core/Gruntfile.js
+++ angular_client/modules/core/Gruntfile.js
@@ -34,7 +34,7 @@ module.exports = function(grunt) {
       js: {
         files: ['<%= yeoman.app %>/scripts/{,*/}*.js', '<%= yeoman.app %>/bower_components/fielddb/{,*/}*.js'],
         // tasks: ['newer:jshint:all'],
-        tasks: ['build'],
+        tasks: ['newer:jshint:all', 'build'],
         options: {
           livereload: 35726
         }
