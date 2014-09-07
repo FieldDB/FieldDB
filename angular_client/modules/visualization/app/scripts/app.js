97d5b1d45936828f770b053d3d50ea0f214c3972
cesine <cesine@yahoo.com>
2014-09-07 10:50:00 -0400

    yo angular

---
+++ angular_client/modules/visualization/app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name visualizationApp
+ * @description
+ * # visualizationApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('visualizationApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
