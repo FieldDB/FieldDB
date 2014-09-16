181507c7f0a95fc69cfe6032d3ed5b45767aaa4a
cesine <cesine@yahoo.com>
2014-09-16 12:45:30 -0400

    turning on app tests

--- tests/app/AppTest.js
+++ tests/app/AppTest.js
@@ -1,17 +1,40 @@
-var OPrime = OPrime || require('../../backbone_client/libs/OPrime.js');
+var App = App || require('../../api/app/App.js').App;
+
+describe("App", function() {
+
+  describe("construction", function() {
+    it("should load", function() {
+      expect(App).toBeDefined();
+    });
+
+    it("should create new apps", function() {
+      var app = new App();
+      expect(app).toBeDefined();
+    });
+  });
+});
+
 
 // Testing to see where the app is running, if it is installed on android,
 // installed in chrome or if it is a web widget.
-xdescribe("App: as a developer I want to deploy to multiple targets", function() {
+describe("App: as a developer I want to deploy to multiple targets", function() {
   it("should not be a Chrome extension", function() {
-    expect(!OPrime.isChromeApp()).toBeTruthy();
+    var app = new App();
+    expect(app.isChromeApp).toBeFalsy();
   });
 
   it("should not be an Offline Android app", function() {
-    expect(!OPrime.isAndroidApp()).toBeTruthy();
+    var app = new App();
+    expect(app.isAndroidApp).toBeFalsy();
   });
 
   it("should not be an CouchApp", function() {
-    expect(!OPrime.isCouchApp()).toBeTruthy();
+    var app = new App();
+    expect(app.isCouchApp).toBeFalsy();
+  });
+
+  it("should be a NodeJS app", function() {
+    var app = new App();
+    expect(app.isNodeJSApp).toBeTruthy();
   });
 });
