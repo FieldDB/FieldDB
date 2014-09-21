5b5077ccbcd339445d7f5dbb4d9c2ad803c4ba8d
cesine <cesine@yahoo.com>
2014-09-21 12:57:24 -0400

    workin gon taking the user to the most appropriate database

--- angular_client/modules/core/app/scripts/directives/fielddb-authentication.js
+++ angular_client/modules/core/app/scripts/directives/fielddb-authentication.js
@@ -10,7 +10,7 @@
  */
 angular.module("fielddbAngularApp").directive("fielddbAuthentication", function() {
 
-  var controller = function($scope, $location) {
+  var controller = function($scope, $location, $timeout) {
     /* initialize or confirm scope is prepared */
     $scope.loginDetails = $scope.loginDetails || {};
     // $scope.application.authentication = $scope.application.authentication || {};
@@ -23,10 +23,15 @@ angular.module("fielddbAngularApp").directive("fielddbAuthentication", function(
     var processUserDetails = function(user) {
       user.authenticated = true;
       user.accessibleDBS = user.accessibleDBS || [];
+      user.mostrecentdb = "/";
       user.roles.map(function(role) {
         var dbname = role.substring(0, role.lastIndexOf("_"));
         if (role.indexOf("-") > -1 && role.indexOf("_reader") > -1 && user.accessibleDBS.indexOf(dbname) === -1) {
+          dbname = dbname.replace("-", "/");
           user.accessibleDBS.push(dbname);
+          if (dbname.indexOf("public") === -1 && dbname.indexOf("lingllama") === -1) {
+            user.mostrecentdb = dbname;
+          }
         }
         return role;
       });
@@ -39,12 +44,23 @@ angular.module("fielddbAngularApp").directive("fielddbAuthentication", function(
       // $scope.team = user;
       // $rootScope.authenticated = true;
       // console.log($scope);
-      if (window.location.pathname === "/welcome" || window.location.pathname === "/bienvenu") {
+
+      if ($scope.application.authentication.user.accessibleDBS.indexOf("sails/fr-ca") > -1) {
+        console.log("Redirecting the user to the manage sails dashboard" + "/sails/fr-ca/datalists");
         $scope.$apply(function() {
-          $location.path("/" + $scope.application.authentication.user.accessibleDBS[0].replace("-", "/"));
+          $location.path("/sails/fr-ca/datalists", false);
+        });
+      } else if ($location.path() === "/welcome" || $location.path() === "/bienvenu" || window.location.pathname === "/welcome" || window.location.pathname === "/bienvenu") {
+        $scope.$apply(function() {
+          //http://joelsaupe.com/programming/angularjs-change-path-without-reloading/
+          $location.path("/" + $scope.application.authentication.user.mostrecentdb, false);
         });
       }
-      $scope.$digest();
+      $timeout(function() {
+        if (!$scope.$$phase) {
+          $scope.$digest(); //$digest or $apply
+        }
+      }, 500);
     };
     $scope.register = function(registerDetails) {
       console.warn("TODO use $scope.corpus.register", registerDetails);
@@ -55,7 +71,10 @@ angular.module("fielddbAngularApp").directive("fielddbAuthentication", function(
       $scope.application.authentication.error = "";
       FieldDB.Database.prototype.login(loginDetails).then(function(user) {
         console.log("User has been downloaded. ", user);
-        $scope.application.authentication.user.merge(user);
+        $scope.application.authentication.user.alwaysConfirmOkay = true;
+        $scope.application.authentication.user.prefs.alwaysConfirmOkay = true;
+        $scope.application.authentication.user.prefs.unicodes.alwaysConfirmOkay = true;
+        $scope.application.authentication.user.merge("self", user);
         processUserDetails($scope.application.authentication.user);
         // $scope.isContactingServer = false;
       }, function(reason) {
@@ -77,14 +96,14 @@ angular.module("fielddbAngularApp").directive("fielddbAuthentication", function(
       FieldDB.Database.prototype.logout().then(function(serverReply) {
         console.log("User has been logged out. ", serverReply);
         $scope.application.authentication = {
-          user: {
+          user: new FieldDB.User({
             authenticated: false
-          }
+          })
         };
         if (window.location.pathname !== "/welcome" && window.location.pathname !== "/bienvenu") {
           $scope.$apply(function() {
-            $location.path("/welcome/");
-            window.location.replace("/welcome");
+            // $location.path("/welcome/", false);
+            window.location.replace("/#/welcome");
           });
         }
         $scope.$digest();
@@ -126,7 +145,7 @@ angular.module("fielddbAngularApp").directive("fielddbAuthentication", function(
 
 
   };
-  controller.$inject = ["$scope", "$location"];
+  controller.$inject = ["$scope", "$location", "$timeout"];
 
   /* Directive declaration */
   var directiveDefinitionObject = {
