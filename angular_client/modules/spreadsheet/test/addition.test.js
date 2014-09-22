c25f0275255743a696b7f29d245da86f0a548c73
cesine <cesine@yahoo.com>
2014-09-22 19:44:37 -0400

    reordering the files into yo angular

--- angular_client/modules/spreadsheet/test/addition.test.js
+++
@@ -1,8 +0,0 @@
-/*global describe it expect */
-describe('addition', function () {
-    'use strict';
- 
-    it('should return 4 when adding 2 and 2', function () {
-        expect(2 + 2).toBe(4);
-    });
-});
