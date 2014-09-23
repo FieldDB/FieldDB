4c81932950a10b0ed6b60d177f671a29dbe5901c
cesine <cesine@yahoo.com>
2014-09-23 10:02:45 -0400

    attaching directives and filters to the spreadsheetApp module directly

--- angular_client/modules/spreadsheet/app/scripts/filters.js
+++ angular_client/modules/spreadsheet/app/scripts/filters.js
@@ -2,77 +2,81 @@
 console.log("Loading the SpreadsheetStyleDataEntryFilters.");
 
 
-var SpreadsheetStyleDataEntryFilters = angular.module(
-  'spreadsheet_filters', []).filter('startFrom', function() {
-  return function(input, start) {
-    if (input === undefined) {
-      return;
-    } else {
-      start = +start; // parse to int
-      return input.slice(start);
-    }
-  };
-}).filter('standardDate', function() {
-  return function(input) {
-    if (!input) {
-      return "--";
-    } else if (input === "2000-09-06T16:31:30.988Z") {
-      return "N/A";
-    } else {
-      var newDate = input.replace(/\"/g, "");
-      var d = new Date(newDate);
-      var t = new Date(newDate);
-      var minutes = t.getMinutes();
-      if (minutes < 10) {
-        minutes = "0" + minutes;
+angular.module('spreadsheetApp')
+  .filter('startFrom', function() {
+    return function(input, start) {
+      if (input === undefined) {
+        return;
+      } else {
+        start = +start; // parse to int
+        return input.slice(start);
       }
-      return d.toLocaleDateString() + " " + t.getHours() + ":" + minutes;
-    }
-  };
-}).filter('standardDateFromTimestamp', function() {
-  return function(input) {
-    if (!input) {
-      return "--";
-    } else if (input === "2000-09-06T16:31:30.988Z") {
-      return "N/A";
-    } else {
-      var newDate = input;
-      var d = new Date(newDate);
-      var t = new Date(newDate);
-      var minutes = t.getMinutes();
-      if (minutes < 10) {
-        minutes = "0" + minutes;
+    };
+  })
+  .filter('standardDate', function() {
+    return function(input) {
+      if (!input) {
+        return "--";
+      } else if (input === "2000-09-06T16:31:30.988Z") {
+        return "N/A";
+      } else {
+        var newDate = input.replace(/\"/g, "");
+        var d = new Date(newDate);
+        var t = new Date(newDate);
+        var minutes = t.getMinutes();
+        if (minutes < 10) {
+          minutes = "0" + minutes;
+        }
+        return d.toLocaleDateString() + " " + t.getHours() + ":" + minutes;
       }
-      return d.toLocaleDateString() + " " + t.getHours() + ":" + minutes;
-    }
-  };
-}).filter('shortDate', function() {
-  return function(input) {
-    if (!input) {
-      return "--";
-    } else if (input === "2000-09-06T16:31:30.988Z") {
-      return "N/A";
-    } else {
-      var newDate = input.replace(/\"/g, "");
-      var d = new Date(newDate);
-      return d.getDate() + "-" + (d.getMonth() + 1) + "-" + d.getFullYear();
-    }
-  };
-}).filter('neverEmpty', function() {
-  return function(input) {
-    if (input === "" || input === undefined || input === " ") {
-      return "--";
-    } else {
-      return input;
-    }
-  };
-}).filter('checkDatumTags', function() {
-  return function(input) {
-    if (input === "Tags") {
-      return "--";
-    } else {
-      return input;
-    }
-  };
-});
-console.log("SpreadsheetStyleDataEntryFilters", SpreadsheetStyleDataEntryFilters);
+    };
+  })
+  .filter('standardDateFromTimestamp', function() {
+    return function(input) {
+      if (!input) {
+        return "--";
+      } else if (input === "2000-09-06T16:31:30.988Z") {
+        return "N/A";
+      } else {
+        var newDate = input;
+        var d = new Date(newDate);
+        var t = new Date(newDate);
+        var minutes = t.getMinutes();
+        if (minutes < 10) {
+          minutes = "0" + minutes;
+        }
+        return d.toLocaleDateString() + " " + t.getHours() + ":" + minutes;
+      }
+    };
+  })
+  .filter('shortDate', function() {
+    return function(input) {
+      if (!input) {
+        return "--";
+      } else if (input === "2000-09-06T16:31:30.988Z") {
+        return "N/A";
+      } else {
+        var newDate = input.replace(/\"/g, "");
+        var d = new Date(newDate);
+        return d.getDate() + "-" + (d.getMonth() + 1) + "-" + d.getFullYear();
+      }
+    };
+  })
+  .filter('neverEmpty', function() {
+    return function(input) {
+      if (input === "" || input === undefined || input === " ") {
+        return "--";
+      } else {
+        return input;
+      }
+    };
+  })
+  .filter('checkDatumTags', function() {
+    return function(input) {
+      if (input === "Tags") {
+        return "--";
+      } else {
+        return input;
+      }
+    };
+  });
