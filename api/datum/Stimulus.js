4eff7aff43573acf68244a8e937183f59ed4a4a4
cesine <cesine@yahoo.com>
2014-09-25 14:47:38 -0400

    adding prime target distractor and layout for oprime integration

--- api/datum/Stimulus.js
+++ api/datum/Stimulus.js
@@ -16,6 +16,73 @@ var Stimulus = function Stimulus(options) {
 Stimulus.prototype = Object.create(Datum.prototype, /** @lends Stimulus.prototype */ {
   constructor: {
     value: Stimulus
+  },
+
+  prime: {
+    get: function() {
+      if (this._prime) {
+        return this._prime;
+      }
+      return {
+        "imageFile": "x.png",
+        "utterance": "χχ",
+        "orthography": "xx",
+        "audioFile": "prime.mp3"
+      };
+    },
+    set: function(value) {
+      this._prime = value;
+    }
+  },
+
+  target: {
+    get: function() {
+      if (this._target) {
+        return this._target;
+      }
+      return {
+        "imageFile": "x.png",
+        "utterance": "χχ",
+        "orthography": "x",
+        "audioFile": "target.mp3"
+      };
+    },
+    set: function(value) {
+      this._target = value;
+    }
+  },
+
+  distractors: {
+    get: function() {
+      if (this._distractors) {
+        return this._distractors;
+      }
+      return [{
+        "imageFile": "placeholder.jpg",
+        "utterance": "ʁχχ",
+        "orthography": "rxx",
+        "audioFile": "distractor.mp3"
+      }];
+    },
+    set: function(value) {
+      this._distractors = value;
+    }
+  },
+
+  layout: {
+    get: function() {
+      if (this._layout) {
+        return this._layout;
+      }
+      return {
+        randomize: false,
+        visualChoiceA: this.target,
+        visualChoiceB: this.distractors[0]
+      };
+    },
+    set: function(value) {
+      this._layout = value;
+    }
   }
 
 });
