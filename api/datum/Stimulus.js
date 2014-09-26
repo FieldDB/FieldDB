22d0bbcab3fbfc3f99bae23bfbcc765335e00fb1
cesine <cesine@yahoo.com>
2014-09-26 09:29:02 -0400

    movign resposne logic into fielddb from oprime-montage
    https://github.com/cesine/oprime-montage/blob/205fd3dcf93c5202e6913907950c31e3f591a6c4/core/abstract-stimulus.js

--- api/datum/Stimulus.js
+++ api/datum/Stimulus.js
@@ -20,15 +20,15 @@ Stimulus.prototype = Object.create(Datum.prototype, /** @lends Stimulus.prototyp
 
   prime: {
     get: function() {
-      if (this._prime) {
+      // if (this._prime) {
         return this._prime;
-      }
-      return {
-        "imageFile": "x.png",
-        "utterance": "χχ",
-        "orthography": "xx",
-        "audioFile": "prime.mp3"
-      };
+      // }
+      // return {
+      //   "imageFile": "x.png",
+      //   "utterance": "χχ",
+      //   "orthography": "xx",
+      //   "audioFile": "prime.mp3"
+      // };
     },
     set: function(value) {
       this._prime = value;
@@ -37,15 +37,15 @@ Stimulus.prototype = Object.create(Datum.prototype, /** @lends Stimulus.prototyp
 
   target: {
     get: function() {
-      if (this._target) {
+      // if (this._target) {
         return this._target;
-      }
-      return {
-        "imageFile": "x.png",
-        "utterance": "χχ",
-        "orthography": "x",
-        "audioFile": "target.mp3"
-      };
+      // }
+      // return {
+      //   "imageFile": "x.png",
+      //   "utterance": "χχ",
+      //   "orthography": "x",
+      //   "audioFile": "target.mp3"
+      // };
     },
     set: function(value) {
       this._target = value;
@@ -54,15 +54,15 @@ Stimulus.prototype = Object.create(Datum.prototype, /** @lends Stimulus.prototyp
 
   distractors: {
     get: function() {
-      if (this._distractors) {
+      // if (this._distractors) {
         return this._distractors;
-      }
-      return [{
-        "imageFile": "placeholder.jpg",
-        "utterance": "ʁχχ",
-        "orthography": "rxx",
-        "audioFile": "distractor.mp3"
-      }];
+      // }
+      // return [{
+      //   "imageFile": "placeholder.jpg",
+      //   "utterance": "ʁχχ",
+      //   "orthography": "rxx",
+      //   "audioFile": "distractor.mp3"
+      // }];
     },
     set: function(value) {
       this._distractors = value;
@@ -71,14 +71,14 @@ Stimulus.prototype = Object.create(Datum.prototype, /** @lends Stimulus.prototyp
 
   layout: {
     get: function() {
-      if (this._layout) {
+      // if (this._layout) {
         return this._layout;
-      }
-      return {
-        randomize: false,
-        visualChoiceA: this.target,
-        visualChoiceB: this.distractors[0]
-      };
+      // }
+      // return {
+      //   randomize: false,
+      //   visualChoiceA: this.target,
+      //   visualChoiceB: this.distractors[0]
+      // };
     },
     set: function(value) {
       this._layout = value;
