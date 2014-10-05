bba1ad6a12ced318ba10d7f9b3f5bbdf7d13d9d4
cesine <cesine@yahoo.com>
2014-10-05 10:02:36 -0400

    the tempaltes are using object datums now instead of two arrays of fields and values

--- angular_client/modules/spreadsheet/app/scripts/directives/fielddb-glosser-input.js
+++ angular_client/modules/spreadsheet/app/scripts/directives/fielddb-glosser-input.js
@@ -8,60 +8,36 @@
  * # fielddbGlosserInput
  */
 
- // var lexiconFactory = LexiconFactory;
- // var corpusSpecificGlosser;
- // /**
- //  * If the glosser & lexicon have not been created, this function makes it possible for users to specify any glosser url or lexicon url to use for downloading the precedece rules.
- //  * @param  {String} pouchname   The database for which the glosser is to be created
- //  * @param  {String} optionalUrl An optional url to a couchdb map reduce which has a format similar to morphemesPrecedenceContext and is able to create tuples used by the lexicon.
- //  */
- // var initGlosserAndLexiconIfNecessary = function(pouchname, optionalUrl){
- //   //If the url isnt specified, use the users lexicon on corpus server
- //   var url =  "https://corpus.lingsync.org/" + pouchname,
- //   showWordBoundaries = true;
+// var lexiconFactory = LexiconFactory;
+// var corpusSpecificGlosser;
+// /**
+//  * If the glosser & lexicon have not been created, this function makes it possible for users to specify any glosser url or lexicon url to use for downloading the precedece rules.
+//  * @param  {String} pouchname   The database for which the glosser is to be created
+//  * @param  {String} optionalUrl An optional url to a couchdb map reduce which has a format similar to morphemesPrecedenceContext and is able to create tuples used by the lexicon.
+//  */
+// var initGlosserAndLexiconIfNecessary = function(pouchname, optionalUrl){
+//   //If the url isnt specified, use the users lexicon on corpus server
+//   var url =  "https://corpus.lingsync.org/" + pouchname,
+//   showWordBoundaries = true;
 
- //   optionalUrl = optionalUrl ||  url + "/_design/lexicon/_view/morphemesPrecedenceContext?group=true";
- //   if (!corpusSpecificGlosser) {
- //     corpusSpecificGlosser = new Glosser({
- //       pouchname: pouchname
- //     });
- //   }
- //   if (!corpusSpecificGlosser.lexicon) {
- //     corpusSpecificGlosser.downloadPrecedenceRules(pouchname, optionalUrl, function(precedenceRelations) {
- //       corpusSpecificGlosser.lexicon = lexiconFactory({
- //         precedenceRelations: precedenceRelations,
- //         dbname: pouchname,
- //         element: document.getElementById(pouchname+"-lexicon-viz"),
- //         dontConnectWordBoundaries: !showWordBoundaries,
- //         url: optionalUrl.replace(url, "")
- //       });
- //     });
- //   }
- // };
-
-
-var convertFieldsIntoDatum = function(fieldLabelHolder, dataHolder) {
-  var datum = {};
-  for (var key in fieldLabelHolder) {
-    if (fieldLabelHolder[key].label === "morphemes") {
-      datum.morphemes = dataHolder[key];
-      datum.morphemesfield = key;
-    }
-    if (fieldLabelHolder[key].label === "gloss") {
-      datum.gloss = dataHolder[key];
-      datum.glossfield = key;
-    }
-    if (fieldLabelHolder[key].label === "utterance") {
-      datum.utterance = dataHolder[key];
-      datum.utterancefield = key;
-    }
-    if (fieldLabelHolder[key].label === "allomorphs") {
-      datum.allomorphs = dataHolder[key];
-      datum.allomorphsfield = key;
-    }
-  }
-  return datum;
-};
+//   optionalUrl = optionalUrl ||  url + "/_design/lexicon/_view/morphemesPrecedenceContext?group=true";
+//   if (!corpusSpecificGlosser) {
+//     corpusSpecificGlosser = new Glosser({
+//       pouchname: pouchname
+//     });
+//   }
+//   if (!corpusSpecificGlosser.lexicon) {
+//     corpusSpecificGlosser.downloadPrecedenceRules(pouchname, optionalUrl, function(precedenceRelations) {
+//       corpusSpecificGlosser.lexicon = lexiconFactory({
+//         precedenceRelations: precedenceRelations,
+//         dbname: pouchname,
+//         element: document.getElementById(pouchname+"-lexicon-viz"),
+//         dontConnectWordBoundaries: !showWordBoundaries,
+//         url: optionalUrl.replace(url, "")
+//       });
+//     });
+//   }
+// };
 
 angular.module('spreadsheetApp')
   .directive('keypressMarkAsEdited', function($rootScope) {
@@ -89,83 +65,68 @@ angular.module('spreadsheetApp')
     };
   })
   .directive('guessUtteranceFromMorphemes', function() {
-  return function(scope, element, attrs) {
+    return function(scope, element, attrs) {
+      var datum = attrs.datum;
+      if (typeof datum === "string") {
+        datum = JSON.parse(attrs.datum);
+      }
+      element.bind('blur', function(e) {
+        console.log("which field is this", attrs.fieldLabel);
+        if (datum && attrs.fieldLabel === "morphemes") {
+          // Ignore arrows
+          var keycodesToIgnore = [40, 38, 39, 37];
+          if (keycodesToIgnore.indexOf(e.keyCode) > -1) {
+            return;
+          }
 
-    element.bind('blur', function(e) {
-      console.log("which field is this", attrs.fieldLabel);
-      if (attrs.fieldLabel === "morphemes") {
-        var justCopyDontGuessIGT = false;
-        if (!attrs.autoGlosserOn || attrs.autoGlosserOn === "false") {
-          justCopyDontGuessIGT = true;
+          datum.pouchname = scope.DB.pouchname;
+          // initGlosserAndLexiconIfNecessary(scope.DB.pouchname);
+          datum = Glosser.guessUtteranceFromMorphemes(datum, scope.useAutoGlosser);
+        }
+
+      });
+    };
+  })
+  .directive('guessMorphemesFromUtterance', function() {
+    return function(scope, element, attrs) {
+      var datum = scope.datum;
+      element.bind('blur', function(e) {
+        console.log("which field is this", attrs.fieldLabel);
+        if (!datum || attrs.fieldLabel !== "utterance") {
+          return;
         }
         // Ignore arrows
         var keycodesToIgnore = [40, 38, 39, 37];
         if (keycodesToIgnore.indexOf(e.keyCode) > -1) {
           return;
         }
-        var dataHolder = scope.fieldData ? scope.fieldData : scope.newFieldData;
-        var datum = convertFieldsIntoDatum(scope.fields, dataHolder);
-        datum.pouchname = scope.DB.pouchname;
-        // initGlosserAndLexiconIfNecessary(scope.DB.pouchname);
-        datum = Glosser.guessUtteranceFromMorphemes(datum, justCopyDontGuessIGT);
-        scope.$apply(function() {
-          dataHolder[datum.utterancefield] = datum.utterance;
-        });
-      }
-
-    });
-  };
-})
-  .directive('guessMorphemesFromUtterance', function() {
-    return function(scope, element, attrs) {
-      element.bind('blur', function(e) {
-        console.log("which field is this", attrs.fieldLabel);
-        if (attrs.fieldLabel === "utterance") {
-          var justCopyDontGuessIGT = false;
-          if (!attrs.autoGlosserOn || attrs.autoGlosserOn === "false") {
-            justCopyDontGuessIGT = true;
-          }
-          // Ignore arrows
-          var keycodesToIgnore = [40, 38, 39, 37];
-          if (keycodesToIgnore.indexOf(e.keyCode) > -1) {
-            return;
-          }
-          var dataHolder = scope.fieldData ? scope.fieldData : scope.newFieldData;
-          var datum = convertFieldsIntoDatum(scope.fields, dataHolder);
+        if (!datum.pouchname) {
           datum.pouchname = scope.DB.pouchname;
-          // initGlosserAndLexiconIfNecessary(scope.DB.pouchname);
-          datum = Glosser.guessMorphemesFromUtterance(datum, justCopyDontGuessIGT);
-          scope.$apply(function() {
-            dataHolder[datum.morphemesfield] = datum.morphemes;
-            dataHolder[datum.glossfield] = datum.gloss;
-          });
         }
-
+        // initGlosserAndLexiconIfNecessary(scope.DB.pouchname);
+        scope.$apply(function() {
+          datum = Glosser.guessMorphemesFromUtterance(datum, scope.useAutoGlosser);
+        });
       });
     };
   })
   .directive('guessGlossFromMorphemes', function() {
     return function(scope, element, attrs) {
+      var datum = attrs.datum;
+      if (typeof datum === "string") {
+        datum = JSON.parse(attrs.datum);
+      }
       element.bind('blur', function(e) {
         console.log("which field is this", attrs.fieldLabel);
-        if (attrs.fieldLabel === "morphemes") {
-          var justCopyDontGuessIGT = false;
-          if (!attrs.autoGlosserOn || attrs.autoGlosserOn === "false") {
-            justCopyDontGuessIGT = true;
-          }
+        if (datum && attrs.fieldLabel === "morphemes") {
           // Ignore arrows
           var keycodesToIgnore = [40, 38, 39, 37];
           if (keycodesToIgnore.indexOf(e.keyCode) > -1) {
             return;
           }
-          var dataHolder = scope.fieldData ? scope.fieldData : scope.newFieldData;
-          var datum = convertFieldsIntoDatum(scope.fields, dataHolder);
           datum.pouchname = scope.DB.pouchname;
           // initGlosserAndLexiconIfNecessary(scope.DB.pouchname);
-          datum = Glosser.guessGlossFromMorphemes(datum, justCopyDontGuessIGT);
-          scope.$apply(function() {
-            dataHolder[datum.glossfield] = datum.gloss;
-          });
+          datum = Glosser.guessGlossFromMorphemes(datum, scope.useAutoGlosser);
         }
 
       });
