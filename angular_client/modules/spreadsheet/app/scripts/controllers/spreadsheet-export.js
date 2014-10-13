0d8c60a375a8b226de7696ac078789f5077ce6d4
cesine <cesine@yahoo.com>
2014-10-13 17:16:15 -0400

    added persistance for using wordpress gloss format for #1687 although it seems to always be undefined in the scope.

--- angular_client/modules/spreadsheet/app/scripts/controllers/spreadsheet-export.js
+++ angular_client/modules/spreadsheet/app/scripts/controllers/spreadsheet-export.js
@@ -12,18 +12,40 @@
 // It is not the same as the $modal service used above.
 // http://angular-ui.github.io/bootstrap/
 // http://stackoverflow.com/questions/19204510/modal-window-issue-unknown-provider-modalinstanceprovider
-angular.module('spreadsheetApp')
-  .controller('SpreadsheetExportController', function($scope, $modalInstance, details) {
+angular.module('spreadsheetApp').controller('SpreadsheetExportController', function($scope, $modalInstance, details) {
 
-    $scope.resultsMessageFromExternalController = details.resultsMessageFromExternalController;
-    $scope.resultsFromExternalController = details.resultsFromExternalController;
+  $scope.resultsMessageFromExternalController = details.resultsMessageFromExternalController;
+  $scope.resultsFromExternalController = details.resultsFromExternalController;
+
+  if (window.location.origin.indexOf("mcgill") > -1) {
     $scope.useWordpressIGTFormat = true;
+  } else {
+    $scope.useWordpressIGTFormat = false;
+  }
 
-    $scope.ok = function() {
-      $modalInstance.close($scope.results, $scope.resultsMessage);
-    };
+  try {
+    var previousValue = localStorage.getItem("useWordpressIGTFormat");
+    if (previousValue === "false") {
+      $scope.useWordpressIGTFormat = false;
+    } else if (previousValue === "true") {
+      $scope.useWordpressIGTFormat = true;
+    }
+  } catch (e) {
+    console.log("useWordpressIGTFormat was not previously set.");
+  }
 
-    $scope.cancel = function() {
-      $modalInstance.dismiss('cancel');
-    };
+  $scope.$watch('useWordpressIGTFormat', function(newvalue, oldvalue) {
+    console.log("useWordpressIGTFormat", oldvalue);
+    if (newvalue !== undefined) {
+      localStorage.setItem("useWordpressIGTFormat", newvalue);
+    }
   });
+
+  $scope.ok = function() {
+    $modalInstance.close($scope.results, $scope.resultsMessage);
+  };
+
+  $scope.cancel = function() {
+    $modalInstance.dismiss('cancel');
+  };
+});
