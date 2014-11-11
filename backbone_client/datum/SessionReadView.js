7b473ea6037e207303644a8838efb21930a58173
cesine <cesine@yahoo.com>
2014-11-11 18:01:31 -0500

    console warning if the session view cant render:

--- backbone_client/datum/SessionReadView.js
+++ backbone_client/datum/SessionReadView.js
@@ -1,6 +1,6 @@
 define([
-    "backbone", 
-    "handlebars", 
+    "backbone",
+    "handlebars",
     "comment/Comment",
     "comment/Comments",
     "comment/CommentReadView",
@@ -11,7 +11,7 @@ define([
     "OPrime"
 ], function(
     Backbone,
-    Handlebars, 
+    Handlebars,
     Comment,
     Comments,
     CommentReadView,
@@ -25,18 +25,18 @@ define([
   {
     /**
      * @class Session Edit View is where the user provides new session details.
-    
+
      * @property {String} format Must be set when the view is
-     * initialized. Valid values are "leftSide", "fullscreen", 
+     * initialized. Valid values are "leftSide", "fullscreen",
      * "centerWell"  and link
      * @extends Backbone.View
      * @constructs
      */
     initialize : function() {
       if (OPrime.debugMode) OPrime.debug("SESSION READ VIEW init: " );
-      
+
       this.changeViewsOfInternalModels();
-      
+
       var self = this;
       this.model.bind('change:sessionFields', function(){
         self.changeViewsOfInternalModels();
@@ -67,7 +67,7 @@ define([
         this.commentEditView.clearCommentForReuse();
         this.updatePouch();
         this.commentReadView.render();
-      }, 
+      },
       //Delete button remove a comment
       "click .remove-comment-button" : function(e) {
         if(e){
@@ -75,33 +75,33 @@ define([
           e.preventDefault();
         }
         this.model.get("comments").remove(this.commentEditView.model);
-      }, 
+      },
 
       "click .icon-resize-small" : 'resizeSmall',
       "click .icon-resize-full" : "resizeLarge",
       "click .icon-edit": "showEditable"
     },
-    
+
     /**
      * The Handlebars template rendered as the Summary.
      */
     templateSummary : Handlebars.templates.session_summary_read_embedded,
-    
+
     /**
      * The Handlebars template rendered as the Embedded.
      */
     templateEmbedded: Handlebars.templates.session_read_embedded,
-    
+
     /**
      * The Handlebars template rendered as the Fullscreen.
      */
     templateFullscreen : Handlebars.templates.session_read_embedded,
-    
+
     /**
      * The Handlebars template rendered as the link format.
      */
     templateLink : Handlebars.templates.session_read_link,
-    
+
     /**
      * Renders the SessionReadView.
      */
@@ -111,13 +111,13 @@ define([
         if (OPrime.debugMode) OPrime.debug("SESSION is undefined, come back later.");
         return this;
       }
-      
+
       try {
         if (this.model.get("sessionFields").where({label: "goal"})[0] == undefined) {
           if (OPrime.debugMode) OPrime.debug("SESSION fields are undefined, come back later.");
           return this;
         }
-        
+
         var jsonToRender = this.model.toJSON();
         jsonToRender.goal = this.model.get("sessionFields").where({label: "goal"})[0].get("mask");
         jsonToRender.consultants = this.model.get("sessionFields").where({label: "consultants"})[0].get("mask");
@@ -130,7 +130,7 @@ define([
         jsonToRender.locale_Show_Fullscreen = Locale.get("locale_Show_Fullscreen");
         jsonToRender.locale_Show_in_Dashboard = Locale.get("locale_Show_in_Dashboard");
         jsonToRender.locale_When = Locale.get("locale_When");
-        
+
         if(this.format != "link"){
           if(window.appView.currentSessionEditView){
             appView.currentSessionEditView.destroy_view();
@@ -141,23 +141,23 @@ define([
           if (OPrime.debugMode) OPrime.debug("SESSION READ LEFTSIDE render: " );
 
           this.setElement("#session-quickview");
-          $(this.el).html(this.templateSummary(jsonToRender)); 
+          $(this.el).html(this.templateSummary(jsonToRender));
 
         } else if (this.format == "centerWell") {
           if (OPrime.debugMode) OPrime.debug("SESSION READ CENTERWELL render: " );
 
           this.setElement("#session-embedded");
           $(this.el).html(this.templateEmbedded(jsonToRender));
-          
-          
+
+
           this.sessionFieldsView.el = this.$(".session-fields-ul");
-          this.sessionFieldsView.render(); 
+          this.sessionFieldsView.render();
           // Display the CommentReadView
-          this.commentReadView.el = $(this.el).find('.comments'); 
+          this.commentReadView.el = $(this.el).find('.comments');
           this.commentReadView.render();
 
           // Display the CommentEditView
-          this.commentEditView.el = $(this.el).find('.new-comment-area'); 
+          this.commentEditView.el = $(this.el).find('.new-comment-area');
           this.commentEditView.render();
 
         } else if (this.format == "fullscreen") {
@@ -165,33 +165,33 @@ define([
 
           this.setElement("#session-fullscreen");
           $(this.el).html(this.templateFullscreen(jsonToRender));
-          
+
           this.sessionFieldsView.el = this.$(".session-fields-ul");
           this.sessionFieldsView.render();
-          
+
           // Display the CommentReadView
           this.commentReadView.el = $(this.el).find('.comments');
           this.commentReadView.render();
-          
+
           // Display the CommentEditView
-          this.commentEditView.el = $(this.el).find('.new-comment-area'); 
+          this.commentEditView.el = $(this.el).find('.new-comment-area');
           this.commentEditView.render();
 
         } else if (this.format == "link") {
           if (OPrime.debugMode) OPrime.debug("SESSION READ LINK render: " );
 
           $(this.el).html(this.templateLink(jsonToRender));
-          
+
         } else {
           throw("You have not specified a format that the SessionReadView can understand.");
         }
       } catch(e) {
-        if (OPrime.debugMode) OPrime.debug("There was a problem rendering the session, probably the datumfields are still arrays and havent been restructured yet.");
+        console.warn("There was a problem rendering the session, probably the datumfields are still arrays and havent been restructured yet.", e, this.model);
       }
-      
+
       return this;
     },
-    
+
     changeViewsOfInternalModels : function(){
       this.sessionFieldsView = new UpdatingCollectionView({
         collection           : this.model.get("sessionFields"),
@@ -199,18 +199,18 @@ define([
         childViewTagName     : "li",
         childViewFormat      : "session"
       });
-      
+
       this.commentReadView = new UpdatingCollectionView({
         collection           : this.model.get("comments"),
         childViewConstructor : CommentReadView,
         childViewTagName     : 'li'
       });
-      
+
       this.commentEditView = new CommentEditView({
         model : new Comment(),
       });
     },
-    
+
     //functions associated with corner icons
     resizeSmall : function(e) {
       if(e){
@@ -219,7 +219,7 @@ define([
       }
       window.location.href = "#render/true";
     },
-    
+
     resizeLarge : function(e) {
       if(e){
         e.stopPropagation();
@@ -229,7 +229,7 @@ define([
       this.render();
       window.app.router.showFullscreenSession();
     },
-    
+
     showEditable :function(e) {
       if(e){
         e.stopPropagation();
@@ -239,10 +239,10 @@ define([
         window.appView.currentSessionEditView.format = this.format;
         window.appView.currentSessionEditView.render();
       }
-    }, 
- 
+    },
+
     /**
-     * 
+     *
      * http://stackoverflow.com/questions/6569704/destroy-or-remove-a-view-in-backbone-js
      */
     destroy_view: function() {
@@ -251,15 +251,15 @@ define([
       //COMPLETELY UNBIND THE VIEW
       this.undelegateEvents();
 
-      $(this.el).removeData().unbind(); 
+      $(this.el).removeData().unbind();
 
       //Remove view from DOM
-//      this.remove();  
+//      this.remove();
 //      Backbone.View.prototype.remove.call(this);
       },
       /* ReadView is supposed to save no change but we want the comments to
-       * be saved. This function saves the change/addition/deletion of the comments. 
-       * Changes in other parts of Session is taken care of the server according to 
+       * be saved. This function saves the change/addition/deletion of the comments.
+       * Changes in other parts of Session is taken care of the server according to
        * users' permissions. */
       updatePouch : function(e) {
         if(e){
@@ -277,8 +277,8 @@ define([
         });
       }
 
-    
+
   });
-  
+
   return SessionReadView;
-}); 
+});
