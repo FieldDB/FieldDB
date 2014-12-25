74125151818612244ffa1f4c09ecf178fcb4ac86
cesine <cesine@yahoo.com>
2014-12-25 21:58:44 +0530

    trying to have travis use the files in the repo

--- scripts/build_spreadsheet_angular.sh
+++ scripts/build_spreadsheet_angular.sh
@@ -4,9 +4,7 @@ CURRENTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../"
 cd angular_client/modules/spreadsheet &&
 npm install || exit 1;
 bower install || exit 1;
-# echo "Using local fielddb angular";
-# ls app/bower_components/fielddb/fielddb.js || {
-#   mkdir app/bower_components/fielddb;
-#   ln -s $CURRENTDIR/fielddb.js $CURRENTDIR/angular_client/modules/spreadsheet/app/bower_components/fielddb/fielddb.js;
-# } &&
+echo "Using local fielddb angular";
+rm -rf bower_components/fielddb-angular/dist
+ln -s $CURRENTDIR/angular_client/modules/core/dist $CURRENTDIR/angular_client/modules/spreadsheet/app/bower_components/fielddb-angular/dist;
 grunt
