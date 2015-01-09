acba97d5701c058383b846a1d6f308b2ceea9e1e
cesine <cesine@yahoo.com>
2015-01-09 12:13:21 +0700

    turning off bower install for angular core

--- angular_client/modules/core/Gruntfile.js
+++ angular_client/modules/core/Gruntfile.js
@@ -27,10 +27,10 @@ module.exports = function(grunt) {
 
     // Watches files for changes and runs tasks based on the changed files
     watch: {
-      bower: {
-        files: ["bower.json"],
-        tasks: ["bowerInstall"]
-      },
+      // bower: {
+      //   files: ["bower.json"],
+      //   tasks: ["bowerInstall"]
+      // },
       js: {
         files: ["<%= yeoman.app %>/scripts/{,*/}*.js", "<%= yeoman.app %>/bower_components/fielddb/{,*/}*.js"],
         // tasks: ["newer:jshint:all"],
