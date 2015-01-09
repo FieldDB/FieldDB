e6092b8b77e8dbaaf6a34df51bc22e80d1963a38
cesine <cesine@yahoo.com>
2015-01-09 11:44:26 +0700

    avoiding bower install on angular core

--- angular_client/modules/core/Gruntfile.js
+++ angular_client/modules/core/Gruntfile.js
@@ -370,7 +370,7 @@ module.exports = function(grunt) {
 
     grunt.task.run([
       "clean:server",
-      "bowerInstall",
+      // "bowerInstall",
       "concurrent:server",
       "autoprefixer",
       "connect:livereload",
