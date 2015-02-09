932fa52d9043af46cdc2167dc8a33cdb46572760
cesine <cesine@yahoo.com>
2015-02-09 11:49:47 +0700

    frequent fields had a copy paste error

--- couchapp_dev/views/get_frequent_fields/map.js
+++ couchapp_dev/views/get_frequent_fields/map.js
@@ -4,19 +4,20 @@ function(doc) {
     if (doc.trashed && doc.trashed.indexOf("deleted") > -1) return;
     if (doc.datumFields && doc.session) {
       emit("datumTotal", 1);
-      for (var i in doc.datumFields) {
-        var fieldKeyName = "label";
-        if (datum.datumFields[i].id && datum.datumFields[i].id.length > 0) {
-          fieldKeyName = "id"; /* update to version 2.35+ */
-        } else {
-          fieldKeyName = "label";
-        }
-        if (doc.datumFields[i].mask) {
+      for (var i = 0; i < doc.datumFields.length; i++) {
+
+        if (doc.datumFields[i] && (doc.datumFields[i].mask || doc.datumFields[i].value)) {
+          var fieldKeyName = "label";
+          if (doc.datumFields[i].id && doc.datumFields[i].id.length > 0) {
+            fieldKeyName = "id"; /* update to version 2.35+ */
+          } else {
+            fieldKeyName = "label";
+          }
           emit(doc.datumFields[i][fieldKeyName], 1);
         }
       }
     }
   } catch (e) {
-    //emit(e, 1);
+    // emit(e, 1);
   }
 }
