c0e43a312fb94f1752546ecc65c16f7cb89bae0f
cesine <cesine@yahoo.com>
2015-02-19 12:32:00 +0700

    trying to use a list for the datalist view rather than a map reduce

---
+++ couchapp_dev/lists/as_data_list.js
@@ -0,0 +1,100 @@
+/* source: https://gist.github.com/xalakox/3026004
+This function itterates through the data and figures out the headers and
+exports the id, key (and all its children) and value  (and all its children) as csv
+*/
+function(head, req) {
+  start({
+    'headers': {
+      'Content-Type': 'application/json; charset=utf-8'
+    }
+  });
+  Array.prototype.unique = function() {
+    var a = this.concat();
+    for (var i = 0; i < a.length; ++i) {
+      for (var j = i + 1; j < a.length; ++j) {
+        if (a[i] === a[j])
+          a.splice(j, 1);
+      }
+    }
+
+    return a;
+  };
+
+  var getheaders = function(name, value, parent) {
+    var ret = "";
+    if (typeof(value) == "object") {
+      for (var x in value) {
+        var family = name;
+        if (parent != "") family = parent + "." + name;
+        ret = ret + getheaders(x, value[x], family);
+      }
+    } else {
+      if (!isNaN(name)) {
+        ret = ret + parent + "[" + name + "],";
+      } else {
+        if (parent != "") {
+          ret = ret + parent + "." + name + ",";
+        } else {
+          ret = ret + name + ",";
+        }
+      }
+    }
+
+    return ret;
+  };
+  var tocsv = function(therow, heads) {
+    var ret = '';
+    for (var h in heads) {
+      var prop = heads[h].toString().replace(/\[/gi, "'][");
+      prop = "['" + prop.replace(/\./gi, "']['");
+      if (prop.charAt(prop.length - 1) !== ']') {
+        prop = prop + "']";
+      }
+      var cmd = 'therow' + prop;
+      var valor;
+      try {
+        valor = eval(cmd);
+        valor = valor.toString().replace(/, /gi, ' ');
+        valor = valor.toString().replace(/(\r\n|\n|\r)/gm, '');
+      } catch (er) {
+        valor = '';
+      }
+      ret = ret + '"' + valor + '",';
+    }
+    return ret;
+  };
+
+  var row;
+  var rows = [];
+  var headers = [];
+  var muestra = 100;
+  var cnt = 1;
+  var yasalio = false;
+
+  while (row = getRow()) {
+    if (cnt < muestra) {
+      header = getheaders("", row, "");
+      header = (header.charAt(header.length - 1) === ',') ? header.slice(0, -1) : header;
+      header = header.split(',');
+      headers = headers.concat(header).unique();
+      rows.push(row);
+    } else {
+      if (cnt == muestra) {
+        send(headers.join() + "\n");
+        for (var x in rows) {
+          send(tocsv(rows[x], headers) + "\n");
+        }
+      }
+      send(tocsv(row, headers) + "\n");
+      yasalio = true;
+    }
+    cnt = cnt + 1;
+  }
+  if (!yasalio) {
+    send(headers.join() + "\n");
+    for (var x in rows) {
+      send(tocsv(rows[x], headers) + "\n");
+    }
+  }
+  return "";
+}
