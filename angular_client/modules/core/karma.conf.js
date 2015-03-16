68c18c58c70a25ba5ea9d2865fbccc597133b578
cesine <cesine@yahoo.com>
2015-03-16 12:01:34 +0700

    loading fielddb earlier in karma

--- angular_client/modules/core/karma.conf.js
+++ angular_client/modules/core/karma.conf.js
@@ -17,6 +17,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
+      'app/bower_components/fielddb/fielddb.js',
       'app/bower_components/ng-file-upload-shim/angular-file-upload-shim.js',
       'app/bower_components/angular/angular.js',
       'app/bower_components/angular-mocks/angular-mocks.js',
@@ -27,7 +28,6 @@ module.exports = function(config) {
       'app/bower_components/angular-sanitize/angular-sanitize.js',
       'app/bower_components/angular-route/angular-route.js',
       'app/bower_components/angular-contenteditable/angular-contenteditable.js',
-      'app/bower_components/fielddb/fielddb.js',
       'app/bower_components/ng-file-upload/angular-file-upload.js',
       'app/bower_components/angular-dragdrop/draganddrop.js',
       'app/scripts/*.js',
