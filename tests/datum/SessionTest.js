7550ddac092f4df7788ebfc2a87cf491c6c583d0
cesine <cesine@yahoo.com>
2015-03-17 16:16:56 +0700

    jshint and clean up of session ot move to commonjs

--- tests/datum/SessionTest.js
+++ tests/datum/SessionTest.js
@@ -3,19 +3,8 @@ var Session = require("./../../api/datum/Session").Session;
 describe("Session: as a linguist I often collect data in an elicitation session", function() {
   it("should load", function() {
     expect(Session).toBeDefined();
-  });
 
-  it("should create new sessions", function() {
     var session = new Session();
     expect(session).toBeDefined();
   });
-
-   it("should assign a sessionID", function() {
-    // var d = new Session();
-    // d.set("SessionID", 0);
-    // expect(0 == d.get("SessionID")).toBeTruthy();
-    expect(true).toBeTruthy();
-
-  });
-
 });
