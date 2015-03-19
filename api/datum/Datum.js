fde82e36a9c6b7fd94dd4ea0f1fb1a42259d2004
cesine <cesine@yahoo.com>
2015-03-19 14:30:30 +0700

    bringing in igt functionality from the lexicon map reduces

--- api/datum/Datum.js
+++ api/datum/Datum.js
@@ -108,43 +108,142 @@ Datum.prototype = Object.create(FieldDBObject.prototype, /** @lends Datum.protot
 
   igt: {
     get: function() {
+      var convertDatumIntoSimpleObject = function(datum) {
+        var obj = {},
+          fieldKeyName = "label";
 
-      // Return a list of strings of the form ['uw1<br />mw1<br />gw1', 'uw2<br />mw2<br />gw2']
-      // where uw1 is the first utterance word, mw1 is the first morphemes word, etc.
-      // Note that any empty strings in params are filtered out (cf. _.compact).  This allows
-      // Datums with only, say, utterance and gloss strings or only morphemes and gloss
-      // strings to be IGT-formatted.
-      var getIGTList = function(params) {
-        return _.map(_.zip.apply(null, _.compact(_.map(params, function(p) {
-          if (p) {
-            return _.map(p.split(/[ \t\n]+/), randomStrings2highlightSpans);
+        for (var i = 0; i < datum.datumFields.length; i++) {
+          if (datum.datumFields[i].id && datum.datumFields[i].id.length > 0) {
+            fieldKeyName = "id"; /* update to version 2.35+ */
           } else {
-            return null;
+            fieldKeyName = "label";
           }
-        }))), function(t) {
-          return t.join('<br />')
-        });
-      }
+          if (datum.datumFields[i].mask) {
+            obj[datum.datumFields[i][fieldKeyName]] = datum.datumFields[i].mask;
+          }
+        }
+        if (datum.session.sessionFields) {
+          for (var j = 0; j < datum.session.sessionFields.length; j++) {
+            if (datum.session.sessionFields[j].id && datum.session.sessionFields[j].id.length > 0) {
+              fieldKeyName = "id"; /* update to version 2.35+ */
+            } else {
+              fieldKeyName = "label";
+            }
+            if (datum.session.sessionFields[j].mask) {
+              obj[datum.session.sessionFields[j][fieldKeyName]] = datum.session.sessionFields[j].mask;
+            }
+          }
+        }
+        obj.utterance = obj.utterance || "";
+        obj.morphemes = obj.morphemes || "";
+        obj.gloss = obj.gloss || "";
+
+        return obj;
+      };
+
+      var convertDatumIntoIGT = function(datum) {
+        var context = {},
+          feederWord,
+          fullWord;
+        if (datum.judgement && datum.judgement.indexOf("*") >= 0) {
+          return;
+        }
+        if (datum.utterance && datum.utterance.indexOf("*") >= 0) {
+          return;
+        }
+        var words = datum.utterance.toLowerCase().replace(/#?!.,\//g, "").split(/[ ]+/);
+        for (var word in words) {
+          // If the token it not null or the empty string
+          if (words[word]) {
+            // Replace (*_) with ""
+            feederWord = words[word].replace(/\(\*[^)]*\)/g, "");
+            // Replace *(_) with _
+            feederWord = feederWord.replace(/\*\(([^)]*)\)/, "$1");
+            // Remove all parentheses and *
+            fullWord = feederWord.replace(/[(*)]/g, "");
+            words[word] = fullWord;
+          }
+        }
+        context.words = words;
+
+        var morphemes = datum.morphemes.replace(/#!,\//g, "").split(/[ ]+/);
+        for (var morphemegroup in morphemes) {
+          // If the token it not null or the empty string
+          if (morphemes[morphemegroup]) {
+            // Replace (*_) with ""
+            feederWord = morphemes[morphemegroup].replace(/\(\*[^)]*\)/g, ""); // DONT replace ? it is used to indicate uncertainty with the data, . is used for fusional morphemes Replace *(_) with _ feederWord = feederWord.replace(/\*\(([^)]*)\)/, "$1");
+            // Remove all parentheses and *
+            fullWord = feederWord.replace(/[(*)]/g, "");
+            morphemes[morphemegroup] = fullWord;
+          }
+        }
+        context.morphemes = morphemes;
+
+        var glosses = datum.gloss.replace(/#!,\//g, "").split(/[ ]+/); // DONT replace
+        // ? it is
+        // used to indicate
+        // uncertainty with the
+        // data, . is used for
+        // fusional morphemes
+        for (var glossgroup in glosses) {
+          // If the token it not null or the empty string
+          if (glosses[glossgroup]) {
+            // Replace (*_) with ""
+            feederWord = glosses[glossgroup].replace(/\(\*[^)]*\)/g, "");
+            // Replace *(_) with _
+            feederWord = feederWord.replace(/\*\(([^)]*)\)/, "$1");
+            // Remove all parentheses and *
+            fullWord = feederWord.replace(/[(*)]/g, "");
+            glosses[glossgroup] = fullWord;
+          }
+        }
+        context.glosses = glosses;
+
+        return context;
+      };
 
       try {
-        var tuple = getIGTList([orthography, utterance, allomorphs, morphemes, gloss]);
-        // if there are only 3 or less words, they probably dont need the alignment visual that much
-        if (this.format === "latexPreviewIGTonly" && tuple && tuple.length < 4) {
-          return this;
-        } else if (this.format === "latexPreviewIGTonly" && tuple && tuple.length > 40) {
-          jsonToRender.scrollable = "scrollable";
+        /* if this document has been deleted, the ignore it and return immediately */
+        if (doc.trashed && doc.trashed.indexOf("deleted") > -1) {
+          return;
         }
-        if (translation != "" && this.format !== "latexPreviewIGTonly") {
-          jsonToRender.translation = "\u2018" + translation + "\u2019";
+        // If the document is a Datum
+        if (!doc.audioVideo) {
+          return;
         }
-        jsonToRender.tuple = tuple;
-        if (judgement !== "") {
-          jsonToRender.judgement = judgement;
+
+        var datum = convertDatumIntoSimpleObject(doc);
+        if (!datum) {
+          return;
+        }
+        var context = convertDatumIntoIGT(datum);
+        if (!context) {
+          return;
+        }
+        var punctuationToRemove = /[#?!,\/\(\)\*\#]/g;
+        // Build triples
+        for (var j in context.words) {
+          // var w = context.words[j];
+          var ms = context.morphemes[j].split("-");
+          var gs = context.glosses[j].split("-");
+          for (var i in ms) {
+            var caseInsensitiveMorpheme = ms[i].toLowerCase();
+            caseInsensitiveMorpheme = caseInsensitiveMorpheme.replace(punctuationToRemove, " ").trim();
+            var caseInsensitiveGloss = gs[i] ? gs[i].toLowerCase() : "??";
+            caseInsensitiveGloss = caseInsensitiveGloss.replace(punctuationToRemove, " ").trim();
+            emit({
+              morpheme: caseInsensitiveMorpheme,
+              gloss: caseInsensitiveGloss
+            }, 1);
+          }
         }
       } catch (e) {
-        console.log("Bug: this datum is not IGT: " + JSON.stringify(e));
-        jsonToRender.translation = "---";
+        //emit(e, 1);
       }
+
+
+
+      paraleltext.translation = "\u2018" + translation + "\u2019";
 
       return {};
     },
