fd408a2b67022e75492cd183efd0b9b7912cf76e
cesine <cesine@yahoo.com>
2015-03-26 19:52:31 +0700

    turning activities into a data list

--- tests/activity/ActivityTest.js
+++ tests/activity/ActivityTest.js
@@ -1,3 +1,4 @@
+var FieldDBObject = require("../../api/FieldDBObject").FieldDBObject;
 var Activity = require("../../api/activity/Activity").Activity;
 var Activities = require("../../api/activity/Activities").Activities;
 
@@ -12,7 +13,8 @@ describe("Activities: Test Activity Feed replication.", function() {
 });
 
 describe(
-  "Activities: As a user, I want to be updated on team news.", function() {
+  "Activities: As a user, I want to be updated on team news.",
+  function() {
 
     describe("construction", function() {
 
@@ -21,50 +23,206 @@ describe(
         expect(Activities).toBeDefined();
       });
 
-      it("should be defined", function() {
-        var activites = new Activities({
-          url: "https://localhost:6984",
-          // dbname: "lingllama-activity_feed",
-          user: {
-            username: "lingllama",
-            gravatar: "54b53868cb4d555b804125f1a3969e87"
-          },
-          teamOrPersonal: "personal"
+      it("should have some set defaults for updating the list", function() {
+        var activityFeed = new Activities();
+
+        expect(activityFeed.primaryKey).toEqual("whenWhatWho");
+
+        expect(activityFeed.api).toEqual("activities");
+        activityFeed.api = "cantchagnetehapiurlsorry";
+        expect(activityFeed.api).toEqual("activities");
+      });
+
+      it("should construct a corpus activity feed based on a corpus", function() {
+        var activityFeed = new Activities();
+
+        expect(activityFeed).toBeDefined();
+        expect(activityFeed.fieldDBtype).toEqual("Activities");
+        expect(activityFeed.dbname).toBeUndefined();
+        expect(activityFeed.url).toBeUndefined();
+
+        activityFeed.parent = {
+          fieldDBtype: "LanguageLearningCorpus",
+          dbname: "community-georgian",
+          connection: new FieldDBObject({
+            protocol: "https://",
+            domain: "localhost",
+            port: "6984",
+            dbname: "community-georgian",
+            path: "",
+            serverLabel: "localhost",
+            authUrls: ["https://localhost:3183"],
+            // corpusUrls: ["https://localhost:6984"],
+            userFriendlyServerName: "Localhost"
+          })
+        };
+
+        expect(activityFeed.dbname).toEqual("community-georgian-activity_feed");
+
+        var serializedForParenttoJSON = activityFeed.toJSON();
+        expect(serializedForParenttoJSON).toBeDefined();
+        expect(serializedForParenttoJSON.dbname).toEqual("community-georgian-activity_feed");
+      });
+
+
+      it("should construct a user activity feed based on a user", function() {
+        var activityFeed = new Activities();
+
+        expect(activityFeed).toBeDefined();
+        expect(activityFeed.fieldDBtype).toEqual("Activities");
+        expect(activityFeed.dbname).toBeUndefined();
+        expect(activityFeed.url).toBeUndefined();
+
+        activityFeed.parent = {
+          fieldDBtype: "User",
+          username: "jenkins",
+          connection: new FieldDBObject({
+            protocol: "https://",
+            domain: "localhost",
+            port: "6984",
+            dbname: "jenkins-activity_feed",
+            path: "",
+            serverLabel: "localhost",
+            authUrls: ["https://localhost:3183"],
+            // corpusUrls: ["https://localhost:6984"],
+            userFriendlyServerName: "Localhost"
+          })
+        };
+
+        expect(activityFeed.dbname).toEqual("jenkins-activity_feed");
+
+        var serializedForParenttoJSON = activityFeed.toJSON();
+        expect(serializedForParenttoJSON).toBeDefined();
+        expect(serializedForParenttoJSON.dbname).toEqual("jenkins-activity_feed");
+      });
+
+      it("should construct using only a connection ", function() {
+        var activityFeed = new Activities({
+          protocol: "https://",
+          domain: "localhost",
+          port: "6984",
+          dbname: "jenkins-firstcorpus-activity_feed",
+          path: "",
+          serverLabel: "localhost",
+          authUrls: ["https://localhost:3183"],
+          // corpusUrls: ["https://localhost:6984"],
+          userFriendlyServerName: "Localhost"
         });
-        expect(activites).toBeDefined();
-        expect(activites.fieldDBtype).toEqual("Activities");
+        expect(activityFeed).toBeDefined();
+        expect(activityFeed.fieldDBtype).toEqual("Activities");
+
+        expect(activityFeed.connection.userFriendlyServerName).toEqual("Localhost");
+        expect(activityFeed.connection.fieldDBtype).toEqual("Connection");
+        expect(activityFeed.connection.corpusUrl).toEqual("https://localhost:6984/jenkins-firstcorpus-activity_feed");
+        expect(activityFeed.url).toEqual("https://localhost:6984/jenkins-firstcorpus-activity_feed");
+
+        expect(activityFeed.teamOrPersonal).toEqual("team");
+        expect(activityFeed.dbname).toEqual("jenkins-firstcorpus-activity_feed");
+      });
+
+
+      it("should deduce database form invalid databases ", function() {
+        var activityFeed = new Activities({
+          protocol: "https://",
+          domain: "localhost",
+          port: "6984",
+          dbname: "Jenkins-Another Corpus-activity_feed",
+          path: "",
+          serverLabel: "localhost",
+          authUrls: ["https://localhost:3183"],
+          // corpusUrls: ["https://localhost:6984"],
+          userFriendlyServerName: "Localhost"
+        });
+        expect(activityFeed).toBeDefined();
+        expect(activityFeed.dbname).toEqual("jenkins-another_corpus-activity_feed");
+
+        activityFeed = new Activities({
+          protocol: "https://",
+          domain: "localhost",
+          port: "6984",
+          dbname: "Jenkins-Another-Corpus-with-dashes-in-its-name-activity_feed",
+          path: "",
+          serverLabel: "localhost",
+          authUrls: ["https://localhost:3183"],
+          // corpusUrls: ["https://localhost:6984"],
+          userFriendlyServerName: "Localhost"
+        });
+        expect(activityFeed).toBeDefined();
+        expect(activityFeed.dbname).toEqual("jenkins-another-corpus_with_dashes_in_its_name-activity_feed");
+
+      });
+
+      it("should construct using a object ", function() {
+        var activityFeed = new Activities({
+          connection: {
+            protocol: "https://",
+            domain: "localhost",
+            port: "6984",
+            dbname: "default",
+            path: "",
+            serverLabel: "localhost",
+            authUrls: ["https://localhost:3183"],
+            // corpusUrls: ["https://localhost:6984"],
+            userFriendlyServerName: "Localhost"
+          },
+          parent: {
+            fieldDBtype: "User",
+            username: "teammatetiger"
+          }
+        });
+        expect(activityFeed).toBeDefined();
+        expect(activityFeed.fieldDBtype).toEqual("Activities");
+        expect(activityFeed.dbname).toEqual("teammatetiger-activity_feed");
+        expect(activityFeed.teamOrPersonal).toEqual("personal");
+      });
+
+    });
+
+    describe("persistance", function() {
+      var activityFeed;
+      beforeEach(function() {
+        activityFeed = new Activities({
+          protocol: "https://",
+          domain: "localhost",
+          port: "6984",
+          dbname: "jenkins-testing-via-jasmine-node-activity_feed",
+          path: "",
+          serverLabel: "localhost",
+          authUrls: ["https://localhost:3183"],
+          // corpusUrls: ["https://localhost:6984"],
+          userFriendlyServerName: "Localhost"
+        });
+      });
+
+      it("should refuse to add incomplete activities to add to the feed", function() {
+        expect(activityFeed).toBeDefined();
+        activityFeed.debugMode = true;
+        var addedResult = activityFeed.add({
+          verb: "",
+          indirectobject: "this doesnt matter, its not part of the subcategorization of activity :)"
+        });
+        expect(addedResult).toBeUndefined();
+        expect(activityFeed.incompleteActivitesStockPile).toBeDefined();
+        expect(activityFeed.incompleteActivitesStockPile.length).toEqual(1);
+        expect(activityFeed.incompleteActivitesStockPile[0]).toBeDefined();
+        expect(activityFeed.incompleteActivitesStockPile[0].activity).toBeDefined();
+        expect(activityFeed.incompleteActivitesStockPile[0].errorMessage).toBeDefined();
+        expect(activityFeed.incompleteActivitesStockPile[0].errorMessage).toEqual("The primary key `whenWhatWho` is undefined on this object, it cannot be added! Type: Activity");
       });
 
       it("should accept bare activities to add to the feed", function() {
-        var activites = new Activities({
-          url: "https://localhost:6984",
-          // dbname: "lingllama-activity_feed",
-          user: {
-            username: "lingllama",
-            gravatar: "54b53868cb4d555b804125f1a3969e87"
-          },
-          teamOrPersonal: "personal"
+        expect(activityFeed).toBeDefined();
+        activityFeed.debugMode = true;
+        activityFeed.add({
+          verb: "modified",
+          directobject: "tusunaywanmi"
         });
-
-        activites.add({
-          verb: "modified"
-        });
-        expect(activites.length).toEqual(1);
-        expect(activites.collection[0].fieldDBtype).toEqual("Activity");
+        expect(activityFeed.length).toEqual(1);
+        // // expect(activityFeed.docs._collection[0].fieldDBtype).toEqual("Activity");
       });
 
-      it("should validate activites' dbname & user information when adding to the feed", function() {
-        var activites = new Activities({
-          url: "https://localhost:6984",
-          // dbname: "lingllama-activity_feed",
-          user: {
-            username: "lingllama",
-            gravatar: "54b53868cb4d555b804125f1a3969e87"
-          },
-          teamOrPersonal: "personal"
-        });
-
-        var activity = activites.add({
+      it("should validate activityFeed' dbname & user information when adding to the feed", function() {
+        var activity = activityFeed.add({
           verb: "logged in",
           verbicon: "icon-check",
           directobjecticon: "icon-user",
@@ -72,19 +230,28 @@ describe(
           indirectobject: "",
           teamOrPersonal: "personal"
         });
-        expect(activites.length).toEqual(1);
-        expect(activity.url).toEqual("https://localhost:6984");
-        expect(activity.dbname).toEqual("lingllama-activity_feed");
-
+        expect(activityFeed.length).toEqual(1);
+        expect(activity.dbname).toEqual("jenkins-testing-via_jasmine_node-activity_feed");
       });
 
     });
 
-    describe("serialization", function() {
+
+    xdescribe("serialization", function() {
 
       it("should save all activities", function() {
-        var activites = new Activities({
-          url: "https://localhost:6984",
+        var activityFeed = new Activities({
+          connection: {
+            protocol: "https://",
+            domain: "localhost",
+            port: "6984",
+            dbname: "default",
+            path: "",
+            serverLabel: "localhost",
+            authUrls: ["https://localhost:3183"],
+            // corpusUrls: ["https://localhost:6984"],
+            userFriendlyServerName: "Localhost"
+          },
           // dbname: "lingllama-activity_feed",
           user: {
             username: "lingllama",
@@ -93,13 +260,13 @@ describe(
           teamOrPersonal: "personal"
         });
 
-        var activity = activites.add({
+        var activity = activityFeed.add({
           verb: "modified"
         });
         expect(activity).toBeDefined();
         expect(activity.verb).toEqual("modified");
 
-        activity = activites.add({
+        activity = activityFeed.add({
           verb: "logged in",
           verbicon: "icon-check",
           directobjecticon: "icon-user",
@@ -110,42 +277,53 @@ describe(
         expect(activity).toBeDefined();
         expect(activity.verb).toEqual("logged in");
 
-        expect(activites.length).toEqual(2);
-        activites.save();
+        expect(activityFeed.length).toEqual(2);
+        // activityFeed.save().then(function(){
+
+        // }, function(){
+
+        // });
+
       });
 
     });
 
-    it("should show my most recent team's activities by default.", function() {
+    xit("should show my most recent team's activities by default.", function() {
       expect(true).toBeTruthy();
     });
     it(
-      "should display a drop down box of my teams and members of my teams.", function() {
+      "should display a drop down box of my teams and members of my teams.",
+      function() {
         expect(true).toBeTruthy();
       });
     it(
-      "should let me enter anyone's user name to see their activity feed if its public.", function() {
+      "should let me enter anyone's user name to see their activity feed if its public.",
+      function() {
         expect(true).toBeTruthy();
       });
   });
 
 describe(
-  "Activities: As a user, I want to make my activity private.", function() {
+  "Activities: As a user, I want to make my activity private.",
+  function() {
     it(
-      "should display a settings that allows a logged in user to make their activities private.", function() {
+      "should display a settings that allows a logged in user to make their activities private.",
+      function() {
         expect(true).toBeTruthy();
       });
   });
 
 describe(
-  "Activities: As a team member, I want to able to delete team activities.", function() {
+  "Activities: As a team member, I want to able to delete team activities.",
+  function() {
     it("should be able to delete one activity.", function() {
       expect(true).toBeTruthy();
     });
   });
 
 describe(
-  "Activities: As a user, I want to  be about see my activity even when I'm offline.", function() {
+  "Activities: As a user, I want to  be about see my activity even when I'm offline.",
+  function() {
     it("should display a user's up to date activity when offline.", function() {
       expect(true).toBeTruthy();
     });
@@ -156,7 +334,8 @@ describe(
   });
 
 describe(
-  "Activities: As a user, I want to learn how to use the system by see how my teammates are using it.", function() {
+  "Activities: As a user, I want to learn how to use the system by see how my teammates are using it.",
+  function() {
 
     it("should have colorful icons to display verbs.", function() {
       var activity = new Activity({
