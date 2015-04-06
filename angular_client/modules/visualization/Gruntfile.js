b28879a6781d1a54df71a092c44bc9a929470630
cesine <cesine@yahoo.com>
2015-04-06 13:25:10 +0700

    removing imagemin and svgmin as dep from all projects, very heavy and unnecesasry dep

--- angular_client/modules/visualization/Gruntfile.js
+++ angular_client/modules/visualization/Gruntfile.js
@@ -248,16 +248,16 @@ module.exports = function (grunt) {
     //   }
     // },
 
-    svgmin: {
-      dist: {
-        files: [{
-          expand: true,
-          cwd: '<%= yeoman.app %>/images',
-          src: '{,*/}*.svg',
-          dest: '<%= yeoman.dist %>/images'
-        }]
-      }
-    },
+    // svgmin: {
+    //   dist: {
+    //     files: [{
+    //       expand: true,
+    //       cwd: '<%= yeoman.app %>/images',
+    //       src: '{,*/}*.svg',
+    //       dest: '<%= yeoman.dist %>/images'
+    //     }]
+    //   }
+    // },
 
     htmlmin: {
       dist: {
@@ -344,7 +344,7 @@ module.exports = function (grunt) {
       dist: [
         'copy:styles',
         // 'imagemin',
-        'svgmin'
+        // 'svgmin'
       ]
     },
 
