6995d33a7bf9f541aff323f2a6e4a9e871914f7d
cesine <cesine@yahoo.com>
2015-04-08 17:04:05 +0700

    running gulp test also

--- scripts/build_corpuspages_angular.sh
+++ scripts/build_corpuspages_angular.sh
@@ -7,6 +7,14 @@ cd angular_client/modules/corpuspages &&
 
 npm install || exit 1;
 bower install || exit 1;
+
+echo "Using local fielddb commonjs";
+rm bower_components/fielddb/fielddb.js;
+# rm bower_components/fielddb/fielddb.min.js;
+ln -s $CURRENTDIR/fielddb.js $CURRENTDIR/angular_client/modules/core/bower_components/fielddb/fielddb.js;
+# ln -s $CURRENTDIR/fielddb.min.js $CURRENTDIR/angular_client/modules/core/bower_components/fielddb/fielddb.min.js;
+ls -al $CURRENTDIR/angular_client/modules/core/bower_components/fielddb/
+
 echo "Using local fielddb angular";
 rm bower_components/fielddb-angular/bower.json
 rm -rf bower_components/fielddb-angular/dist
@@ -14,4 +22,19 @@ ln -s $CURRENTDIR/angular_client/modules/core/bower.json $CURRENTDIR/angular_cli
 ln -s $CURRENTDIR/angular_client/modules/core/dist $CURRENTDIR/angular_client/modules/corpuspages/bower_components/fielddb-angular/dist;
 ls -al $CURRENTDIR/angular_client/modules/corpuspages/bower_components
 ls -al $CURRENTDIR/angular_client/modules/corpuspages/bower_components/fielddb-angular/
-gulp
+
+gulp {
+  echo "Gulp was sucessfull";
+
+  gulp test {
+    echo "Gulp test was sucessfull";
+    exit 0
+  } || {
+    echo "gulp failed";
+    exit 8
+  }
+
+} || {
+  echo "gulp failed";
+  exit 8
+}
