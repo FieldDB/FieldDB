da2621e4842081d77a68176c84f81b419468eaec
cesine <cesine@yahoo.com>
2015-04-16 14:20:05 +0700

    copy-o was not symlinking in corpus pages

--- scripts/build_corpuspages_angular.sh
+++ scripts/build_corpuspages_angular.sh
@@ -11,9 +11,9 @@ bower install || exit 1;
 echo "Using local fielddb commonjs";
 rm bower_components/fielddb/fielddb.js;
 rm bower_components/fielddb/fielddb.min.js;
-ln -s $CURRENTDIR/fielddb.js $CURRENTDIR/angular_client/modules/core/bower_components/fielddb/fielddb.js;
-ln -s $CURRENTDIR/fielddb.min.js $CURRENTDIR/angular_client/modules/core/bower_components/fielddb/fielddb.min.js;
-ls -al $CURRENTDIR/angular_client/modules/core/bower_components/fielddb/
+ln -s $CURRENTDIR/fielddb.js $CURRENTDIR/angular_client/modules/corpuspages/bower_components/fielddb/fielddb.js;
+ln -s $CURRENTDIR/fielddb.min.js $CURRENTDIR/angular_client/modules/corpuspages/bower_components/fielddb/fielddb.min.js;
+ls -al $CURRENTDIR/angular_client/modules/corpuspages/bower_components/fielddb/
 
 echo "Using local fielddb angular";
 rm bower_components/fielddb-angular/bower.json
