87add12b9b944e8723cf681ea9097b217c3efb6f
cesine <cesine@yahoo.com>
2015-04-16 13:16:34 +0700

    Symlinking minified version of library

--- scripts/build_corpuspages_angular.sh
+++ scripts/build_corpuspages_angular.sh
@@ -10,9 +10,9 @@ bower install || exit 1;
 
 echo "Using local fielddb commonjs";
 rm bower_components/fielddb/fielddb.js;
-# rm bower_components/fielddb/fielddb.min.js;
+rm bower_components/fielddb/fielddb.min.js;
 ln -s $CURRENTDIR/fielddb.js $CURRENTDIR/angular_client/modules/core/bower_components/fielddb/fielddb.js;
-# ln -s $CURRENTDIR/fielddb.min.js $CURRENTDIR/angular_client/modules/core/bower_components/fielddb/fielddb.min.js;
+ln -s $CURRENTDIR/fielddb.min.js $CURRENTDIR/angular_client/modules/core/bower_components/fielddb/fielddb.min.js;
 ls -al $CURRENTDIR/angular_client/modules/core/bower_components/fielddb/
 
 echo "Using local fielddb angular";
