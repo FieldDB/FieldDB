fd0ba789218dcb214052daa3be9b9e45ae1c08bb
cesine <cesine@yahoo.com>
2015-04-16 14:13:15 +0700

    having gulp watch css and js deps

--- angular_client/modules/corpuspages/gulp/watch.js
+++ angular_client/modules/corpuspages/gulp/watch.js
@@ -8,20 +8,27 @@ function isOnlyChange(event) {
 }
 
 module.exports = function(options) {
-  gulp.task('watch', ['inject'], function () {
+  gulp.task('watch', ['inject'], function() {
 
     gulp.watch([options.src + '/*.html', 'bower.json'], ['inject']);
 
-    gulp.watch(options.src + '/{app,components}/**/*.css', function(event) {
-      if(isOnlyChange(event)) {
+    gulp.watch([
+      options.src + '/{app,components}/**/*.css',
+      'bower_components/fielddb-angular/dist/styles/fielddb-angular.css'
+    ], function(event) {
+      if (isOnlyChange(event)) {
         browserSync.reload(event.path);
       } else {
         gulp.start('inject');
       }
     });
 
-    gulp.watch(options.src + '/{app,components}/**/*.js', function(event) {
-      if(isOnlyChange(event)) {
+    gulp.watch([
+      options.src + '/{app,components}/**/*.js',
+      'bower_components/fielddb/fielddb.js',
+      'bower_components/fielddb-angular/dist/scripts/fielddb-angular.js'
+    ], function(event) {
+      if (isOnlyChange(event)) {
         gulp.start('scripts');
       } else {
         gulp.start('inject');
