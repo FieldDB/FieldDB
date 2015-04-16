74232176f54c75b89b9c9e4b259093d29bf797bf
cesine <cesine@yahoo.com>
2015-04-16 13:16:59 +0700

    watching changes in commonjs

--- angular_client/modules/core/gulp/watch.js
+++ angular_client/modules/core/gulp/watch.js
@@ -8,20 +8,23 @@ function isOnlyChange(event) {
 }
 
 module.exports = function(options) {
-  gulp.task('watch', ['inject'], function () {
+  gulp.task('watch', ['inject'], function() {
 
     gulp.watch([options.src + '/*.html', 'bower.json'], ['inject']);
 
     gulp.watch(options.src + '/{app,components}/**/*.css', function(event) {
-      if(isOnlyChange(event)) {
+      if (isOnlyChange(event)) {
         browserSync.reload(event.path);
       } else {
         gulp.start('inject');
       }
     });
 
-    gulp.watch(options.src + '/{app,components}/**/*.js', function(event) {
-      if(isOnlyChange(event)) {
+    gulp.watch([
+      options.src + '/{app,components}/**/*.js',
+      'bower_components/fielddb/fielddb.js'
+    ], function(event) {
+      if (isOnlyChange(event)) {
         gulp.start('scripts');
       } else {
         gulp.start('inject');
