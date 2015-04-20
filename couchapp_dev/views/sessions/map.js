d70a7b01e9c7687607028a1090ba0036f2bb0644
cesine <cesine@yahoo.com>
2015-04-20 19:48:56 +0700

    session map reduce wasnt able to handle timestamp date modifieds

--- couchapp_dev/views/sessions/map.js
+++ couchapp_dev/views/sessions/map.js
@@ -6,9 +6,19 @@ function(doc) {
       return;
     }
     if (doc.fieldDBtype === "Session" || doc.collection === "sessions" || doc.fieldDBtype === "Session" || (doc.sessionFields && !doc.datumFields)) {
-      var date = doc.dateModified ? doc.dateModified.replace(/["\\]/g, '') : "";
+      var dateModified = doc.dateModified;
+      if (dateModified) {
+        try {
+          dateModified = dateModified.replace(/["\\]/g, '');
+          dateModified = new Date(dateModified);
+          /* Use date modified as a timestamp if it isnt one already */
+          dateModified = dateModified.getTime();
+        } catch (e) {
+          //emit(error, null);
+        }
+      }
       //doc.fieldDBtype = "Session";
-      emit(date, doc);
+      emit(dateModified, doc);
     }
   } catch (e) {
     //emit(e, 1);
