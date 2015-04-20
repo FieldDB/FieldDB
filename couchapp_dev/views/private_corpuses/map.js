81aae00d2e87c6a4a52556e4d4449ce44f3a7034
cesine <cesine@yahoo.com>
2015-04-20 22:11:37 +0700

    adding back the private corpuses map reduce, otherwise the old spreadhset version are broken

---
+++ couchapp_dev/views/private_corpuses/map.js
@@ -0,0 +1,14 @@
+/* updated to be compatible with pre-1.38 databases */
+function(doc) {
+  try {
+    /* if this document has been deleted, the ignore it and return immediately */
+    if (doc.trashed && doc.trashed.indexOf("deleted") > -1) return;
+
+    if (doc.fieldDBtype === "Corpus" || doc.collection == "private_corpora" || doc.collection == "private_corpuses" || doc.api === "private_corpora" || doc.api === "private_corpuses" || (doc.confidential && doc.confidential.secretkey)) {
+      //doc.fieldDBtype = "Corpus";
+      emit(doc.timestamp, doc);
+    }
+  } catch (e) {
+    //emit(e, 1);
+  }
+};
