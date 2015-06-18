f002b4566936c5d22a036c78be40ee999ebb6eb9
cesine <cesine@yahoo.com>
2015-06-18 14:59:21 -0400

    created a word cloud template for corpora with word cloud functionality

---
+++ couchapp_wordcloud/views/clouds/map.js
@@ -0,0 +1,27 @@
+/* updated to be compatible with pre-1.38 databases */
+
+function(doc) {
+  try {
+    /* if this document has been deleted, the ignore it and return immediately */
+    if (doc.trashed && doc.trashed.indexOf("deleted") > -1) {
+      return;
+    }
+    if (doc.fieldDBtype === "Datum" || doc.collection == "datums" || (doc.datumFields && doc.session)) {
+      var dateModified = doc.dateModified;
+      if (dateModified) {
+        try {
+          dateModified = dateModified.replace(/["\\]/g, '');
+          dateModified = new Date(dateModified);
+          /* Use date modified as a timestamp if it isnt one already */
+          dateModified = dateModified.getTime();
+        } catch (e) {
+          //emit(error, null);
+        }
+      }
+      //doc.fieldDBtype = "Datum";
+      emit(dateModified, doc);
+    }
+  } catch (e) {
+    //emit(e, 1);
+  }
+}
