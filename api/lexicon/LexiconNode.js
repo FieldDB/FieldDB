aae300411e36bd16e3ab51ac75c005d33d2504b3
cesine <cesine@yahoo.com>
2015-07-23 12:27:13 -0400

    Revert "using lexicon and glosser which was moved to a separate repository"
    This reverts commit 6f4141828e14db7d0763806904062310a15bc53b.

--- api/lexicon/LexiconNode.js
+++ api/lexicon/LexiconNode.js
@@ -1,3 +1,41 @@
-var LexiconNode = require("fielddb-glosser/lib/LexiconNode").LexiconNode;
-
-exports.LexiconNode = LexiconNode;
+define([
+    "backbone"
+], function(
+    Backbone
+) {  
+  var LexiconNode = Backbone.Model.extend(   
+  /** @lends LexiconNode.prototype */ 
+  {
+    /**
+     * @class Lexicon Node is key value pair with an index of related datum. It allows the search to index
+     *        the corpus to find datum, it is also used by the default glosser to guess glosses based on what the user inputs on line 1 (utterance/orthography).
+     * 
+     * @description Lexicon Node is key value pair with an index of related datum. It allows the search to index
+     *        the corpus to find datum, it is also used by the default glosser to guess glosses based on what the user inputs on line 1 (utterance/orthography).
+     * 
+     * 
+     * @extends Backbone.Model
+     * 
+     * @constructs
+     * 
+     */
+    initialize : function() {
+    },
+    
+    defaults: {
+      morpheme: "",
+      allomorphs: [],
+      gloss: "",
+      value: 0,
+      data: []  
+    },
+    
+    // Internal models: used by the parse function
+    internalModels : {
+      // There are no nested models
+    }
+  }); 
+  
+  return LexiconNode; 
+  
+}); 
