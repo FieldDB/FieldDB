66173f61aea964ad8a41bb441303795adfd8acce
cesine <cesine@yahoo.com>
2015-08-19 14:05:15 -0400

    adding authentication to the navbar

--- angular_client/modules/core/src/app/components/navbar/navbar.controller.js
+++ angular_client/modules/core/src/app/components/navbar/navbar.controller.js
@@ -1,5 +1,6 @@
 "use strict";
 
 angular.module("fielddbAngular")
-  .controller("NavbarCtrl", function () {
+  .controller("NavbarCtrl", function ($scope) {
+    $scope.navbarCollapsed = true;
   });
