aee33fd31ea2de46b3d650c7284b142a5bcd3027
cesine <cesine@yahoo.com>
2015-08-24 19:45:19 -0400

    added group to run reduce portion

--- api/map_reduce/MapReduce.js
+++ api/map_reduce/MapReduce.js
@@ -7,6 +7,24 @@ var MapReduceFactory = function(options) {
     reduce: null
   };
   options.rows = options.rows || [];
+
+  var sum = function(values) {
+    console.log("summing ", values);
+    if (!values || !values.reduce) {
+      return values;
+    }
+    return values.reduce(function(a, b) {
+      return a + b;
+    });
+  };
+  var count = function(keys, values, rereduce) {
+    if (rereduce) {
+      return sum(values);
+    } else {
+      return values.length;
+    }
+  };
+
   options.emit = options.emit || function(key, val) {
     if (debugMode) {
       console.log(options);
@@ -16,12 +34,54 @@ var MapReduceFactory = function(options) {
       value: val
     });
   };
-  var scopedEmit = function(key, value) {
-    rows.push({
-      key: key,
-      value: value,
-      custom: "custom"
+  options._count = options._count || count;
+
+  options.group = options.group || function(rows) {
+    if (debugMode) {
+      console.log(options);
+    }
+    options.reduce = options.reduce || options._count;
+    rows = rows || options.rows;
+
+    // Group all rows by their key
+    var rowsGroupedById = {};
+    rows.map(function(row) {
+      var key = row.key;
+      if (typeof key !== "string") {
+        key = JSON.stringify(row.key);
+      }
+      if (!rowsGroupedById[key]) {
+        rowsGroupedById[key] = [row.value];
+      } else {
+        rowsGroupedById[key].push(row.value);
+      }
     });
+    console.log("regrouped ", rowsGroupedById);
+
+    // Reduce all grouped items
+    var reducedRows = [];
+    for (var key in rowsGroupedById) {
+      if (rowsGroupedById.hasOwnProperty(key)) {
+        reducedRows.push({
+          key: key,
+          value: options.reduce(key, rowsGroupedById[key], false)
+        });
+      }
+    }
+    console.log("reduced ", reducedRows);
+
+    // Re-reduce all grouped items
+    reducedRows = reducedRows.map(function(groupedRow) {
+      groupedRow.value = options.reduce(null, groupedRow.value, true);
+      return groupedRow;
+    })
+    console.log("re-reduced ", reducedRows);
+
+    return {
+      rows: reducedRows.sort(function(a, b) {
+        return a.key - b.key;
+      })
+    };
   };
 
   options.customMap = function(doc, emit, rows) {
@@ -31,7 +91,7 @@ var MapReduceFactory = function(options) {
         // ugly way to make sure references to 'emit' in map/reduce bind to the above emit at run time
         eval("options.map = " + options.map.toString() + ";");
         options.mustRescopeEmit = true;
-        console.log("customizing emit")
+        console.log("customizing emit");
       } catch (e) {
         console.warn("Probably running in a Chrome app or other context where eval is not permitted. Using global emit and results for options");
       }
@@ -76,10 +136,18 @@ var MapReduceFactory = function(options) {
       eval("options.map = " + options.mapString.toString() + ";");
     } catch (exception) {
       console.log("Unable to parse the map reduce ", exception.stack);
-      var emit = options.emit;
       options.map = function() {
-        emit("error", "unable to load map reduce");
+        options.emit("error", "unable to load map reduce");
       };
+    }
+  }
+  if (!options.reduce && options.reduceString) {
+    try {
+      // ugly way to make sure references to 'emit' in reduce/reduce bind to the above emit
+      /*jslint evil: true */
+      eval("options.reduce = " + options.reduceString.toString() + ";");
+    } catch (exception) {
+      console.log("Unable to parse the map reduce ", exception.stack);
     }
   }
   return options;
