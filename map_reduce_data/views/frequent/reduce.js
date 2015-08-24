7b02ef8fd5f3f35bdf5b91b848b40b5440347162
cesine <cesine@yahoo.com>
2015-08-24 19:49:18 -0400

    updating frequent map reduce to emit field or tag

--- map_reduce_data/views/frequent/reduce.js
+++ map_reduce_data/views/frequent/reduce.js
@@ -1,1 +1,42 @@
-_count
+function unique(keys, values, rereduce) {
+  if (rereduce) {
+    return sum(values);
+  } else {
+    /** 
+     * return only unique values
+     */
+    var slowIsOkay = true;
+    var findUniqueValues = function() {
+      var uniqueHash = {},
+        valueIndex,
+        valuesCount = values.length,
+        uniqueValues = [];
+
+      for (valueIndex = 0; valueIndex < valuesCount; valueIndex += 1) {
+        uniqueHash[values[valueIndex]] = values[valueIndex];
+      }
+
+      for (valueIndex in uniqueHash) {
+        if (uniqueHash.hasOwnProperty(valueIndex)) {
+          uniqueValues.push(uniqueHash[valueIndex]);
+        }
+      }
+      return uniqueValues;
+    }
+
+    // Normal behavior is count of values
+    if (!slowIsOkay) {
+      return values.length;
+    }
+
+    // Has to reduce faster
+    return findUniqueValues(values).length;
+  }
+}
+
+
+try {
+  exports.unique = unique;
+} catch (e) {
+  //  // DEBUG console.log("not in a node context")
+}
