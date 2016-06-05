41be0b466429971ed94fba2cc40ac5357dfb1f21
cesine <cesine@yahoo.com>
2016-06-05 16:24:01 -0400

    updating datums test
    helped find the circularity in datums and datum

--- backbone_client/datum/DatumsTest.js
+++ backbone_client/datum/DatumsTest.js
@@ -1,217 +1,239 @@
-require([
-  "datum/Datums",
-  "datum/Datum"
-], function(Datums, Datum) {
+define(["datum/Datums"], function(Datums) {
+  "use strict";
 
-  describe("Test Datums", function() {
-    it("should print collection", function() {
-      var dc = new Datums();
-      dc.add([{
-        utterance: "tusunayawanmi"
-      }, {
-        utterance: "purunaywanmi"
-      }, {
-        utterance: "allillanchu"
-      }]);
-      //console.log(JSON.stringify(dc));
-      //				expect(JSON.stringify(dc)).toEqual('[{"utterance":"tusunayawanmi","morphemes":"tusa-naya-wa-n","gloss":"dance-IMP-1OM-3SG","translation":"I feel like dancing.","grammaticalTags":"impulsative","sessionID":0,"status":{"statuses":[{"id":1,"label":"Checked"},{"id":2,"label":"To be checked","selected":true},{"id":3,"label":"Deleted"}],"active":0,"defaultStatus":0},"datumField":{"script":"","context":"","semanticDenotation":"","ipa":"","segmentation":"tusu-naya-wa-n","other":""},"datumTag":{}},{"utterance":"purunaywanmi","morphemes":"tusa-naya-wa-n","gloss":"dance-IMP-1OM-3SG","translation":"I feel like dancing.","grammaticalTags":"impulsative","sessionID":0,"status":{"statuses":[{"id":1,"label":"Checked"},{"id":2,"label":"To be checked","selected":true},{"id":3,"label":"Deleted"}],"active":0,"defaultStatus":0},"datumField":{"script":"","context":"","semanticDenotation":"","ipa":"","segmentation":"tusu-naya-wa-n","other":""},"datumTag":{}},{"utterance":"allillanchu","morphemes":"tusa-naya-wa-n","gloss":"dance-IMP-1OM-3SG","translation":"I feel like dancing.","grammaticalTags":"impulsative","sessionID":0,"status":{"statuses":[{"id":1,"label":"Checked"},{"id":2,"label":"To be checked","selected":true},{"id":3,"label":"Deleted"}],"active":0,"defaultStatus":0},"datumField":{"script":"","context":"","semanticDenotation":"","ipa":"","segmentation":"tusu-naya-wa-n","other":""},"datumTag":{}}]');
-    });
-
-    it("should filter collection", function() {
-      var dc = new Datums();
-      dc.add([{
-        utterance: "tusunayawanmi"
-      }, {
-        utterance: "purunaywanmi"
-      }, {
-        utterance: "allillanchu"
-      }]);
-      var fildc = dc.where({
-        utterance: "macaroni"
+  function registerTests() {
+    describe("Datums", function() {
+      it("should print collection", function() {
+        var data = new Datums();
+        data.add([{
+          utterance: "tusunayawanmi"
+        }, {
+          utterance: "purunaywanmi"
+        }, {
+          utterance: "allillanchu"
+        }]);
+        // console.log(data.toJSON());
+        expect(JSON.stringify(data.toJSON())).toEqual("[{\"utterance\":\"tusunayawanmi\"},{\"utterance\":\"purunaywanmi\"},{\"utterance\":\"allillanchu\"}]");
       });
-      //console.log(JSON.stringify(fildc));
-      expect(fildc).toEqual([]);
-    });
 
-    it("should remove a datum from collection", function() {
-      var dc = new Datums();
-      dc.add([{
-        utterance: "tusunayawanmi"
-      }, {
-        utterance: "purunaywanmi"
-      }, {
-        utterance: "allillanchu"
-      }]);
-      var remdc = dc.remove([{
-        utterance: "tusunayawanmi"
-      }]);
-      //It doesn't actually appear to remove anything....
-      console.log(JSON.stringify(remdc));
-      expect(remdc.length).toEqual(3);
-    });
-
-    it("should get a datum from a collection", function() {
-      var dc = new Datums();
-      dc.add([{
-        id: 99,
-        utterance: "tusunayawanmi"
-      }, {
-        id: 77,
-        utterance: "purunaywanmi"
-      }, {
-        utterance: "allillanchu"
-      }]);
-      var d = dc.get(77);
-      //id's need to be assigned, they are not automatically generated.
-      //console.log(d);
-      //				expect(d.get("utterance")).toEqual("purunaywanmi");
-    });
-    it("should get a datum by client ID", function() {
-      var dc = new Datums();
-      dc.add([{
-        id: 99,
-        utterance: "tusunayawanmi"
-      }, {
-        id: 77,
-        utterance: "purunaywanmi"
-      }, {
-        utterance: "allillanchu"
-      }]);
-      var d = dc.get(77);
-      //cid ARE automatically generated.
-      //console.log(d.cid);
-      //expect(d.cid).toEqual("c16");
-    });
-    it("should get a datum from a collected at an index", function() {
-      var dc = new Datums();
-      dc.add([{
-        id: 99,
-        utterance: "tusunayawanmi"
-      }, {
-        id: 77,
-        utterance: "purunaywanmi"
-      }, {
-        utterance: "allillanchu"
-      }]);
-      var d = dc.at(2);
-      //console.log(d);
-      expect(d.get("utterance")).toEqual("allillanchu");
-    });
-    it("should add a datum to the end of the collection", function() {
-      var dc = new Datums();
-      dc.add([{
-        id: 99,
-        utterance: "tusunayawanmi"
-      }, {
-        id: 77,
-        utterance: "purunaywanmi"
-      }, {
-        utterance: "allillanchu"
-      }]);
-      dc.push({
-        utterance: "qaparinayawan"
+      it("should filter collection", function() {
+        var data = new Datums();
+        data.add([{
+          utterance: "tusunayawanmi"
+        }, {
+          utterance: "purunaywanmi"
+        }, {
+          utterance: "allillanchu"
+        }]);
+        var fildata = data.where({
+          utterance: "macaroni"
+        });
+        // console.log(JSON.stringify(fildata));
+        expect(fildata).toEqual([]);
       });
-      var d = dc.at(3);
-      console.log(d);
-      expect(d.get("utterance")).toEqual("qaparinayawan");
-    });
-    it("should pop the last datum", function() {
-      var dc = new Datums();
-      dc.add([{
-        id: 99,
-        utterance: "tusunayawanmi"
-      }, {
-        id: 77,
-        utterance: "purunaywanmi"
-      }, {
-        utterance: "allillanchu"
-      }]);
-      var d = dc.pop();
-      console.log(d);
-      expect(d.get("utterance")).toEqual("allillanchu");
 
-    });
-    it("should add a datum to the beginning of the collection", function() {
-      var dc = new Datums();
-      dc.add([{
-        id: 99,
-        utterance: "tusunayawanmi"
-      }, {
-        id: 77,
-        utterance: "purunaywanmi"
-      }, {
-        utterance: "allillanchu"
-      }]);
-      dc.unshift({
-        utterance: "qaparinayawan"
+      it("should remove a datum from collection", function() {
+        var data = new Datums();
+        data.add([{
+          utterance: "tusunayawanmi"
+        }, {
+          _id: 99,
+          utterance: "purunaywanmi"
+        }, {
+          utterance: "allillanchu"
+        }]);
+        var remdata = data.remove([data.get(99)]);
+        //It doesn't actually appear to remove anything....
+        // console.log(JSON.stringify(remdata));
+        expect(remdata.length).toEqual(1);
+        expect(remdata[0].get("utterance")).toEqual("purunaywanmi");
+        expect(data.length).toEqual(2);
       });
-      var d = dc.at(0);
-      console.log(d);
-      expect(d.get("utterance")).toEqual("qaparinayawan");
+
+      it("should get a datum from a collection", function() {
+        var data = new Datums();
+        data.add([{
+          _id: 99,
+          utterance: "tusunayawanmi"
+        }, {
+          _id: 77,
+          utterance: "purunaywanmi"
+        }, {
+          utterance: "allillanchu"
+        }]);
+        var datum = data.get(77);
+        //id's need to be assigned, they are not automatically generated.
+        // console.log(datum);
+        expect(datum.get("utterance")).toEqual("purunaywanmi");
+      });
+
+      it("should get a datum by client ID", function() {
+        var data = new Datums();
+        data.add([{
+          _id: 99,
+          utterance: "tusunayawanmi"
+        }, {
+          _id: 77,
+          utterance: "purunaywanmi"
+        }, {
+          utterance: "allillanchu"
+        }]);
+        var datum = data.get(77);
+        //cid ARE automatically generated.
+        // console.log(datum.cid);
+        expect(datum.cid).toBeDefined();
+        expect(datum.cid).not.toEqual(77);
+      });
+
+      it("should get a datum from a collected at an index", function() {
+        var data = new Datums();
+        data.add([{
+          _id: 99,
+          utterance: "tusunayawanmi"
+        }, {
+          _id: 77,
+          utterance: "purunaywanmi"
+        }, {
+          utterance: "allillanchu"
+        }]);
+        var datum = data.at(2);
+        // console.log(datum);
+        expect(datum.get("utterance")).toEqual("allillanchu");
+      });
+
+      it("should add a datum to the end of the collection", function() {
+        var data = new Datums();
+        data.add([{
+          _id: 99,
+          utterance: "tusunayawanmi"
+        }, {
+          _id: 77,
+          utterance: "purunaywanmi"
+        }, {
+          utterance: "allillanchu"
+        }]);
+        data.push({
+          utterance: "qaparinayawan"
+        });
+        var datum = data.at(3);
+        // console.log(datum);
+        expect(datum.get("utterance")).toEqual("qaparinayawan");
+      });
+
+      it("should pop the last datum", function() {
+        var data = new Datums();
+        data.add([{
+          _id: 99,
+          utterance: "tusunayawanmi"
+        }, {
+          _id: 77,
+          utterance: "purunaywanmi"
+        }, {
+          utterance: "allillanchu"
+        }]);
+        var datum = data.pop();
+        // console.log(datum);
+        expect(datum.get("utterance")).toEqual("allillanchu");
+      });
+
+      it("should add a datum to the beginning of the collection", function() {
+        var data = new Datums();
+        data.add([{
+          _id: 99,
+          utterance: "tusunayawanmi"
+        }, {
+          _id: 77,
+          utterance: "purunaywanmi"
+        }, {
+          utterance: "allillanchu"
+        }]);
+        data.unshift({
+          utterance: "qaparinayawan"
+        });
+        var datum = data.at(0);
+        // console.log(datum);
+        expect(datum.get("utterance")).toEqual("qaparinayawan");
+      });
+
+      it("should pop the first datum", function() {
+        var data = new Datums();
+        data.add([{
+          _id: 99,
+          utterance: "tusunayawanmi"
+        }, {
+          _id: 77,
+          utterance: "purunaywanmi"
+        }, {
+          utterance: "allillanchu"
+        }]);
+        var datum = data.shift();
+        // console.log(datum);
+        expect(datum.get("utterance")).toEqual("tusunayawanmi");
+      });
+
+      it("count the data in the collection", function() {
+        var data = new Datums();
+        data.add([{
+          _id: 99,
+          utterance: "tusunayawanmi"
+        }, {
+          _id: 77,
+          utterance: "purunaywanmi"
+        }, {
+          utterance: "allillanchu"
+        }]);
+        var datum = data.length;
+        // console.log(datum);
+        expect(datum).toEqual(3);
+      });
+
+      it("should pluck the data in the collection", function() {
+        var data = new Datums();
+        data.add([{
+          _id: 99,
+          utterance: "tusunayawanmi"
+        }, {
+          _id: 77,
+          utterance: "purunaywanmi"
+        }, {
+          utterance: "allillanchu"
+        }]);
+
+        var utterances = data.pluck("utterance");
+
+        expect(JSON.stringify(utterances)).toEqual("[\"tusunayawanmi\",\"purunaywanmi\",\"allillanchu\"]");
+      });
+
+      it("should fetch the data in the collection", function(done) {
+        var data = new Datums();
+        data.add([{
+          _id: 99,
+          utterance: "tusunayawanmi"
+        }, {
+          _id: 77,
+          utterance: "purunaywanmi"
+        }, {
+          utterance: "allillanchu"
+        }]);
+
+        var datum = data.create({
+          _id: "abc"
+        });
+        expect(datum.fetch({
+          success: function() {
+            done();
+          },
+          error: function(err) {
+            // console.log(err);
+            expect(err).toBeDefined();
+            done();
+          }
+        }));
+      });
     });
-    it("should pop the first datum", function() {
-      var dc = new Datums();
-      dc.add([{
-        id: 99,
-        utterance: "tusunayawanmi"
-      }, {
-        id: 77,
-        utterance: "purunaywanmi"
-      }, {
-        utterance: "allillanchu"
-      }]);
-      var d = dc.shift();
-      console.log(d);
-      expect(d.get("utterance")).toEqual("tusunayawanmi");
+  }
 
-    });
-    it("count the data in the collection", function() {
-      var dc = new Datums();
-      dc.add([{
-        id: 99,
-        utterance: "tusunayawanmi"
-      }, {
-        id: 77,
-        utterance: "purunaywanmi"
-      }, {
-        utterance: "allillanchu"
-      }]);
-      var d = dc.length;
-      console.log(d);
-      expect(d).toEqual(3);
-
-    });
-    it("should pluck the data in the collection", function() {
-      var dc = new Datums();
-      dc.add([{
-        id: 99,
-        utterance: "tusunayawanmi"
-      }, {
-        id: 77,
-        utterance: "purunaywanmi"
-      }, {
-        utterance: "allillanchu"
-      }]);
-
-      var utterances = dc.pluck("utterance");
-
-      expect(JSON.stringify(utterances)).toEqual('["tusunayawanmi","purunaywanmi","allillanchu"]');
-
-    });
-    //			it("should fetch the data in the collection", function(){
-    //				var dc = new Datums();
-    //				dc.add([
-    //				        {id: 99, utterance: "tusunayawanmi"},
-    //				        {id: 77, utterance: "purunaywanmi"},
-    //				        {utterance:"allillanchu"}
-    //				        ]);
-    //				
-    //				var d = dc.create({
-    //					utterance: "+Hall-pa-nay-wa-n "
-    //				});
-    //				expect().toFail.
-    //			});
-
-  });
-
+  return {
+    describe: registerTests
+  };
 });
