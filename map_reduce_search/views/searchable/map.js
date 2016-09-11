384597ee72a1189bcc8f20e4a095edf3abb02c83
cesine <cesine@yahoo.com>
2016-09-11 17:12:16 -0400

    including comments and media in searchables

--- map_reduce_search/views/searchable/map.js
+++ map_reduce_search/views/searchable/map.js
@@ -1,4 +1,5 @@
 function(doc) {
+  var includeEmpty = false;
 
   var convertDatumIntoSimpleObject = function(datum) {
     var obj = {},
@@ -12,6 +13,8 @@ function(doc) {
       }
       if (datum.datumFields[i].mask) {
         obj[datum.datumFields[i][fieldKeyName]] = datum.datumFields[i].mask;
+      } else if (includeEmpty) {
+        obj[datum.datumFields[i][fieldKeyName]] = "";
       }
     }
     if (datum.session.sessionFields) {
@@ -23,12 +26,16 @@ function(doc) {
         }
         if (datum.session.sessionFields[j].mask) {
           obj[datum.session.sessionFields[j][fieldKeyName]] = datum.session.sessionFields[j].mask;
+        } else if (includeEmpty) {
+          obj[datum.datumFields[i][fieldKeyName]] = "";
         }
       }
     }
-    obj.utterance = obj.utterance || "";
-    obj.morphemes = obj.morphemes || "";
-    obj.gloss = obj.gloss || "";
+    if (includeEmpty) {
+      obj.utterance = obj.utterance || "";
+      obj.morphemes = obj.morphemes || "";
+      obj.gloss = obj.gloss || "";
+    }
 
     return obj;
   };
@@ -42,6 +49,42 @@ function(doc) {
       if (!datum) {
         return;
       }
+
+      // Index limited info about the comments
+      if (doc.comments && doc.comments.length) {
+        datum.comments = doc.comments.map(function(comment) {
+          return {
+            username: comment.username,
+            text: comment.text,
+            timestamp: comment.timestamp
+          };
+        });
+      } else if (includeEmpty) {
+        datum.comments = [];
+      }
+
+      // Index limited info about the media
+      if (doc.audioVideo && doc.audioVideo.length && typeof doc.audioVideo.map === "function") {
+        datum.media = doc.audioVideo.map(function(media) {
+          var small = {
+            filename: media.filename,
+            timestamp: media.timestamp
+          };
+
+          if (media.details.syllablesAndUtterances) {
+            small.syllableCount = media.details.syllablesAndUtterances.syllableCount || 0;
+            small.pauseCount = media.details.syllablesAndUtterances.pauseCount || 0;
+            small.speakingTotalDuration = media.details.syllablesAndUtterances.speakingTotalDuration || 0;
+            small.speakingRate = media.details.syllablesAndUtterances.speakingRate || 0;
+            small.articulationRate = media.details.syllablesAndUtterances.articulationRate || 0;
+          }
+
+          return small;
+        });
+      } else if (includeEmpty) {
+        datum.media = [];
+      }
+
       emit(datum, doc._id);
     }
   } catch (e) {
