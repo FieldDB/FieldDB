44e23e4db2ce6d99f3292160f776e468132853cd
cesine <cesine@yahoo.com>
2017-04-17 11:44:55 +0100

    test data

---
+++ test/fixtures/search/community-georgian.js
@@ -0,0 +1,176 @@
+{
+  "took": 2,
+  "timed_out": false,
+  "_shards": {
+    "total": 5,
+    "successful": 5,
+    "failed": 0
+  },
+  "hits": {
+    "total": 4,
+    "max_score": 2.1850724,
+    "hits": [{
+      "_index": "community-georgian",
+      "_type": "datum",
+      "_id": "d3bf748b16ac4054680f27a6401efdfa",
+      "_score": 2.1850724,
+      "_source": {
+        "utterance": "vitsi",
+        "morphemes": "vitsi",
+        "gloss": "i.know",
+        "translation": "I know",
+        "tags": "Combined",
+        "validationStatus": "Checked",
+        "enteredByUser": "xxxx",
+        "modifiedByUser": "xxxx",
+        "orthography": "ვიცი",
+        "context": "general purpose",
+        "russian": "знаю",
+        "media": [{
+          "filename": "vitsi.mp3",
+          "type": "audio",
+          "description": " Downloaded Praat TextGrid which contained a count of roughly 1 syllables and auto detected utterances for vitsi The utterances were not automatically transcribed for you, you can either save the textgrid and transcribe them using Praat, or continue to import them and transcribe them after.",
+          "syllableCount": "1",
+          "pauseCount": "0",
+          "speakingTotalDuration": "0.70",
+          "speakingRate": "1.42",
+          "articulationRate": "1.42"
+        }]
+      },
+      "highlight": {
+        "orthography": ["<em>ვიცი</em>"]
+      }
+    }, {
+      "_index": "community-georgian",
+      "_type": "datum",
+      "_id": "d3bf748b16ac4054680f27a6401c0d6e",
+      "_score": 1.3164577,
+      "_source": {
+        "utterance": "ar vitsi",
+        "morphemes": "ar vitsi",
+        "gloss": "not I.know",
+        "translation": "I dunno.",
+        "tags": "SampleData",
+        "validationStatus": "Checked",
+        "enteredByUser": "xxxx",
+        "modifiedByUser": "xxxx",
+        "orthography": "არ ვიცი",
+        "goal": "Frequent phrases uttered in natural contexts which learners can review by watching/listening to the movie ჩემი ცოლის დაქალის ქორწილი [HD].mp4",
+        "dateElicited": "2013",
+        "media": [{
+          "filename": "ar_vitsi.mp3",
+          "type": "audio"
+        }, {
+          "filename": "uqastravat_me_ar_vitsi.mp3",
+          "type": "audio"
+        }, {
+          "filename": "vitsi.mp3",
+          "type": "audio",
+          "description": " Downloaded Praat TextGrid which contained a count of roughly 1 syllables and auto detected utterances for vitsi The utterances were not automatically transcribed for you, you can either save the textgrid and transcribe them using Praat, or continue to import them and transcribe them after.",
+          "syllableCount": "1",
+          "pauseCount": "0",
+          "speakingTotalDuration": "0.70",
+          "speakingRate": "1.42",
+          "articulationRate": "1.42"
+        }, {
+          "filename": "vitsi_da_damalaparake_erti.mp3",
+          "type": "audio",
+          "description": " Downloaded Praat TextGrid which contained a count of roughly 8 syllables and auto detected utterances for vitsi_da_damalaparake_erti The utterances were not automatically transcribed for you, you can either save the textgrid and transcribe them using Praat, or continue to import them and transcribe them after.",
+          "syllableCount": "8",
+          "pauseCount": "0",
+          "speakingTotalDuration": "2.11",
+          "speakingRate": "3.78",
+          "articulationRate": "3.78"
+        }, {
+          "filename": "ar_vitsi.png",
+          "description": "Screenshot from movie where main character is talking with friends"
+        }]
+      },
+      "highlight": {
+        "orthography": ["არ <em>ვიცი</em>"]
+      }
+    }, {
+      "_index": "community-georgian",
+      "_type": "datum",
+      "_id": "d3bf748b16ac4054680f27a6401fab52",
+      "_score": 1.1887426,
+      "_source": {
+        "utterance": "vitsi da damalaparake erti",
+        "morphemes": "vitsi da damalaparake erti",
+        "gloss": "i.know and I.speak.to.them one",
+        "translation": "I know, can I speak to him/her?",
+        "tags": "Combined",
+        "validationStatus": "ToBeChecked",
+        "enteredByUser": "xxxx",
+        "modifiedByUser": "xxxx",
+        "orthography": "ვიცი და დამალაპარაკე ერთი",
+        "context": "on the phone",
+        "goal": "Frequent phrases uttered in natural contexts which learners can review by watching/listening to the movie ჩემი ცოლის დაქალის ქორწილი [HD].mp4",
+        "dateElicited": "2013",
+        "media": [{
+          "filename": "vitsi_da_damalaparake_erti.mp3",
+          "type": "audio",
+          "description": " Downloaded Praat TextGrid which contained a count of roughly 8 syllables and auto detected utterances for vitsi_da_damalaparake_erti The utterances were not automatically transcribed for you, you can either save the textgrid and transcribe them using Praat, or continue to import them and transcribe them after.",
+          "syllableCount": "8",
+          "pauseCount": "0",
+          "speakingTotalDuration": "2.11",
+          "speakingRate": "3.78",
+          "articulationRate": "3.78"
+        }]
+      },
+      "highlight": {
+        "orthography": ["<em>ვიცი</em> და დამალაპარაკე ერთი"]
+      }
+    }, {
+      "_index": "community-georgian",
+      "_type": "datum",
+      "_id": "d3bf748b16ac4054680f27a640204a76",
+      "_score": 1.0149328,
+      "_source": {
+        "utterance": "itsi nanadeda tʃamovida",
+        "morphemes": "itsi nana-deda tʃamovida",
+        "gloss": "you.know grand-mother? ?",
+        "validationStatus": "ToBeChecked",
+        "enteredByUser": "xxxx",
+        "modifiedByUser": "xxxx",
+        "orthography": "ვიცი ნანადედა ჩამოვიდა",
+        "goal": "Frequent phrases uttered in natural contexts which learners can review by watching/listening to the movie ჩემი ცოლის დაქალის ქორწილი [HD].mp4",
+        "dateElicited": "2013",
+        "media": [{
+          "filename": "itsi_nanadeda_tSamovida.mp3",
+          "type": "audio",
+          "description": " Downloaded Praat TextGrid which contained a count of roughly 6 syllables and auto detected utterances for itsi_nanadeda_tSamovida The utterances were not automatically transcribed for you, you can either save the textgrid and transcribe them using Praat, or continue to import them and transcribe them after.",
+          "syllableCount": "6",
+          "pauseCount": "0",
+          "speakingTotalDuration": "1.54",
+          "speakingRate": "3.90",
+          "articulationRate": "3.90"
+        }]
+      },
+      "highlight": {
+        "orthography": ["<em>ვიცი</em> ნანადედა ჩამოვიდა"]
+      }
+    }]
+  },
+  "original": {
+    "query": {
+      "bool": {
+        "must": [{
+          "term": {
+            "orthography": "ვიცი"
+          }
+        }],
+        "must_not": [],
+        "should": []
+      }
+    },
+    "highlight": {
+      "fields": {
+        "orthography": {}
+      }
+    },
+    "from": 0,
+    "size": 50,
+    "sort": []
+  }
+}
