fae1f707f1bfbd15a23c1f34e7572f5c612cb950
cesine <cesine@yahoo.com>
2017-04-17 19:00:50 +0100

    renaming files to corpora

--- app/components/Corpora/Corpora.jsx
+++ app/components/Corpora/Corpora.jsx
@@ -4,21 +4,21 @@ import { List } from 'immutable'
 import CorpusMaskCreate from '../CorpusMaskCreate.jsx'
 
 class Corpora extends Component {
-  render () {
+  render() {
     return (
       <div>
         <CorpusMaskCreate user={this.props.user} />
         Corpora component
         {
-          this.props.corpora.map((q) => {
-            let id = q.get('dbname')
-            return (
-              <div key={dbname}>
+      this.props.corpora.map((q) => {
+        let dbname = q.get('dbname')
+        return (
+          <div key={dbname}>
                 <Link to={`/corpora/${dbname}`}>{ q.get('title') }</Link>
               </div>
-            )
-          })
-        }
+        )
+      })
+      }
         <Link to={`/corpora/not-found`}> This link would be redirected to Index</Link>
       </div>
     )
--- app/components/Corpora/actions.js
+++ app/components/Corpora/actions.js
@@ -2,7 +2,7 @@ import { CALL_API, CHAIN_API } from '../../middleware/api'
 
 export const LOADED_CORPORA = Symbol('LOADED_CORPORA')
 export const ADD_CORPUS_MASK = Symbol('ADD_CORPUS_MASK')
-export function loadCorpora () {
+export function loadCorpora() {
   console.log('calling loadCorpora');
   return {
     [CALL_API]: {
