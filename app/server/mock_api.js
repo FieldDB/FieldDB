44e6d0802caad4e23728f60231b4488d2fb3ab5c
cesine <cesine@yahoo.com>
2017-04-20 11:10:59 +0100

    cleanig cruft mock api functions which are not used

--- app/server/mock_api.js
+++ app/server/mock_api.js
@@ -1,17 +1,3 @@
 import Corpora from 'fielddb/api/corpus/Corpora'
 
-import user from '../../routes/user'
-import corpus from '../../routes/corpus'
-
-function corpusMask (dbname) {
-  return corpus.getCorpusMask(dbname, console.log)
-}
-
 export const corpora = new Corpora.Corpora([]).toJSON()
-
-export function getUser (id) {
-  return user.getUserMask(id, console.log)
-}
-export function getCorpusMask (dbname) {
-  return corpusMask(dbname)
-}
