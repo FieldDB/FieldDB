ea8fb0b6d53560d6ac69bcb6ea0288ac89169763
cesine <cesine@yahoo.com>
2017-04-21 10:50:08 +0100

    simplifying corpus request since we dont use chaining

--- app/components/CorpusMask/actions.js
+++ app/components/CorpusMask/actions.js
@@ -1,21 +1,15 @@
 import { CALL_API, CHAIN_API } from '../../middleware/api'
 
 export const LOADED_CORPUS_MASK_DETAIL = Symbol('LOADED_CORPUS_MASK_DETAIL')
-export function loadCorpusMaskDetail ({teamname, dbname, history}) {
+export function loadCorpusMaskDetail({teamname, dbname, history}) {
   return {
-    [CHAIN_API]: [
-      () => {
-        return {
-          [CALL_API]: {
-            method: 'get',
-            path: `/api/${teamname || 'corpora'}/${dbname}`,
-            successType: LOADED_CORPUS_MASK_DETAIL,
-            afterError: () => {
-              history.push('/')
-            }
-          }
-        }
+    [CALL_API]: {
+      method: 'get',
+      path: `/api/${teamname || 'corpora'}/${dbname}`,
+      successType: LOADED_CORPUS_MASK_DETAIL,
+      afterError: () => {
+        history.push('/')
       }
-    ]
+    }
   }
 }
