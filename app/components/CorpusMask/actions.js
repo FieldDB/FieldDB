8ce5251a82e6d947213d5cc0f4eaf478689bc0dd
cesine <cesine@yahoo.com>
2017-04-21 16:23:28 +0100

    whitespace

--- app/components/CorpusMask/actions.js
+++ app/components/CorpusMask/actions.js
@@ -1,7 +1,7 @@
 import { CALL_API, CHAIN_API } from '../../middleware/api'
 
 export const LOADED_CORPUS_MASK_DETAIL = Symbol('LOADED_CORPUS_MASK_DETAIL')
-export function loadCorpusMaskDetail({teamname, dbname, history}) {
+export function loadCorpusMaskDetail ({teamname, dbname, history}) {
   return {
     [CALL_API]: {
       method: 'get',
