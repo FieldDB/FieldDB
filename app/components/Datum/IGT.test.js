702beb33f7d137a549d75e4dbd31d53ee05c5109
cesine <cesine@yahoo.com>
2017-04-23 11:18:32 +0100

    testing IGT order comes from the corpus, not the igt passed in

--- app/components/Datum/IGT.test.js
+++ app/components/Datum/IGT.test.js
@@ -1,26 +1,33 @@
-import Immutable from 'immutable'
+import fixtures from 'fixturefiles'
 import React from 'react'
 import { shallow } from 'enzyme'
 
 import IGT from './IGT.jsx'
 
 describe('Component::IGT', function () {
-  let props
-  beforeEach(function () {
-    props = {
-      datalist: Immutable.fromJS({})
-    }
-  })
-
-  function renderDoc () {
+  const corpus = fixtures.corpus['community-georgian']
+  function renderDoc (props) {
     return shallow(<IGT {...props} />)
   }
 
-  it('should render', function () {
-    let doc = renderDoc()
-    let element = doc.find('div')
+  it('should render in the order of the corpus fields', function () {
+    let doc = renderDoc({
+      fields: corpus.datumFields,
+      igt: {
+        'gloss': 'i.know',
+        'orthography': 'ვიცი',
+        'morphemes': 'vitsi',
+        'notaknownfieldincorpus': 'some stuff',
+        'syntacticCategory': '',
+        'utterance': 'vitsi'
+      }
+    })
 
+    let element = doc.find('span')
     expect(element).to.exist
     expect(element.length).to.equal(1)
+    expect(element.node.props.className).to.equal('glossCouplet')
+
+    expect(element.node.props.dangerouslySetInnerHTML.__html).to.equal('ვიცი <br/>vitsi <br/>vitsi <br/>i.know')
   })
 })
