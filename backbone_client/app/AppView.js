f1427d7ea9c44b402b0b291a421c228d80186807
cesine <cesine@yahoo.com>
2017-05-13 20:13:59 -0400

    checking appview inside the bug alert rather than only registering it if appview is ready

--- backbone_client/app/AppView.js
+++ backbone_client/app/AppView.js
@@ -106,12 +106,11 @@ define([
     initialize: function() {
       if (OPrime.debugMode) OPrime.debug("APPVIEW init: " + this.el);
 
-      if (window.appView && typeof window.appView.toastUser === "function") {
-        FieldDB.FieldDBObject.bug = function(message) {
+      FieldDB.FieldDBObject.bug = function(message) {
+        if (window.appView && typeof window.appView.toastUser === "function") {
           window.appView.toastUser(message, "alert-danger");
-        };
-      }
-
+        }
+      };
       FieldDB.FieldDBObject.confirm = function(message, optionalLocale) {
         var deferred = FieldDB.Q.defer();
         console.warn(message);
