b870e8c3f661e594f6317ea2bf3743e0651282e6
cesine <cesine@yahoo.com>
2017-05-14 12:26:37 -0400

    tested with empty localstorage

--- backbone_client/app/AppTest.js
+++ backbone_client/app/AppTest.js
@@ -32,7 +32,9 @@ define([
             expect(app.get("authentication")).toBeDefined();
             expect(app.get("currentSession")).toBeDefined();
             expect(app.get("currentDataList")).toBeDefined();
-            expect(FieldDB.FieldDBObject.application.serverLabel).toEqual("production");
+            if (FieldDB.FieldDBObject.application) {
+              expect(FieldDB.FieldDBObject.application.serverLabel).toEqual("production");
+            }
 
             done();
           },500);
