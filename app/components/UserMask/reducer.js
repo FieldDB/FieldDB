c8e7bbc04f1a8965c8a79630ee3646503e108f8a
cesine <cesine@yahoo.com>
2017-05-14 22:21:49 -0400

    defaulting usermask to an object

--- app/components/UserMask/reducer.js
+++ app/components/UserMask/reducer.js
@@ -2,10 +2,13 @@ import Immutable from 'immutable'
 
 import * as ActionType from './actions'
 
-let defaultState = Immutable.fromJS([])
+let defaultState = Immutable.fromJS({})
 function userMaskDetail (state = defaultState, action) {
   switch (action.type) {
     case ActionType.LOADED_USER_MASK:
+      if (!action.response || !action.response.username) {
+        return state
+      }
       return Immutable.fromJS(action.response)
     default:
       return state
