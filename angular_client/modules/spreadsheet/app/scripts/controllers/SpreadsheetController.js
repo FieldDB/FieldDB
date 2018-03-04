8682d28a253d1ad15c9494047aa040c31bb9eaab
cesine <cesine@yahoo.com>
2018-03-04 16:44:05 -0500

    v5.45.4

--- angular_client/modules/spreadsheet/app/scripts/controllers/SpreadsheetController.js
+++ angular_client/modules/spreadsheet/app/scripts/controllers/SpreadsheetController.js
@@ -177,7 +177,7 @@ var SpreadsheetStyleDataEntryController = function($scope, $rootScope, $resource
     };
   }
 
-  $rootScope.appVersion = "5.45.3.22.05ss";
+  $rootScope.appVersion = "5.45.4.16.41ss";
 
   // Functions to open/close generic notification modal
   $rootScope.openNotification = function(size, showForgotPasswordInstructions) {
