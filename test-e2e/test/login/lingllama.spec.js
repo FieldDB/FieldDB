3c48086184538a9968fb39acab0e1dd66cc1e87a
cesine <cesine@yahoo.com>
2020-03-14 23:12:16 -0400

    testing chrome extension isnt possible

--- test-e2e/test/login/lingllama.spec.js
+++ test-e2e/test/login/lingllama.spec.js
@@ -1,4 +1,47 @@
+const puppeteer = require('puppeteer');
+const path = require('path');
+
+const debug = () => {};
+const {
+  HEADLESS,
+} = process.env;
+
 describe('Login', () => {
-  it('should login lingllama sample user', () => {
+  let browser;
+  let page;
+
+  before(async () => {
+    const pathToExtension = path.join(__dirname, '../../../backbone_client');
+    browser = await puppeteer.launch({
+      timeout: 10000,
+      slowMo: 50,
+      headless: HEADLESS && HEADLESS !== 'false',
+      args: [
+        `--disable-extensions-except=${pathToExtension}`,
+        `--load-extension=${pathToExtension}`,
+      ],
+    });
+    const targets = await browser.targets();
+    debug('targets', targets);
+    const pageTarget = targets.find((target) => {
+      debug('target.type()', target.type());
+      // return target.type() === 'background_page'
+      return target.type() === 'page';
+    });
+    page = await pageTarget.page();
+  });
+
+  after(async () => {
+    await browser.close();
+  });
+
+  it.only('should login lingllama sample user', async () => {
+    debug(`URL: ${process.env.URL}/corpus.html`);
+    await page.goto(`${process.env.URL}/corpus.html`, {
+      waitUntil: 'networkidle0',
+    });
+    await page.screenshot({
+      path: 'screenshots/example.png',
+    });
   });
 });
