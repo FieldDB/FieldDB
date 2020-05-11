ea4a58e6d47dd59e58b9bc239ff7860071257f6e
cesine <cesine@yahoo.com>
2020-05-10 21:35:46 -0400

    reuse page across tests

--- test-e2e/test/login/lingllama.spec.js
+++ test-e2e/test/login/lingllama.spec.js
@@ -1,66 +1,10 @@
-const debug = require('debug');
 const { expect } = require('chai');
-const path = require('path');
-const puppeteer = require('puppeteer');
-
-const debugBrowserConsole = debug('browser:console');
-const debugBrowserRequest = debug('browser:request');
-const {
-  HEADLESS,
-} = process.env;
 
 describe('Login', () => {
-  let browser;
   let page;
 
   before(async () => {
-    browser = await puppeteer.launch({
-      timeout: 10000,
-      slowMo: 50,
-      headless: HEADLESS && HEADLESS !== 'false',
-      ignoreHTTPSErrors: true,
-      args: [
-        '--window-size=1000,1080',
-        '--auto-open-devtools-for-tabs',
-      ],
-    });
-    page = await browser.newPage();
-    await page.setViewport({
-      width: 1920,
-      height: 1080,
-    });
-    await page.setRequestInterception(true);
-    page.on('request', (interceptedRequest) => {
-      const url = interceptedRequest.url();
-      if (url.endsWith('.png') || url.endsWith('.jpg')) {
-        // eslint-disable-next-line no-console
-        console.log(`Aborting ${url}`);
-        interceptedRequest.abort();
-      } else if (url.includes('google-analytics.com')) {
-        // eslint-disable-next-line no-console
-        console.log(`Aborting ${url}`);
-        interceptedRequest.abort();
-      } else {
-        debugBrowserRequest(`Continuing ${url}`);
-        interceptedRequest.continue();
-      }
-    });
-    page
-      .on('console', (message) => debugBrowserConsole(`${message.type().substr(0, 3).toUpperCase()} ${message.text()}`))
-      .on('pageerror', ({
-        message,
-        // eslint-disable-next-line no-console
-      }) => console.log(message))
-      .on('response', (response) => debugBrowserRequest(`${response.status()} ${response.url()}`))
-      .on('requestfailed', (request) => debugBrowserRequest(`${request.failure().errorText} ${request.url()}`));
-  });
-
-  after(async () => {
-    await page.screenshot({
-      path: path.join(__dirname, '../../screenshots/lingllama.png'),
-    });
-    await page.close();
-    await browser.close();
+    page = global.reusedPage;
   });
 
   it('should login lingllama sample user', async () => {
